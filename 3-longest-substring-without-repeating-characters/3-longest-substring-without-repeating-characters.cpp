3a116369be3dc7f9858e96589b6291acdeabec33
Tyler Le <tylerle99@yahoo.com>
2022-06-11 21:48:39 -0700

    Time: 23 ms (56.84%), Space: 8.3 MB (58.55%) - LeetHub

--- 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.cpp
+++ 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.cpp
@@ -2,7 +2,6 @@
 #include <algorithm>
 
 
-
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
@@ -15,15 +14,19 @@ public:
         
         while(end < S_LEN) {
             auto mapping = map.find(s[end]);
+            
+            // if char not in map or its value is 0, increment char in map and slide the                end of window to the right
             if (mapping == map.end() || mapping->second == 0) {
                 map[s[end]]++;
                 end++;
                 count++;
             }
             
+            // if dupl char found, delete char at position 'begin' and slide start of                    window over by 1 and decrement count
+            // then check for new max
             else {
                 max_so_far = std::max(count, max_so_far);
-                map[s[begin]] = 0;
+                map[s[begin]]--;
                 begin++;
                 count--;
 
