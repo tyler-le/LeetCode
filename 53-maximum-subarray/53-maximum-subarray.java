bc2258b136718a38d29aa45d033ee14d133fb628
Tyler Le <tylerle99@yahoo.com>
2022-06-11 19:16:35 -0700

    Time: 2 ms (59.70%), Space: 73.7 MB (48.62%) - LeetHub

---
+++ 53-maximum-subarray/53-maximum-subarray.java
@@ -0,0 +1,24 @@
+import java.lang.Math;
+
+class Solution {
+    public int maxSubArray(int[] nums) {
+        // we will use kadanes algo
+        // this vid helped me a lot https://www.youtube.com/watch?v=jnoVtCKECmQ
+        
+        // set the max to the first
+        int max_sum = nums[0];
+        int curr_sum = max_sum;
+        
+        for (int i = 1; i < nums.length; i++) {            
+            // after each element, we will choose to EITHER add it to the current sum or to start a new subarray
+            // if adding nums[i] to currSum is bigger than nums[i] alone, we will set that to currSum
+            // if restarting subarray is better, we will set only nums[i] to currSum (basically restart sum)
+            curr_sum = Math.max(nums[i] + curr_sum, nums[i]);
+            
+            // then we will keep the max of currSum and max 
+            max_sum = Math.max(curr_sum, max_sum);
+        }
+        
+        return max_sum;
+    }
+}
