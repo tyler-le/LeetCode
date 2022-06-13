1b3d3b15644948a790f6341c2e663fbff1fd6bcc
Tyler Le <tylerle99@yahoo.com>
2022-06-12 17:34:49 -0700

    Time: 694 ms (5.04%), Space: 26.9 MB (16.88%) - LeetHub

--- 977-squares-of-a-sorted-array/977-squares-of-a-sorted-array.cpp
+++ 977-squares-of-a-sorted-array/977-squares-of-a-sorted-array.cpp
@@ -1,24 +1,47 @@
+#include <stdlib.h>     /* abs */
+#include <vector>
+
 class Solution {
 public:
     vector<int> sortedSquares(vector<int>& nums) {
         
         /**
-        * This naive approach is O(nlogn)
+        * This naive approach is O(nlogn) due to sorting
         **/
-        vector<int> squared;
-        for (auto& num : nums) {
-            squared.push_back(num*num);
-        }
+//         vector<int> squared;
+//         for (auto& num : nums) {
+//             squared.push_back(num*num);
+//         }
         
-        std::sort(squared.begin(), squared.end());
+//         std::sort(squared.begin(), squared.end());
         
-        return squared;
+//         return squared;
         
         /**
         * This approach is O(n)
         **/
         
-        // Should use absolute value somewhere.
+        vector<int> squared;
+        int l = 0;
+        int r = nums.size() - 1;
         
+        if (nums.size() == 0) return squared;
+        
+        while (l < r) {
+            if (abs(nums[l]) > abs(nums[r])) {
+                squared.insert(squared.begin(), nums[l] * nums[l]); // vector has no 'push_front' 
+                l++;
+            }
+            
+            else if (abs(nums[l]) <= abs(nums[r])) {
+                squared.insert(squared.begin(), nums[r] * nums[r]);
+                r--;
+            }
+            
+        }
+
+        squared.insert(squared.begin(), nums[l] * nums[l]);
+        
+        return squared;
     }
 };
