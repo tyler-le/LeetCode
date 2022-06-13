157be8e6be539f245f0fbae4b7447aac1bb45513
Tyler Le <tylerle99@yahoo.com>
2022-06-12 17:40:40 -0700

    Time: 711 ms (5.04%), Space: 27 MB (16.88%) - LeetHub

--- 977-squares-of-a-sorted-array/977-squares-of-a-sorted-array.cpp
+++ 977-squares-of-a-sorted-array/977-squares-of-a-sorted-array.cpp
@@ -4,20 +4,6 @@ public:
     vector<int> sortedSquares(vector<int>& nums) {
         
         /**
-        * This naive approach is O(nlogn) due to sorting
-        **/
-//         vector<int> squared;
-//         for (auto& num : nums) {
-//             squared.push_back(num*num);
-//         }
-        
-//         std::sort(squared.begin(), squared.end());
-        
-//         return squared;
-        
-        
-        
-        /**
         * This approach is O(n)
         **/
         
