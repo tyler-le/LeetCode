94e1d0a2539cd18221eafd57f108dc8ba6a58053
Tyler Le <tylerle99@yahoo.com>
2022-06-14 16:56:00 -0700

    Time: 59 ms (44.24%), Space: 19.8 MB (25.20%) - LeetHub

--- 142-linked-list-cycle-ii/142-linked-list-cycle-ii.py
+++ 142-linked-list-cycle-ii/142-linked-list-cycle-ii.py
@@ -13,19 +13,14 @@ class Solution(object):
         
         slow = fast = head
         
-        # Look for cycle
         while fast is not None:
             if fast.next is None or fast.next.next is None:
                 return None
-            
             slow = slow.next
             fast = fast.next.next
-            
             if slow is fast:
                 break
                 
-        # cycle is found, look for head of cycle.
-        # keep incrementing curr and fast and when they equal, that is the head of the cycle
         curr = head
         while curr is not fast:
             curr = curr.next
