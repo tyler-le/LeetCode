97973bd71caf7fd537d81e18828ab20863bf99e3
Tyler Le <tylerle99@yahoo.com>
2022-06-15 15:13:30 -0700

    Time: 153 ms (16.95%), Space: 17.3 MB (92.20%) - LeetHub

--- 57-insert-interval/57-insert-interval.py
+++ 57-insert-interval/57-insert-interval.py
@@ -3,24 +3,24 @@ class Solution:
         res = []
         
         for i in range (len(intervals)):
+            # if newInterval comes before current interval and is disjoint, 
+            # put it up front and attach the rest of intervals (which is nonoverlapping) and return
             if newInterval[1] < intervals[i][0]:
                 res.append(newInterval)
                 res+=intervals[i:]
                 return res
             
+            # else if the newInterval is not connected to curr interval, we can add the interval[i] 
+            # and continue without appending
             elif newInterval[0] > intervals[i][1]:
                 res.append(intervals[i])
                 
+            # else there is overlap, so we update the newInterval continue without appending
             else:
                 newInterval = [min(intervals[i][0], newInterval[0]), max(intervals[i][1], newInterval[1])]
         
         res.append(newInterval)
         return res
-            
-    
-                
-            
-        return res
-                
+                            
             
         
