7627df56e2cc8c77c958d245e9336baf791b7f0c
Tyler Le <tylerle99@yahoo.com>
2022-06-14 17:43:20 -0700

    Time: 46 ms (14.37%), Space: 13.3 MB (63.22%) - LeetHub

---
+++ 202-happy-number/202-happy-number.py
@@ -0,0 +1,35 @@
+class Solution(object):
+
+    def sum_of_squares(self, num):
+        sum = 0
+        for digit in str(num):
+            sum += int(digit) * int(digit)
+        return sum
+     
+    
+    def isHappy(self, n):
+        """
+        :type n: int
+        :rtype: bool
+        """
+        slow = fast = n
+        while (True):
+            slow = self.sum_of_squares(slow)
+            fast = self.sum_of_squares(self.sum_of_squares(fast))
+            
+            if slow == fast and slow != 1:
+                return False
+            
+            elif slow == 1 or fast == 1:
+                return True
+            
+        return fast == 1
+    
+    
+   
+   
+            
+    
+            
+            
+        
