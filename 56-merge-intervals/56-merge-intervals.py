6c5c37d27c1eb61810e655d2502c880f9da72396
Tyler Le <tylerle99@yahoo.com>
2022-06-16 17:54:32 -0700

    Time: 259 ms (7.76%), Space: 19.4 MB (22.22%) - LeetHub

--- 56-merge-intervals/56-merge-intervals.py
+++ 56-merge-intervals/56-merge-intervals.py
@@ -1,20 +1,25 @@
-class Solution:
-    def merge(self, intervals: List[List[int]]) -> List[List[int]]:
+class Solution(object):
+    def merge(self, intervals):
+        """
+        :type intervals: List[List[int]]
+        :rtype: List[List[int]]
+        """
         
-        # custom sort based on first element in each sub-array
         intervals.sort(key = lambda i : i[0])
-        output = [intervals[0]]
+        
+        res = []
+        res.append(intervals[0])
         
         for start,end in intervals[1:]:
-            last_in_output = output[-1]
-
-            if last_in_output[1] >= start:
-                if last_in_output[1] <= end:
-                    last_in_output[1] = end
-                    
+            last_in_res = res[-1]
+            a_overlaps_b = start>=last_in_res[0] and start <=last_in_res[1]
+            b_overlaps_a = last_in_res[0]>=start and last_in_res[0]<=end
+            
+            if a_overlaps_b or b_overlaps_a:
+                res[-1] = [min(start, last_in_res[0]), max(end, last_in_res[1])]
                 
             else:
-                output.append([start,end])
-                
-        return output
+                res.append([start,end])
+
+        return res
         
