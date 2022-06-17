61f7d9cdde96ca5b63630533012cb5d9077cbd37
Tyler Le <tylerle99@yahoo.com>
2022-06-17 13:33:21 -0700

    Time: 465 ms (56.64%), Space: 22.1 MB (75.80%) - LeetHub

--- 448-find-all-numbers-disappeared-in-an-array/448-find-all-numbers-disappeared-in-an-array.py
+++ 448-find-all-numbers-disappeared-in-an-array/448-find-all-numbers-disappeared-in-an-array.py
@@ -3,16 +3,20 @@ class Solution:
         count = []
         res = []
         
+        # mark all vals in count as 0
         for index in range(len(nums)+1):
             count.append(0)
             
+        # loop through nums and record each value as the index in count   
         for index in range(len(nums)):
             count[nums[index]]+=1
             
+        # if any value in count is 0, the value of the index is missing from nums
         for index in range(1,len(count)):
             if count[index] == 0:
                 res.append(index)
                 
         return res
+    
             
         
