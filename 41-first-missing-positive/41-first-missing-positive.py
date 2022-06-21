428c11d6eea69b019e5090a71365c992f43a792d
Tyler Le <tylerle99@yahoo.com>
2022-06-20 20:49:29 -0700

    Time: 1343 ms (19.32%), Space: 52.2 MB (56.27%) - LeetHub

--- 41-first-missing-positive/41-first-missing-positive.py
+++ 41-first-missing-positive/41-first-missing-positive.py
@@ -6,7 +6,7 @@ class Solution(object):
         """
         
         for i in range (len(nums)):
-            while nums[i] != i+1 and nums[i] < len(nums) and nums[i] > 0 and nums[i] != nums[nums[i]-1]:
+            while nums[i] > 0 and nums[i] <= len(nums) and nums[i] != nums[nums[i]-1]:
                 temp = nums[nums[i]-1]
                 nums[nums[i]-1] = nums[i]
                 nums[i]=temp
