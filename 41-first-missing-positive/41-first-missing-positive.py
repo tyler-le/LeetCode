5fac0cdcdf8c52205c534730d82d4e156f85e523
Tyler Le <tylerle99@yahoo.com>
2022-06-20 20:38:52 -0700

    Time: 1321 ms (20.83%), Space: 52.1 MB (72.83%) - LeetHub

---
+++ 41-first-missing-positive/41-first-missing-positive.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def firstMissingPositive(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        
+        for i in range (len(nums)):
+            while nums[i] != i+1 and nums[i] < len(nums) and nums[i] > 0 and nums[i] != nums[nums[i]-1]:
+                temp = nums[nums[i]-1]
+                nums[nums[i]-1] = nums[i]
+                nums[i]=temp
+        
+        for j in range (len(nums)):
+            if nums[j] != j+1:
+                return j+1
+            
+        return len(nums)+1
+        
