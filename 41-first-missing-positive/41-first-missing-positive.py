c525734631f2d7f863f5f8bc2fe7c71e552a0d33
Tyler Le <tylerle99@yahoo.com>
2022-06-20 20:49:55 -0700

    Time: 1124 ms (39.05%), Space: 52.1 MB (72.83%) - LeetHub

--- 41-first-missing-positive/41-first-missing-positive.py
+++ 41-first-missing-positive/41-first-missing-positive.py
@@ -4,9 +4,9 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        
+        length = len(nums)
         for i in range (len(nums)):
-            while nums[i] > 0 and nums[i] <= len(nums) and nums[i] != nums[nums[i]-1]:
+            while nums[i] > 0 and nums[i] <= length and nums[i] != nums[nums[i]-1]:
                 temp = nums[nums[i]-1]
                 nums[nums[i]-1] = nums[i]
                 nums[i]=temp
