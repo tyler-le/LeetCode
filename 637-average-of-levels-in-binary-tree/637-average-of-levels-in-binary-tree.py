a2140989193423398b6d04aab32104a4e4040ae6
Tyler Le <tylerle99@yahoo.com>
2022-06-21 20:04:43 -0700

    Time: 77 ms (45.15%), Space: 16.5 MB (87.81%) - LeetHub

---
+++ 637-average-of-levels-in-binary-tree/637-average-of-levels-in-binary-tree.py
@@ -0,0 +1,36 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def averageOfLevels(self, root: Optional[TreeNode]) -> List[float]:
+        res, q = [], deque()
+        
+        if not root: 
+            return 0
+        
+        q.append(root)
+        
+        while q:
+            level_sum, level_size = 0, len(q)
+            
+            
+            
+            for i in range(level_size):
+                popped = q.popleft()
+                level_sum+=popped.val
+                
+                if popped.left:
+                    q.append(popped.left)
+                if popped.right:
+                    q.append(popped.right)
+                    
+            level_avg = level_sum / level_size
+            res.append(level_avg)
+            
+        return res
+            
+            
+        
