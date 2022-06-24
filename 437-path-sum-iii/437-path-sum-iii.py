6315cf9e6a7b5973906a6f79205d8072220ceee2
Tyler Le <tylerle99@yahoo.com>
2022-06-24 11:36:13 -0700

    Time: 423 ms (37.09%), Space: 15.3 MB (77.71%) - LeetHub

---
+++ 437-path-sum-iii/437-path-sum-iii.py
@@ -0,0 +1,32 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def pathSum(self, root: Optional[TreeNode], targetSum: int) -> int:
+        
+        def path_sum_helper(root, targetSum, curr_path, count):
+            
+            if root is None:
+                return 0
+            
+            curr_path.append(root.val)
+                
+            # check sums of all subpaths in curr_path here
+            count, curr_sum = 0, 0
+            for i in range(len(curr_path)-1, -1, -1): # reverse iteration
+                curr_sum+=curr_path[i]
+                if curr_sum == targetSum:
+                    count += 1
+                
+            count += path_sum_helper(root.left, targetSum, curr_path, count)
+            count += path_sum_helper(root.right, targetSum, curr_path, count)
+            
+            del curr_path[-1]
+            
+            return count
+        
+        return path_sum_helper(root, targetSum, [], 0)
+        
