9175a31a65403fd611db1755d2cc09cee5c02e75
Tyler Le <tylerle99@yahoo.com>
2022-06-26 17:48:11 -0700

    Time: 250 ms (41.21%), Space: 15.3 MB (32.36%) - LeetHub

--- 438-find-all-anagrams-in-a-string/438-find-all-anagrams-in-a-string.py
+++ 438-find-all-anagrams-in-a-string/438-find-all-anagrams-in-a-string.py
@@ -6,15 +6,15 @@ class Solution:
         
         while r < len(s):
             substr_count[s[r]] = 1 + substr_count.get(s[r], 0)
+            
             while r-l+1 > window_size:
                 substr_count[s[l]]-=1
-                if substr_count[s[l]] == 0:
+                if substr_count[s[l]] == 0: 
                     del substr_count[s[l]]
                 l+=1
                 
             if p_count == substr_count:
                 res.append(l)
-            #print(p_count, substr_count)
                 
             r+=1
             
