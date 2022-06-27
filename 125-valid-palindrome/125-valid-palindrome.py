69c65c42f87e90504cee13e7104402056a4374a3
Tyler Le <tylerle99@yahoo.com>
2022-06-26 21:19:32 -0700

    Time: 58 ms (58.28%), Space: 15.4 MB (22.53%) - LeetHub

---
+++ 125-valid-palindrome/125-valid-palindrome.py
@@ -0,0 +1,21 @@
+class Solution(object):
+    def isPalindrome(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        constructed = re.sub(r'[^a-zA-Z0-9]', '', s).lower()
+        
+        l,r = 0, len(constructed)-1
+
+        while l < r:
+            
+            if constructed[l] != constructed[r]:
+                return False
+            
+            l+=1
+            r-=1
+        return True
+        
+        
+        
