619e65bd7052ec78e3febab6ccd2820a5f0e2cc5
Tyler Le <tylerle99@yahoo.com>
2022-06-26 21:06:54 -0700

    Time: 285 ms (57.41%), Space: 15.2 MB (50.70%) - LeetHub

--- 581-shortest-unsorted-continuous-subarray/581-shortest-unsorted-continuous-subarray.py
+++ 581-shortest-unsorted-continuous-subarray/581-shortest-unsorted-continuous-subarray.py
@@ -3,12 +3,15 @@ class Solution:
         l,r = 0, len(nums)-1
         
         while l < r:
+            
             while l<r and nums[l] <= nums[l+1]:
                 l+=1
             while l<r and nums[r]>=nums[r-1]:
                 r-=1
+                
             if not l<r:
                 return 0
+            
             min_subarray, max_subarray = min(nums[l:r+1]), max(nums[l:r+1])
             
             while l>0 and nums[l-1] > min_subarray:
