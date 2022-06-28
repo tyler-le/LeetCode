7e1deb8d18437a2f35d11a137ec97ccb68c5ce35
Tyler Le <tylerle99@yahoo.com>
2022-06-27 20:11:41 -0700

    Time: 95 ms (17.73%), Space: 14.7 MB (40.29%) - LeetHub

--- 35-search-insert-position/35-search-insert-position.py
+++ 35-search-insert-position/35-search-insert-position.py
@@ -1,29 +1,17 @@
-class Solution(object):
-    def searchInsert(self, nums, target):
-        """
-        :type nums: List[int]
-        :type target: int
-        :rtype: int
-        """
+class Solution:
+    def searchInsert(self, nums: List[int], target: int) -> int:
+        low, high = 0, len(nums) - 1
         
-        def modified_binary_search(nums, target, low, high):
-        
-            mid = low + ( (high - low) / 2)
-            print (mid)
-            if low > high:
-                if target > nums[len(nums) - 1]:
-                    return len(nums)
-                elif target < nums[0]:
-                    return 0
-                else:
-                    return mid+1
+        while low <= high:
+            mid = int((low + high) / 2)
             
             if nums[mid] < target:
-                return modified_binary_search(nums, target, mid+1, high)
+                low = mid + 1
             elif nums[mid] > target:
-                return modified_binary_search(nums, target, low, mid-1)
+                high = mid - 1
             else:
                 return mid
             
-        return modified_binary_search(nums, target, 0, len(nums)-1)
+        return low
+                
         
