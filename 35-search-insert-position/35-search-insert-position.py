252861b66d8653cbe0f1567e439a81e4ec139c2a
Tyler Le <tylerle99@yahoo.com>
2022-06-28 18:53:35 -0700

    Time: 31 ms (94.31%), Space: 14.3 MB (7.90%) - LeetHub

--- 35-search-insert-position/35-search-insert-position.py
+++ 35-search-insert-position/35-search-insert-position.py
@@ -1,5 +1,11 @@
-class Solution:
-    def searchInsert(self, nums: List[int], target: int) -> int:
+class Solution(object):
+    def searchInsert(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+        
         low, high = 0, len(nums) - 1
         
         while low <= high:
@@ -13,5 +19,4 @@ class Solution:
                 return mid
             
         return low
-                
         
