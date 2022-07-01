aa0e3f3e043d8972b298c74ec65544b4bb25c138
Tyler Le <tylerle99@yahoo.com>
2022-06-30 21:18:45 -0700

    Time: 128 ms (79.22%), Space: 19.5 MB (22.61%) - LeetHub

--- 56-merge-intervals/56-merge-intervals.py
+++ 56-merge-intervals/56-merge-intervals.py
@@ -4,18 +4,18 @@ class Solution(object):
         :type intervals: List[List[int]]
         :rtype: List[List[int]]
         """
-        start,end=0,1
         intervals.sort(key = lambda i : i[0])
+        
         res = [intervals[0]]
         
-        for i in range (1, len(intervals)):
-            has_overlap = (res[-1][start]>=intervals[i][start] and res[-1][start]<=intervals[i][end]) or (intervals[i][start]>=res[-1][start] and intervals[i][start]<=res[-1][end])
+        for start, end in intervals[1:]:
+            has_overlap = start >= res[-1][0] and start <= res[-1][1]
             
             if has_overlap:
-                res[-1][start], res[-1][end] = min(res[-1][start], intervals[i][start]), max(res[-1][end], intervals[i][end])
+                res[-1][0] = min(res[-1][0], start)
+                res[-1][1] = max(res[-1][1], end)
+            
             else:
-                res.append(intervals[i])
-                
+                res.append([start, end])
+        
         return res
-        
-        
