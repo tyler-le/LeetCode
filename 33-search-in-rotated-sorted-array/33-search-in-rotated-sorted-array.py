0a5094c5629c16466c9f05a00421d90444790ed5
Tyler Le <tylerle99@yahoo.com>
2022-06-30 20:10:01 -0700

    Time: 27 ms (89.22%), Space: 13.5 MB (87.27%) - LeetHub

--- 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.py
+++ 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.py
@@ -1,6 +1,12 @@
-class Solution:
-    def search(self, nums: List[int], target: int) -> int:
+class Solution(object):
+    def search(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
         
+        # binary search to find pivot
         low, high = 0, len(nums) - 1
         
         while low <= high:
@@ -9,17 +15,17 @@ class Solution:
             if nums[mid] == target:
                 return mid
             
-            # nums[low] to nums[mid] is sorted
-            if nums[low] <= nums[mid]:
+            elif nums[low] <= nums[mid]:
                 if target >= nums[low] and target < nums[mid]:
                     high = mid - 1
                 else:
                     low = mid + 1
-                    
+            
             else:
                 if target > nums[mid] and target <= nums[high]:
                     low = mid + 1
                 else:
                     high = mid - 1
-            
+                    
         return -1
+        
