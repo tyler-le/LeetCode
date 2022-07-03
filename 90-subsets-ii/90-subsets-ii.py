b1122e66f89d4b0fb60a9858cf3350086e33f070
Tyler Le <tylerle99@yahoo.com>
2022-07-02 19:58:37 -0700

    Time: 79 ms (13.20%), Space: 14.1 MB (93.69%) - LeetHub

--- 90-subsets-ii/90-subsets-ii.py
+++ 90-subsets-ii/90-subsets-ii.py
@@ -6,14 +6,16 @@ class Solution:
         added_last_step = 0
         
         for i in range(len(nums)):
-            
+    
             current_level = []
+            
+            # if duplicate, append nums[i] to all subsets created in last iter
             if i > 0 and nums[i] == nums[i-1]:
                 for j in range(len(power_set) - added_last_step, len(power_set)):
                     new_subset = power_set[j] + [nums[i]]
                     current_level.append(new_subset)
-                    
-                    
+                 
+            # if not duplicate, append nums[i] to all subsets in power set.
             else:
                 for subset in power_set:
                     new_subset = subset + [nums[i]]
