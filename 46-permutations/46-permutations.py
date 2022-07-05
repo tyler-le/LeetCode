634580977207c70282d9750edecfdee91cb4ee3e
Tyler Le <tylerle99@yahoo.com>
2022-07-04 21:00:07 -0700

    Time: 102 ms (5.47%), Space: 14.2 MB (21.86%) - LeetHub

--- 46-permutations/46-permutations.py
+++ 46-permutations/46-permutations.py
@@ -1,21 +1,26 @@
 class Solution:
     def permute(self, nums: List[int]) -> List[List[int]]:
-    
-    
+        
         res = []
+        used_chars = set()
+        permutation = []
         
-        if len(nums) == 1:
-            return [nums.copy()]
-        
-        for i in range(len(nums)):
-            removed = nums.pop(0)
-            perms = self.permute(nums)
+        def backtrack():
+            if len(permutation) == len(nums):
+                res.append(permutation[::])
+                return
             
-            for perm in perms:
-                perm.append(removed)
-                res.append(perm)
-                
-            nums.append(removed)
-            
+            for i in range(len(nums)):
+                if nums[i] not in used_chars:
+                    permutation.append(nums[i])
+                    used_chars.add(nums[i])
+                    
+                    backtrack()
+                    
+                    permutation.pop()
+                    used_chars.remove(nums[i])    
+                    
+        backtrack()       
         return res
+            
         
