1761b541775c860bc61d0a5ca9648e613334f7e5
Tyler Le <tylerle99@yahoo.com>
2022-07-09 14:36:35 -0700

    Time: 986 ms (72.31%), Space: 22.8 MB (29.77%) - LeetHub

--- 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.py
+++ 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.py
@@ -1,14 +1,21 @@
-class Solution:
-    def maxProfit(self, prices: List[int]) -> int:
+class Solution(object):
+    def maxProfit(self, prices):
+        """
+        :type prices: List[int]
+        :rtype: int
+        """
         buy_day, sell_day = 0, 1
-        max_profit = 0
+        curr_profit, max_profit = 0, 0
         
         while sell_day < len(prices):
+            
             if prices[buy_day] > prices[sell_day]:
                 buy_day = sell_day
+                sell_day += 1
                 
-            profit = prices[sell_day] - prices[buy_day]            
-            max_profit = max(max_profit, profit)
-            sell_day += 1
-    
+            else:
+                curr_profit = prices[sell_day] - prices[buy_day]
+                max_profit = max(max_profit, curr_profit)
+                sell_day += 1
+                
         return max_profit
