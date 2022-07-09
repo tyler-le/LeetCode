a323b726471e08f2562dab0f158ff9265bb2478f
Tyler Le <tylerle99@yahoo.com>
2022-07-09 12:57:15 -0700

    Time: 169 ms (33.27%), Space: 13.9 MB (82.19%) - LeetHub

--- 36-valid-sudoku/36-valid-sudoku.py
+++ 36-valid-sudoku/36-valid-sudoku.py
@@ -1,47 +1,27 @@
-class Solution(object):
-    def isValidSudoku(self, board):
-        """
-        :type board: List[List[str]]
-        :rtype: bool
-        """
-
-        # loop thru rows
-        for i in range(len(board)):
-            my_set = set()
-            for j in range(len(board[0])):
-                if board[i][j] == '.':
-                    continue
-                if board[i][j] in my_set:
-                    return False
-                my_set.add(board[i][j])
-
-        # loop thru cols
-        for i in range(len(board)):
-            my_set = set()
-            for j in range(len(board[0])):
-                if board[j][i] == '.':
-                    continue
-                if board[j][i] in my_set:
-                    return False
-                my_set.add(board[j][i])
-                
-        # loop 3x3 subgrid
-        squares = {} # key is (row, col)
+class Solution:
+    def isValidSudoku(self, board: List[List[str]]) -> bool:
+        cols = collections.defaultdict(set)
+        rows = collections.defaultdict(set)
+        subgrid = collections.defaultdict(set) # key = (r /3, c /3)
         
         for i in range(9):
             for j in range(9):
-                if board[i][j] == '.':
+                
+                curr_num = board[i][j]
+                if curr_num == '.':
                     continue
                     
-                if (i//3, j//3) not in squares:
-                    squares[(i//3, j//3)] = set()
-                if board[i][j] in squares[(i//3, j//3)]:
+                if (i//3, j//3) not in subgrid:
+                    subgrid[(i//3, j//3)] = set()
+                    
+                if curr_num in rows[i] or curr_num in cols[j] or curr_num in subgrid[(i//3,j//3)]:
+                    print(rows)
+
                     return False
-                else:
-                    squares[(i//3,j//3)].add(board[i][j])
+                
+                rows[i].add(curr_num)
+                cols[j].add(curr_num)
+                subgrid[(i//3, j//3)].add(curr_num)
                 
         return True
-                
-                
-            
                 
