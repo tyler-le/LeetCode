a21ac3d32769f4839684554fb1bb9fbfcb10e3a9
Tyler Le <tylerle99@yahoo.com>
2022-07-15 17:51:43 -0700

    Time: 78 ms (94.96%), Space: 31.2 MB (53.70%) - LeetHub

--- 143-reorder-list/143-reorder-list.py
+++ 143-reorder-list/143-reorder-list.py
@@ -1,39 +1,46 @@
 # Definition for singly-linked list.
-# class ListNode:
+# class ListNode(object):
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
-class Solution:
-    def reorderList(self, head: Optional[ListNode]) -> None:
+class Solution(object):
+    def reorderList(self, head):
         """
-        Do not return anything, modify head in-place instead.
+        :type head: ListNode
+        :rtype: None Do not return anything, modify head in-place instead.
         """
         
-        # find middle of list
         slow = fast = head
-        while fast != None and fast.next != None:
+        
+        # get middle of linked list
+        while fast and fast.next:
             slow = slow.next
             fast = fast.next.next
             
-        # reverse second half
-        prev = None
-        while slow != None:
-            tmp = slow.next
-            slow.next = prev
-            prev = slow
-            slow = tmp
-        
-        # reconnect zigzag list
-        l,r,l_next,r_next = head, prev, head.next, prev.next
-       
-        while l != r_next and r != l_next:
+        # reverse linked list from fast to end
+        prev, curr, next_node = None, slow, None
+        while curr:
+            next_node = curr.next
+            curr.next = prev
+            
+            prev = curr
+            curr = next_node
+            
+        # inter-leave pointers
+        l,r = head, prev
+        while l and r:
+            temp_l = l.next
             l.next = r
-            r.next = l_next
-            l = l_next
-            r = r_next
-            if l==r:
-                break
-                
-            l_next = l.next
-            r_next = r.next
+            l = temp_l
+            temp_r = r.next
+            r.next = l
+            r = temp_r
+            
+        if not l and r:
+            r.next = l
+        if l and not r:
+            l.next = r
+            
+        
+            
         
