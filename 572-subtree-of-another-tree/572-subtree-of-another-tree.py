3d24399ff1763bdb772dcb675feaf2637ff22014
Tyler Le <tylerle99@yahoo.com>
2022-07-17 10:27:51 -0700

    Time: 121 ms (90.55%), Space: 14.6 MB (21.72%) - LeetHub

--- 572-subtree-of-another-tree/572-subtree-of-another-tree.py
+++ 572-subtree-of-another-tree/572-subtree-of-another-tree.py
@@ -1,30 +1,28 @@
 # Definition for a binary tree node.
-# class TreeNode:
+# class TreeNode(object):
 #     def __init__(self, val=0, left=None, right=None):
 #         self.val = val
 #         self.left = left
 #         self.right = right
-class Solution:
-    def isSubtree(self, root: Optional[TreeNode], subRoot: Optional[TreeNode]) -> bool:
-            
+class Solution(object):
+    def isSubtree(self, root, subRoot):
+        """
+        :type root: TreeNode
+        :type subRoot: TreeNode
+        :rtype: bool
+        """
+        def isSameTree(p, q):
+            if not p and not q:
+                return True
+            if (p and not q) or (not p and q) or (p.val != q.val):
+                return False
+            return isSameTree(p.left, q.left) and isSameTree(p.right, q.right)
+        
         if not root:
             return False
         
-        if root and subRoot and root.val == subRoot.val and self.isSameTree(root, subRoot):
-            return True
-        
-        else:
-            return self.isSubtree(root.left, subRoot) or self.isSubtree(root.right, subRoot)
-        
-    def isSameTree(self, p, q):
-        if not p and not q:
-            return True
-
-        if (not p and q) or (p and not q):
-            return False
-
-        if p.val != q.val:
-            return False
-
-        else:
-            return self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
+        if root and subRoot and root.val == subRoot.val:
+            if isSameTree(root, subRoot):
+                return True
+            
+        return self.isSubtree(root.left, subRoot) or self.isSubtree(root.right, subRoot)
