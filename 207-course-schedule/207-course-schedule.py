d5681422dcfd298dee6019aed2eda4ba9c1519ee
Tyler Le <tylerle99@yahoo.com>
2022-07-18 21:16:09 -0700

    Time: 128 ms (71.11%), Space: 17.2 MB (25.63%) - LeetHub

--- 207-course-schedule/207-course-schedule.py
+++ 207-course-schedule/207-course-schedule.py
@@ -1,45 +1,24 @@
-class Solution(object):
-    def canFinish(self, numCourses, prerequisites):
-        """
-        :type numCourses: int
-        :type prerequisites: List[List[int]]
-        :rtype: bool
-        """
-        # create an empty prereqlist for each course
-        prereq_map = { i:[] for i in range(numCourses)} 
-        
-        # map each course to prereq list
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        prereq_map = {i:[] for i in range(numCourses)}
         for course, prereq in prerequisites:
             prereq_map[course].append(prereq)
-        
-        # needed for backtracking
-        visited = set()
-        
-        def dfs(course):
-            # if already visited, we are in a loop
-            if course in visited:
-                return False
             
-            # if course has no prereqs, we hit a base case and return True
-            if prereq_map[course] == []:
-                return True
+        visited = set()
+        def dfs(course):
+            if course in visited: return False
+            if prereq_map[course] == []: return True
             
             visited.add(course)
-            
-            # for each prereq in our course list, run dfs on it
             for prereq in prereq_map[course]:
                 if not dfs(prereq): return False
                 
-            # unvisit current course    
             visited.remove(course)
-            
-            # going this far means the course can be taken. to speed up future calculations, we               just set prereqs to [] to immediately get to base case
             prereq_map[course] = []
             return True
         
-        # we need the for loop in the case of an unconnected graph
         for i in range(numCourses):
             if not dfs(i): return False
+            
         return True
-
-
+            
