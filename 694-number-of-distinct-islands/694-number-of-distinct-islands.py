6d5de7421be0a3c51f4f6dbc3380ad16d25724c9
Tyler Le <tylerle99@yahoo.com>
2022-07-22 13:13:45 -0700

    Time: 234 ms (87.13%), Space: 17.9 MB (35.28%) - LeetHub

--- 694-number-of-distinct-islands/694-number-of-distinct-islands.py
+++ 694-number-of-distinct-islands/694-number-of-distinct-islands.py
@@ -26,6 +26,6 @@ class Solution:
         for i in range(len(grid)):
             for j in range(len(grid[0])):
                 if grid[i][j] == 1 and (i,j) not in visited:
-                    paths.add(dfs(i,j, 'Start'))
+                    paths.add(dfs(i,j, ''))
 
         return len(paths)
