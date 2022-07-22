1556c83e1588f28ac88167f51b772154a9c0832b
Tyler Le <tylerle99@yahoo.com>
2022-07-22 14:36:06 -0700

    Time: 93 ms (78.02%), Space: 13.9 MB (68.06%) - LeetHub

--- 567-permutation-in-string/567-permutation-in-string.py
+++ 567-permutation-in-string/567-permutation-in-string.py
@@ -1,44 +1,21 @@
-class Solution(object):
-    def checkInclusion(self, s1, s2):
-        """
-        :type s1: str
-        :type s2: str
-        :rtype: bool
-        """
+class Solution:
+    def checkInclusion(self, s1: str, s2: str) -> bool:
         
-        if len(s1) > len(s2):
-            return False
+        perm = Counter(s1)
+        count = {}
         
-        s1_count, s2_count= {}, {}        
-        
-        # populate s1_count
-        for s in s1:
-            s1_count[s] = 1 + s1_count.get(s,0)
+        l = 0
+        for r in range(len(s2)):
+            count[s2[r]] = 1 + count.get(s2[r], 0)
             
-        # get all substrings of length s1 in s2
-        substrings = []
-        for i in range(len(s2) - len(s1) + 1):
-            substrings.append(s2[i:i+len(s1)])
-            
-            
-        # check each substring to see if its an anagram of s1
-        for substr in substrings:
-            substr_dict = {}
-            for ch in substr:
-                substr_dict[ch] = 1 + substr_dict.get(ch, 0)
-            if substr_dict == s1_count:
-                return True
+            if r-l+1 > len(s1): 
+                count[s2[l]] -= 1
+                
+                if count[s2[l]] == 0: 
+                    del count[s2[l]]
+                l+=1
+                    
+            if dict(perm) == count: return True
             
         return False
                 
-                    
-                    
-            
-        
-        
-            
-        
-        
-        
-        
-        
