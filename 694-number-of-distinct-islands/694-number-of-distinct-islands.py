5bdc346cddbc7a5732f0325da8d75abc86b3e07d
Tyler Le <tylerle99@yahoo.com>
2022-07-22 13:12:49 -0700

    Time: 215 ms (93.30%), Space: 18 MB (32.04%) - LeetHub

--- 694-number-of-distinct-islands/694-number-of-distinct-islands.py
+++ 694-number-of-distinct-islands/694-number-of-distinct-islands.py
@@ -3,7 +3,7 @@ class Solution:
     
         visited = set()
 
-        def dfs(i,j, direction):
+        def dfs(i,j, path):
             if i < 0 or j < 0 or i >= len(grid) or j >= len(grid[0]) or grid[i][j] == 0:
                 return ""
 
@@ -12,7 +12,6 @@ class Solution:
                         
             visited.add((i,j))
             
-            path = direction
             
             path += dfs(i-1,j, 'N')
             path += dfs(i,j+1, 'E')
@@ -23,9 +22,10 @@ class Solution:
             return path
         
         paths = set()
+        
         for i in range(len(grid)):
             for j in range(len(grid[0])):
                 if grid[i][j] == 1 and (i,j) not in visited:
                     paths.add(dfs(i,j, 'Start'))
-        print(paths)
+
         return len(paths)
