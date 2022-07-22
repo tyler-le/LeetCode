347100d3e011ee783966568c202db320a0c60cc9
Tyler Le <tylerle99@yahoo.com>
2022-07-21 21:26:43 -0700

    Time: 215 ms (43.00%), Space: 14.6 MB (28.52%) - LeetHub

--- 1254-number-of-closed-islands/1254-number-of-closed-islands.py
+++ 1254-number-of-closed-islands/1254-number-of-closed-islands.py
@@ -1,27 +1,38 @@
 class Solution:
     def closedIsland(self, grid: List[List[int]]) -> int:
-        def dfs(i,j):
-            if i < 0 or j < 0 or i >= len(grid) or j >= len(grid[0]):
-                return False
-                
-            if grid[i][j] == 1 or (i,j) in visited:
-                return True
+        def flood_fill(i,j):
+            if i < 0 or j < 0 or i >= len(grid) or j >= len(grid[0]) or grid[i][j] == 1:
+                return
             
-            visited.add((i,j))
-            is_closed = True
+            grid[i][j] = 1
             
-            is_closed &= dfs(i+1,j)  
-            is_closed &= dfs(i-1,j) 
-            is_closed &= dfs(i,j+1) 
-            is_closed &= dfs(i,j-1)
-            return is_closed
+            flood_fill(i+1,j)
+            flood_fill(i-1,j)
+            flood_fill(i,j+1)
+            flood_fill(i,j-1)
             
+        # perform flood fill on borders
+        for row in range(len(grid)):
+            for col in range(len(grid[0])):
+                if col == 0 or col == len(grid[0]) - 1 or row == 0 or row == len(grid) - 1:
+                    flood_fill(row,col)
         
+        def dfs(i,j):
+            if i < 0 or j < 0 or i >= len(grid) or j >= len(grid[0]) or grid[i][j] == 1:
+                return
+            
+            grid[i][j] = 1
+            dfs(i+1,j)
+            dfs(i-1,j)
+            dfs(i,j+1)
+            dfs(i,j-1)
+        
+        # count remaining islands
         res = 0
-        visited = set()
-        for i in range(len(grid)):
-            for j in range (len(grid[0])):
-                if grid[i][j] == 0 and (i,j) not in visited:
-                    if dfs(i,j):
-                        res += 1
+        for row in range(len(grid)):
+            for col in range(len(grid[0])):
+                if grid[row][col] == 0:
+                    dfs(row,col)
+                    res += 1
         return res
+        
