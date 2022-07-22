3cc032eb3ff5fbe82626c5aacf4e25f332ed11af
Tyler Le <tylerle99@yahoo.com>
2022-07-22 13:15:46 -0700

    Time: 318 ms (53.31%), Space: 18.2 MB (23.38%) - LeetHub

--- 694-number-of-distinct-islands/694-number-of-distinct-islands.py
+++ 694-number-of-distinct-islands/694-number-of-distinct-islands.py
@@ -1,28 +1,26 @@
 class Solution:
     def numDistinctIslands(self, grid: List[List[int]]) -> int:
     
-        visited = set()
+        visited, paths = set(), set()
 
         def dfs(i,j, path):
             if i < 0 or j < 0 or i >= len(grid) or j >= len(grid[0]) or grid[i][j] == 0:
                 return ""
 
-            if (i,j) in visited:
+            if (i,j) in visited: 
                 return ""
                         
             visited.add((i,j))
-            
             
             path += dfs(i-1,j, 'N')
             path += dfs(i,j+1, 'E')
             path += dfs(i+1,j, 'S')
             path += dfs(i,j-1, 'W')
             
-            path += 'B'
+            path += 'Back'
+            
             return path
-        
-        paths = set()
-        
+                
         for i in range(len(grid)):
             for j in range(len(grid[0])):
                 if grid[i][j] == 1 and (i,j) not in visited:
