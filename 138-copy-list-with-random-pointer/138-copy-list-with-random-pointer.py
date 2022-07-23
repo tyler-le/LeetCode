84f7f7a30739b7511412415e2c21c1f4977014bd
Tyler Le <tylerle99@yahoo.com>
2022-07-22 19:23:10 -0700

    Time: 38 ms (96.76%), Space: 14.2 MB (87.48%) - LeetHub

--- 138-copy-list-with-random-pointer/138-copy-list-with-random-pointer.py
+++ 138-copy-list-with-random-pointer/138-copy-list-with-random-pointer.py
@@ -1,31 +1,31 @@
 """
 # Definition for a Node.
 class Node:
-    def __init__(self, x: int, next: 'Node' = None, random: 'Node' = None):
+    def __init__(self, x, next=None, random=None):
         self.val = int(x)
         self.next = next
         self.random = random
 """
 
-class Solution:
-    def copyRandomList(self, head: 'Optional[Node]') -> 'Optional[Node]':
-        old_to_new = {}
-        old_to_new[None] = None
+class Solution(object):
+    def copyRandomList(self, head):
+        """
+        :type head: Node
+        :rtype: Node
+        """
+        old2new = {None:None}
         
-        # first pass - create a copy of all nodes (unlinked) and map orig to new_node
         curr = head
         while curr:
-            new_node = Node(curr.val)
-            old_to_new[curr] = new_node
+            copy = Node(curr.val)
+            old2new[curr] = copy
             curr = curr.next
-            
-        # second pass - connect all the pointers
+        
         curr = head
         while curr:
-            copy = old_to_new[curr]
-            copy.next = old_to_new[curr.next]
-            copy.random = old_to_new[curr.random]
+            old2new[curr].next = old2new[curr.next]
+            old2new[curr].random = old2new[curr.random]
             curr = curr.next
-            
-        return old_to_new[head]
+        
+        return old2new[head]
         
