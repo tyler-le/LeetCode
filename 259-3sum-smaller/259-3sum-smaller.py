4d295e620909be7804f59fcc2bcafcf337e87e99
Tyler Le <tylerle99@yahoo.com>
2022-07-23 13:58:10 -0700

    Time: 1208 ms (40.23%), Space: 13.9 MB (89.98%) - LeetHub

--- 259-3sum-smaller/259-3sum-smaller.py
+++ 259-3sum-smaller/259-3sum-smaller.py
@@ -5,9 +5,9 @@ class Solution:
         
         for i in range(len(nums)):
             l,r = i+1, len(nums)-1
+            
             while l < r:
-                curr_sum = sum((nums[i], nums[l], nums[r]))
-                if curr_sum < target:
+                if nums[i] + nums[l] + nums[r] < target:
                     res += r-l
                     l+=1
                 else:
