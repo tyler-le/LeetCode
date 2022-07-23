464de17fea7c42de490e7bbd720b1f59081f8df2
Tyler Le <tylerle99@yahoo.com>
2022-07-22 22:15:22 -0700

    Time: 1394 ms (47.34%), Space: 20.1 MB (88.10%) - LeetHub

--- 904-fruit-into-baskets/904-fruit-into-baskets.py
+++ 904-fruit-into-baskets/904-fruit-into-baskets.py
@@ -1,35 +1,18 @@
-class Solution(object):
-    def totalFruit(self, fruits):
-        """
-        :type fruits: List[int]
-        :rtype: int
-        """
-        if len(fruits)<2:
-            return len(fruits)
+class Solution:
+    def totalFruit(self, fruits: List[int]) -> int:
+        res = 0
         
-        max_trees = -(sys.maxint-1)
-        start,end = 0,1
-        my_dict = {}
+        l = 0
+        basket = {}
         
-        my_dict[fruits[start]] = 1
-        
-        while end < len(fruits):
-            max_trees = max(max_trees, end-start)
+        for r in range (len(fruits)):
+            basket[fruits[r]] = 1 + basket.get(fruits[r], 0)
             
-            if fruits[end] in my_dict:
-                my_dict[fruits[end]]+=1
-            else:
-                my_dict[fruits[end]]=1
+            while len(basket) > 2:
+                basket[fruits[l]] -= 1
+                if basket[fruits[l]] == 0: 
+                    del basket[fruits[l]]
+                l+=1
+            res = max(res, r-l+1)
             
-            while len(my_dict) > 2:
-                if my_dict[fruits[start]]>0:
-                    my_dict[fruits[start]]-=1
-                
-                if my_dict[fruits[start]] == 0:
-                    del my_dict[fruits[start]]
-                    
-                start+=1
-                
-            end+=1
-        
-        return max(max_trees, end-start)
+        return res
