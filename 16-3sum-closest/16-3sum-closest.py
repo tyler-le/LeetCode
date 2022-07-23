d5a3bf5b190ee2f0681d977a05908ab333a57d57
Tyler Le <tylerle99@yahoo.com>
2022-07-23 13:05:46 -0700

    Time: 8722 ms (12.19%), Space: 13.6 MB (26.26%) - LeetHub

--- 16-3sum-closest/16-3sum-closest.py
+++ 16-3sum-closest/16-3sum-closest.py
@@ -6,7 +6,7 @@ class Solution(object):
         :rtype: int
         """
         closest = float('inf')
-        curr_sum = 0
+        res = 0
         nums.sort()
         
         for index in range(0, len(nums)-1):
@@ -20,14 +20,14 @@ class Solution(object):
                 elif nums[index] + nums[l] + nums[r] < target:
                     if abs(target - (nums[index] + nums[l] + nums[r])) < closest:
                         closest = abs(target - (nums[index] + nums[l] + nums[r]))
-                        curr_sum = (nums[index] + nums[l] + nums[r])
+                        res = (nums[index] + nums[l] + nums[r])
                     l+=1
                     
                 elif nums[index] + nums[l] + nums[r] > target:
                     if abs(target - (nums[index] + nums[l] + nums[r])) < closest:
                         closest = abs(target - (nums[index] + nums[l] + nums[r]))
-                        curr_sum = (nums[index] + nums[l] + nums[r])    
+                        res = (nums[index] + nums[l] + nums[r])    
                     r-=1
                     
                     
-        return curr_sum
+        return res
