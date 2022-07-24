1c8f7951d23f6138860a7814f6bb6de7fc40cd07
Tyler Le <tylerle99@yahoo.com>
2022-07-23 19:46:24 -0700

    Time: 37 ms (46.06%), Space: 13.6 MB (22.42%) - LeetHub

--- 767-reorganize-string/767-reorganize-string.py
+++ 767-reorganize-string/767-reorganize-string.py
@@ -13,17 +13,17 @@ class Solution(object):
         
         while max_heap:
             popped = heappop(max_heap)
-            freq, letter = popped[0], popped[1]
-            res += [letter]
+            curr_freq, curr_letter  = popped[0], popped[1]
+            res += [curr_letter]
             
             # since prev was on hold, put prev back in
             if prev and prev[0] < 0:
                 heappush(max_heap, prev)
                 
-            freq+=1
+            curr_freq+=1
             
             # now put the letter that we just inserted on hold
-            prev = [freq, letter]
+            prev = [curr_freq, curr_letter]
         
         res = ''.join(res)
         return res if len(res)==len(s) else ""
