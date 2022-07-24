c9665fb38ab2bc24a4873314c300bbe98c8f2282
Tyler Le <tylerle99@yahoo.com>
2022-07-23 18:41:29 -0700

    Time: 100 ms (76.90%), Space: 19.9 MB (6.74%) - LeetHub

--- 49-group-anagrams/49-group-anagrams.py
+++ 49-group-anagrams/49-group-anagrams.py
@@ -5,13 +5,11 @@ class Solution(object):
         :rtype: List[List[str]]
         """
         res = collections.defaultdict(list)
-        out = []
-        # map (char count) -> list
         
+        # map (char count) -> list
         for s in strs:
             char_count = [0 for i in range(26)]
-            for ch in s:
-                char_count[ord(ch) - ord('a')] += 1
+            for ch in s: char_count[ord(ch) - ord('a')] += 1
             res[tuple(char_count)].append(s)
             
         return list(res.values())
