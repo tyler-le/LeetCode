cf0069953a926bae107ef74c623245d4d6038a54
Tyler Le <tylerle99@yahoo.com>
2022-07-23 17:18:36 -0700

    Time: 252 ms (32.50%), Space: 14 MB (32.40%) - LeetHub

--- 1710-maximum-units-on-a-truck/1710-maximum-units-on-a-truck.py
+++ 1710-maximum-units-on-a-truck/1710-maximum-units-on-a-truck.py
@@ -10,16 +10,10 @@ class Solution(object):
         res = 0
         
         for num_boxes, num_units in boxTypes:
-            take_box = min(truckSize, num_boxes)
-            res += take_box * num_units
-            truckSize-=take_box
+            boxes_to_take = min(truckSize, num_boxes)
+            res += boxes_to_take * num_units
+            truckSize -= boxes_to_take
             
-#             while truckSize and num_boxes > 0:
-#                 res += num_units
-#                 num_boxes -= 1
-#                 truckSize -= 1
-                
-            if truckSize == 0:
-                return res
+            if truckSize == 0: return res
             
         return res
