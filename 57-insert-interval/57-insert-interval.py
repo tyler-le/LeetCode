1dd8733566f114804d263919960a4ba7c388953b
Tyler Le <tylerle99@yahoo.com>
2022-07-24 19:18:06 -0700

    Time: 107 ms (28.88%), Space: 16.7 MB (93.20%) - LeetHub

--- 57-insert-interval/57-insert-interval.py
+++ 57-insert-interval/57-insert-interval.py
@@ -5,27 +5,27 @@ class Solution(object):
         :type newInterval: List[int]
         :rtype: List[List[int]]
         """
+        if not intervals: return [newInterval]
+        
+        intervals.sort(key = lambda i : i[0])
+        START,END = 0,1
         res = []
         
-        if len(intervals) == 0:
-            return [newInterval]
-        
-        for i in range(len(intervals)):
-            currInterval_start, currInterval_end = intervals[i][0], intervals[i][1]
-            newInterval_start, newInterval_end = newInterval[0], newInterval[1]
+        for i in range (len(intervals)):
+            comes_before = newInterval[END] < intervals[i][START]
+            comes_after = newInterval[START] > intervals[i][END]
             
-            comes_before_no_overlap = newInterval_end < currInterval_start
-            if comes_before_no_overlap:
+            if comes_before: 
                 res.append(newInterval)
-                res+=intervals[i:]
+                res += intervals[i:]
                 return res
             
-            goes_after_no_overlap = newInterval_start > currInterval_end
-            if goes_after_no_overlap:
+            if comes_after:
                 res.append(intervals[i])
-            
-            else:
-                newInterval[0], newInterval[1] = min(newInterval_start, currInterval_start), max(newInterval_end, currInterval_end)
                 
-        res.append(newInterval)
+            else:
+                newInterval = [min(newInterval[START], intervals[i][START]),
+                               max(newInterval[END], intervals[i][END])]
+        res.append(newInterval)        
         return res
+            
