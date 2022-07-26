fb9d09213c11630c3c8ba3a196a1e30dfff81e0b
Tyler Le <tylerle99@yahoo.com>
2022-07-25 20:28:21 -0700

    Time: 1607 ms (14.37%), Space: 20.5 MB (37.55%) - LeetHub

--- 973-k-closest-points-to-origin/973-k-closest-points-to-origin.py
+++ 973-k-closest-points-to-origin/973-k-closest-points-to-origin.py
@@ -6,13 +6,15 @@ class Solution:
         for x,y in points:
             # euclidean distance
             dist = math.sqrt(x**2 + y**2)
+            
             # create tuple (dist, x, y)
-            to_insert = (dist,x, y)
-            heappush(heap, to_insert)
+            tupl = (dist,x, y)
+            heappush(heap, tupl)
         
         for i in range(k):
-            dist,x,y = heappop(heap)
+            _, x, y = heappop(heap)
             res.append([x,y])
+            
         return res
         
             
