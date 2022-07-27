8d0c8794c3af6d9ec10dd44313368cb78a53d933
Tyler Le <tylerle99@yahoo.com>
2022-07-26 20:52:50 -0700

    Time: 51 ms (32.94%), Space: 13.9 MB (49.61%) - LeetHub

--- 74-search-a-2d-matrix/74-search-a-2d-matrix.py
+++ 74-search-a-2d-matrix/74-search-a-2d-matrix.py
@@ -7,22 +7,26 @@ class Solution(object):
         """
         
         # binary search to find appropriate row
-        num_rows, num_cols = len(matrix) - 1, len(matrix[0]) - 1
-        
+        num_rows = len(matrix) - 1
         low, high = 0, num_rows
+        
         while low <= high:
             mid = low + ((high - low) // 2)
             row = matrix[mid]
+            
             if target < row[0]: high = mid - 1
             elif target > row[-1]: low = mid + 1
             else: break
 
+                
         low, high = 0, len(row) - 1
         while low <= high:
             mid = low + ((high - low) // 2)
+            
             if row[mid] == target: return True
             if row[mid] < target: low = mid + 1
             if row[mid] > target: high = mid - 1
+                
         return False
             
         
