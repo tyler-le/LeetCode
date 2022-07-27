924c234719b59ae9af7d356c151b383cebd2120b
Tyler Le <tylerle99@yahoo.com>
2022-07-26 20:49:11 -0700

    Time: 30 ms (89.33%), Space: 14.2 MB (6.59%) - LeetHub

--- 74-search-a-2d-matrix/74-search-a-2d-matrix.py
+++ 74-search-a-2d-matrix/74-search-a-2d-matrix.py
@@ -1,19 +1,28 @@
-class Solution:
-    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
-        # create an array of the elems in sorted order
-        nums = []
-        for i in range(len(matrix)):
-            for j in range(len(matrix[0])):
-                nums.append(matrix[i][j])
+class Solution(object):
+    def searchMatrix(self, matrix, target):
+        """
+        :type matrix: List[List[int]]
+        :type target: int
+        :rtype: bool
+        """
         
-        # run binary search on resulting array
-        low, high = 0, len(nums)-1
+        # binary search to find row
+        num_rows, num_cols = len(matrix) - 1, len(matrix[0]) - 1
+        
+        low, high = 0, num_rows
         while low <= high:
             mid = low + ((high - low) // 2)
-            if nums[mid] == target: return True
-            if nums[mid] < target: low = mid + 1
-            if nums[mid] > target: high = mid - 1
+            row_to_search = matrix[mid]
+            if target < row_to_search[0]: high = mid - 1
+            elif target > row_to_search[-1]: low = mid + 1
+            else: break
+
+        low, high = 0, len(row_to_search) - 1
+        while low <= high:
+            mid = low + ((high - low) // 2)
+            if row_to_search[mid] == target: return True
+            if row_to_search[mid] < target: low = mid + 1
+            if row_to_search[mid] > target: high = mid - 1
         return False
-    
-        
+            
         
