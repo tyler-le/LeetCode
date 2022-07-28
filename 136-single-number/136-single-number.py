087ce1fe48d3c8ff623b5cc892a33d3029744bff
Tyler Le <tylerle99@yahoo.com>
2022-07-27 20:57:21 -0700

    Time: 134 ms (96.56%), Space: 16.7 MB (50.23%) - LeetHub

--- 136-single-number/136-single-number.py
+++ 136-single-number/136-single-number.py
@@ -1,9 +1,10 @@
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
         # Using XOR to find pairs
-
         xor = nums[0]
+        
         for num in nums[1:]:
             xor ^= num
+            
         return xor
         
