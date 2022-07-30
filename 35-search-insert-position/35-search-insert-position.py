552b56487924f942a7d814c42730e11fcbc67e44
Tyler Le <tylerle99@yahoo.com>
2022-07-29 20:54:42 -0700

    Time: 58 ms (36.38%), Space: 14.1 MB (55.28%) - LeetHub

--- 35-search-insert-position/35-search-insert-position.py
+++ 35-search-insert-position/35-search-insert-position.py
@@ -10,13 +10,9 @@ class Solution(object):
         
         while lo <= hi:
             mid = lo + ((hi - lo) // 2)
-            
-            if nums[mid] < target:
-                lo = mid + 1
-            elif nums[mid] > target:
-                hi = mid - 1
-            else:
-                return mid
-            
+            if nums[mid] == target: return mid
+            elif nums[mid] < target: lo = mid + 1
+            else: hi = mid - 1
+                
         return lo
         
