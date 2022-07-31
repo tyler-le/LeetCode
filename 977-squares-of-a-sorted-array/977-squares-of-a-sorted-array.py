bf1364a13ddad92a6cf61e2588dedab5529bf02e
Tyler Le <tylerle99@yahoo.com>
2022-07-30 20:07:04 -0700

    Time: 278 ms (77.44%), Space: 16.3 MB (17.03%) - LeetHub

--- 977-squares-of-a-sorted-array/977-squares-of-a-sorted-array.py
+++ 977-squares-of-a-sorted-array/977-squares-of-a-sorted-array.py
@@ -1,18 +1,14 @@
-class Solution(object):
-    def sortedSquares(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: List[int]
-        """
-        l,r = 0,len(nums) - 1
-        res=[]
+class Solution:
+    def sortedSquares(self, nums: List[int]) -> List[int]:
+        l,r = 0,len(nums)-1
+        
+        res = deque()
         
         while l <= r:
-            if abs(nums[l]) > abs(nums[r]):
-                res.insert(0, nums[l]*nums[l])
-                l+=1
-            else:
-                res.insert(0, nums[r]*nums[r])
+            if abs(nums[l]) < abs(nums[r]):
+                res.appendleft(nums[r] ** 2)
                 r-=1
-        
+            else:
+                res.appendleft(nums[l] ** 2)
+                l+=1
         return res
