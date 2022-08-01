801441f23a8bea6e63480a15ffe49ebbf3ff8e4e
Tyler Le <tylerle99@yahoo.com>
2022-07-31 21:07:52 -0700

    Time: 176 ms (75.23%), Space: 14.9 MB (13.12%) - LeetHub

--- 283-move-zeroes/283-move-zeroes.py
+++ 283-move-zeroes/283-move-zeroes.py
@@ -1,14 +1,13 @@
-class Solution:
-    def moveZeroes(self, nums: List[int]) -> None:
+class Solution(object):
+    def moveZeroes(self, nums):
         """
-        Do not return anything, modify nums in-place instead.
+        :type nums: List[int]
+        :rtype: None Do not return anything, modify nums in-place instead.
         """
+        
         l = 0
-
         for r in range(len(nums)):
-            if nums[r] != 0:
+            if nums[r]:
                 nums[l], nums[r] = nums[r], nums[l]
                 l+=1
-            
-            
         
