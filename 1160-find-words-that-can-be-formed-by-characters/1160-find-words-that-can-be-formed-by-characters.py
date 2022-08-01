eabbe8b9e51a80555f764c18facbfe382f2406f7
Tyler Le <tylerle99@yahoo.com>
2022-07-31 19:54:40 -0700

    Time: 320 ms (32.77%), Space: 14.5 MB (85.11%) - LeetHub

--- 1160-find-words-that-can-be-formed-by-characters/1160-find-words-that-can-be-formed-by-characters.py
+++ 1160-find-words-that-can-be-formed-by-characters/1160-find-words-that-can-be-formed-by-characters.py
@@ -4,10 +4,10 @@ class Solution:
         char_count = Counter(chars)
         
         for word in words:
-            flag, ch_count = True, char_count.copy()
+            flag = True
             word_count = Counter(word)
             for ch in word:
-                if ch not in ch_count or ch_count[ch] < word_count[ch]: 
+                if ch not in char_count or char_count[ch] < word_count[ch]: 
                     flag = False
                     
             if flag: 
