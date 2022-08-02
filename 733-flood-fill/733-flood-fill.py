5cd7fcd958e08683df5507ffe122f227d2d8d394
Tyler Le <tylerle99@yahoo.com>
2022-08-01 20:24:36 -0700

    Time: 177 ms (5.16%), Space: 14.2 MB (14.38%) - LeetHub

--- 733-flood-fill/733-flood-fill.py
+++ 733-flood-fill/733-flood-fill.py
@@ -1,24 +1,20 @@
-class Solution(object):
-    def floodFill(self, image, sr, sc, color):
-        """
-        :type image: List[List[int]]
-        :type sr: int
-        :type sc: int
-        :type color: int
-        :rtype: List[List[int]]
-        """
-        val = image[sr][sc]
+class Solution:
+    def floodFill(self, image: List[List[int]], sr: int, sc: int, color: int) -> List[List[int]]:
+        starting_pixel = image[sr][sc]
         
-        def dfs(i, j, val):
-            if i < 0 or j < 0 or i >= len(image) or j >= len(image[0]) or image[i][j] != val or image[i][j] == color: 
+        def dfs(i, j):
+            if i < 0 or j < 0 or i >= len(image) or j >= len(image[0]):
+                return
+            if image[i][j] != starting_pixel:
                 return
             
             image[i][j] = color
             
-            dfs(i-1, j, val)
-            dfs(i+1, j, val)
-            dfs(i, j-1, val)
-            dfs(i, j+1, val)
-
-        dfs(sr, sc, val)
+            dfs(i+1,j)
+            dfs(i-1,j)
+            dfs(i,j+1)
+            dfs(i,j-1)
+            
+        if image[sr][sc] != color:
+            dfs(sr,sc)
         return image
