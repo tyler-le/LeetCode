08dc915461217e3724efce7e6426b0567a8dca11
Tyler Le <tylerle99@yahoo.com>
2022-08-01 20:15:52 -0700

    Time: 284 ms (56.73%), Space: 14.8 MB (8.26%) - LeetHub

--- 704-binary-search/704-binary-search.py
+++ 704-binary-search/704-binary-search.py
@@ -1,13 +1,17 @@
-class Solution:
-    def search(self, nums: List[int], target: int) -> int:
+class Solution(object):
+    def search(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+        # Iterative
         lo, hi = 0, len(nums)-1
         
         while lo <= hi:
             mid = lo + ((hi - lo) // 2)
             if nums[mid] == target: return mid
-            elif nums[mid] < target: lo = mid+1
+            elif nums[mid] < target: lo = mid + 1
             else: hi = mid - 1
                 
         return -1
-    
-    
