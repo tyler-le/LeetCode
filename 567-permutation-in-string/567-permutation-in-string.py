31954cdab29589ed74e370db9f8b138f16ba6b8f
Tyler Le <tylerle99@yahoo.com>
2022-08-04 20:36:30 -0700

    Time: 100 ms (73.74%), Space: 13.9 MB (68.65%) - LeetHub

--- 567-permutation-in-string/567-permutation-in-string.py
+++ 567-permutation-in-string/567-permutation-in-string.py
@@ -4,10 +4,12 @@ class Solution:
         
         for r in range(len(s2)):
             s2_count[s2[r]] = 1 + s2_count.get(s2[r], 0)
+            
             if r-l+1 > len(s1):
                 s2_count[s2[l]]-=1
                 if s2_count[s2[l]] == 0: del s2_count[s2[l]]
                 l+=1
+                
             if s1_count == s2_count: return True
         
         return False
