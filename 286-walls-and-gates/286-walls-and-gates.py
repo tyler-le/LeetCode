0a3b5e61bbde265b1083fef03b6705f2f042163a
Tyler Le <tylerle99@yahoo.com>
2022-08-04 21:15:52 -0700

    Time: 404 ms (57.19%), Space: 17.1 MB (56.44%) - LeetHub

---
+++ 286-walls-and-gates/286-walls-and-gates.py
@@ -0,0 +1,26 @@
+class Solution:
+    def wallsAndGates(self, rooms: List[List[int]]) -> None:
+        """
+        Do not return anything, modify rooms in-place instead.
+        """
+        if not rooms:
+            return
+        
+        ROWS = len(rooms)
+        COLS = len(rooms[0])
+        GATE, WALL, OBSTACLE = 0, -1, 2147483647
+        q = deque()
+        for i in range(ROWS):
+            for j in range(COLS):
+                if rooms[i][j] == GATE:
+                    q.append((i,j))
+        
+        while q:
+            row, col = q.popleft()
+            dist = rooms[row][col] + 1
+            for dy, dx in (-1, 0), (1, 0), (0, -1), (0, 1):
+                r = row + dy
+                c = col + dx
+                if 0 <= r < ROWS and 0 <= c < COLS and rooms[r][c] == OBSTACLE:
+                    rooms[r][c] = dist
+                    q.append((r,c))
