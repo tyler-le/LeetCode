3d5a9376a714a8d8d6a51139bd1d28fff406a068
Tyler Le <tylerle99@yahoo.com>
2022-08-05 15:09:39 -0700

    Time: 688 ms (90.25%), Space: 17.3 MB (42.94%) - LeetHub

--- 542-01-matrix/542-01-matrix.py
+++ 542-01-matrix/542-01-matrix.py
@@ -1,28 +1,25 @@
 class Solution:
     def updateMatrix(self, mat: List[List[int]]) -> List[List[int]]:
         q, visited = deque(), set()
+        ROWS, COLS = len(mat), len(mat[0])
         
-        for i in range(len(mat)):
-            for j in range(len(mat[0])):
-                if mat[i][j] == 0: 
+        for i in range(ROWS):
+            for j in range(COLS):
+                if mat[i][j] == 0:
                     q.append((i,j))
                     visited.add((i,j))
-                
         while q:
-            i, j = q.popleft()
-            for x, y in [(0, 1), (0, -1), (1, 0), (-1, 0)]:
-                new_x, new_y = i + x, j + y
-
-                if new_x < 0 or new_y < 0 \
-                    or new_x == len(mat) or new_y == len(mat[0]) \
-                    or (new_x, new_y) in visited:
+            x, y = q.popleft()
+            
+            for dx, dy in (-1, 0), (1,0), (0,1), (0,-1):
+                r = x + dx
+                c = y + dy
+                
+                if r < 0 or c < 0 or r == ROWS or c == COLS or (r,c) in visited:
                     continue
-
-                mat[new_x][new_y] = 1 + mat[i][j]
-                q.append((new_x, new_y))
-                visited.add((new_x, new_y))
+                
+                mat[r][c] = 1 + mat[x][y]
+                q.append((r,c))
+                visited.add((r,c))
+        
         return mat
-                    
-                    
-                    
-                        
