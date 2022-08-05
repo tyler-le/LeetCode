ea77a83fd2fdba737bbe6871a6c7003b4af22dcd
Tyler Le <tylerle99@yahoo.com>
2022-08-04 20:34:00 -0700

    Time: 6156 ms (6.26%), Space: 64.6 MB (6.94%) - LeetHub

--- 567-permutation-in-string/567-permutation-in-string.py
+++ 567-permutation-in-string/567-permutation-in-string.py
@@ -1,19 +1,14 @@
 class Solution:
     def checkInclusion(self, s1: str, s2: str) -> bool:
+        s1_count, s2_count, l, substrs = Counter(s1), {}, 0, []
         
-        perm = Counter(s1)
-        count = {}
-        
-        l = 0
         for r in range(len(s2)):
-            count[s2[r]] = 1 + count.get(s2[r], 0)
-            
-            if r-l+1 > len(s1): 
-                count[s2[l]] -= 1
-                if count[s2[l]] == 0: del count[s2[l]]
+            s2_count[s2[r]] = 1 + s2_count.get(s2[r], 0)
+            while r-l+1 > len(s1):
+                s2_count[s2[l]]-=1
                 l+=1
-                    
-            if dict(perm) == count: return True
-            
+            substrs.append(s2[l:r+1])
+        
+        for substr in substrs:
+            if s1_count == Counter(substr): return True
         return False
-                
