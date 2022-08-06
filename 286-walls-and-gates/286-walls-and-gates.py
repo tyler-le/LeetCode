54a170a210750a304df8ecba700326928b9f61d7
Tyler Le <tylerle99@yahoo.com>
2022-08-05 18:21:18 -0700

    Time: 271 ms (81.22%), Space: 21 MB (28.28%) - LeetHub

--- 286-walls-and-gates/286-walls-and-gates.py
+++ 286-walls-and-gates/286-walls-and-gates.py
@@ -1,23 +1,28 @@
-class Solution:
-    def wallsAndGates(self, rooms: List[List[int]]) -> None:
+class Solution(object):
+    def wallsAndGates(self, rooms):
         """
-        Do not return anything, modify rooms in-place instead.
+        :type rooms: List[List[int]]
+        :rtype: None Do not return anything, modify rooms in-place instead.
         """
-        if not rooms: return
+        q, visited = deque(), set()
+        ROWS, COLS = len(rooms), len(rooms[0])
+        WALL, GATE, EMPTY = -1, 0, 2147483647
         
-        q, ROWS, COLS, GATE, OBSTACLE, EMPTY = deque(), len(rooms), len(rooms[0]), 0, -1, 2147483647
-
         for i in range(ROWS):
             for j in range(COLS):
                 if rooms[i][j] == GATE:
                     q.append((i,j))
-        
+                    visited.add((i,j))
+                    
         while q:
-            row, col = q.popleft()
-            dist = rooms[row][col] + 1
-            for dy, dx in (-1, 0), (1, 0), (0, -1), (0, 1):
-                r = row + dy
-                c = col + dx
-                if 0 <= r < ROWS and 0 <= c < COLS and rooms[r][c] == EMPTY:
-                    rooms[r][c] = dist
-                    q.append((r,c))
+            x,y = q.popleft()
+            dist = rooms[x][y] + 1
+            for dx, dy in (1,0), (-1,0), (0,1), (0,-1):
+                r, c = x+dx, y+dy
+                
+                if r < 0 or c < 0 or r == ROWS or c == COLS or rooms[r][c] != EMPTY or (r,c) in visited:
+                    continue
+                
+                rooms[r][c] = dist
+                q.append((r,c))
+                visited.add((r,c))
