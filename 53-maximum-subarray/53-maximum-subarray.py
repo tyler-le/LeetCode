584ecc456a8c5d0ad75a4f2e36364d25b0b4a1cc
Tyler Le <tylerle99@yahoo.com>
2022-08-06 21:04:16 -0700

    Time: 662 ms (78.33%), Space: 25.4 MB (89.85%) - LeetHub

--- 53-maximum-subarray/53-maximum-subarray.py
+++ 53-maximum-subarray/53-maximum-subarray.py
@@ -8,9 +8,7 @@ class Solution(object):
         
         for num in nums[1:]:
             curr_sum+=num
-            
-            if curr_sum < num:
-                curr_sum = num
+            if curr_sum < num: curr_sum = num
             max_sum = max(max_sum, curr_sum)
                 
         return max_sum
