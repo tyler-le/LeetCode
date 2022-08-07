3c9581a2a5733ff3ba82a7cf2403a199e00ff3a8
Tyler Le <tylerle99@yahoo.com>
2022-08-06 20:34:51 -0700

    Time: 890 ms (86.49%), Space: 22.7 MB (54.54%) - LeetHub

--- 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.py
+++ 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.py
@@ -4,9 +4,10 @@ class Solution(object):
         :type prices: List[int]
         :rtype: int
         """
-        buy_day = 0
-        max_profit = 0
-        curr_profit = 0
+        
+        # KEEP THIS SOLUTION
+    
+        buy_day = max_profit = 0
         for sell_day in range(len(prices)):
             if prices[sell_day] - prices[buy_day] < 0:
                 buy_day = sell_day
