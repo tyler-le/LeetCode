1e5998d9ec1863210f22d12bad7e97fb35fe69b7
Tyler Le <tylerle99@yahoo.com>
2022-08-07 15:30:15 -0700

    Time: 445 ms (91.38%), Space: 40.2 MB (23.73%) - LeetHub

--- 148-sort-list/148-sort-list.py
+++ 148-sort-list/148-sort-list.py
@@ -8,6 +8,7 @@ class Solution:
     def sortList(self, head: Optional[ListNode]) -> Optional[ListNode]:
         
         if not head: return None
+        
         # store (key, Node) pairs in array, sort, reconnect
         container = []
         
@@ -18,11 +19,11 @@ class Solution:
             
         container.sort(key = lambda i : i[0])
         
-        prev = None
-        head = container[0][1]
+        prev, head = None, container[0][1]
         for val, node in container:
             if prev: prev.next = node
             prev = node
+            
         prev.next = None
         return head
                 
