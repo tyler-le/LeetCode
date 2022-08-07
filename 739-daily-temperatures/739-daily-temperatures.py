5b3963472d0372da48bd78ff8854430a969903d2
Tyler Le <tylerle99@yahoo.com>
2022-08-06 21:58:30 -0700

    Time: 1363 ms (63.97%), Space: 20.7 MB (77.79%) - LeetHub

--- 739-daily-temperatures/739-daily-temperatures.py
+++ 739-daily-temperatures/739-daily-temperatures.py
@@ -4,13 +4,13 @@ class Solution(object):
         :type temperatures: List[int]
         :rtype: List[int]
         """
-        res, stack, TEMP = [0 for _ in temperatures], [], 0
+        res, stack, TEMP = [0]*len(temperatures), [], 0
        
         for index, temp in enumerate(temperatures):
-            
             while stack and temp > stack[-1][TEMP]:
                 top_temp, top_index = stack.pop()
                 res[top_index] = index - top_index
+                
             stack.append((temp, index))
             
         return res
