90cd29ce4a1b92bfb84edb2d26823bbe191f6c64
Tyler Le <tylerle99@yahoo.com>
2022-08-07 15:29:50 -0700

    Time: 352 ms (96.22%), Space: 40 MB (23.86%) - LeetHub

---
+++ 148-sort-list/148-sort-list.py
@@ -0,0 +1,30 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+
+class Solution:
+    def sortList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        
+        if not head: return None
+        # store (key, Node) pairs in array, sort, reconnect
+        container = []
+        
+        curr = head
+        while curr:
+            container.append([curr.val, curr])
+            curr = curr.next
+            
+        container.sort(key = lambda i : i[0])
+        
+        prev = None
+        head = container[0][1]
+        for val, node in container:
+            if prev: prev.next = node
+            prev = node
+        prev.next = None
+        return head
+                
+            
+            
