c20bb7622608dd78516456f6d636c670739848e4
Tyler Le <tylerle99@yahoo.com>
2022-08-08 22:44:20 -0700

    Time: 71 ms (59.04%), Space: 16.6 MB (17.17%) - LeetHub

--- 116-populating-next-right-pointers-in-each-node/116-populating-next-right-pointers-in-each-node.py
+++ 116-populating-next-right-pointers-in-each-node/116-populating-next-right-pointers-in-each-node.py
@@ -1,35 +1,32 @@
 """
 # Definition for a Node.
-class Node:
-    def __init__(self, val: int = 0, left: 'Node' = None, right: 'Node' = None, next: 'Node' = None):
+class Node(object):
+    def __init__(self, val=0, left=None, right=None, next=None):
         self.val = val
         self.left = left
         self.right = right
         self.next = next
 """
 
-class Solution:
-    def connect(self, root: 'Optional[Node]') -> 'Optional[Node]':
+class Solution(object):
+    def connect(self, root):
+        """
+        :type root: Node
+        :rtype: Node
+        """
+        if not root: return None
         q = deque()
-        if not root:
-            return
-        
         q.append(root)
         
         while q:
             level_size = len(q)
             for i in range(level_size):
-                removed = q.popleft()
+                popped = q.popleft()
+                popped.next = None if (i == level_size-1) else q[0]
+                if popped.left: q.append(popped.left)
+                if popped.right: q.append(popped.right)
                 
-                if removed.left:
-                    q.append(removed.left)
-                    
-                if removed.right:
-                    q.append(removed.right)
-                    
-                if i == level_size-1:
-                    removed.next = None
-                else:
-                    removed.next = q[0]
+                
+                
         return root
-        
+                
