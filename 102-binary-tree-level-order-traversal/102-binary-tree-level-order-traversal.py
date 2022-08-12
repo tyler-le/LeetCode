0e34d971857d98b215c3c9785037516efed82b7b
Tyler Le <tylerle99@yahoo.com>
2022-08-11 20:12:50 -0700

    Time: 44 ms (75.95%), Space: 14.2 MB (52.87%) - LeetHub

--- 102-binary-tree-level-order-traversal/102-binary-tree-level-order-traversal.py
+++ 102-binary-tree-level-order-traversal/102-binary-tree-level-order-traversal.py
@@ -1,27 +1,33 @@
 # Definition for a binary tree node.
-# class TreeNode(object):
+# class TreeNode:
 #     def __init__(self, val=0, left=None, right=None):
 #         self.val = val
 #         self.left = left
 #         self.right = right
-class Solution(object):
-    def levelOrder(self, root):
-        """
-        :type root: TreeNode
-        :rtype: List[List[int]]
-        """
-        if not root: return []
-        q = deque()
+class Solution:
+    def levelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
+        if not root: 
+            return []
+        
+        queue = deque()
+        queue.appendleft(root)
         res = []
-        q.append(root)
-        while q:
-            level_size = len(q)
-            level = []
-            for _ in range(level_size):
-                popped = q.popleft()
-                if popped.left: q.append(popped.left)
-                if popped.right: q.append(popped.right)
-                level.append(popped.val)
-            res.append(level)
+        
+        while queue:
+            level_size = len(queue)
+            curr_level = []
             
+            for i in range(level_size):
+                removed = queue.pop()
+                curr_level.append(removed.val)
+                if removed.left:
+                    queue.appendleft(removed.left)
+                if removed.right:
+                    queue.appendleft(removed.right)
+                
+            res.append(curr_level)
+        
         return res
+            
+        
+        
