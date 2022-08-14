617d0af1ee40176157821048eed5092e5d9f2f76
Tyler Le <tylerle99@yahoo.com>
2022-08-14 09:46:08 -0700

    Time: 2017 ms (31.51%), Space: 18 MB (72.41%) - LeetHub

--- 15-3sum/15-3sum.py
+++ 15-3sum/15-3sum.py
@@ -5,23 +5,24 @@ class Solution:
         
         for i in range(len(nums)):
             if i > 0 and nums[i] == nums[i-1]: continue
-            target = 0-nums[i]
-            j, k = i+1, len(nums)-1
+            target, j, k = 0-nums[i], i+1, len(nums)-1
+
             while j < len(nums) and k >= 0 and j < k:
                 if nums[j] + nums[k] == target:
                     res.append([nums[i], nums[j], nums[k]])
-                    print(i,j,k)
+
                     while j+1 < len(nums) and nums[j+1] == nums[j]: 
                         j+=1
+                    
                     while k-1 >= 0 and nums[k-1] == nums[k]: 
                         k-=1
+                    
                     j+=1
                     k-=1
+                    
                 elif nums[j] + nums[k] < target: j+=1
                 else: k-=1
 
-                
-            
         return res
                             
                             
