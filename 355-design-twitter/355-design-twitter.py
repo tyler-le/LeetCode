5dfe7da396674dab5667e07fac74222c0457da9e
Tyler Le <tylerle99@yahoo.com>
2022-08-17 19:34:53 -0700

    Time: 64 ms (12.15%), Space: 14.1 MB (19.93%) - LeetHub

---
+++ 355-design-twitter/355-design-twitter.py
@@ -0,0 +1,49 @@
+class Tweet:
+    def __init__(self, user_id, tweet_id):
+        self.user_id = user_id
+        self.tweet_id = tweet_id
+
+class User:
+    def __init__(self):
+        self.following = set()
+        
+
+class Twitter:
+
+    def __init__(self):
+        self.user_map = defaultdict(User)
+        self.all_tweets = []
+
+    def postTweet(self, userId: int, tweetId: int) -> None:
+        tweet = Tweet(userId, tweetId)
+        self.all_tweets.append(tweet)
+            
+
+    def getNewsFeed(self, userId: int) -> List[int]:
+        if userId not in self.user_map[userId].following:
+            self.user_map[userId].following.add(userId)
+        count, res = 0, []
+        for i in range(len(self.all_tweets)-1, -1, -1):
+            if count == 10: break
+            if self.all_tweets[i].user_id in self.user_map[userId].following:
+                res.append(self.all_tweets[i].tweet_id)
+                count+=1
+        return res
+            
+
+    def follow(self, followerId: int, followeeId: int) -> None:
+        if followerId not in self.user_map[followerId].following:
+            self.user_map[followerId].following.add(followerId)
+        self.user_map[followerId].following.add(followeeId)
+
+    def unfollow(self, followerId: int, followeeId: int) -> None:
+        if followerId in self.user_map: 
+            self.user_map[followerId].following.remove(followeeId)
+
+
+# Your Twitter object will be instantiated and called as such:
+# obj = Twitter()
+# obj.postTweet(userId,tweetId)
+# param_2 = obj.getNewsFeed(userId)
+# obj.follow(followerId,followeeId)
+# obj.unfollow(followerId,followeeId)
