50cbadfab3cdf184e7057cb44c1f9cb36832465f
Tyler Le <tylerle99@yahoo.com>
2022-08-20 09:14:00 -0700

    Time: 112 ms (37.08%), Space: 17.8 MB (47.95%) - LeetHub

--- 243-shortest-word-distance/243-shortest-word-distance.py
+++ 243-shortest-word-distance/243-shortest-word-distance.py
@@ -1,19 +1,22 @@
 class Solution:
     def shortestDistance(self, wordsDict: List[str], word1: str, word2: str) -> int:
         res = float('inf')
-        
         hmap = collections.defaultdict(list)
         
+        # map word : list of indices (naturally sorted)
         for i, word in enumerate(wordsDict):
-            hmap[word].append(i)
+            if word == word1 or word == word2:
+                hmap[word].append(i)
             
-        l1, l2 = hmap[word1], hmap[word2]
+        # get the lists of indices corresponding to 
+        word1_positions, word2_positions = hmap[word1], hmap[word2]
         
         l = r = 0
         
-        while l < len(l1) and r < len(l2):
-            res = min(res, abs(l1[l] - l2[r]))
-            if l1[l] < l2[r]: l+=1
+        # find min distance between two sorted lists
+        while l < len(word1_positions) and r < len(word2_positions):
+            res = min(res, abs(word1_positions[l] - word2_positions[r]))
+            if word1_positions[l] < word2_positions[r]: l+=1
             else: r+=1
         
         return res
