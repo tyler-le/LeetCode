c7c5eb88428cf2b1127cf8ca15bd4330a65d6030
Tyler Le <tylerle99@yahoo.com>
2022-08-20 09:14:47 -0700

    Time: 100 ms (51.50%), Space: 17.8 MB (47.95%) - LeetHub

--- 243-shortest-word-distance/243-shortest-word-distance.py
+++ 243-shortest-word-distance/243-shortest-word-distance.py
@@ -12,8 +12,8 @@ class Solution:
         word1_positions, word2_positions = hmap[word1], hmap[word2]
         
         l = r = 0
-        
-        # find min distance between two sorted lists
+    
+        # find min distance between two sorted lists using two pointers
         while l < len(word1_positions) and r < len(word2_positions):
             res = min(res, abs(word1_positions[l] - word2_positions[r]))
             if word1_positions[l] < word2_positions[r]: l+=1
