89162e703939202642109e4f69fb3e397627abb4
Tyler Le <tylerle99@yahoo.com>
2022-08-20 11:45:44 -0700

    Time: 2008 ms (10.91%), Space: 14.9 MB (21.92%) - LeetHub

--- 729-my-calendar-i/729-my-calendar-i.py
+++ 729-my-calendar-i/729-my-calendar-i.py
@@ -3,24 +3,10 @@ class MyCalendar:
     def __init__(self):
         self.intervals = []
         
-    def merge(self, intervals):
-     
-        intervals.sort(key = lambda i : i[0])
-        res = [intervals[0]]
-        start,end = 0,1
-        for interval in intervals:
-            has_overlap = interval[start] <= res[-1][end] and interval[start] >= res[-1][start]
-            
-            if has_overlap:
-                res[-1] = [min(interval[start], res[-1][start]), max(interval[end], res[-1][end])]
-            else:
-                res.append(interval)
-                
-        return res
-
+    
     def book(self, start: int, end: int) -> bool:
-        #self.intervals.sort(key = lambda i : i[0])
-        if self.intervals: self.merge(self.intervals)
+        self.intervals.sort(key = lambda i : i[0])
+
         flag = True
         
         for s, e in self.intervals:
@@ -31,6 +17,7 @@ class MyCalendar:
         if flag: 
             self.intervals.append([start, end])
             return True
+        
         return False
 
 # Your MyCalendar object will be instantiated and called as such:
