af9a4aebb1998ebcce9fc5bdf796826a430beb57
Tyler Le <tylerle99@yahoo.com>
2022-08-21 12:50:34 -0700

    Time: 357 ms (62.11%), Space: 23.8 MB (22.11%) - LeetHub

---
+++ 1166-design-file-system/1166-design-file-system.py
@@ -0,0 +1,56 @@
+class MultiWayTrieNode():
+    def __init__(self, directory):
+        self.children = {}
+        self.value = -1
+        self.dir = directory
+        
+        
+class FileSystem(object):
+
+    def __init__(self):
+        self.root = MultiWayTrieNode("root")
+
+    def createPath(self, path, value):
+        """
+        :type path: str
+        :type value: int
+        :rtype: bool
+        """
+        directories = path.split('/')
+        curr = self.root
+        path_exists = True
+        
+        for i in range(1, len(directories)):
+            name = directories[i]
+            
+            if name not in curr.children: 
+                if i == len(directories)-1:
+                    curr.children[name] = MultiWayTrieNode(name)
+                else:
+                    return False
+            curr = curr.children[name]
+        if curr.value != -1: return False
+        curr.value = value
+        return True
+        
+        
+
+    def get(self, path):
+        """
+        :type path: str
+        :rtype: int
+        """
+        directories = path.split('/')
+        curr = self.root
+        
+        for i in range(1, len(directories)):
+            name = directories[i]
+            if name not in curr.children: 
+                return -1
+            curr = curr.children[name]
+        return curr.value
+
+# Your FileSystem object will be instantiated and called as such:
+# obj = FileSystem()
+# param_1 = obj.createPath(path,value)
+# param_2 = obj.get(path)
