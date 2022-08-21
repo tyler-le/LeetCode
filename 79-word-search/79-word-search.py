03fbf6a323e01a5336c55c987dbb70b55196bc2a
Tyler Le <tylerle99@yahoo.com>
2022-08-20 19:23:14 -0700

    Time: 6930 ms (59.21%), Space: 13.6 MB (23.34%) - LeetHub

--- 79-word-search/79-word-search.py
+++ 79-word-search/79-word-search.py
@@ -1,24 +1,33 @@
-class Solution:
-    def exist(self, board: List[List[str]], word: str) -> bool:
+class Solution(object):
+    def exist(self, board, word):
+        """
+        :type board: List[List[str]]
+        :type word: str
+        :rtype: bool
+        """
+        ROWS, COLS = len(board), len(board[0])
+        visited = set()
         
-        def backtrack(r,c,i):
-            if i == len(word):
-                return True
-            if r < 0 or r >= rows or c < 0 or c >= cols or (r,c) in path or word[i] != board[r][c]:
-                return False
+        def backtrack(r, c, index):
+            if index == len(word): return True
+            if r < 0 or c < 0 or r == ROWS or c == COLS: return False
+            if (r,c) in visited: return False
+            if board[r][c] != word[index]: return False
             
-            path.add((r,c))
-            res =  backtrack(r-1,c,i+1) or backtrack(r,c+1,i+1) or backtrack(r+1,c,i+1) or backtrack(r,c-1,i+1)
-            path.remove((r,c))
+            
+            visited.add((r,c))
+            
+            res = backtrack(r+1,c,index+1) \
+                or backtrack(r-1,c,index+1) \
+                or backtrack(r,c+1,index+1) \
+                or backtrack(r,c-1,index+1)
+            
+            visited.remove((r,c))
             return res
         
         
-        rows, cols, path = len(board), len(board[0]), set()
-        for row in range(rows):
-            for col in range(cols):
-                if backtrack(row,col,0):
+        for i in range(ROWS):
+            for j in range(COLS):
+                if backtrack(i, j, 0): 
                     return True
         return False
-                
-            
-        
