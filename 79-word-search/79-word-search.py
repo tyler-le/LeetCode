ecb54c2ea1c0d3e4f2b2c30cccdd7bf3ad75e9d8
Tyler Le <tylerle99@yahoo.com>
2022-08-20 20:06:48 -0700

    Time: 8931 ms (17.35%), Space: 13.3 MB (91.77%) - LeetHub

--- 79-word-search/79-word-search.py
+++ 79-word-search/79-word-search.py
@@ -9,25 +9,36 @@ class Solution(object):
         visited = set()
         
         def backtrack(r, c, index):
+            # we found the word
             if index == len(word): return True
+            
+            # out of bounds
             if r < 0 or c < 0 or r == ROWS or c == COLS: return False
+            
+            # cannot revisit
             if (r,c) in visited: return False
+            
+            # the current position in the board is not the correct char
             if board[r][c] != word[index]: return False
             
-            
+            # mark current cell as visited
             visited.add((r,c))
             
+            # visit all neighbors
             res = backtrack(r+1,c,index+1) \
                 or backtrack(r-1,c,index+1) \
                 or backtrack(r,c+1,index+1) \
                 or backtrack(r,c-1,index+1)
             
+            # unvisit neighbor
             visited.remove((r,c))
+            
+            # if we ever returned true, then we found the word
             return res
         
         
         for i in range(ROWS):
             for j in range(COLS):
-                if backtrack(i, j, 0): 
+                if board[i][j] == word[0] and backtrack(i, j, 0): 
                     return True
         return False
