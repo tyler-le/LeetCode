26990b995023984406ec64b5f34d82ef5bd1fc5d
Tyler Le <tylerle99@yahoo.com>
2022-08-31 14:29:17 -0700

    Time: 31 ms (39.10%), Space: 13.2 MB (98.09%) - LeetHub

--- 198-house-robber/198-house-robber.py
+++ 198-house-robber/198-house-robber.py
@@ -6,11 +6,16 @@ class Solution(object):
         """
         if not nums: return 0
         if len(nums) == 1: return nums[0]
+        
         memo = [-1 for _ in range(len(nums))]
         memo[0], memo[1] = nums[0], max(nums[0], nums[1])
     
         for i in range(2, len(nums)):
-            memo[i] = max(memo[i-1], memo[i-2] + nums[i])
+            one_house_before = memo[i-1]
+            two_houses_before = memo[i-2]
+            current_house = nums[i]
+            # curr house is the max(loot of prev house, loot of two houses down and rob current house )
+            memo[i] = max(one_house_before, two_houses_before + current_house)
             
         return memo[-1]
             
