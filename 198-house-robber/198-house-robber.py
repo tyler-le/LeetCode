d0e45c6712afe39df617f05a1c669dc2e9002434
Tyler Le <tylerle99@yahoo.com>
2022-08-31 13:58:40 -0700

    Time: 25 ms (63.95%), Space: 13.3 MB (68.83%) - LeetHub

--- 198-house-robber/198-house-robber.py
+++ 198-house-robber/198-house-robber.py
@@ -8,8 +8,8 @@ class Solution(object):
         memo[0], memo[1] = 0, nums[0]
         
         for i in range(1, len(nums)):
-            current_house = nums[i]
-            memo[i+1] = max(memo[i], memo[i-1] + current_house);
-
+            two_before = nums[i]
+            memo[i+1] = max(memo[i], memo[i-1] + two_before);
+        print(memo)
         return memo[-1]
             
