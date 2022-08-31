b3949075baf0d20b50c8f1cd0865b38c07769b3c
Tyler Le <tylerle99@yahoo.com>
2022-08-31 14:18:51 -0700

    Time: 28 ms (97.94%), Space: 13.9 MB (65.50%) - LeetHub

--- 198-house-robber/198-house-robber.py
+++ 198-house-robber/198-house-robber.py
@@ -3,11 +3,11 @@ class Solution:
         if not nums: return 0
         one_house_down, two_houses_down = 0, 0
         
-        for num in nums:
+        for curr_house in nums:
             temp = one_house_down
             
             # calculate new previous house for next iteration
-            one_house_down = max(two_houses_down + num, one_house_down)
+            one_house_down = max(two_houses_down + curr_house, one_house_down)
             
             # set new two_houses_down for next iteration
             two_houses_down = temp
