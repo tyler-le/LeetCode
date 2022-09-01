21577e904a3191322300494cf44f7b93f14a682e
Tyler Le <tylerle99@yahoo.com>
2022-08-31 20:01:45 -0700

    Time: 34 ms (26.81%), Space: 13.4 MB (41.56%) - LeetHub

---
+++ 213-house-robber-ii/213-house-robber-ii.py
@@ -0,0 +1,31 @@
+class Solution(object):
+    def rob_linear(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        if not nums: return 0
+        if len(nums) == 1: return nums[0]
+        
+        memo = [-1 for _ in range(len(nums))]
+        memo[0], memo[1] = nums[0], max(nums[0], nums[1])
+    
+        for i in range(2, len(nums)):
+            one_house_before = memo[i-1]
+            two_houses_before = memo[i-2]
+            current_house = nums[i]
+        
+            memo[i] = max(one_house_before, two_houses_before + current_house)
+            
+        return memo[-1]
+    
+    
+    def rob(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        if len(nums) == 0: return 0
+        if len(nums) == 1: return nums[0]
+        return max(self.rob_linear(nums[0:len(nums)-1]), self.rob_linear(nums[1:]))
+        
