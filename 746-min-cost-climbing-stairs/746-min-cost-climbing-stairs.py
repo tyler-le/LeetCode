5da7db4158ed14360956eb3712bd2a3a740b958f
Tyler Le <tylerle99@yahoo.com>
2022-09-01 20:59:43 -0700

    Time: 130 ms (8.13%), Space: 14.1 MB (40.86%) - LeetHub

--- 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.py
+++ 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.py
@@ -1,20 +1,13 @@
-class Solution(object):
-    def minCostClimbingStairs(self, cost):
-        """
-        :type cost: List[int]
-        :rtype: int
-        """
-        def min_cost(i):
-            if i < 0: return 0
-            if i == 0 or i == 1: return cost[i]
-            if memo[i] != -1: return memo[i]
-            
-            res = cost[i] + min(min_cost(i-1), min_cost(i-2))
-            memo[i] = res
-            return res
+class Solution:
+    def minCostClimbingStairs(self, cost: List[int]) -> int:
         
-        memo = [-1 for _ in range (len(cost))]
         n = len(cost)
-        return min(min_cost(n-1), min_cost(n-2))
+        dp = [-1 for _ in range(n)]
+        
+        dp[0], dp[1] = cost[0], cost[1]
+        
+        for i in range(2, n):
+            dp[i] = cost[i] + min(dp[i-1], dp[i-2])
+        return min(dp[n-1], dp[n-2])
     
             
