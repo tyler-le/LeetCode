543133187147678da86d33c85f3032559f032a28
Tyler Le <tylerle99@yahoo.com>
2022-09-02 21:13:54 -0700

    Time: 169 ms (42.98%), Space: 17.1 MB (26.07%) - LeetHub

--- 207-course-schedule/207-course-schedule.py
+++ 207-course-schedule/207-course-schedule.py
@@ -1,24 +1,31 @@
 class Solution:
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
-        prereq_map = {i:[] for i in range(numCourses)}
-        for course, prereq in prerequisites:
-            prereq_map[course].append(prereq)
+        def dfs(node):
+            if edge_map[node] == []: return True
+            if node in visited: return False
             
-        visited = set()
-        def dfs(course):
-            if course in visited: return False
-            if prereq_map[course] == []: return True
+            visited.add(node)
             
-            visited.add(course)
-            for prereq in prereq_map[course]:
-                if not dfs(prereq): return False
-                
-            visited.remove(course)
-            prereq_map[course] = []
+            for neighbor in edge_map[node]:
+                if not dfs(neighbor): return False
+            
+            visited.remove(node)
+            edge_map[node] = []
             return True
         
-        for i in range(numCourses):
+        
+        edge_map, visited = collections.defaultdict(list), set()
+        
+        # create adjacency list
+        for course, prereq in prerequisites:
+            edge_map[course].append(prereq)
+            
+        # run dfs on each node
+        for i in range(0, numCourses):
             if not dfs(i): return False
-            
         return True
+        
+        
             
+        
+        
