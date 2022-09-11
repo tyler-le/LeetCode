cc97b219fd8743a322ee319b228bbc0d6008f336
Tyler Le <tylerle99@yahoo.com>
2022-09-11 13:32:20 -0700

    Time: 19 ms (97.37%), Space: 13.7 MB (23.42%) - LeetHub

--- 90-subsets-ii/90-subsets-ii.py
+++ 90-subsets-ii/90-subsets-ii.py
@@ -4,34 +4,28 @@ class Solution(object):
         :type nums: List[int]
         :rtype: List[List[int]]
         """
-     
         
         # BACKTRACKING SOLUTION
         
-        
-        
-        
         def backtrack(i):
-            # at each step, we choose to include nums[i] or exclude nums[i]
-            
-            # base case
             if i >= len(nums):
-                res.append(subset*1)
-                return
-                
+                # make a copy because subset is a reference 
+                return res.append(subset*1) 
+            
             # include nums[i]
             subset.append(nums[i])
             backtrack(i+1)
-            
-            # skip dups
-            while i+1 < len(nums) and nums[i] == nums[i+1]:
+                
+            # skip duplicates
+            while i + 1 < len(nums) and nums[i] == nums[i+1]:
                 i+=1
             
             # exclude nums[i]
             subset.pop()
             backtrack(i+1)
-        
+
         nums.sort()
         subset, res = [], []
         backtrack(0)
         return res
+        
