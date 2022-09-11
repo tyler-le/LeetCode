944c690103c26d12f142cb6bc37f9caa1d2fb3b8
Tyler Le <tylerle99@yahoo.com>
2022-09-11 12:23:30 -0700

    Time: 1544 ms (5.05%), Space: 16.7 MB (21.31%) - LeetHub

--- 76-minimum-window-substring/76-minimum-window-substring.py
+++ 76-minimum-window-substring/76-minimum-window-substring.py
@@ -1,35 +1,42 @@
-class Solution:
-    def minWindow(self, s: str, t: str) -> str:
-        l = r = 0
-        res, min_len = [], float('inf')
-        t_map, s_map = Counter(t), {}
+class Solution(object):
+    def minWindow(self, s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: str
+        """
         
-        
-        for r in range(len(s)):
-            s_map[s[r]] = 1 + s_map.get(s[r], 0)
-            
-            is_valid = True
-            
-            # check if valid
+        '''
+        Checks if all chars in t_map are in s_map
+        '''
+        def is_valid(s_map, t_map):
+            # checks if t_map is a subset of s_map
+            flag = True
             for key, value in t_map.items():
-                if key not in s_map or s_map[key] < value:
-                    is_valid = False
-                
-            # shrink window until no longer valid and record min
-            while is_valid:
-                if r-l+1 < min_len:
-                    min_len = r-l+1
-                    res=[l,r]
+                if key not in s_map or s_map[key] < value: 
+                    flag = False
+            return flag
+            
+            
+        
+        min_window_len = float('inf')
+        min_window_pos = [-1, -1]
+        s_map, t_map, l = collections.defaultdict(int), Counter(t), 0
+
+        for r in range(len(s)):
+            # extend the window
+            s_map[s[r]]+=1
+            
+            while is_valid(s_map, t_map):
+                # update min
+                if r-l+1 < min_window_len:
+                    min_window_len = r-l+1
+                    min_window_pos = [l, r]
                     
+                # shrink window
                 s_map[s[l]]-=1
-                
-                if s_map[s[l]] == 0:
-                    del s_map[s[l]]
-                    
-                for key, value in t_map.items():
-                    if key not in s_map or s_map[key] < value:
-                        is_valid = False
-                
+                if s_map[s[l]] == 0: del s_map[s[l]]
                 l+=1
-                
-        return "" if len(res) == 0 else s[res[0]:res[1]+1]
+            
+        
+        return "" if min_window_pos == [-1, -1] else s[min_window_pos[0]: min_window_pos[1] + 1]
