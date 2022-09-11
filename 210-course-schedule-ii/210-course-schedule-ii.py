4ffa96e5b4fd93a72eec5911b038ba05222a964f
Tyler Le <tylerle99@yahoo.com>
2022-09-10 22:19:57 -0700

    Time: 252 ms (11.27%), Space: 15.5 MB (89.08%) - LeetHub

--- 210-course-schedule-ii/210-course-schedule-ii.py
+++ 210-course-schedule-ii/210-course-schedule-ii.py
@@ -8,6 +8,7 @@ class Solution:
         hmap, indegree, q, res = collections.defaultdict(list), {}, deque(), []
         
         # build directed adjacency list
+        # u -> v where u is a prereq of v
         for dest, src in prerequisites:
             hmap[src].append(dest)
             indegree[dest] = 1 + indegree.get(dest, 0)
@@ -17,18 +18,19 @@ class Solution:
             if i not in indegree: q.append(i)
         
         while q:
-            # visit all neighbors in each node of q 
+            # visit all neighbors of current node
             curr = q.popleft()
             
-            # add curr node to res
+            # add curr node to res since it has in-degree zero (no prereqs)
             res.append(curr)
             
-            # decrement indegree. if zero, add to q
+            # decrement indegree of each neighbor since we cherry-picked curr out. 
+            # if it ends up at zero, append to q
             for nbor in hmap[curr]:
                 indegree[nbor]-=1
-                if indegree[nbor] == 0: q.append(nbor)
+                if indegree[nbor] == 0: 
+                    q.append(nbor)
 
-            
         return res if len(res) == numCourses else []
         
         
