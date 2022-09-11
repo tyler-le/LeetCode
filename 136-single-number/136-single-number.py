124a3280ec021270d71b22f32d4425a9ec303e45
Tyler Le <tylerle99@yahoo.com>
2022-09-11 02:13:15 -0700

    Time: 206 ms (40.71%), Space: 15.5 MB (79.91%) - LeetHub

--- 136-single-number/136-single-number.py
+++ 136-single-number/136-single-number.py
@@ -1,10 +1,10 @@
-class Solution:
-    def singleNumber(self, nums: List[int]) -> int:
-        # Using XOR to find pairs
-        xor = nums[0]
-        
+class Solution(object):
+    def singleNumber(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        res = nums[0]
         for num in nums[1:]:
-            xor ^= num
-            
-        return xor
-        
+            res ^= num
+        return res
