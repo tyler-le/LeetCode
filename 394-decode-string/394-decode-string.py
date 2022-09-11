ddc0b61ce972c34cab0ba71673b0173ddd236d73
Tyler Le <tylerle99@yahoo.com>
2022-09-10 19:38:25 -0700

    Time: 70 ms (5.35%), Space: 14 MB (20.25%) - LeetHub

--- 394-decode-string/394-decode-string.py
+++ 394-decode-string/394-decode-string.py
@@ -7,6 +7,7 @@ class Solution:
             if ch != ']': 
                 stack.append(ch)
                 continue
+                
             while stack[-1] != '[': 
                 decoded += stack.pop()
 
@@ -16,7 +17,6 @@ class Solution:
             base = 1
             num_repeats = 0
             while stack and stack[-1].isdigit():
-                # print(stack.pop())
                 num_repeats = num_repeats + (int(stack.pop()) - int('0')) * base
                 base*=10
 
