00401a46fa634774bf2c956bc50b40f6caa5e910
Tyler Le <tylerle99@yahoo.com>
2022-09-10 22:36:05 -0700

    Time: 1300 ms (5.01%), Space: 14.8 MB (36.80%) - LeetHub

--- 76-minimum-window-substring/76-minimum-window-substring.py
+++ 76-minimum-window-substring/76-minimum-window-substring.py
@@ -1,18 +1,21 @@
 class Solution:
     def minWindow(self, s: str, t: str) -> str:
-        l,r,res,min_len,t_map,s_map = 0,0,[],float('inf'),{},{}
+        l = r = 0
+        res, min_len = [], float('inf')
+        t_map, s_map = Counter(t), {}
         
-        for ch in t:
-            t_map[ch] = 1+t_map.get(ch, 0)
         
         for r in range(len(s)):
             s_map[s[r]] = 1 + s_map.get(s[r], 0)
             
             is_valid = True
+            
+            # check if valid
             for key, value in t_map.items():
                 if key not in s_map or s_map[key] < value:
                     is_valid = False
                 
+            # shrink window until no longer valid and record min
             while is_valid:
                 if r-l+1 < min_len:
                     min_len = r-l+1
@@ -30,4 +33,3 @@ class Solution:
                 l+=1
                 
         return "" if len(res) == 0 else s[res[0]:res[1]+1]
-        
