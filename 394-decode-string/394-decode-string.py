b41f34a8df83e29aa8dbcb693209d39be7d49263
Tyler Le <tylerle99@yahoo.com>
2022-09-10 19:28:46 -0700

    Time: 61 ms (13.47%), Space: 14 MB (20.25%) - LeetHub

---
+++ 394-decode-string/394-decode-string.py
@@ -0,0 +1,27 @@
+class Solution:
+    def decodeString(self, s: str) -> str:
+        stack = []
+        decoded = ""
+        
+        for ch in s:
+            if ch == ']':
+                while stack[-1] != '[':
+                    decoded += stack.pop()
+                    
+                stack.pop() # pop the '['
+                # get the number k (aka num_repeats)
+                base = 1
+                num_repeats = 0
+                while stack and stack[-1].isdigit():
+                    # print(stack.pop())
+                    num_repeats = num_repeats + (int(stack.pop()) - int('0')) * base
+                    base*=10
+
+                decoded = decoded + (decoded * (int(num_repeats) - 1))
+                stack.extend(list(reversed(decoded)))
+                decoded = ""
+                
+            else:
+                stack.append(ch)
+                
+        return "".join(stack)
