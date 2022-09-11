edc1ee34bc75ec69005e628072253b2dc58653e8
Tyler Le <tylerle99@yahoo.com>
2022-09-10 19:32:25 -0700

    Time: 59 ms (17.62%), Space: 13.9 MB (71.61%) - LeetHub

--- 394-decode-string/394-decode-string.py
+++ 394-decode-string/394-decode-string.py
@@ -5,10 +5,10 @@ class Solution:
         
         for ch in s:
             if ch == ']':
-                while stack[-1] != '[':
-                    decoded += stack.pop()
+                while stack[-1] != '[': decoded += stack.pop()
                     
                 stack.pop() # pop the '['
+                
                 # get the number k (aka num_repeats)
                 base = 1
                 num_repeats = 0
@@ -16,12 +16,12 @@ class Solution:
                     # print(stack.pop())
                     num_repeats = num_repeats + (int(stack.pop()) - int('0')) * base
                     base*=10
-
+                
+                # extend it k-1 more times (k is same as num_repeats)
                 decoded = decoded + (decoded * (int(num_repeats) - 1))
                 stack.extend(list(reversed(decoded)))
                 decoded = ""
                 
-            else:
-                stack.append(ch)
+            else: stack.append(ch)
                 
         return "".join(stack)
