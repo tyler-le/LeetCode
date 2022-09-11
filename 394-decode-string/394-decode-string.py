e3d373cb7bec4e9b974c807997726071958f0b31
Tyler Le <tylerle99@yahoo.com>
2022-09-10 19:36:43 -0700

    Time: 64 ms (8.86%), Space: 13.9 MB (20.25%) - LeetHub

--- 394-decode-string/394-decode-string.py
+++ 394-decode-string/394-decode-string.py
@@ -4,24 +4,26 @@ class Solution:
         decoded = ""
         
         for ch in s:
-            if ch == ']':
-                while stack[-1] != '[': decoded += stack.pop()
-                    
-                stack.pop() # pop the '['
+            if ch != ']': 
+                stack.append(ch)
+                continue
+            while stack[-1] != '[': 
+                decoded += stack.pop()
+
+            stack.pop() # pop the '['
+
+            # get the number k (aka num_repeats)
+            base = 1
+            num_repeats = 0
+            while stack and stack[-1].isdigit():
+                # print(stack.pop())
+                num_repeats = num_repeats + (int(stack.pop()) - int('0')) * base
+                base*=10
+
+            # extend it k-1 more times (k is same as num_repeats)
+            decoded = decoded + (decoded * (int(num_repeats) - 1))
+            stack.extend(list(reversed(decoded)))
+            decoded = ""
                 
-                # get the number k (aka num_repeats)
-                base = 1
-                num_repeats = 0
-                while stack and stack[-1].isdigit():
-                    # print(stack.pop())
-                    num_repeats = num_repeats + (int(stack.pop()) - int('0')) * base
-                    base*=10
-                
-                # extend it k-1 more times (k is same as num_repeats)
-                decoded = decoded + (decoded * (int(num_repeats) - 1))
-                stack.extend(list(reversed(decoded)))
-                decoded = ""
-                
-            else: stack.append(ch)
                 
         return "".join(stack)
