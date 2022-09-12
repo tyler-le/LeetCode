5c1156b9460e5abcff6cd9e6c831c201ee2e7ae5
Tyler Le <tylerle99@yahoo.com>
2022-09-12 10:49:43 -0700

    Time: 374 ms (42.99%), Space: 15.2 MB (81.06%) - LeetHub

--- 437-path-sum-iii/437-path-sum-iii.py
+++ 437-path-sum-iii/437-path-sum-iii.py
@@ -14,17 +14,18 @@ class Solution:
             
             curr_path.append(root.val)
                 
-            # check sums of all subpaths in curr_path here
             count, curr_sum = 0, 0
+            
+            # check sums of all subpaths in curr_path here
             for i in range(len(curr_path)-1, -1, -1): # reverse iteration
-                curr_sum+=curr_path[i]
+                curr_sum += curr_path[i]
                 if curr_sum == targetSum:
                     count += 1
                 
             count += path_sum_helper(root.left, targetSum, curr_path)
             count += path_sum_helper(root.right, targetSum, curr_path)
             
-            del curr_path[-1]
+            curr_path.pop()
             
             return count
         
