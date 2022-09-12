c86942ee08780e81647dd7bfef18b0608db5a1e0
Tyler Le <tylerle99@yahoo.com>
2022-09-12 14:27:27 -0700

    Time: 106 ms (90.81%), Space: 17.1 MB (25.75%) - LeetHub

--- 207-course-schedule/207-course-schedule.py
+++ 207-course-schedule/207-course-schedule.py
@@ -1,45 +1,28 @@
-class Solution(object):
-    def canFinish(self, numCourses, prerequisites):
-        """
-        :type numCourses: int
-        :type prerequisites: List[List[int]]
-        :rtype: bool
-        """
-        # create an empty prereqlist for each course
-        prereq_map = { i:[] for i in range(numCourses)} 
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
         
-        # map each course to prereq list
-        for course, prereq in prerequisites:
-            prereq_map[course].append(prereq)
-        
-        # needed for backtracking
-        visited = set()
-        
-        def dfs(course):
-            # if already visited, we are in a loop
-            if course in visited:
-                return False
+        def dfs(course, visited):
+            if course in visited: return False
             
-            # if course has no prereqs, we hit a base case and return True
-            if prereq_map[course] == []:
-                return True
-            
+            neighbors = adjacency_list[course]
             visited.add(course)
             
-            # for each prereq in our course list, run dfs on it
-            for prereq in prereq_map[course]:
-                if not dfs(prereq): return False
+            for nbor in neighbors:
+                if not dfs(nbor, visited): return False
                 
-            # unvisit current course    
+            adjacency_list[course] = []
             visited.remove(course)
             
-            # going this far means the course can be taken. to speed up future calculations, we just set prereqs to [] to immediately get to base case
-            prereq_map[course] = []
             return True
+                
+       
+        adjacency_list = collections.defaultdict(list)
         
-        # we need the for loop in the case of an unconnected graph
+        for course, prereq in prerequisites:
+            adjacency_list[course].append(prereq)
+        
         for i in range(numCourses):
-            if not dfs(i): return False
+            visited = set()
+            if not dfs(i, visited): return False
+            
         return True
-
-
