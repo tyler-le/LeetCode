51349763d22fecafde0cc418141fb4bc1b76133d
Tyler Le <tylerle99@yahoo.com>
2022-09-12 15:28:19 -0700

    Time: 59 ms (17.86%), Space: 13.8 MB (57.05%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.py
+++ 70-climbing-stairs/70-climbing-stairs.py
@@ -1,9 +1,9 @@
 class Solution:
     def climbStairs(self, n: int) -> int:
         # Base cases start at n = 1, 2
-        
         if n <= 2: return n
         
+        # n+1 because we skip 0th element
         memo = [-1 for _ in range (n+1)]
         memo[1], memo[2] = 1, 2
         
