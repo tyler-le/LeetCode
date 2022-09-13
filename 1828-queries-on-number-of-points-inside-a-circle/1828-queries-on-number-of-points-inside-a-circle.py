e56b2134f496c45aa0bfda4bbec75a187cb5e19a
Tyler Le <tylerle99@yahoo.com>
2022-09-12 20:18:02 -0700

    Time: 819 ms (90.98%), Space: 13.9 MB (17.29%) - LeetHub

--- 1828-queries-on-number-of-points-inside-a-circle/1828-queries-on-number-of-points-inside-a-circle.py
+++ 1828-queries-on-number-of-points-inside-a-circle/1828-queries-on-number-of-points-inside-a-circle.py
@@ -10,9 +10,11 @@ class Solution(object):
         for x_query, y_query, radius in queries:
             count = 0
             for x_point, y_point in points:
+                # calculate dist from center of query to point
                 dist = math.sqrt((x_query - x_point)**2 + (y_query - y_point)**2)
-                if dist <= radius:
-                    count+=1
+                
+                # if distance is less than radius, it is within the query circle
+                if dist <= radius: count+=1
             res.append(count)
             
         return res
