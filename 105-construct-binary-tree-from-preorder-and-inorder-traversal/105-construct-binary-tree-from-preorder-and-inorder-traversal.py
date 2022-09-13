7c1492f7788ed76d707405a8e2085ddc7cbe68f7
Tyler Le <tylerle99@yahoo.com>
2022-09-13 12:28:58 -0700

    Time: 107 ms (78.89%), Space: 18.9 MB (87.39%) - LeetHub

--- 105-construct-binary-tree-from-preorder-and-inorder-traversal/105-construct-binary-tree-from-preorder-and-inorder-traversal.py
+++ 105-construct-binary-tree-from-preorder-and-inorder-traversal/105-construct-binary-tree-from-preorder-and-inorder-traversal.py
@@ -1,28 +1,36 @@
 # Definition for a binary tree node.
-# class TreeNode(object):
+# class TreeNode:
 #     def __init__(self, val=0, left=None, right=None):
 #         self.val = val
 #         self.left = left
 #         self.right = right
-class Solution(object):
-    def buildTree(self, preorder, inorder):
-        """
-        :type preorder: List[int]
-        :type inorder: List[int]
-        :rtype: TreeNode
-        """
-        if not preorder or not inorder: return None
+class Solution:
+    def buildTree(self, preorder: List[int], inorder: List[int]) -> Optional[TreeNode]:
+        def helper(left, right):
+            
+            nonlocal preorder_index
+            
+            if left > right: return None
+            root_value = preorder[preorder_index]
+            preorder_index+=1
+            root = TreeNode(root_value)
+            
+            inorder_index = inorder_index_map[root_value]
+            
+            root.left = helper(left, inorder_index-1)
+            root.right = helper(inorder_index+1, right)
+            
+            return root
+            
+            
+            
+            
         
-        # inorder_index_map = {}
-        # for index, elem in enumerate(inorder):
-        #     inorder_index_map[elem] = index
-        # print(inorder_index_map)
         
-        root_value = preorder[0]
-        root = TreeNode(root_value)
-        preorder_index = inorder.index(root_value)
-        
-        root.left = self.buildTree(preorder[1:preorder_index+1], inorder[0:preorder_index])
-        root.right = self.buildTree(preorder[preorder_index+1:], inorder[preorder_index+1:])
-        
-        return root
+        # get indices of each element in inorder for quick access
+        inorder_index_map = {}
+        for index, elem in enumerate(inorder):
+            inorder_index_map[elem] = index
+            
+        preorder_index = 0
+        return helper(0, len(preorder)-1)
