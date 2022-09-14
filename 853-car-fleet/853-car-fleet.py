223ddb6b892b34ae96d7dd8b580cbba65b4b4997
Tyler Le <tylerle99@yahoo.com>
2022-09-13 19:29:56 -0700

    Time: 2591 ms (5.02%), Space: 36.2 MB (52.51%) - LeetHub

---
+++ 853-car-fleet/853-car-fleet.py
@@ -0,0 +1,17 @@
+class Solution:
+    def carFleet(self, target: int, position: List[int], speed: List[int]) -> int:
+        pairs = [(p, s) for p, s in zip(position, speed)]
+        pairs.sort()
+        
+        fleet_time = -float('inf')
+        res = 0
+        
+        for pos, speed in reversed(pairs):
+            time = float(target - pos) / speed
+            if time > fleet_time: 
+                res+=1
+                fleet_time = time
+                
+        return res
+            
+            
