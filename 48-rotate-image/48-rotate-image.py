6b30d6e6e992254c584048b7fad32488ccec31db
Tyler Le <tylerle99@yahoo.com>
2022-09-14 16:26:23 -0700

    Time: 43 ms (79.78%), Space: 13.8 MB (73.99%) - LeetHub

--- 48-rotate-image/48-rotate-image.py
+++ 48-rotate-image/48-rotate-image.py
@@ -3,13 +3,13 @@ class Solution:
         """
         Do not return anything, modify matrix in-place instead.
         """
-        # A more fancy way is to transpose (flip across diagonal) and then to reflect (reverse).
-        # This is the same as rotating it
-        
+        ''' 
+        A more fancy way is to transpose (flip across diagonal) and then to reflect(reverse). 
+        This is the same as rotating it
+        '''
         rows = cols = len(matrix)
         
         def transpose():
-            
             for r in range(rows):
                 for c in range(r+1, cols):
                     matrix[r][c], matrix[c][r] = matrix [c][r], matrix[r][c]
