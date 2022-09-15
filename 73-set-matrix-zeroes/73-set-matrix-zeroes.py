42ed38468e49c749b4b97e155b1198045420a250
Tyler Le <tylerle99@yahoo.com>
2022-09-15 12:43:13 -0700

    Time: 237 ms (13.03%), Space: 14.3 MB (52.55%) - LeetHub

--- 73-set-matrix-zeroes/73-set-matrix-zeroes.py
+++ 73-set-matrix-zeroes/73-set-matrix-zeroes.py
@@ -4,15 +4,47 @@ class Solution(object):
         :type matrix: List[List[int]]
         :rtype: None Do not return anything, modify matrix in-place instead.
         """
-        row_set, col_set = set(), set()
+        rows, cols = len(matrix), len(matrix[0])
+        zero_first_row, zero_first_col = False, False
         
-        for i in range(len(matrix)):
-            for j in range(len(matrix[0])):
-                if matrix[i][j] == 0:
-                    row_set.add(i)
-                    col_set.add(j)
+        # determine if we need to zero-out first row
+        for i in range(cols): 
+            if matrix[0][i] == 0: 
+                zero_first_row = True
+                break
+                
+        # determine if we need to zero-out first col
+        for i in range(rows):
+            if matrix[i][0] == 0: 
+                zero_first_col = True
+                break
+                
+        
+        # perform flagging on matrix (except first row, first col)
+        for i in range(1, rows):
+            for j in range(1, cols):
+                if matrix[i][j] == 0: 
+                    matrix[0][j] = 0
+                    matrix[i][0] = 0
                     
-        for i in range(len(matrix)):
-            for j in range(len(matrix[0])):
-                if i in row_set or j in col_set:
+        # start to zero-out besides first row/col
+        for i in range(1, rows):
+            for j in range(1, cols):
+                if matrix[i][0] == 0 or matrix[0][j] == 0: 
                     matrix[i][j] = 0
+        
+        
+        # process first col
+        if zero_first_col:
+            for i in range(rows):
+                matrix[i][0] = 0
+        
+        # process first row
+        if zero_first_row:
+            for i in range(cols):
+                matrix[0][i] = 0
+        
+        
+            
+        
+            
