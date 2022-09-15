e68cf383b258b7fcd794e1a3707be9b63e1115b8
Tyler Le <tylerle99@yahoo.com>
2022-09-14 19:32:52 -0700

    Time: 25 ms (78.87%), Space: 13.4 MB (43.13%) - LeetHub

--- 48-rotate-image/48-rotate-image.py
+++ 48-rotate-image/48-rotate-image.py
@@ -1,22 +1,26 @@
-class Solution:
-    def rotate(self, matrix: List[List[int]]) -> None:
+class Solution(object):
+    def rotate(self, matrix):
         """
-        Do not return anything, modify matrix in-place instead.
+        :type matrix: List[List[int]]
+        :rtype: None Do not return anything, modify matrix in-place instead.
         """
-        ''' 
-        A more fancy way is to transpose (flip across diagonal) and then to reflect(reverse). 
-        This is the same as rotating it
-        '''
-        rows = cols = len(matrix)
         
-        def transpose():
-            for r in range(rows):
-                for c in range(r+1, cols):
-                    matrix[r][c], matrix[c][r] = matrix [c][r], matrix[r][c]
+        left, right = 0, len(matrix)-1
         
-        def reflect():
-            for row in matrix:
-                row = row.reverse()
+        while left < right:
+            for i in range(right - left):
+                top, bottom = left, right
+                
+                top_left = matrix[top][left+i]
+                
+                matrix[top][left+i] = matrix[bottom-i][left]
+                matrix[bottom-i][left] = matrix[bottom][right-i]
+                matrix[bottom][right-i] = matrix[top+i][right]
+                matrix[top+i][right] = top_left
+                
+            left+=1
+            right-=1
+                
+            
+            
         
-        transpose()
-        reflect()
