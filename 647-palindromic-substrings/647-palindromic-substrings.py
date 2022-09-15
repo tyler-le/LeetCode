595e150b84bcce7d0a0e867b18a84f0f9e78bb48
Tyler Le <tylerle99@yahoo.com>
2022-09-15 15:37:12 -0700

    Time: 289 ms (37.76%), Space: 13.9 MB (75.85%) - LeetHub

--- 647-palindromic-substrings/647-palindromic-substrings.py
+++ 647-palindromic-substrings/647-palindromic-substrings.py
@@ -1,28 +1,30 @@
 class Solution:
     def countSubstrings(self, s: str) -> int:
-        
+        '''
+        Intuition: For each char in s, we can expand from the center and count the number of palindromes with that char as the center. We do this for all such chars.
+        '''
         def count(i):
-            ans = 0
+            num_palindromes = 0
             
-            # odd length
+            # find num of odd length palindromes
             l, r = i, i+1
             while l >= 0 and r < len(s) and s[l] == s[r]:
-                ans+=1
+                num_palindromes+=1
                 l-=1
                 r+=1
                 
-            # even length
+            # find num of even length palindromes
             l = r = i
             while l >= 0 and r < len(s) and s[l] == s[r]:
-                ans+=1
+                num_palindromes+=1
                 l-=1
                 r+=1
-            
-            return ans
+                
+            return num_palindromes
         
         
         res = 0
-        
         for i in range(len(s)):
             res+=count(i)
+            
         return res
