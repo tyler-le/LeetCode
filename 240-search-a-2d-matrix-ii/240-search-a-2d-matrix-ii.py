15d6c203b99c96eebde7d2dbcb221c68f23a9a5d
Tyler Le <tylerle99@yahoo.com>
2022-09-15 19:21:59 -0700

    Time: 345 ms (20.90%), Space: 20.4 MB (82.56%) - LeetHub

--- 240-search-a-2d-matrix-ii/240-search-a-2d-matrix-ii.py
+++ 240-search-a-2d-matrix-ii/240-search-a-2d-matrix-ii.py
@@ -1,23 +1,16 @@
-class Solution(object):
-    def searchMatrix(self, matrix, target):
-        """
-        :type matrix: List[List[int]]
-        :type target: int
-        :rtype: bool
-        """
-        # 'Search space reduction' by starting at bottom left
-        
-        # matrix[i][j] < target then row--
-        # matrix[i][j] > target then col++
-        
-        rows, cols = len(matrix), len(matrix[0])
-        
-        # start at bottom left
-        curr_x, curr_y = rows-1, 0
-        
-        while (curr_x >= 0) and (curr_y < cols):
-            if matrix[curr_x][curr_y] > target: curr_x-=1
-            elif matrix[curr_x][curr_y] < target: curr_y+=1
-            else: return True
-    
+class Solution:
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        def binary_search(row):
+            low, high = 0, len(row)-1
+            while low <= high:
+                mid = low + ((high - low) // 2)
+                
+                if target < row[mid]: high = mid - 1
+                elif target > row[mid]: low = mid + 1
+                else: return True
+                
+            return False
+            
+        for row in matrix:
+            if binary_search(row): return True
         return False
