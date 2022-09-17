77e840f4f857de80933121d9e87adce908bab2ac
Tyler Le <tylerle99@yahoo.com>
2022-09-16 17:44:36 -0700

    Time: 180 ms (19.50%), Space: 16.1 MB (6.98%) - LeetHub

--- 152-maximum-product-subarray/152-maximum-product-subarray.py
+++ 152-maximum-product-subarray/152-maximum-product-subarray.py
@@ -1,14 +1,13 @@
 class Solution:
     def maxProduct(self, nums: List[int]) -> int:
-        curr_prod = 1
-        pre_product, post_product = [], []
-        max_product = nums[0]
+        pre_product, post_product, max_product = [], [], nums[0]
         
+        curr_prod = 1
         for num in nums:
             curr_prod*=num
             pre_product.append(curr_prod)
             max_product = max(max_product, curr_prod)
-            if not curr_prod: curr_prod = 1
+            if curr_prod == 0: curr_prod = 1
             
         
         curr_prod = 1    
@@ -16,6 +15,6 @@ class Solution:
             curr_prod*=num
             post_product.append(curr_prod)
             max_product = max(max_product, curr_prod)
-            if not curr_prod: curr_prod = 1
+            if curr_prod == 0: curr_prod = 1
                     
         return max(nums+pre_product+post_product)
