1989ac2e257e8670f95b5f766e7d8c290d9edce9
Tyler Le <tylerle99@yahoo.com>
2022-09-16 17:45:47 -0700

    Time: 214 ms (6.06%), Space: 15.8 MB (6.98%) - LeetHub

--- 152-maximum-product-subarray/152-maximum-product-subarray.py
+++ 152-maximum-product-subarray/152-maximum-product-subarray.py
@@ -3,18 +3,24 @@ class Solution:
         pre_product, post_product, max_product = [], [], nums[0]
         
         curr_prod = 1
+        # calculate prefix products
         for num in nums:
             curr_prod*=num
             pre_product.append(curr_prod)
             max_product = max(max_product, curr_prod)
+            
+            # if the product happens to be zero, reset it to 1 so that the remaining products aren't affected
             if curr_prod == 0: curr_prod = 1
             
         
-        curr_prod = 1    
+        curr_prod = 1  
+        # calculate postfix products
         for num in nums[::-1]:
             curr_prod*=num
             post_product.append(curr_prod)
             max_product = max(max_product, curr_prod)
+            
+            # if the product happens to be zero, reset it to 1 so that the remaining products aren't affected
             if curr_prod == 0: curr_prod = 1
                     
-        return max(nums+pre_product+post_product)
+        return max(pre_product + post_product)
