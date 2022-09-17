81cd040b6310ecba7acc3cc98d22d720757132fd
Tyler Le <tylerle99@yahoo.com>
2022-09-16 17:55:08 -0700

    Time: 180 ms (19.50%), Space: 15.7 MB (6.98%) - LeetHub

--- 152-maximum-product-subarray/152-maximum-product-subarray.py
+++ 152-maximum-product-subarray/152-maximum-product-subarray.py
@@ -2,25 +2,32 @@ class Solution:
     def maxProduct(self, nums: List[int]) -> int:
         pre_product, post_product, max_product = [], [], nums[0]
         
-        curr_prod = 1
         # calculate prefix products
+        curr_prod = 1
         for num in nums:
-            curr_prod*=num
+            # extend the window
+            curr_prod*=num 
+            
+            # record the current product of the window
             pre_product.append(curr_prod)
             max_product = max(max_product, curr_prod)
             
-            # if the product happens to be zero, reset it to 1 so that the remaining products aren't affected
-            if curr_prod == 0: curr_prod = 1
+            # reset the window if product evals to 0 for further calculations
+            if curr_prod == 0: curr_prod = 1 
             
         
-        curr_prod = 1  
+        
         # calculate postfix products
+        curr_prod = 1
         for num in nums[::-1]:
+            # extend the window
             curr_prod*=num
+            
+            # record the current product of the window
             post_product.append(curr_prod)
             max_product = max(max_product, curr_prod)
             
-            # if the product happens to be zero, reset it to 1 so that the remaining products aren't affected
+            # reset the window if product evals to 0 for further calculations
             if curr_prod == 0: curr_prod = 1
                     
         return max(pre_product + post_product)
