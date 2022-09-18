22f836d4fd5ed943977652614292b917243a6662
Tyler Le <tylerle99@yahoo.com>
2022-09-17 17:15:04 -0700

    Time: 26 ms (57.50%), Space: 13.6 MB (16.06%) - LeetHub

--- 54-spiral-matrix/54-spiral-matrix.py
+++ 54-spiral-matrix/54-spiral-matrix.py
@@ -1,31 +1,37 @@
-class Solution:
-    def spiralOrder(self, matrix: List[List[int]]) -> List[int]:
-        res = []
+class Solution(object):
+    def spiralOrder(self, matrix):
+        """
+        :type matrix: List[List[int]]
+        :rtype: List[int]
+        """
         rows, cols = len(matrix), len(matrix[0])
-        top = left = 0
-        right = cols - 1
-        bottom = rows - 1
+        left, right = 0, len(matrix[0])
+        top, bottom = 0, len(matrix)
+        res = []
         
-        while len(res) < rows*cols:
-            # go left
-            for i in range(left, right+1):
-                res.append(matrix[top][i])
-            
+        while len(res) < (rows*cols):
+            # go right
+            for col in range(left, right):
+                print("right", [top,col])
+                res.append(matrix[top][col])
             
             # go down
-            for i in range(top+1, bottom+1):
-                res.append(matrix[i][right])
-            
-            # go right
-            if top != bottom:
-                for i in range(right-1, left-1, -1):
-                    res.append(matrix[bottom][i])
-            
-            # go up
-            if left != right:
-                for i in range(bottom-1, top, -1):
-                    res.append(matrix[i][left])
+            for row in range(top+1, bottom):
+                print("down", [row, right-1])
+                res.append(matrix[row][right-1])
                 
+            # go left
+            if top != bottom-1:
+                for col in range(right-2, left-1, -1):
+                    print("left", [bottom-1, col])
+                    res.append(matrix[bottom-1][col])
+                
+            # go up
+            if left != right-1:
+                for row in range(bottom-2, top, -1):
+                    print("up", [row, left])
+                    res.append(matrix[row][left])
+                    
             left+=1
             right-=1
             top+=1
