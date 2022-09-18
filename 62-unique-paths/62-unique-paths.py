30604bb781e89fbfeb0c626b1141adec5bbf21d0
Tyler Le <tylerle99@yahoo.com>
2022-09-18 14:24:24 -0700

    Time: 52 ms (45.83%), Space: 13.8 MB (98.22%) - LeetHub

--- 62-unique-paths/62-unique-paths.py
+++ 62-unique-paths/62-unique-paths.py
@@ -1,11 +1,8 @@
 class Solution:
     def uniquePaths(self, m: int, n: int) -> int:
         
-        # memo[i][j] is the number of paths from [0,0] to [i,j]
-        memo = [[1 for j in range(n)]for i in range(m)]
+        # calculate n choose k directly
         
-        for i in range(1, m):
-            for j in range(1, n):
-                memo[i][j] = memo[i][j-1] + memo[i-1][j]
-        
-        return memo[m-1][n-1]
+        # we have m-1 'right' moves and n-1 'down' moves
+        # hence (m-1) + (n-1) Choose n
+        return comb((m-1) + (n-1), n-1)
