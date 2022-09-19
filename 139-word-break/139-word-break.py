03efd229375f512b2d2c1aaeaad474b8724090fd
Tyler Le <tylerle99@yahoo.com>
2022-09-19 15:09:12 -0700

    Time: 65 ms (48.60%), Space: 13.9 MB (94.58%) - LeetHub

--- 139-word-break/139-word-break.py
+++ 139-word-break/139-word-break.py
@@ -1,14 +1,14 @@
 class Solution:
-    def wordBreak(self, s: str, wordDict: List[str]) -> bool:
-        word_dict = set(wordDict)
-        
+    def wordBreak(self, s: str, wordDict: List[str]) -> bool:        
         dp = [False for _ in range(len(s) + 1)]
         dp[-1] = True
         
-        
         for i in range(len(s)-1, -1, -1):
             for word in wordDict:
-                if s[i:i + len(word)] == word and dp[i+len(word)]:
+                # if there is a word from i:i+len(word) 
+                # and there exists a valid solution from i+len(word) onward, 
+                # then we can successfully break from i onward
+                if s[i:i + len(word)] == word and dp[i+len(word)]: 
                     dp[i] = True
                 if dp[i]: break # early exit
         return dp[0]
