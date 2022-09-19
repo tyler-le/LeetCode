8e75e847509056c7bd1fa2d20bf8e3c3bb223cc7
Tyler Le <tylerle99@yahoo.com>
2022-09-18 17:17:13 -0700

    Time: 8 ms (97.20%), Space: 10.9 MB (33.12%) - LeetHub

--- 1-two-sum/1-two-sum.cpp
+++ 1-two-sum/1-two-sum.cpp
@@ -1,18 +1,19 @@
-#include <unordered_map>
-
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        unordered_map <int, int> map;
-        int complement;
+        unordered_map<int, int> hmap;
         
-        for (int i = 0; i < nums.size(); i++) {
-            complement = target - nums[i];
+        
+        for(int i = 0; i < nums.size(); i++){
+            auto complement = target - nums[i];
+            if (hmap.find(complement) == hmap.end()){
+                hmap[nums[i]] = i;
+            }
+            else {
+                return {i, hmap[complement]};
+            }
             
-            if (map.find(complement) != map.end()) return { i, map[complement] };
-            map[nums[i]] = i;
         }
-        
         return {};
     }
 };
