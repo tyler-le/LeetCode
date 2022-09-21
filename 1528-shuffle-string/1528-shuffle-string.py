5878ce236f015c6b3ea633fac577722d739ef766
Tyler Le <tylerle99@yahoo.com>
2022-09-20 19:34:42 -0700

    Time: 112 ms (28.47%), Space: 13.8 MB (96.74%) - LeetHub

--- 1528-shuffle-string/1528-shuffle-string.py
+++ 1528-shuffle-string/1528-shuffle-string.py
@@ -2,14 +2,14 @@ class Solution:
     def restoreString(self, s: str, indices: List[int]) -> str:
         tups = []
         res = ""
-        
-        for index, ch in zip(indices, s):
-            tups.append((index,ch))
+    
+        # make (index, ch) pairs and then sort
+        tups = sorted([(index, ch) for index, ch in zip(indices, s)])
             
-        tups.sort()
-        
+        # since the indices are in order now, we can build the string
         for _, ch in tups:
             res+=ch
+            
         return res
             
             
