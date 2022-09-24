f77ff63897c550881ce86da889022fef12a53389
Tyler Le <tylerle99@yahoo.com>
2022-09-23 17:22:49 -0700

    Time: 711 ms (5.01%), Space: 22.3 MB (24.21%) - LeetHub

--- 238-product-of-array-except-self/238-product-of-array-except-self.py
+++ 238-product-of-array-except-self/238-product-of-array-except-self.py
@@ -1,25 +1,22 @@
-class Solution(object):
-    def productExceptSelf(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: List[int]
-        """
-        output = []
+class Solution:
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+        prefix_prod, postfix_prod = 1, 1
+        prefix, postfix = [], []
+        res, n = [1 for num in nums], len(nums)
         
-        prefix_prod = 1
-        output.append(prefix_prod)
-
-        for i in range(1, len(nums)):
-            prefix_prod *= nums[i-1]
-            output.append(prefix_prod)
-            
-        postfix_prod = 1
-        for i in range(len(nums) - 1, 0, -1):
-            postfix_prod *= nums[i]
-            output[i-1] = postfix_prod * output[i-1]
+        for num in nums:
+            prefix_prod*=num
+            prefix.append(prefix_prod)
         
-        return output
+        for num in nums[::-1]:
+            postfix_prod*=num
+            postfix.append(postfix_prod)
             
+        for i in range(n):
+            if i == 0: res[i] = postfix[-2]
+            elif i == n - 1: res[i] = prefix[i - 1]
+            else: res[i] = prefix[i-1]*postfix[n - i - 2]
             
-            
-            
+        print(prefix, postfix)
+        return res
+        
