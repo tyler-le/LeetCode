03a91ce233c625ac26540db8f138aafb88948278
Tyler Le <tylerle99@yahoo.com>
2022-09-25 20:41:10 -0700

    Time: 173 ms (30.00%), Space: 13.9 MB (98.46%) - LeetHub

--- 39-combination-sum/39-combination-sum.py
+++ 39-combination-sum/39-combination-sum.py
@@ -1,34 +1,17 @@
-class Solution(object):
-    def combinationSum(self, candidates, target):
-        """
-        :type candidates: List[int]
-        :type target: int
-        :rtype: List[List[int]]
-        """
+class Solution:
+    def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
         res = []
         
-        def backtrack(combination, nums, curr_sum, index):
-            # if target found, add to res and backtrack
-            if curr_sum == target:
-                res.append(combination[::])
+        def dfs(curr_list, nums):
+            if sum(curr_list) > target:
+                return
+            if sum(curr_list) == target:
+                res.append(curr_list.copy())
                 return
             
-            # if sum too big, backtrack
-            if curr_sum > target: 
-                return
-            
-            # we need 'i' to avoid duplicates
             for i in range(len(nums)):
+                dfs(curr_list + [nums[i]], nums[i:])
                 
-                combination.append(nums[i])
-                curr_sum+=nums[i]
-                
-                backtrack(combination, nums[i:], curr_sum, i)
-                
-                combination.pop()
-                curr_sum-=nums[i]
-        
-        backtrack([], candidates, 0, 0)
+        dfs([], candidates)
         return res
-        
-        
+                
