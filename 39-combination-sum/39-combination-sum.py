096ac807f747d7bcb34f5dbd5138369ea96b0ddb
Tyler Le <tylerle99@yahoo.com>
2022-09-25 20:42:53 -0700

    Time: 168 ms (32.58%), Space: 13.9 MB (98.46%) - LeetHub

--- 39-combination-sum/39-combination-sum.py
+++ 39-combination-sum/39-combination-sum.py
@@ -2,15 +2,19 @@ class Solution:
     def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
         res = []
         
-        def dfs(curr_list, nums):
-            if sum(curr_list) > target:
-                return
-            if sum(curr_list) == target:
-                res.append(curr_list.copy())
+        def dfs(combination, nums):
+            # if sum too big, stop 
+            if sum(combination) > target: 
                 return
             
+            # if valid combination, add to res
+            if sum(combination) == target:
+                res.append(combination.copy())
+                return
+            
+            # else for every other number, try to dfs
             for i in range(len(nums)):
-                dfs(curr_list + [nums[i]], nums[i:])
+                dfs(combination + [nums[i]], nums[i:])
                 
         dfs([], candidates)
         return res
