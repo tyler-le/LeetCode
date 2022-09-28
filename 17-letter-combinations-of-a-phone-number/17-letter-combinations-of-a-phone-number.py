18d779e16446e2b62b4ef60f5993a54f6bb3efea
Tyler Le <tylerle99@yahoo.com>
2022-09-27 20:19:50 -0700

    Time: 20 ms (81.95%), Space: 13.8 MB (15.69%) - LeetHub

--- 17-letter-combinations-of-a-phone-number/17-letter-combinations-of-a-phone-number.py
+++ 17-letter-combinations-of-a-phone-number/17-letter-combinations-of-a-phone-number.py
@@ -7,17 +7,19 @@ class Solution(object):
         hmap = ["", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"]
         res = []
 
-        if digits == "": 
-            return []
+        if digits == "": return []
         
         def backtrack(index, combination):
+            # base case
             if len(combination) > len(digits): 
                 return
             
+            # base case
             elif len(combination) == len(digits):
                 res.append("".join(combination))
                 return
             
+            # loop thu each char in the corresponding digit and perform backtracking on the next digit and its chars
             for ch in hmap[digits[index]]:
                 combination.append(ch)
                 backtrack(index+1, combination)
