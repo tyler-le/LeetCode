8234bf974f8c82c57325a3ce57015d43a5b52175
Tyler Le <tylerle99@yahoo.com>
2022-09-29 20:06:27 -0700

    Time: 711 ms (87.71%), Space: 72.8 MB (69.65%) - LeetHub

--- 981-time-based-key-value-store/981-time-based-key-value-store.py
+++ 981-time-based-key-value-store/981-time-based-key-value-store.py
@@ -1,25 +1,40 @@
-class TimeMap:
-# key:array of values, perform binary search
+class TimeMap(object):
+
     def __init__(self):
-        self.hmap = defaultdict(list)
-
-    def set(self, key: str, value: str, timestamp: int) -> None:
-        self.hmap[key].append([value, timestamp])
-
-    def get(self, key: str, timestamp: int) -> str:
-        KEY, TIME = 0, 1
-        if key not in self.hmap: return ""
+        self.hmap = collections.defaultdict(list)
         
+
+    def set(self, key, value, timestamp):
+        """
+        :type key: str
+        :type value: str
+        :type timestamp: int
+        :rtype: None
+        """
+        self.hmap[key].append((timestamp, value))
+        
+
+    def get(self, key, timestamp):
+        """
+        :type key: str
+        :type timestamp: int
+        :rtype: str
+        """
+        TIME, VAL = 0, 1
         vals = self.hmap[key]
+        if not vals: return ""
+        
         low, high = 0, len(vals)-1
         
         while low <= high:
-            mid = low + ((high - low) // 2)
-            if vals[mid][TIME] == timestamp: return vals[mid][KEY]
-            elif vals[mid][TIME] < timestamp: low = mid + 1
-            else: high = mid - 1
+            mid = low + (high - low) // 2            
 
-        return vals[high][KEY] if vals[high][TIME] <= timestamp else ""
+            if vals[mid][TIME] == timestamp: return vals[mid][VAL]
+            elif vals[mid][TIME] > timestamp: high = mid - 1
+            else: low = mid + 1
+                
+        return vals[high][VAL] if vals[high][TIME] <= timestamp else ""
+                
         
 
 
