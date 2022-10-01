0c14cce48e6a80e4d3a17361f198a8ce710c27d4
Tyler Le <tylerle99@yahoo.com>
2022-09-30 20:26:22 -0700

    Time: 459 ms (15.66%), Space: 17.4 MB (70.09%) - LeetHub

--- 378-kth-smallest-element-in-a-sorted-matrix/378-kth-smallest-element-in-a-sorted-matrix.py
+++ 378-kth-smallest-element-in-a-sorted-matrix/378-kth-smallest-element-in-a-sorted-matrix.py
@@ -9,6 +9,7 @@ class Solution(object):
         '''
         Intuition: Treat each list separately and have n pointers to the start of each list (this will be emulated through a heap). Since the top of the min-heap will point to the smallest value, we can pop it and push the next value in that specific list. After doing this k times, we will have found the answer.
         '''
+        
         min_heap = []
         n = len(matrix)
         for row in range(n):
@@ -16,16 +17,11 @@ class Solution(object):
         
         # technically don't need to heapify since sorted
         # heapify(min_heap) 
-        
-        # while k:
-        #     val, r, c = heappop(min_heap)
-        #     if c+1 < n:
-        #         heappush(min_heap, (matrix[r][c+1], r, c+1))
-        #     k-=1
             
+        # pop the k-1 smallest elements and 'increment pointers'
         for i in range(k-1):
             val, r, c = heappop(min_heap)
             if c+1 < n:
                 heappush(min_heap, (matrix[r][c+1], r, c+1))
-        print(min_heap)
+        # the kth smallest is now at the top
         return min_heap[0][0]
