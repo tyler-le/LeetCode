a8abe662a445910b1e23f579f16220be81589b00
Tyler Le <tylerle99@yahoo.com>
2022-10-01 14:48:43 -0700

    Time: 231 ms (5.24%), Space: 13.6 MB (19.22%) - LeetHub

--- 36-valid-sudoku/36-valid-sudoku.py
+++ 36-valid-sudoku/36-valid-sudoku.py
@@ -4,44 +4,34 @@ class Solution(object):
         :type board: List[List[str]]
         :rtype: bool
         """
-
-        # loop thru rows
-        for i in range(len(board)):
-            my_set = set()
-            for j in range(len(board[0])):
-                if board[i][j] == '.':
-                    continue
-                if board[i][j] in my_set:
-                    return False
-                my_set.add(board[i][j])
-
-        # loop thru cols
-        for i in range(len(board)):
-            my_set = set()
-            for j in range(len(board[0])):
-                if board[j][i] == '.':
-                    continue
-                if board[j][i] in my_set:
-                    return False
-                my_set.add(board[j][i])
-                
-        # loop 3x3 subgrid
-        squares = {} # key is (row, col)
         
-        for i in range(9):
-            for j in range(9):
-                if board[i][j] == '.':
-                    continue
-                    
-                if (i//3, j//3) not in squares:
-                    squares[(i//3, j//3)] = set()
-                if board[i][j] in squares[(i//3, j//3)]:
-                    return False
-                else:
-                    squares[(i//3,j//3)].add(board[i][j])
-                
+        # check rows
+        digits = set()
+        for row in board:
+            digits = set()
+            for r in row:
+                if r == '.': continue
+                elif r in digits: return False
+                else: digits.add(r)
+        
+        # check cols
+        for col in range(len(board[0])):
+            digits = set()
+            for row in range(len(board)):
+                if board[row][col] == '.': continue
+                elif board[row][col] in digits: return False
+                else: digits.add(board[row][col])
+        
+        # check 3x3 subgrid
+        subgrid = collections.defaultdict(set)
+        for i in range(len(board)):
+            for j in range(len(board[0])):
+                digit = board[i][j]
+                if digit == '.': continue
+                elif digit in subgrid[(i//3, j//3)]: return False
+                else: subgrid[(i//3, j//3)].add(digit)
+        
         return True
-                
-                
-            
+                    
+        
                 
