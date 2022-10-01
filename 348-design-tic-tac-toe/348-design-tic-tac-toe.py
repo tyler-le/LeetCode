47b9500dcea9f0d5778df8594442bf9bb6611905
Tyler Le <tylerle99@yahoo.com>
2022-09-30 22:39:42 -0700

    Time: 65 ms (94.78%), Space: 15.7 MB (51.74%) - LeetHub

--- 348-design-tic-tac-toe/348-design-tic-tac-toe.py
+++ 348-design-tic-tac-toe/348-design-tic-tac-toe.py
@@ -25,12 +25,10 @@ class TicTacToe(object):
         self.cols[col]+=current_player
         
         if row == col: self.diag+=current_player
-            
         if row == n-col-1: self.anti_diag+=current_player
             
         if (abs(self.rows[row]) == n or abs(self.cols[col]) == n 
             or abs(self.diag) == n or abs(self.anti_diag) == n):
-            
             return player
         
         return 0
