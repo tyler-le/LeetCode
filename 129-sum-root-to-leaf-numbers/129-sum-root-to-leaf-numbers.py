038c9fa5b291ce0d9681b5710766c84d5b548c3d
Tyler Le <tylerle99@yahoo.com>
2022-10-01 17:18:37 -0700

    Time: 11 ms (98.92%), Space: 13.6 MB (5.52%) - LeetHub

--- 129-sum-root-to-leaf-numbers/129-sum-root-to-leaf-numbers.py
+++ 129-sum-root-to-leaf-numbers/129-sum-root-to-leaf-numbers.py
@@ -1,22 +1,27 @@
 # Definition for a binary tree node.
-# class TreeNode:
+# class TreeNode(object):
 #     def __init__(self, val=0, left=None, right=None):
 #         self.val = val
 #         self.left = left
 #         self.right = right
-class Solution:
-    def sumNumbers(self, root: Optional[TreeNode]) -> int:
-        return self.sum_numbers_helper(root, 0)
+class Solution(object):
+    def sumNumbers(self, root):
+        """
+        :type root: TreeNode
+        :rtype: int
+        """
+        self.res = 0
         
-    def sum_numbers_helper(self, root, path_sum):
-        if not root:
-            return 0
-        
-        path_sum = (10*path_sum)+root.val
-        
-        is_leaf = root.right is None and root.left is None
-        if is_leaf:
-            return path_sum
-        
-        return self.sum_numbers_helper(root.left, path_sum) + self.sum_numbers_helper(root.right, path_sum)
-        
+        def dfs(node, curr_sum):
+            
+            if not node.right and not node.left:
+                self.res+=curr_sum + node.val
+                return
+            
+            curr_sum+=node.val
+            
+            if node.left: dfs(node.left, curr_sum*10)
+            if node.right: dfs(node.right, curr_sum*10)
+                
+        dfs(root, 0)
+        return self.res
