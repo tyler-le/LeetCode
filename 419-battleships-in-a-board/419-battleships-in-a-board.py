626b6773e24e8651e0522be5d460342d3a1b691c
Tyler Le <tylerle99@yahoo.com>
2022-10-01 21:41:43 -0700

    Time: 160 ms (5.28%), Space: 17.1 MB (10.57%) - LeetHub

---
+++ 419-battleships-in-a-board/419-battleships-in-a-board.py
@@ -0,0 +1,26 @@
+class Solution(object):
+    def countBattleships(self, board):
+        """
+        :type board: List[List[str]]
+        :rtype: int
+        """
+        visited = set()
+        def dfs(i,j):
+            if i < 0 or j < 0 or i == len(board) or j == len(board[0]):
+                return
+            if board[i][j] == '.' or ((i,j)) in visited: return
+            visited.add((i,j))
+            board[i][j] = '.'
+            dfs(i+1, j)
+            dfs(i-1, j)
+            dfs(i, j+1)
+            dfs(i, j-1)
+            
+        res = 0
+        for i in range(len(board)):
+            for j in range(len(board[0])):
+                if board[i][j] == 'X':
+                    res+=1
+                    dfs(i, j)
+        return res
+        
