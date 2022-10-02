2446d66842920753c35b02a10f97f63e61632e80
Tyler Le <tylerle99@yahoo.com>
2022-10-01 17:54:55 -0700

    Time: 51 ms (88.16%), Space: 14.3 MB (29.24%) - LeetHub

--- 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.py
+++ 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.py
@@ -4,17 +4,17 @@ class Solution(object):
         :type s: str
         :rtype: int
         """
-        count = collections.defaultdict(int)
         l = 0
         res = 0
+        count = collections.defaultdict(int)
         
         for r in range(len(s)):
             count[s[r]]+=1
             
             while count[s[r]] > 1:
-                count[s[l]]-=1 
+                count[s[l]]-=1
                 l+=1
             
             res = max(res, r-l+1)
-        
+                
         return res
