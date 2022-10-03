f2999de98d128eb479fcf374b433ff6235c33106
Tyler Le <tylerle99@yahoo.com>
2022-10-02 18:00:45 -0700

    Time: 215 ms (23.64%), Space: 16.7 MB (52.28%) - LeetHub

--- 348-design-tic-tac-toe/348-design-tic-tac-toe.py
+++ 348-design-tic-tac-toe/348-design-tic-tac-toe.py
@@ -1,41 +1,29 @@
-class TicTacToe(object):
+class TicTacToe:
 
-    def __init__(self, n):
-        """
-        :type n: int
-        """
-        self.board = [[0 for _ in range(n)] for _ in range(n)]
-        self.rows = [0]*n
-        self.cols = [0]*n
+    def __init__(self, n: int):
+        self.rows = [0] * n
+        self.cols = [0] * n
         self.diag = 0
         self.anti_diag = 0
-        
+        self.n = n
 
-    def move(self, row, col, player):
-        """
-        :type row: int
-        :type col: int
-        :type player: int
-        :rtype: int
-        """
-        n = len(self.board)
-        current_player = 1 if player == 1 else -1
-            
-        self.rows[row]+=current_player
-        self.cols[col]+=current_player
+    def move(self, row: int, col: int, player: int) -> int:
+        # player1 -> +1
+        # player2 -> -1
+        curr_player = 1 if player == 1 else -1
+        n = self.n
         
-        if row == col: self.diag+=current_player
-        if row == n-col-1: self.anti_diag+=current_player
-            
+        self.rows[row]+=curr_player
+        self.cols[col]+=curr_player
+        if row == col: self.diag+=curr_player
+        if (row) == (n - col - 1): self.anti_diag+=curr_player
         if (abs(self.rows[row]) == n or abs(self.cols[col]) == n 
             or abs(self.diag) == n or abs(self.anti_diag) == n):
             return player
-        
         return 0
         
-        
-        
-        
+
+
 # Your TicTacToe object will be instantiated and called as such:
 # obj = TicTacToe(n)
 # param_1 = obj.move(row,col,player)
