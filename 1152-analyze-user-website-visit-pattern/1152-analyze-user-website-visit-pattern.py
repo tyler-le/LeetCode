903f1fc3086b65b4caded21e6c2f37f8e09ca36d
Tyler Le <tylerle99@yahoo.com>
2022-10-04 11:48:55 -0700

    Time: 141 ms (8.31%), Space: 15 MB (17.19%) - LeetHub

--- 1152-analyze-user-website-visit-pattern/1152-analyze-user-website-visit-pattern.py
+++ 1152-analyze-user-website-visit-pattern/1152-analyze-user-website-visit-pattern.py
@@ -6,9 +6,13 @@ class Solution:
         for user, site, time in sorted(zip(username, website, timestamp), key = lambda i : (i[0], i[2])):
             hmap[user].append(site)
             
-        patterns = Counter()
+        patterns = collections.defaultdict(int)
 
         for user, sites in hmap.items():
-            patterns.update(Counter(set(combinations(sites, 3))))
+            #patterns.update(Counter())
+            for pattern in set(combinations(sites, 3)):
+                patterns[pattern]+=1
+            
+            print(patterns)
 
         return max(sorted(patterns), key=patterns.get)
