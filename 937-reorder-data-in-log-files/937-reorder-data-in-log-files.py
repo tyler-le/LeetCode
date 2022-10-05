511f0d648d3da1edae41a4b68b801330824ecccf
Tyler Le <tylerle99@yahoo.com>
2022-10-05 09:20:47 -0700

    Time: 77 ms (27.30%), Space: 14 MB (39.49%) - LeetHub

--- 937-reorder-data-in-log-files/937-reorder-data-in-log-files.py
+++ 937-reorder-data-in-log-files/937-reorder-data-in-log-files.py
@@ -3,18 +3,11 @@ class Solution:
         letter_logs, digit_logs = [], []
 
         for log in logs:
-            if log.split()[1].isdigit(): 
-                digit_logs.append(log) 
-            
-            else:
-                letter_logs.append(log)
+            if log.split()[1].isdigit(): digit_logs.append(log) 
+            else: letter_logs.append(log)
                 
-                
-                
-        # when suffix is tie, sort by identifier
-        letter_logs.sort(key = lambda i : i.split()[0])
-        #sort by suffix
-        letter_logs.sort(key = lambda i : i.split()[1:])
+        # first sort by contents. if ties, sort by identifier
+        letter_logs.sort(key = lambda i : (i.split(' ')[1:], i.split()[0]))
         
         return letter_logs + digit_logs
      
