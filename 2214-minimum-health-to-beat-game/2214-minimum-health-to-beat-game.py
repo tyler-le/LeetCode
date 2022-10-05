cbe49b66595b3768e06513c640360c694dff0b24
Tyler Le <tylerle99@yahoo.com>
2022-10-05 09:52:52 -0700

    Time: 1434 ms (17.40%), Space: 27.5 MB (95.07%) - LeetHub

--- 2214-minimum-health-to-beat-game/2214-minimum-health-to-beat-game.py
+++ 2214-minimum-health-to-beat-game/2214-minimum-health-to-beat-game.py
@@ -3,11 +3,11 @@ class Solution:
         max_damage = max(damage)
         
         # get all occurrences where we WONT use armor
-        dmg_without_max = sum(damage) - max_damage
+        no_armor = sum(damage) - max_damage
         
         # get the occurrence where we DO use armor
         # return the max of max_damage - armor or 0 (if armor > max_damage)
-        use_armor = max(max_damage - armor, 0)
+        yes_armor = max(max_damage - armor, 0)
         
-        return dmg_without_max + use_armor + 1
+        return no_armor + yes_armor + 1
     
