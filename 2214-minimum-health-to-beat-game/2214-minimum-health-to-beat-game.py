0617faf4a910e8be7a89c484da6d4188147e158d
Tyler Le <tylerle99@yahoo.com>
2022-10-05 09:46:02 -0700

    Time: 1398 ms (20.56%), Space: 27.7 MB (72.36%) - LeetHub

--- 2214-minimum-health-to-beat-game/2214-minimum-health-to-beat-game.py
+++ 2214-minimum-health-to-beat-game/2214-minimum-health-to-beat-game.py
@@ -2,6 +2,13 @@ class Solution:
     def minimumHealth(self, damage: List[int], armor: int) -> int:
         maxi = max(damage)
         health = 0
+        
+        dmg_without_maxi = sum(damage) - maxi
+        use_armor = max(maxi-armor, 0)
+        
+        return dmg_without_maxi + use_armor + 1
+        
+        
         armor_used = False
         for dmg in damage:
             if dmg == maxi:
