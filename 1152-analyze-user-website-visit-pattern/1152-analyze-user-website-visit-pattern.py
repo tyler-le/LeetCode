dd6a26dbb3b2cd0ff901f776feb7ee11506f6da9
Tyler Le <tylerle99@yahoo.com>
2022-10-10 11:14:07 -0700

    Time: 179 ms (7.24%), Space: 13.8 MB (98.03%) - LeetHub

--- 1152-analyze-user-website-visit-pattern/1152-analyze-user-website-visit-pattern.py
+++ 1152-analyze-user-website-visit-pattern/1152-analyze-user-website-visit-pattern.py
@@ -1,15 +1,22 @@
-class Solution:
-    def mostVisitedPattern(self, username: List[str], timestamp: List[int], website: List[str]) -> List[str]:
+class Solution(object):
+    def mostVisitedPattern(self, username, timestamp, website):
+        """
+        :type username: List[str]
+        :type timestamp: List[int]
+        :type website: List[str]
+        :rtype: List[str]
+        """
+        triplets = [(u, t, w) for u, t, w in zip(username, timestamp, website)]
+        triplets.sort(key = lambda i : (i[1]))
+
         hmap = collections.defaultdict(list)
         patterns = collections.defaultdict(int)
         
-        for user, site, time in sorted(zip(username, website, timestamp), key = lambda i : (i[2])):
-            hmap[user].append(site)
-            
-            
-        patterns = collections.defaultdict(int)
-        for user, sites in hmap.items():
-            for pattern in set(combinations(sites, 3)):
-                patterns[pattern]+=1
+        for user, time, website in triplets:
+            hmap[user].append(website)
         
+        for user, history in hmap.items():
+            for pattern in (set(combinations(history, 3))):
+                patterns[pattern]+=1
+        print(patterns)
         return max(sorted(patterns), key=patterns.get)
