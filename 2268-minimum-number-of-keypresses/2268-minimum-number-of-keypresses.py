2376299ee99ff422abfe7fb462979de52a3f6aef
Tyler Le <tylerle99@yahoo.com>
2022-10-11 14:52:44 -0700

    Time: 231 ms (25.17%), Space: 15 MB (76.92%) - LeetHub

--- 2268-minimum-number-of-keypresses/2268-minimum-number-of-keypresses.py
+++ 2268-minimum-number-of-keypresses/2268-minimum-number-of-keypresses.py
@@ -5,15 +5,14 @@ class Solution(object):
         :rtype: int
         """
         res, count, max_heap = 0, Counter(s), []
-        
-        for ch, freq in count.items():
-            max_heap.append((-freq, ch))
-            
+        max_heap = [(-v, k) for k,v in Counter(s).items()]
         heapify(max_heap)
         
         # most freq go in first position
+        # populate numpad (i//9) decides the order in a specific key location
         for i in range(len(max_heap)):
             freq, char = heappop(max_heap)
+            # map to a key location and calc how many presses needed for this char
             res+=(((i//9) + 1) * -freq)
             
         return res
