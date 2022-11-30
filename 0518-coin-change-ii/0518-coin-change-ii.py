71fa3860040ba3ce6b84ccde8b060c8f0294bc2a
Tyler Le <tylerle99@yahoo.com>
2022-11-30 12:38:51 -0800

    Time: 728 ms (48.27%), Space: 48.4 MB (20.01%) - LeetHub

---
+++ 0518-coin-change-ii/0518-coin-change-ii.py
@@ -0,0 +1,15 @@
+class Solution:
+    def change(self, amount: int, coins: List[int]) -> int:
+        A = [[1]+[0]*amount for _ in range(len(coins)+1)]
+
+        A[0][0] = 1
+            
+        for i in range(1, len(coins)+1):
+            for j in range(1, amount+1):
+                if j-coins[i-1] >= 0: IN = A[i][j-coins[i-1]]
+                else: IN = 0
+                OUT = A[i-1][j]
+                A[i][j] = IN + OUT
+        print(A)
+        return A[-1][-1]
+        
