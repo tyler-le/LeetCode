b8be4abb9850f8c9e3c28816f13ed2e2178313ee
Tyler Le <tylerle99@yahoo.com>
2022-12-02 18:43:21 -0800

    Time: 384 ms (46.84%), Space: 17.7 MB (36.37%) - LeetHub

---
+++ 0072-edit-distance/0072-edit-distance.py
@@ -0,0 +1,25 @@
+class Solution:
+    def minDistance(self, word1: str, word2: str) -> int:
+        n = len(word1)
+        m = len(word2)
+        INF = float('inf')
+        
+        # create a n x m matrix of infinities
+        
+        A = [[INF for x in range(m+1)] for y in range(n+1)] 
+        
+        for row in range(n+1):
+            A[row][0] = row
+            
+        for col in range(m+1):
+            A[0][col] = col
+
+        for i in range(1, n+1):
+            for j in range(1, m+1):
+                if word1[i-1] == word2[j-1]:
+                    A[i][j] = A[i-1][j-1]
+                else:
+                    A[i][j] = 1 + min(A[i-1][j-1], A[i-1][j], A[i][j-1])
+                    
+        return A[-1][-1]
+                    
