49eb1768627a240ae439f788717d03df78c51683
Tyler Le <tylerle99@yahoo.com>
2022-12-02 22:17:44 -0800

    Time: 20 ms (86.47%), Space: 13.6 MB (17.46%) - LeetHub

---
+++ 0198-house-robber/0198-house-robber.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def rob(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        n = len(nums)
+        dp = [0]*n;
+
+        dp[0] = nums[0];
+        
+        for i in range (1,n):
+            IN = nums[i] + dp[i-2] if (i > 1) else nums[i]
+            OUT = dp[i-1];
+            dp[i] = max(IN, OUT);
+        
+        return dp[n-1];
