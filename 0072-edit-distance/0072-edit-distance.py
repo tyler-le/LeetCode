20412062871cd325eece33e2ca329cedac31408f
Tyler Le <tylerle99@yahoo.com>
2022-12-02 18:44:17 -0800

    Time: 770 ms (5.03%), Space: 17.9 MB (18.79%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.py
+++ 0072-edit-distance/0072-edit-distance.py
@@ -17,9 +17,9 @@ class Solution:
         for i in range(1, n+1):
             for j in range(1, m+1):
                 if word1[i-1] == word2[j-1]:
-                    A[i][j] = A[i-1][j-1]
+                     A[i][j] = min(0+A[i-1][j-1], 1+A[i-1][j], 1+A[i][j-1])
                 else:
-                    A[i][j] = 1 + min(A[i-1][j-1], A[i-1][j], A[i][j-1])
+                    A[i][j] = min(1+A[i-1][j-1], 1+A[i-1][j], 1+A[i][j-1])
                     
         return A[-1][-1]
                     
