5d0496d2554b4aec39dcfb95e6eaf3fe2425e37e
Tyler Le <tylerle99@yahoo.com>
2023-01-02 06:26:42 -0800

    Time: 37 ms (68.40%), Space: 13.9 MB (74.14%) - LeetHub

--- 0290-word-pattern/0290-word-pattern.py
+++ 0290-word-pattern/0290-word-pattern.py
@@ -1,16 +1,14 @@
 class Solution:
     def wordPattern(self, pattern: str, s: str) -> bool:
-        s = s.split(" ")
+        s, hmap, idx = s.split(" "), collections.defaultdict(str), 0
         if len(s) != len(pattern): return False
-        hmap = collections.defaultdict(str)
-        idx = 0
+        
         for p in pattern:
-            if p not in hmap:
+            if p not in hmap: 
                 hmap[p] = s[idx]
-            else:
-                if s[idx] != hmap[p]: 
-                    return False
+            else: 
+                if s[idx] != hmap[p]: return False
             idx+=1
-        if len(set(hmap.values())) != len(hmap): return False
-        return True
+            
+        return False if len(set(hmap.values())) != len(hmap) else True
                 
