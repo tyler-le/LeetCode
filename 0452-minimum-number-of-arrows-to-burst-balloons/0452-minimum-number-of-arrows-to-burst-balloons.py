ac07641c6abf87b53631f34dcaa8f351a28252a2
Tyler Le <tylerle99@yahoo.com>
2023-01-04 19:57:11 -0800

    Time: 2775 ms (44.78%), Space: 59.9 MB (35.51%) - LeetHub

--- 0452-minimum-number-of-arrows-to-burst-balloons/0452-minimum-number-of-arrows-to-burst-balloons.py
+++ 0452-minimum-number-of-arrows-to-burst-balloons/0452-minimum-number-of-arrows-to-burst-balloons.py
@@ -1,15 +1,15 @@
 class Solution:
     def findMinArrowShots(self, points: List[List[int]]) -> int:
         # sort by end pos
-        # merge intervals
-        res = 1
+        # interval problem
+        
         points.sort(key=lambda x: x[1] )
-        prev_start, prev_end = points[0]
+        res, prev_end = 1, points[0][1]
+        
         for curr_start, curr_end in points:
-                
             # does not overlap
             if curr_start > prev_end:
-                prev_start, prev_end = curr_start, curr_end
+                prev_end = curr_end
                 res+=1
-        print(points)
+
         return res
