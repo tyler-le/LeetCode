d8593e9ea1bb9bfdfe768b2d92fccdb1d24f2813
Tyler Le <tylerle99@yahoo.com>
2023-01-08 08:27:00 -0800

    Time: 161 ms (60.01%), Space: 14 MB (53.74%) - LeetHub

--- 0149-max-points-on-a-line/0149-max-points-on-a-line.py
+++ 0149-max-points-on-a-line/0149-max-points-on-a-line.py
@@ -3,18 +3,20 @@ class Solution:
         def calc_slope(p1, p2):
             x1,y1 = p1
             x2,y2 = p2
+            
             # vertical line case
             if (x2-x1 == 0): return 
             else: return (y2-y1) / (x2-x1)
         
         n = len(points)
-        res = 1
+        res = 1 # base case: always one point on a line
+        
         for i in range(n):
             hmap = collections.defaultdict(int)
             for j in range(i+1,n):
                 slope = calc_slope(points[i], points[j])
                 hmap[slope]+=1
-                res = max(res, max(hmap.values())+1)
+                res = max(res, max(hmap.values())+1) # add one to include points[i]
         return res
         
                 
