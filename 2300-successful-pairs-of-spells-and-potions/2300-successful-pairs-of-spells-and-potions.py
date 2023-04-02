8e46f67da29c498f9a61d28d3b8a5e4116ea021e
Tyler Le <tylerle99@yahoo.com>
2023-04-02 10:34:23 -0700

    Time: 2040 ms (25.78%), Space: 37.2 MB (38.53%) - LeetHub

--- 2300-successful-pairs-of-spells-and-potions/2300-successful-pairs-of-spells-and-potions.py
+++ 2300-successful-pairs-of-spells-and-potions/2300-successful-pairs-of-spells-and-potions.py
@@ -2,9 +2,10 @@ class Solution:
     def successfulPairs(self, spells: List[int], potions: List[int], success: int) -> List[int]:
         ret = []
         potions.sort()
+        p = len(potions)
         
         for spell in spells:
-            low, high = 0, len(potions) - 1
+            low, high = 0, p - 1
             
             while low < high:
                 mid = (high + low) // 2
@@ -13,9 +14,9 @@ class Solution:
                 elif potions[mid] * spell >= success:
                     high = mid
                     
-            if low == len(potions) or potions[low] * spell < success: 
+            if low == p or potions[low] * spell < success: 
                 ret.append(0)
             else: 
-                ret.append(len(potions) - low)
+                ret.append(p - low)
             
         return ret
