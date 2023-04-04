ef26d7ba8d578bec978a0a9fea7dfdb7516b085d
Tyler Le <tylerle99@yahoo.com>
2023-04-03 18:26:21 -0700

    Time: 233 ms (15.17%), Space: 14.6 MB (90.90%) - LeetHub

--- 2405-optimal-partition-of-string/2405-optimal-partition-of-string.py
+++ 2405-optimal-partition-of-string/2405-optimal-partition-of-string.py
@@ -1,18 +1,18 @@
 class Solution:
     def partitionString(self, s: str) -> int:
         hmap = collections.defaultdict(int)
-        out = []
+        out = 0
         l = 0
         
         for r in range(len(s)):
             if hmap[s[r]] > 0:
-                out.append(s[l:r])
+                out+=1
                 hmap.clear()
                 l = r
                 hmap[s[l]]+=1
             else:
                 hmap[s[r]]+=1
                 
-        out.append(s[l:r])
-        return len(out)
+        out+=1
+        return out
                 
