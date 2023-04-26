650ba5fa803eefa1e935e3135ce8dc012f91bc14
Tyler Le <tylerle99@yahoo.com>
2023-04-25 19:43:18 -0700

    Time: 49 ms (13.32%), Space: 14.3 MB (12.20%) - LeetHub

--- 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.py
+++ 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.py
@@ -10,13 +10,16 @@ class Solution:
             if nums[mid-1] > nums[mid]:
                 return nums[mid]
             
+            # Check if the pivot occurs near mid
             elif nums[mid] > nums[mid+1]:
                 return nums[mid+1]
             
+            # Left portion is sorted, search for pivot on right
             if nums[low] < nums[mid]:
                 low = mid + 1
-            
+            # Right portion is sorted, search for pivot on left
             else:
                 high = mid - 1
-        
+                
+        # Pivot never found, array is already sorted
         return nums[0]
