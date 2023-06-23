a9d497c335924fae2a38eb5b718c9fad03cb1c1b
Tyler Le <tylerle99@yahoo.com>
2023-06-22 18:28:26 -0700

    Time: 74 ms (68.30%), Space: 16.6 MB (6.41%) - LeetHub

---
+++ 0039-combination-sum/0039-combination-sum.py
@@ -0,0 +1,20 @@
+class Solution:
+    def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
+        res = []
+        
+        def helper(combination, cands):
+            curr_sum = sum(combination)
+            if curr_sum == target: 
+                res.append(combination.copy())
+                return
+            elif curr_sum > target: 
+                return
+            else:
+                for i in range(len(cands)):
+                    combination.append(cands[i])
+                    helper(combination, cands[i:])
+                    combination.pop()
+                
+        
+        helper([], candidates)
+        return res
