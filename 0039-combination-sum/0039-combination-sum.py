cba9b51845f825ecb79af0b5473524d90cc31b1c
Tyler Le <tylerle99@yahoo.com>
2023-06-22 18:46:13 -0700

    Time: 84 ms (36.96%), Space: 16.4 MB (81.36%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.py
+++ 0039-combination-sum/0039-combination-sum.py
@@ -3,17 +3,24 @@ class Solution:
         res = []
         
         def helper(combination, cands):
+
             curr_sum = sum(combination)
+            
             if curr_sum == target: 
                 res.append(combination.copy())
                 return
+            
             elif curr_sum > target: 
                 return
+            
             else:
                 for i in range(len(cands)):
                     combination.append(cands[i])
                     helper(combination, cands[i:])
+                    
+                    # backtrack step
                     combination.pop()
+
                 
         
         helper([], candidates)
