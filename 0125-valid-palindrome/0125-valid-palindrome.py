ed2953e2f284595758eeb3959045296ed5fd4f72
Tyler Le <tylerle99@yahoo.com>
2023-06-24 20:05:23 -0700

    Time: 47 ms (44.76%), Space: 14.1 MB (73.98%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -4,10 +4,15 @@ class Solution(object):
         :type s: str
         :rtype: bool
         """
-        s = re.sub(r'[^a-zA-Z0-9]', '', s).lower()
         l, r = 0, len(s) - 1
+        s = s.lower()
         
         while l < r:
+            while l < len(s)-1 and not s[l].isalnum():
+                l+=1
+            while r >= 0 and not s[r].isalnum():
+                r-=1
+            
             if s[l] == s[r]:
                 l+=1
                 r-=1
