44a6b811899e55ff3f93276feebc6807daf5a12e
Tyler Le <tylerle99@yahoo.com>
2023-07-09 12:42:28 -0700

    Time: 248 ms (84.59%), Space: 28.7 MB (76.50%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -4,23 +4,29 @@ class Solution(object):
         :type grid: List[List[str]]
         :rtype: int
         """
-        visited, res = set(), 0
+        n, m, res = len(grid), len(grid[0]), 0
         
-        def dfs(i, j):
-            if i < 0 or i == len(grid) or j < 0 or j == len(grid[0]):
-                return
-            if grid[i][j] == "0" or (i,j) in visited:
-                return
-            visited.add((i,j))
-            dfs(i+1,j)
-            dfs(i-1,j)
-            dfs(i,j+1)
-            dfs(i,j-1)
-        
-        for i in range(len(grid)):
-            for j in range(len(grid[0])):
-                if grid[i][j] == "1" and (i,j) not in visited:
-                    res+=1
-                    dfs(i,j)
-        return res
+        def dfs(r,c):
             
+            if r < 0 or c < 0 or r >= n or c >= m:
+                return 
+            
+            if grid[r][c] == "0":
+                return 
+            
+            grid[r][c] = "0"
+            
+            dfs(r+1,c)
+            dfs(r-1,c)
+            dfs(r,c+1)
+            dfs(r,c-1)
+            
+            return
+            
+        
+        for r in range(n):
+            for c in range(m):
+                if grid[r][c] == "1":
+                    dfs(r,c)
+                    res+=1
+        return res
