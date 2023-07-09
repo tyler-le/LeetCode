6ad735351150001c08d37d72ba56038f94d9f839
Tyler Le <tylerle99@yahoo.com>
2023-07-09 13:09:06 -0700

    Time: 32 ms (93.58%), Space: 13.7 MB (58.38%) - LeetHub

---
+++ 0133-clone-graph/0133-clone-graph.py
@@ -0,0 +1,29 @@
+"""
+# Definition for a Node.
+class Node(object):
+    def __init__(self, val = 0, neighbors = None):
+        self.val = val
+        self.neighbors = neighbors if neighbors is not None else []
+"""
+
+class Solution(object):
+    def cloneGraph(self, node):
+        """
+        :type node: Node
+        :rtype: Node
+        """
+        hmap = dict()
+        
+        if not node: return None
+        
+        def dfs(node):
+            hmap[node] = Node(node.val, [])
+
+            for nbor in node.neighbors:
+                if nbor not in hmap:
+                    dfs(nbor)
+                hmap[node].neighbors.append(hmap[nbor])
+                
+            return hmap[node]
+            
+        return dfs(node)
