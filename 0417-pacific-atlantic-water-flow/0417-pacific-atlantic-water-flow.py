849a70d364ceb74c3ab3f048621b99524b07687d
Tyler Le <tylerle99@yahoo.com>
2023-07-11 20:39:03 -0700

    Time: 292 ms (48.74%), Space: 17.9 MB (95.05%) - LeetHub

---
+++ 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.py
@@ -0,0 +1,39 @@
+class Solution:
+    def pacificAtlantic(self, heights: List[List[int]]) -> List[List[int]]:
+        pac, atl = set(), set()
+        n, m = len(heights), len(heights[0])
+        
+        def dfs(r, c, s, prev):
+            if r < 0 or c < 0 or r >= n or c >= m:
+                return
+            
+            if (r,c) in s:
+                return
+            
+            if heights[r][c] < prev:
+                return
+            
+            s.add((r,c))
+            
+            dfs(r+1,c,s, heights[r][c])
+            dfs(r-1,c,s, heights[r][c])
+            dfs(r,c+1,s, heights[r][c])
+            dfs(r,c-1,s, heights[r][c])
+            
+        
+        # gather all nodes reachable from pacific
+        for row in range(n):
+            dfs(row, 0, pac, heights[row][0])
+        for col in range(m):
+            dfs(0, col, pac, heights[0][col])
+            
+        # gather all nodes reachable from atlantic
+        for row in range(n):
+            dfs(row, m-1, atl, heights[row][m-1])
+        for col in range(m):
+            dfs(n-1, col, atl, heights[n-1][col])
+            
+        return pac.intersection(atl)
+    
+        
+        
