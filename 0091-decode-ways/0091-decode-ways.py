d0774449c5fc5af7860c40590e96deb4b9b023a7
Tyler Le <tylerle99@yahoo.com>
2023-07-16 20:09:44 -0700

    Time: 55 ms (20.70%), Space: 16.5 MB (33.17%) - LeetHub

--- 0091-decode-ways/0091-decode-ways.py
+++ 0091-decode-ways/0091-decode-ways.py
@@ -1,15 +1,17 @@
 class Solution:
     def numDecodings(self, s: str) -> int:
+        # map i to number of ways to decode s[i:]
         dp = {len(s) : 1}
-        
+
         def helper(i):
-            if i in dp:
-                return dp[i]
-            if s[i] == "0":
-                return 0
+            # base case(s)
+            if i in dp: return dp[i]
+            if s[i] == "0": return 0
             
+            # solve s[(i+1):]
             res = helper(i+1)
             
+            # if we can group s[i] ++ s[i+1], then solve s[(i+2):]
             if (i+1 < len(s)) and ((s[i] == "1")
                 or s[i] == "2" and  s[i+1] in "0123456"):
                 res+=helper(i+2)
