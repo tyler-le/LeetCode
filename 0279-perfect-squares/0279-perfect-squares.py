5dfe2cb3540f1ac8971fe06034857065884fdfd4
Tyler Le <tylerle99@yahoo.com>
2023-07-18 21:35:11 -0700

    Time: 4504 ms (42.76%), Space: 16.3 MB (80.44%) - LeetHub

--- 0279-perfect-squares/0279-perfect-squares.py
+++ 0279-perfect-squares/0279-perfect-squares.py
@@ -4,17 +4,10 @@ class Solution:
         dp[0] = 0
         dp[1] = 1
         
-        def helper(num):
-            ret = []
-            for i in range(1, num+1):
-                if sqrt(i) == int(sqrt(i)):
-                    ret.append(i)
-            return ret
-
         for i in range(2, n+1):
             square = 1
-            while square*square <= i:            
-                dp[i] = min(dp[i - square*square] + 1, dp[i])
+            while (square*square) <= i:            
+                dp[i] = min(dp[i - (square*square)] + 1, dp[i])
                 square+=1
 
         return dp[n]
