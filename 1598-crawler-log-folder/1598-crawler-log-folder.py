cbfda88e9a4aa7210037be2f71a41d0a7af815a9
Tyler Le <tylerle99@yahoo.com>
2023-07-29 19:38:19 -0700

    Time: 50 ms (98.58%), Space: 16.5 MB (53.34%) - LeetHub

--- 1598-crawler-log-folder/1598-crawler-log-folder.py
+++ 1598-crawler-log-folder/1598-crawler-log-folder.py
@@ -1,10 +1,12 @@
 class Solution:
     def minOperations(self, logs: List[str]) -> int:
-        depth = 0
+        res = 0
         for log in logs:
-            if log == '../':
-                if depth > 0:  # Can only go back if not at root
-                    depth -= 1
-            elif log != './':  # This is a move to a child directory
-                depth += 1
-        return depth
+            if log.startswith("../"):
+                if res > 0:
+                    res-=1
+            elif log.startswith("./"):
+                res+=0
+            else:
+                res+=1
+        return max(0, res)
