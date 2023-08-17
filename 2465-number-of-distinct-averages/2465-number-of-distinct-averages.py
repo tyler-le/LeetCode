17c99a8a15978ec481cff513564447c748930a96
Tyler Le <tylerle99@yahoo.com>
2023-08-16 22:04:20 -0700

    Time: 45 ms (55.68%), Space: 16.3 MB (32.30%) - LeetHub

---
+++ 2465-number-of-distinct-averages/2465-number-of-distinct-averages.py
@@ -0,0 +1,23 @@
+class Solution:
+    def distinctAverages(self, nums: List[int]) -> int:
+        averages = set()
+        
+        nums.sort()
+        
+        def recurse(sublist):
+            if len(sublist) == 0:
+                return
+                
+            # calculate average
+            mini, maxi = sublist[0], sublist[-1]
+            average = (mini + maxi) / 2
+            
+            # put in set
+            averages.add(average)
+            
+            # recurse
+            recurse(sublist[1:-1])
+            
+        recurse(nums)
+        print(averages)
+        return len(averages)
