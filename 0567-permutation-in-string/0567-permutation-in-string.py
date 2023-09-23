cfbc46c8ad57e66976d7dea3a1ca52d5aa833585
Tyler Le <tylerle99@yahoo.com>
2023-09-22 17:09:17 -0700

    Time: 108 ms (34.09%), Space: 16.3 MB (78.87%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.py
+++ 0567-permutation-in-string/0567-permutation-in-string.py
@@ -5,15 +5,15 @@ class Solution:
         s1_map = Counter(s1)
         s2_map = Counter()
         window_size = len(s1)
-        
         l = 0
+        
         for r in range (len(s2)):
             s2_map[s2[r]]+=1
+            
             while r-l+1 > window_size:
                 s2_map[s2[l]]-=1
-                if s2_map[s2[l]] == 0:
-                    del s2_map[s2[l]]
+                if s2_map[s2[l]] == 0: del s2_map[s2[l]]
                 l+=1
-            if s1_map == s2_map:
-                return True
+            if s1_map == s2_map: return True
+            
         return False
