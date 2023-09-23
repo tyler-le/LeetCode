02d83270fa3de4fd9a871f5babbf05ff78131af4
Tyler Le <tylerle99@yahoo.com>
2023-09-22 17:23:56 -0700

    Time: 48 ms (39.39%), Space: 17 MB (86.02%) - LeetHub

--- 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.py
+++ 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.py
@@ -1,32 +1,29 @@
 # Definition for a binary tree node.
-# class TreeNode(object):
+# class TreeNode:
 #     def __init__(self, val=0, left=None, right=None):
 #         self.val = val
 #         self.left = left
 #         self.right = right
-class Solution(object):
-    def levelOrder(self, root):
-        """
-        :type root: TreeNode
-        :rtype: List[List[int]]
-        """
+class Solution:
+    def levelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
+        q = deque()
+        q.append(root)
+        res = []
         
-        if not root: return []
-        
-        q, out = deque(), []
-        q.appendleft(root)
-        
-        while q:
-            level_size = len(q)
-            curr_level = []
-            for i in range(level_size):
-                popped = q.pop()
-                curr_level.append(popped.val)
-                if popped.left: q.appendleft(popped.left)
-                if popped.right: q.appendleft(popped.right)
-            out.append(curr_level)
-            
-        return out
-                
+        if not root: return res
 
+        while q:
+            q_size = len(q)
+            sublist = []
+            
+            for i in range(q_size):
+                popped = q.popleft()
+                sublist.append(popped.val)
+                if popped.left: q.append(popped.left)
+                if popped.right: q.append(popped.right)
+                    
+            res.append(sublist)
+        
+        return res
+            
         
