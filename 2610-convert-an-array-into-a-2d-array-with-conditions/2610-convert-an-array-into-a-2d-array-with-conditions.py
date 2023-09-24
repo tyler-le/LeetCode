d69ad3994bbcb634a303a7b84f6b92acccbd0c9a
Tyler Le <tylerle99@yahoo.com>
2023-09-23 18:34:14 -0700

    Time: 58 ms (59.97%), Space: 16.4 MB (13.98%) - LeetHub

--- 2610-convert-an-array-into-a-2d-array-with-conditions/2610-convert-an-array-into-a-2d-array-with-conditions.py
+++ 2610-convert-an-array-into-a-2d-array-with-conditions/2610-convert-an-array-into-a-2d-array-with-conditions.py
@@ -5,13 +5,13 @@ class Solution:
         # val 2 : occurrences 1
         # val 3 : occurrences 2
         
-        
         hmap = Counter(nums)
         res = [[] for _ in range(max(hmap.values()))]
         
         for num, occ in hmap.items():
             for i in range(occ):
                 res[i].append(num)
+                
         return res
         
         
