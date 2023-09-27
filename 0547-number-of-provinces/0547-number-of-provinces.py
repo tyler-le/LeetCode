99220edc9572c49c764ab806a2653ff781a2e250
Tyler Le <tylerle99@yahoo.com>
2023-09-26 21:37:52 -0700

    Time: 259 ms (8.45%), Space: 22.8 MB (7.78%) - LeetHub

---
+++ 0547-number-of-provinces/0547-number-of-provinces.py
@@ -0,0 +1,20 @@
+class Solution:
+    def findCircleNum(self, isConnected: List[List[int]]) -> int:
+        n = len(isConnected)
+        res = 0
+        
+        def dfs(u):
+            
+            for v in range(n):
+                if isConnected[u][v] == 1:
+                    isConnected[u][v] = 0
+                    isConnected[v][u] = 0
+                    dfs(v)
+
+        for i in range(n):
+            if isConnected[i][i] == 1:
+                res+=1
+                dfs(i)
+
+        return res
+            
