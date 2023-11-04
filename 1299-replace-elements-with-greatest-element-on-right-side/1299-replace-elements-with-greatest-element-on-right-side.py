ae05a3a7e0728eadcca4755399fbe8a360364bba
Tyler Le <tylerle99@yahoo.com>
2023-11-03 20:22:22 -0700

    Time: 623 ms (17.12%), Space: 18 MB (21.81%) - LeetHub

--- 1299-replace-elements-with-greatest-element-on-right-side/1299-replace-elements-with-greatest-element-on-right-side.py
+++ 1299-replace-elements-with-greatest-element-on-right-side/1299-replace-elements-with-greatest-element-on-right-side.py
@@ -1,13 +1,11 @@
 class Solution:
     def replaceElements(self, arr: List[int]) -> List[int]:
-        postfix = []
         maxi = arr[-1]
         
         for i in range(len(arr)-1, 0, -1):
             maxi = max(arr[i], maxi)
-            postfix.append(maxi)
+            arr[i] = maxi
             
-        postfix.reverse()
-        postfix.append(-1)
+        arr.append(-1)
+        return arr[1:]
         
-        return(postfix)
