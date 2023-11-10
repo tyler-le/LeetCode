e93767b71faecbed927f639c2588a5b688a5d069
Tyler Le <tylerle99@yahoo.com>
2023-11-09 18:53:22 -0800

    Time: 15 ms (53.39%), Space: 13.3 MB (39.16%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.py
+++ 0062-unique-paths/0062-unique-paths.py
@@ -1,15 +1,18 @@
-class Solution:
-    def uniquePaths(self, m: int, n: int) -> int:
-        dp = collections.defaultdict(int)
+class Solution(object):
+    def uniquePaths(self, m, n):
+        """
+        :type m: int
+        :type n: int
+        :rtype: int
+        """
         
-        def dfs(row, col):
-            if row < 0 or col < 0 or row >= m or col >= n : return 0
-            if row == m-1 and col == n-1: return 1
-            
-            if (row,col) in dp: return dp[(row,col)]
-            dp[(row,col)] = dfs(row+1, col) + dfs(row, col+1)
-            return dp[(row, col)]
+        dp = [[0 for c in range(n)] for r in range(m)]
         
-        return dfs(0,0)
+        for r in range(m): dp[r][0] = 1
+        for c in range(n): dp[0][c] = 1
         
-        
+        for i in range(1,m):
+            for j in range(1,n):
+                dp[i][j] = dp[i-1][j] + dp[i][j-1]
+                
+        return dp[m-1][n-1]
