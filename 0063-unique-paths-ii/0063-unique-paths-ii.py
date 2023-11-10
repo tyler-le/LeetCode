879d5571a0e823ae54b127d5257fd2d88b4c068a
Tyler Le <tylerle99@yahoo.com>
2023-11-09 19:21:40 -0800

    Time: 51 ms (38.04%), Space: 16.2 MB (84.00%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.py
+++ 0063-unique-paths-ii/0063-unique-paths-ii.py
@@ -2,12 +2,12 @@ class Solution:
     def uniquePathsWithObstacles(self, obstacleGrid: List[List[int]]) -> int:
         
         m, n, OBSTACLE = len(obstacleGrid), len(obstacleGrid[0]), 1
+        dp = [[0 for c in range(n)] for r in range(m)]
         
         # edge case - if start/goal is an obstacle
         if obstacleGrid[0][0] == OBSTACLE: return 0
         if obstacleGrid[m-1][n-1] == OBSTACLE: return 0
-
-        dp = [[0 for c in range(n)] for r in range(m)]
+        
         # Set the first cell
         dp[0][0] = 0 if obstacleGrid[0][0] == OBSTACLE else 1
 
@@ -18,20 +18,6 @@ class Solution:
         # Initialize the first row
         for c in range(1, n): 
             dp[0][c] = 0 if obstacleGrid[0][c] == OBSTACLE or dp[0][c-1] == 0 else 1
-
-#         for r in range(m): 
-#             if obstacleGrid[r][0] == OBSTACLE: 
-#                 dp[r][0] = 0
-#             else:
-#                 dp[r][0] = 1
-                
-#         for c in range(n): 
-#             if obstacleGrid[0][c] == OBSTACLE:
-#                 dp[0][c] = 0
-#             else:
-#                 dp[0][c] = 1
-
-        print(dp)
         
         for i in range(1,m):
             for j in range(1,n):
@@ -39,5 +25,4 @@ class Solution:
                 up = 0 if obstacleGrid[i][j-1] == OBSTACLE else dp[i][j-1]
                 dp[i][j] = left + up
                 
-        # print(dp)       
         return dp[m-1][n-1]
