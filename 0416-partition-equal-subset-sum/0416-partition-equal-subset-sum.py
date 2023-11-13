56adad808b6e1761218ea26eb1c4ca1a71cae3d6
Tyler Le <tylerle99@yahoo.com>
2023-11-12 18:56:53 -0800

    Time: 2503 ms (16.25%), Space: 32.6 MB (35.95%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.py
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.py
@@ -1,30 +1,23 @@
 class Solution:
     def canPartition(self, nums: List[int]) -> bool:
-        s = sum(nums)
-
-        # if 's' is a an odd number, we can't have two subsets with same total
-        if s % 2 != 0: return False
-
-        # we are trying to find a subset of given numbers that has a total sum of 's/2'.
-        s = int(s / 2)
-
         n = len(nums)
-        dp = [[False for x in range(s+1)] for y in range(n)]
-
-        # populate the s=0 columns, as we can always for '0' sum with an empty set
-        for i in range(0, n):
+        if sum(nums) % 2: return False
+        
+        ROWS, COLS = len(nums), (sum(nums) // 2) + 1
+        
+        dp = [[False for _ in range(COLS)] for _ in range(ROWS)]
+        
+        for i in range(ROWS):
             dp[i][0] = True
-
-      # with only one number, we can form a subset only when the required sum is
-      # equal to its value
-        for j in range(1, s+1):
-            dp[0][j] = nums[0] == j
-
-        for i in range(1, n):
-          for j in range(1, s+1):        
-            IN = dp[i-1][j-nums[i]] if (j-nums[i] >= 0) else False
-            OUT = dp[i-1][j]
-            dp[i][j] = IN or OUT
-
-        return dp[n - 1][s]
-
+            
+        for i in range(1, ROWS):
+            for j in range(1, COLS):
+                
+                if nums[i] <= j: include = dp[i-1][j - nums[i]] 
+                else: include = False
+                
+                exclude = dp[i-1][j]
+        
+                dp[i][j] = include or exclude
+            
+        return dp[-1][sum(nums) // 2]
