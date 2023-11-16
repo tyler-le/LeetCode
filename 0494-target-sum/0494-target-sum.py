f0359dfa6b1d8bf579ce3967416099f09ee5a1df
Tyler Le <tylerle99@yahoo.com>
2023-11-15 21:37:47 -0800

    Time: 279 ms (30.99%), Space: 37.1 MB (50.83%) - LeetHub

---
+++ 0494-target-sum/0494-target-sum.py
@@ -0,0 +1,28 @@
+class Solution:
+    def findTargetSumWays(self, nums: List[int], target: int) -> int:        
+        # map (acc, index) : number of ways target can be formed
+        dp = collections.defaultdict(int)
+        
+        def backtrack(acc, index):
+            
+            # check cache
+            if (acc, index) in dp: 
+                return dp[(acc, index)]
+            
+            # base case
+            if index == len(nums):
+                return 1 if (acc == target) else 0
+            
+            # make the choice
+            add = backtrack(acc+nums[index], index+1)   
+            
+            # make the second choice
+            sub = backtrack(acc-nums[index], index+1)
+            
+            # cache the result
+            dp[(acc, index)] = add + sub
+            
+            return dp[(acc, index)]
+            
+        return backtrack(0, 0)
+            
