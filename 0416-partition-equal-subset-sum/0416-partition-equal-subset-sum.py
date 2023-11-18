de21b0344929634bfe3c233f6223843ad023276b
Tyler Le <tylerle99@yahoo.com>
2023-11-17 19:55:42 -0800

    Time: 6443 ms (5.07%), Space: 45.9 MB (17.81%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.py
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.py
@@ -1,23 +1,30 @@
-class Solution:
-    def canPartition(self, nums: List[int]) -> bool:
-        n = len(nums)
+class Solution(object):
+    def canPartition(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: bool
+        """
+        rows = len(nums) + 1
+        cols = sum(nums) + 1
+        
         if sum(nums) % 2: return False
         
-        ROWS, COLS = len(nums), (sum(nums) // 2) + 1
+        dp = [[False for _ in range(cols)] for _ in range(rows)]
         
-        dp = [[False for _ in range(COLS)] for _ in range(ROWS)]
-        
-        for i in range(ROWS):
-            dp[i][0] = True
+        for c in range(cols): dp[0][c] = False
             
-        for i in range(1, ROWS):
-            for j in range(1, COLS):
-                
-                if nums[i] <= j: include = dp[i-1][j - nums[i]] 
-                else: include = False
+        for r in range(rows): dp[r][0] = True
+            
+        for i in range(1, rows):
+            for j in range(1, cols):
                 
                 exclude = dp[i-1][j]
-        
+                
+                if j - nums[i-1] < 0: include = False
+                else: include = dp[i-1][j-nums[i-1]]
+                    
                 dp[i][j] = include or exclude
-            
+
         return dp[-1][sum(nums) // 2]
+        
+        
