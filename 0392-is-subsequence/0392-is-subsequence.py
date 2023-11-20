901dc800ae6be4714361fe40fd2163166348dcd9
Tyler Le <tylerle99@yahoo.com>
2023-11-19 17:04:30 -0800

    Time: 33 ms (90.42%), Space: 16.3 MB (29.39%) - LeetHub

--- 0392-is-subsequence/0392-is-subsequence.py
+++ 0392-is-subsequence/0392-is-subsequence.py
@@ -3,7 +3,7 @@ class Solution:
         idx = 0
         
         for ch in t:
-            if idx < len(s) and s[idx] == ch:
+            if idx < len(s) and ch == s[idx]:
                 idx+=1
         
         return idx == len(s)
