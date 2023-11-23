182648fa0898d1862d2b4582df52db677079ceb3
Tyler Le <tylerle99@yahoo.com>
2023-11-23 11:43:31 -0800

    Time: 41 ms (40.41%), Space: 16.5 MB (11.01%) - LeetHub

--- 1768-merge-strings-alternately/1768-merge-strings-alternately.py
+++ 1768-merge-strings-alternately/1768-merge-strings-alternately.py
@@ -1,33 +1,16 @@
-class Solution(object):
-    def mergeAlternately(self, word1, word2):
-        """
-        :type word1: str
-        :type word2: str
-        :rtype: str
-        """
-       
-        i, j = 0, 0
-        count = 0
-        word3 = ""
+class Solution:
+    def mergeAlternately(self, word1: str, word2: str) -> str:
+        res = ""
         
-        while i < len(word1) and j < len(word2):
-            # odd iter
-            if count % 2: 
-                word3+=word2[j]
-                j+=1
-            
-            # even iter
-            else: 
-                word3+=word1[i]
-                i+=1
-            
-            count+=1
-            
-        if i == len(word1): word3+=word2[j:]
-        elif j == len(word2): word3+=word1[i:]
+        p1, p2 = 0, 0
         
-        return word3
-                
+        while p1 < len(word1) and p2 < len(word2):
+            res+=word1[p1]
+            res+=word2[p2]
+            p1+=1
+            p2+=1
             
+        if p1 == len(word1): res+=word2[p2:]
+        elif p2 == len(word2): res+=word1[p1:]
             
-            
+        return res
