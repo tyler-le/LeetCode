ac8900313cb0228b7cd27db9d87fa62fa36bb05c
Tyler Le <tylerle99@yahoo.com>
2023-11-25 10:03:57 -0800

    Time: 170 ms (5.15%), Space: 16.9 MB (6.61%) - LeetHub

---
+++ 0605-can-place-flowers/0605-can-place-flowers.py
@@ -0,0 +1,15 @@
+class Solution:
+    def canPlaceFlowers(self, flowerbed: List[int], n: int) -> bool:
+        
+        acc = 0
+        
+        for i in range(len(flowerbed)):
+            left_empty = (i == 0) or (flowerbed[i-1] == 0)
+            right_empty = (i == len(flowerbed) - 1) or (flowerbed[i+1] == 0)
+            can_plant = left_empty and right_empty and flowerbed[i] == 0
+            
+            if can_plant:
+                flowerbed[i] = 1
+                acc+=1
+                
+        return acc >= n
