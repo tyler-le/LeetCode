a9c9858cda23785b8d40d1af9f85ec62dacfbf15
Tyler Le <tylerle99@yahoo.com>
2023-11-26 19:44:30 -0800

    Time: 1896 ms (38.02%), Space: 43.1 MB (39.98%) - LeetHub

--- 0935-knight-dialer/0935-knight-dialer.py
+++ 0935-knight-dialer/0935-knight-dialer.py
@@ -1,6 +1,7 @@
 class Solution:
     def knightDialer(self, n: int) -> int:
-        
+
+        res = 0
         dp = dict()
         possible_jumps = [
             [4, 6],
@@ -14,22 +15,22 @@ class Solution:
             [1, 3],
             [2, 4]
         ]
-
+        
         def rec(i, j):
-            if i == 0: 
-                return 1
-            if (i, j) in dp: 
-                return dp[(i, j)]
-
+            if i == 0: return 1
+            if (i, j) in dp: return dp[(i,j)]
+            
             acc = 0
+        
+            # explore all combinations from current square
             for pj in possible_jumps[j]:
-                acc += rec(i - 1, pj) % (10**9 + 7)
+                acc+=rec(i-1, pj) % (10**9 + 7)
 
-            dp[(i, j)] = acc 
+            dp[(i,j)] = acc 
             return acc
-
-        res = 0
+        
         for sq in range(10):
             res += rec(n - 1, sq) % (10**9 + 7)
-
+            
         return res % (10**9 + 7)
+            
