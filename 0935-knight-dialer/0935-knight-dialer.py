8ceb5e0907c6643c78a893a249c603298d83404c
Tyler Le <tylerle99@yahoo.com>
2023-11-26 19:42:57 -0800

    Time: 1800 ms (40.21%), Space: 43.1 MB (39.98%) - LeetHub

---
+++ 0935-knight-dialer/0935-knight-dialer.py
@@ -0,0 +1,35 @@
+class Solution:
+    def knightDialer(self, n: int) -> int:
+        
+        dp = dict()
+        possible_jumps = [
+            [4, 6],
+            [6, 8],
+            [7, 9],
+            [4, 8],
+            [3, 9, 0],
+            [],
+            [1, 7, 0],
+            [2, 6],
+            [1, 3],
+            [2, 4]
+        ]
+
+        def rec(i, j):
+            if i == 0: 
+                return 1
+            if (i, j) in dp: 
+                return dp[(i, j)]
+
+            acc = 0
+            for pj in possible_jumps[j]:
+                acc += rec(i - 1, pj) % (10**9 + 7)
+
+            dp[(i, j)] = acc 
+            return acc
+
+        res = 0
+        for sq in range(10):
+            res += rec(n - 1, sq) % (10**9 + 7)
+
+        return res % (10**9 + 7)
