c02cd0ea8d5ea9bc51ef01c0251a1c04e77c51ec
Tyler Le <tylerle99@yahoo.com>
2023-11-30 21:21:09 -0800

    Time: 80 ms (54.14%), Space: 23.9 MB (31.64%) - LeetHub

--- 0124-binary-tree-maximum-path-sum/0124-binary-tree-maximum-path-sum.py
+++ 0124-binary-tree-maximum-path-sum/0124-binary-tree-maximum-path-sum.py
@@ -1,36 +1,33 @@
 # Definition for a binary tree node.
-# class TreeNode(object):
+# class TreeNode:
 #     def __init__(self, val=0, left=None, right=None):
 #         self.val = val
 #         self.left = left
 #         self.right = right
-class Solution(object):
-    def maxPathSum(self, root):
-        """
-        :type root: TreeNode
-        :rtype: int
-        """
-        def helper(root):
+class Solution:
+    def maxPathSum(self, root: Optional[TreeNode]) -> int:
+        
+        maxi = -float('inf')
+        
+        def rec(root):
+            nonlocal maxi
+            
+            # base case
             if not root: return 0
-            
-            # recursively find left and right
-            # if they are negative then we choose not to pick them
-            left, right = max(helper(root.left), 0), max(helper(root.right), 0)
-            
-            # update maxes
-            local_max = left + right + root.val 
-            self.global_max = max(self.global_max, local_max)
-            
-            # can only take left branch OR right branch, 
-            # but not both in next recursive call
-            # "A node can only appear in the sequence at most once"
-            return max(left, right) + root.val
         
-        self.global_max = -float('inf')
-        helper(root)
-        return self.global_max
+            # recurse
+            left = max(rec(root.left), 0)
+            right = max(rec(root.right), 0)
+            
+            # calculate result for current tree
+            maxi = max(maxi, left + right + root.val)
+            
+            return max(left+root.val, right+root.val)
+            
+                    
         
+        rec(root)
+        return maxi
         
             
             
-        
