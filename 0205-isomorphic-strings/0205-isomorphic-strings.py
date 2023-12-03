0af85f346d365884a90f87e0110bbd135dfda349
Tyler Le <tylerle99@yahoo.com>
2023-12-02 21:54:25 -0800

    Time: 55 ms (16.26%), Space: 16.6 MB (32.83%) - LeetHub

---
+++ 0205-isomorphic-strings/0205-isomorphic-strings.py
@@ -0,0 +1,22 @@
+class Solution:
+    def isIsomorphic(self, s: str, t: str) -> bool:
+        
+        if len(s) != len(t): return False
+        hmap_s_t = defaultdict(str)
+        hmap_t_s = defaultdict(str)
+        res = ""
+        
+        for s1, t1 in zip(s, t):
+            if (s1 not in hmap_s_t) and (t1 not in hmap_t_s):
+                hmap_s_t[s1] = t1
+                hmap_t_s[t1] = s1
+                
+            elif hmap_s_t[s1] != t1 or hmap_t_s[t1] != s1: 
+                return False
+        
+        for ch in s:
+            res+=hmap_s_t[ch]
+        
+        return res == t
+        
+        
