617b0824f67f40c32bf8066089b351a6f75e7439
Tyler Le <tylerle99@yahoo.com>
2023-12-15 21:27:24 -0800

    Time: 522 ms (59.98%), Space: 29.8 MB (21.21%) - LeetHub

--- 0219-contains-duplicate-ii/0219-contains-duplicate-ii.py
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii.py
@@ -4,8 +4,11 @@ class Solution:
         hmap = dict()
 
         for i, num in enumerate(nums):
-            if num in hmap and i - hmap[num] <= k: 
-                return True
+            # check if we found a pair
+            if num in hmap and i - hmap[num] <= k: return True
+            
+            # if (i - hmap[num] > k) then we need to replace 
+            # the value of hmap[num] with 'i'
             else: hmap[num] = i
             
         return False
