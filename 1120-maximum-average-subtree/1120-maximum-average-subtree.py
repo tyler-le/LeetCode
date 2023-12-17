ffe829f7b14bee3ac21714f3eb5cc437298d136a
Tyler Le <tylerle99@yahoo.com>
2023-12-17 13:27:33 -0800

    Time: 49 ms (82.47%), Space: 19.2 MB (83.96%) - LeetHub

---
+++ 1120-maximum-average-subtree/1120-maximum-average-subtree.py
@@ -0,0 +1,25 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def maximumAverageSubtree(self, root: Optional[TreeNode]) -> float:
+        res = 0
+        
+        def rec(node):
+            nonlocal res
+            if not node: return (0, 0)
+            
+            left_sum, left_size = rec(node.left)
+            right_sum, right_size = rec(node.right)
+            
+            sumi = node.val + left_sum + right_sum
+            size = 1 + left_size + right_size
+            res = max(res, sumi/size)
+            
+            return (sumi, size)
+        
+        rec(root)
+        return res
