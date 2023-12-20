c878e29602da05011505dc4855785848d4ba3c57
Tyler Le <tylerle99@yahoo.com>
2023-12-19 17:30:03 -0800

    Time: 144 ms (69.09%), Space: 18 MB (33.59%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.py
+++ 0283-move-zeroes/0283-move-zeroes.py
@@ -3,12 +3,10 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
+        ptr = 0
         
-        
-        n = len(nums)
-        l = 0
-        for r in range(n):
-            if nums[r] != 0:
-                nums[l], nums[r] = nums[r], nums[l]
-                l+=1
-                
+        for i in range(len(nums)):
+            if nums[i] != 0:
+                nums[ptr], nums[i] = nums[i], nums[ptr]
+                ptr+=1
+            
