4521c62d03f6ec9f475c4b694657cc9ea92b4542
Tyler Le <tylerle99@yahoo.com>
2023-12-20 21:14:37 -0800

    Time: 444 ms (99.70%), Space: 17.8 MB (6.26%) - LeetHub

--- 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.py
+++ 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.py
@@ -1,25 +1,18 @@
-class Solution(object):
-    def longestOnes(self, nums, k):
-        """
-        :type nums: List[int]
-        :type k: int
-        :rtype: int
-        """
+class Solution:
+    def longestOnes(self, nums: List[int], k: int) -> int:
+        flipped = 0
         res = 0
-        
-        # map bit to frequency
-        count = [0,0]
-        
         l = 0
         
-        for r in range (len(nums)):
-            count[nums[r]] += 1
+        for r in range(len(nums)):
+            if not nums[r]:
+                flipped+=1
             
-            num_replacements = (r-l+1) - count[1]
-            while num_replacements > k:
-                count[nums[l]] -= 1
+            while flipped > k:
+                if not nums[l]:
+                    flipped-=1
                 l+=1
-                num_replacements = (r-l+1) - count[1]    
+                
             res = max(res, r-l+1)
-            
+        
         return res
