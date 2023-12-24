d6c4c779cb80a4ab616cd170145b88da67217f03
Tyler Le <tylerle99@yahoo.com>
2023-12-23 23:08:24 -0800

    Time: 44 ms (88.66%), Space: 17.6 MB (5.67%) - LeetHub

--- 1758-minimum-changes-to-make-alternating-binary-string/1758-minimum-changes-to-make-alternating-binary-string.py
+++ 1758-minimum-changes-to-make-alternating-binary-string/1758-minimum-changes-to-make-alternating-binary-string.py
@@ -1,28 +1,17 @@
 class Solution:
     def minOperations(self, s: str) -> int:
-        res = float("inf")
+        c1 = c2 = 0
         
         # 0 1 0 1 ...
-        cnt = 0
         for i in range(len(s)):
-            if i % 2 == 0 and s[i] == "1":
-                cnt+=1
-            elif i % 2 == 1 and s[i] == "0":
-                cnt+=1
-                
-        print(cnt)    
-        res = min(cnt, res)
+            if i % 2 == 0 and s[i] == "1": c1+=1
+            elif i % 2 == 1 and s[i] == "0": c1+=1                
             
         # 1 0 1 0 ...
-        cnt = 0
         for i in range(len(s)):
-            if i % 2 == 0 and s[i] == "0":
-                cnt+=1
-            elif i % 2 == 1 and s[i] == "1":
-                cnt+=1
+            if i % 2 == 0 and s[i] == "0": c2+=1
+            elif i % 2 == 1 and s[i] == "1": c2+=1
                 
-        res = min(cnt, res)
-            
-        return res
-            
+        return min(c1, c2)
+                        
             
