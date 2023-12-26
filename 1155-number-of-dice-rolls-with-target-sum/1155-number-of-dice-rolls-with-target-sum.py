9b2c1bd242b8c4323a475aa27be3deca5f7c7e0a
Tyler Le <tylerle99@yahoo.com>
2023-12-26 13:57:51 -0800

    Time: 532 ms (28.31%), Space: 17.9 MB (48.69%) - LeetHub

--- 1155-number-of-dice-rolls-with-target-sum/1155-number-of-dice-rolls-with-target-sum.py
+++ 1155-number-of-dice-rolls-with-target-sum/1155-number-of-dice-rolls-with-target-sum.py
@@ -1,14 +1,21 @@
 class Solution:
     def numRollsToTarget(self, n: int, k: int, target: int) -> int:
-        dp = [[0 for _ in range(target + 1)] for _ in range(n + 1)]
         
-        for c in range(1, target + 1):
-            if c <= k: dp[1][c] = 1
+        # dp[i][j] = number of ways to roll i dice with target j
+        
+        # return dp[n][target]
+        
+        dp = [[0 for _ in range(target+1)] for _ in range(n+1)]        
+        dp[0][0] = 1
         
         for i in range(1, n+1):
             for j in range(1, target+1):
-                for d in range(1, k+1):
-                    if j - d >= 0: 
-                        dp[i][j] += dp[i-1][j - d]
-                        
-        return (dp[n][target]) % (10**9 + 7)
+                for x in range(1, k+1):
+                    if (j-x) >= 0: 
+                        dp[i][j] = (dp[i][j] + dp[i-1][j-x]) % (10**9 + 7)
+        
+        return dp[n][target]
+        
+    
+    
+    
