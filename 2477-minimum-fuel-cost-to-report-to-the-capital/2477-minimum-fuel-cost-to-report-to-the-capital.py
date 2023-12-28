ddd5eacd7814d48adae72c7a66ca202fcca2af5c
Tyler Le <tylerle99@yahoo.com>
2023-12-27 17:55:39 -0800

    Time: 3346 ms (5.06%), Space: 82 MB (93.39%) - LeetHub

---
+++ 2477-minimum-fuel-cost-to-report-to-the-capital/2477-minimum-fuel-cost-to-report-to-the-capital.py
@@ -0,0 +1,34 @@
+class Solution:
+    def minimumFuelCost(self, roads: List[List[int]], seats: int) -> int:
+        
+        # if 'i' people meet on the same node, 
+        # the number of cars needed is ceil (i / seats)
+        
+        # for each nbor of the capital, add the result of ceil(size / seats)
+        
+        def dfs(curr, people=1):
+            nonlocal res
+            if curr in visited: return 0
+            
+            visited.add(curr)
+            
+            for nbor in adjacency[curr]:
+                people+=dfs(nbor)
+            
+            if curr != capital:
+                res += ceil(people / seats)
+                
+            return people
+        
+        adjacency = collections.defaultdict(list)
+        visited = set()
+        res = 0
+        capital = 0
+        
+        # create adjacency matrix
+        for u, v in roads:
+            adjacency[u].append(v)
+            adjacency[v].append(u)
+        
+        dfs(capital)
+        return res
