773a85389d2cd7509081f66e8cdd8035ca5a766e
Tyler Le <tylerle99@yahoo.com>
2023-12-29 16:18:02 -0800

    Time: 343 ms (83.11%), Space: 30.8 MB (5.33%) - LeetHub

--- 1423-maximum-points-you-can-obtain-from-cards/1423-maximum-points-you-can-obtain-from-cards.py
+++ 1423-maximum-points-you-can-obtain-from-cards/1423-maximum-points-you-can-obtain-from-cards.py
@@ -1,24 +1,16 @@
-class Solution(object):
-    def maxScore(self, cardPoints, k):
-        """
-        :type cardPoints: List[int]
-        :type k: int
-        :rtype: int
-        """
-        total_points = sum(cardPoints)
-        window_size = len(cardPoints) - k
-        curr_sum = 0
-        l=0
-        res = 0
-        for r in range(len(cardPoints)):
-            curr_sum += cardPoints[r]
-            if r-l+1 > window_size:
-                curr_sum -= cardPoints[l]
+class Solution:
+    def maxScore(self, cardPoints: List[int], k: int) -> int:
+        l, curr_sum, res = 0, 0, -float("inf")
+        sumi, n = sum(cardPoints), len(cardPoints)
+        
+        for r in range(n):
+            curr_sum+=cardPoints[r]
+            
+            while (r - l + 1) > (n - k):
+                curr_sum-=cardPoints[l]
                 l+=1
                 
-            if r-l+1 == window_size:
-                res = max(total_points - curr_sum, res)
-                
-
+            if (r - l + 1) == (n - k):   
+                res = max(res, sumi - curr_sum)
+        
         return res
-            
