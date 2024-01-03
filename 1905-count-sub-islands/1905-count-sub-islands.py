45f3c26a042ad308a43452be7697df4eb1982f9d
Tyler Le <tylerle99@yahoo.com>
2024-01-02 22:11:35 -0800

    Time: 3247 ms (11.86%), Space: 28.3 MB (92.65%) - LeetHub

--- 1905-count-sub-islands/1905-count-sub-islands.py
+++ 1905-count-sub-islands/1905-count-sub-islands.py
@@ -7,31 +7,33 @@ class Solution:
         res = 0
         n, m = len(grid2), len(grid2[0])
         
+        # fill island with 0's
         def dfs(i, j):
+            if i < 0 or j < 0 or i >= n or j >= m: 
+                return
             
-            if i < 0 or j < 0 or i >= n or j >= m: return True
+            if grid2[i][j]:
+                
+                grid2[i][j] = 0
+                
+                dfs(i-1,j)
+                dfs(i+1,j)
+                dfs(i,j-1)
+                dfs(i,j+1)
             
-            if grid1[i][j] == 0 and grid2[i][j] == 0: return True
-            
-            if grid1[i][j] == 1 and grid2[i][j] == 0: return True
-            
-            if grid1[i][j] == 0 and grid2[i][j] == 1: return False
-            
-            grid2[i][j] = 0
-            
-            left = dfs(i, j-1)
-            right = dfs(i, j+1)
-            up = dfs(i-1, j)
-            down = dfs(i+1, j)
-            
-            return left and right and up and down
+
                 
             
         
         for i in range(n):
             for j in range(m):
-                if grid2[i][j] == 1: 
-                    if dfs(i, j): 
-                        res+=1
+                if not grid1[i][j] and grid2[i][j]: 
+                    dfs(i, j)
+        
+        for i in range(n):
+            for j in range(m):
+                if grid2[i][j]:
+                    dfs(i,j)
+                    res+=1
         
         return res
