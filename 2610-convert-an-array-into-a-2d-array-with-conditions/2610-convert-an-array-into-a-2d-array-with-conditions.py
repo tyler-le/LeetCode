766b9d048eaa035368d7737b1199781654aaed61
Tyler Le <tylerle99@yahoo.com>
2024-01-02 21:36:15 -0800

    Time: 128 ms (5.03%), Space: 17.2 MB (21.59%) - LeetHub

--- 2610-convert-an-array-into-a-2d-array-with-conditions/2610-convert-an-array-into-a-2d-array-with-conditions.py
+++ 2610-convert-an-array-into-a-2d-array-with-conditions/2610-convert-an-array-into-a-2d-array-with-conditions.py
@@ -1,19 +1,18 @@
 class Solution:
     def findMatrix(self, nums: List[int]) -> List[List[int]]:
         
-        # val 1 : freq 3
-        # val 2 : freq 1
-        # val 3 : freq 2
         
-        hmap = Counter(nums)
-        res = [[] for _ in range(max(hmap.values()))]
+        # 4 - 1
+        # 3 - 2 
+        # 2 - 1
+        # 1 - 3
         
-        for num, freq in hmap.items():
+        counts = Counter(nums)
+        res = [[] for _ in range(max(counts.values()))]
+        
+        for x, freq in counts.items():
             for i in range(freq):
-                res[i].append(num)
-                
+                res[i].append(x)        
+        
         return res
-        
-        
-        
-        
+    
