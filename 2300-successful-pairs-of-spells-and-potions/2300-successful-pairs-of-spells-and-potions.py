459d463b4d01db0566eabdbaf55358d2053620ae
Tyler Le <tylerle99@yahoo.com>
2024-01-04 13:23:46 -0800

    Time: 1255 ms (69.48%), Space: 40.3 MB (20.86%) - LeetHub

--- 2300-successful-pairs-of-spells-and-potions/2300-successful-pairs-of-spells-and-potions.py
+++ 2300-successful-pairs-of-spells-and-potions/2300-successful-pairs-of-spells-and-potions.py
@@ -1,26 +1,25 @@
-class Solution(object):
-    def successfulPairs(self, spells, potions, success):
-        """
-        :type spells: List[int]
-        :type potions: List[int]
-        :type success: int
-        :rtype: List[int]
-        """
-        out = []
+class Solution:
+    def successfulPairs(self, spells: List[int], potions: List[int], success: int) -> List[int]:
+        # for each spell, binary search to find the minimum value that is successful
+        res = []
         potions.sort()
-        m = len(potions)
+        
+        def binary_search(spell):
+            low, high = 0, len(potions) - 1
+            
+            while low <= high:
+                mid = low + ((high - low) // 2)
+                if spell * potions[mid] >= success: high = mid - 1
+                else: low = mid + 1
+            
+            return high
+        
         
         for spell in spells:
-            low, high = 0, m
+            res.append(len(potions) - binary_search(spell) - 1)
             
-            while low < high:
-                mid = (high + low) // 2
-                if potions[mid] * spell < success:
-                    low = mid + 1
-                elif potions[mid] * spell >= success:
-                    high = mid
-                    
-            
-            out.append(m - low)
-            
-        return out
+        
+        return res
+    
+    
+    
