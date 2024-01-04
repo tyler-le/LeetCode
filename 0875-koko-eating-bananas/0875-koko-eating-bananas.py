e5b3d4ad346ee43f799e9635f3ec23e6989dad51
Tyler Le <tylerle99@yahoo.com>
2024-01-04 14:01:52 -0800

    Time: 255 ms (97.17%), Space: 18.9 MB (8.26%) - LeetHub

--- 0875-koko-eating-bananas/0875-koko-eating-bananas.py
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.py
@@ -1,15 +1,26 @@
 class Solution:
     def minEatingSpeed(self, piles: List[int], h: int) -> int:
+        
+        # binary search on the eating speed k
+        
         low, high = 1, max(piles)
-
-        while low < high:
-            mid = low + (high - low) // 2
-            total_hours = 0
+        
+        while low < high: 
             
+            mid = low + ((high - low) // 2)
+            cnt = 0
+            
+            # can we eat all piles in 'h' hours with eating speed 'k' ?
             for pile in piles:
-                total_hours += math.ceil(pile / mid)
+                cnt+=ceil(pile / mid)
+            
+            
+            # decrease the eating speed
+            if cnt <= h: high = mid 
                 
-            if total_hours <= h: high = mid
+            # increase the eating speed
             else: low = mid + 1
                 
         return low
+        
+ 
