4b77bb4304c5f9127ff802ea5128fab85582c13c
Tyler Le <tylerle99@yahoo.com>
2024-01-05 19:19:08 -0800

    Time: 41 ms (44.57%), Space: 17.4 MB (13.92%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.py
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.py
@@ -1,37 +1,30 @@
 class Solution:
     def letterCombinations(self, digits: str) -> List[str]:
-        hmap = collections.defaultdict(list)
-        hmap["2"] = ["a", "b", "c"]
-        hmap["3"] = ["d", "e", "f"]
-        hmap["4"] = ["g", "h", "i"]
-        hmap["5"] = ["j", "k", "l"]
-        hmap["6"] = ["m", "n", "o"]
-        hmap["7"] = ["p", "q", "r", "s"]
-        hmap["8"] = ["t", "u", "v"]
-        hmap["9"] = ["w", "x", "y", "z"]
-        
-        n = len(digits)
+        hmap = {
+            "2" : ["a", "b", "c"],
+            "3" : ["d", "e", "f"],
+            "4" : ["g", "h", "i"],
+            "5" : ["j", "k", "l"],
+            "6" : ["m", "n", "o"],
+            "7" : ["p", "q", "r", "s"],
+            "8" : ["t", "u", "v"],
+            "9" : ["w", "x", "y", "z"]
+        }
         res = []
+        if not digits: return res
         
-        def backtrack(i, perm):
-            
-            if not n: 
-                return []
-            
-            if len(perm) == n:
-                res.append("".join(perm))
+        def backtrack(idx, curr):
+            if idx >= len(digits): 
+                res.append("".join(curr))
                 return
+
+            for x in hmap[digits[idx]]:
+                curr.append(x)
+                backtrack(idx+1, curr)
+                curr.pop()
             
-            candidates = hmap[digits[i]]
             
-            for cand in candidates:
-                perm.append(cand)
-                backtrack(i+1, perm)
-                perm.pop()
             
         backtrack(0, [])
-        
         return res
-            
-            
             
