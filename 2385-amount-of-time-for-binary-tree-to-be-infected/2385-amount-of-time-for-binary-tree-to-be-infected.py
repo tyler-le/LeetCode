6e68b5b9e08abe8bd8dac0e264cc9118631dfa18
Tyler Le <tylerle99@yahoo.com>
2024-01-09 18:46:08 -0800

    Time: 373 ms (93.30%), Space: 63.1 MB (77.28%) - LeetHub

---
+++ 2385-amount-of-time-for-binary-tree-to-be-infected/2385-amount-of-time-for-binary-tree-to-be-infected.py
@@ -0,0 +1,45 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def amountOfTime(self, root: Optional[TreeNode], start: int) -> int:
+        graph = defaultdict(list)
+        
+        def dfs(node):
+
+            if node.left: 
+                graph[node.val].append(node.left.val)
+                graph[node.left.val].append(node.val)
+                dfs(node.left)
+                
+            if node.right:
+                graph[node.val].append(node.right.val)
+                graph[node.right.val].append(node.val)
+                dfs(node.right)
+                
+        
+        dfs(root)
+        q = deque([start])
+        res = 0
+        visited = set([start])
+        
+        while q:
+
+            level_size = len(q)
+            
+            for _ in range(level_size):
+                popped = q.popleft()
+                
+                for nbor in graph[popped]:
+                    if nbor not in visited:
+                        q.append(nbor)
+                        visited.add(nbor)
+            res+=1
+
+                    
+            
+            
+        return res - 1
