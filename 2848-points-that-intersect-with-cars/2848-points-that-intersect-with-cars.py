5681db2a81d25e48434a87fc146cd437da23264c
Tyler Le <tylerle99@yahoo.com>
2024-01-09 20:28:41 -0800

    Time: 62 ms (37.61%), Space: 13.4 MB (17.95%) - LeetHub

--- 2848-points-that-intersect-with-cars/2848-points-that-intersect-with-cars.py
+++ 2848-points-that-intersect-with-cars/2848-points-that-intersect-with-cars.py
@@ -1,17 +1,13 @@
-class Solution:
-    def numberOfPoints(self, nums: List[List[int]]) -> int:
-        mini, maxi = float("inf"), -float("inf")
-        
-        for start, end in nums:
-            mini = min(mini, start)
-            maxi = max(maxi, end)
-            
-        arr = [0 for _ in range(101)]
+class Solution(object):
+    def numberOfPoints(self, nums):
+        """
+        :type nums: List[List[int]]
+        :rtype: int
+        """
+        seen = set()
         
         for start, end in nums:
             for i in range(start, end+1):
-                arr[i] = 1
-        
-        return sum(arr)
-            
+                seen.add(i)
+        return len(seen)
         
