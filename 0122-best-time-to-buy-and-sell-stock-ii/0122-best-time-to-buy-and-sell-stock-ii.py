c2924ddbd8dfd4f5896fabf9a110fecf842049d6
Tyler Le <tylerle99@yahoo.com>
2024-01-14 17:56:41 -0800

    Time: 59 ms (76.58%), Space: 18.3 MB (34.01%) - LeetHub

---
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.py
@@ -0,0 +1,16 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        # greedily buy low and sell whenever there is a chance for profit
+        # [7,1,5,3,4,6] --> 4 + 1 + 2 
+        
+        buy = prices[0]
+        res = 0
+        
+        for sell in prices[1:]:
+            if sell < buy:
+                buy = sell
+            else:
+                res+=(sell-buy)
+                buy = sell
+        return res
+            
