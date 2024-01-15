6cda1b4acd880d14d3d1dfb91b72e5c9086fd6d3
Tyler Le <tylerle99@yahoo.com>
2024-01-14 19:15:25 -0800

    Time: 41 ms (36.23%), Space: 17.4 MB (16.90%) - LeetHub

--- 0228-summary-ranges/0228-summary-ranges.py
+++ 0228-summary-ranges/0228-summary-ranges.py
@@ -1,28 +1,23 @@
 class Solution:
     def summaryRanges(self, nums: List[int]) -> List[str]:
-        if not nums: return []
-        l = 0
-        n = len(nums)
-        interval = [nums[0], nums[0]]
-        res = []
         
-        for r in range(1, n):
-            if nums[r] == interval[1] + 1:
-                interval[1] = nums[r]
+        if not nums: return []
+        n = len(nums)
+        interval, res = [nums[0], nums[0]], []
+        
+        for i in range(1, n):
+            if nums[i] == interval[1] + 1:
+                interval[1] = nums[i]
             else:
-                if interval[0] != interval[1]:
+                if interval[0] != interval[1]: 
                     res.append(f"{interval[0]}->{interval[1]}")
-                
-                else:
+                else: 
                     res.append(str(interval[0]))
                 
-                interval = [nums[r], nums[r]]
+                interval = [nums[i], nums[i]]
                 
-        if interval[0] != interval[1]:
-            res.append(f"{interval[0]}->{interval[1]}")
-                
-        else:
-            res.append(str(interval[0]))
+        if interval[0] != interval[1]: res.append(f"{interval[0]}->{interval[1]}")  
+        else: res.append(str(interval[0]))
 
         return res
                 
