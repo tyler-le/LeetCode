ae2633585e829b1793ff5c48b44c4279e996ece8
Tyler Le <tylerle99@yahoo.com>
2024-01-15 19:48:39 -0800

    Time: 247 ms (97.20%), Space: 64.2 MB (43.69%) - LeetHub

--- 0380-insert-delete-getrandom-o1/0380-insert-delete-getrandom-o1.py
+++ 0380-insert-delete-getrandom-o1/0380-insert-delete-getrandom-o1.py
@@ -1,25 +1,40 @@
 class RandomizedSet:
 
     def __init__(self):
-        self.hmap = defaultdict(int)
+        self.hmap = {} # map num : index in array
+        self.list = []
         
 
     def insert(self, val: int) -> bool:
-        res = False
-        if val not in self.hmap: res = True
-        self.hmap[val]+=1
-        return res
-        
+        if val not in self.hmap:
+            self.list.append(val)
+            self.hmap[val] = len(self.list) - 1
+            return True
+        return False
 
     def remove(self, val: int) -> bool:
-        res = False
         if val in self.hmap:
-            res = True
+            # Index of the element to be removed
+            idx = self.hmap[val]
+
+            # Swap the element with the last one in the list
+            self.list[idx], self.list[-1] = self.list[-1], self.list[idx]
+
+            # Update the index of the swapped element in the hashmap
+            self.hmap[self.list[idx]] = idx
+
+            # Remove the last element from the list and hashmap
+            self.list.pop()
             del self.hmap[val]
-        return res
+
+            return True
+
+        return False
+
+            
 
     def getRandom(self) -> int:
-        return random.choice(list(self.hmap.keys()))
+        return random.choice(self.list)
         
 
 
