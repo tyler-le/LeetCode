d9a2ea03fe639656b99cc0a0ea45b4a6b4dd2257
Tyler Le <tylerle99@yahoo.com>
2024-01-17 21:37:20 -0800

    Time: 38 ms (71.91%), Space: 17.5 MB (50.62%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.py
+++ 0022-generate-parentheses/0022-generate-parentheses.py
@@ -1,19 +1,22 @@
 class Solution:
     def generateParenthesis(self, n: int) -> List[str]:
         
-        res = []
         
-        def backtrack(num_open, num_closed, comb):
+        def backtrack(num_open, num_closed, curr):
 
-            if num_open == n and num_closed == n:
-                res.append(comb)
+            if num_closed > num_open: 
+                return
+        
+            if num_open > n or num_closed > n:
                 return
             
-            if num_closed > num_open: return
-            if num_open > n or num_closed > n: return
+            if num_open == n and num_closed == n:
+                res.append(curr)
+                return
             
-            backtrack(num_open + 1, num_closed, comb + "(")
-            backtrack(num_open, num_closed + 1, comb + ")")
+            backtrack(num_open + 1, num_closed, curr + "(")
+            backtrack(num_open, num_closed + 1, curr + ")")
             
+        res = []
         backtrack(0, 0, "")
         return res
