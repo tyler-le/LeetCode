a31d1b092713648ab17ba319597b987d632e4f0b
Tyler Le <tylerle99@yahoo.com>
2024-01-17 18:12:45 -0800

    Time: 41 ms (68.08%), Space: 17.4 MB (47.58%) - LeetHub

--- 0090-subsets-ii/0090-subsets-ii.py
+++ 0090-subsets-ii/0090-subsets-ii.py
@@ -6,12 +6,19 @@ class Solution:
                 res.append(path.copy())
                 return
             
-            backtrack(idx+1, path+[nums[idx]])
+            # dfs on future elements with nums[idx] anchored
+            backtrack(idx+1, path+[nums[idx]]) 
+            
+            # skip duplicates from this anchored position 
+            # since we already explored all possibilities with nums[idx]
+            # that are anchored here
             while idx + 1 < len(nums) and nums[idx] == nums[idx+1]:
                 idx+=1
+                
+            # dfs on future elements without nums[idx] anchored
             backtrack(idx+1, path)
         
         res = []
-        nums.sort()
+        nums.sort() # to avoid dupes
         backtrack(0, [])
         return res
