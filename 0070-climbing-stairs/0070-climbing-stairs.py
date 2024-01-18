262e3cb6f51901d000ddc5be6f70b0af88f8ac82
Tyler Le <tylerle99@yahoo.com>
2024-01-17 18:02:04 -0800

    Time: 34 ms (75.77%), Space: 17.3 MB (15.77%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.py
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -1,15 +1,9 @@
-class Solution(object):
-    def climbStairs(self, n):
-        """
-        :type n: int
-        :rtype: int
-        """
-        
-        memo = [0] * (n+1)
-        memo[0] = memo[1] = 1
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        dp = [0] * (n+1)
+        dp[0] = dp[1] = 1
         
         for i in range(2, n+1):
-            memo[i] = memo[i-1] + memo[i-2]
-        
-        return memo[-1]
-        
+            dp[i] = dp[i-1] + dp[i-2]
+            
+        return dp[-1]
