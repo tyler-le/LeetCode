1497b72d7772acf5f6c889214589fe77b12b4c48
Tyler Le <tylerle99@yahoo.com>
2024-01-17 17:47:39 -0800

    Time: 82 ms (35.29%), Space: 17.5 MB (6.70%) - LeetHub

--- 0040-combination-sum-ii/0040-combination-sum-ii.py
+++ 0040-combination-sum-ii/0040-combination-sum-ii.py
@@ -3,19 +3,28 @@ class Solution:
         
         
         def backtrack(idx, path):
+            
+            # we found a valid combination 
             if sum(path) == target: 
                 res.append(path.copy())
                 return
             
+            # we added too much to the combination
             if sum(path) > target:
                 return
             
+            # we have no more items to consider
             if idx >= len(candidates):
                 return
             
+            # dfs on all future elements and include candidates[idx]
             backtrack(idx+1, path + [candidates[idx]])
+            
+            # after we've considered candidates[idx] at this position, we need to skip over all identical candidates
             while idx + 1 < len(candidates) and candidates[idx] == candidates[idx+1]:
                 idx+=1
+            
+            # dfs on all future elements and don't include candidates[idx]
             backtrack(idx+1, path)
         
         res = []
