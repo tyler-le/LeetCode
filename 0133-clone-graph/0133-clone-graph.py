128c33a252f5390edf86585e294e35da9637b6dc
Tyler Le <tylerle99@yahoo.com>
2024-01-18 16:32:23 -0800

    Time: 32 ms (97.47%), Space: 17.9 MB (7.21%) - LeetHub

--- 0133-clone-graph/0133-clone-graph.py
+++ 0133-clone-graph/0133-clone-graph.py
@@ -1,29 +1,28 @@
 """
 # Definition for a Node.
-class Node(object):
+class Node:
     def __init__(self, val = 0, neighbors = None):
         self.val = val
         self.neighbors = neighbors if neighbors is not None else []
 """
 
-class Solution(object):
-    def cloneGraph(self, node):
-        """
-        :type node: Node
-        :rtype: Node
-        """
-        hmap = dict()
+from typing import Optional
+class Solution:
+    def cloneGraph(self, node: Optional['Node']) -> Optional['Node']:
+        # map node : copy
+        hmap = {}
         
-        if not node: return None
-        
-        def dfs(node):
-            hmap[node] = Node(node.val, [])
-            for nbor in node.neighbors:
-                if nbor not in hmap:
-                    dfs(nbor)
-                hmap[node].neighbors.append(hmap[nbor])
-
-        dfs(node)
-        return hmap[node]
-    
-    
+        def dfs(curr):
+            if not curr: return None
+            if curr in hmap: return hmap[curr]
+            
+            copy = Node(curr.val)
+            hmap[curr] = copy
+            nbors = []
+            
+            for nbor in curr.neighbors:
+                copy.neighbors.append(dfs(nbor))
+            
+            return copy
+            
+        return dfs(node)
