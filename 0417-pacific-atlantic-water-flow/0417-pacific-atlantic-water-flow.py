0752879bcb06ba2a65fd90153305fab53b9b248f
Tyler Le <tylerle99@yahoo.com>
2024-01-18 18:18:47 -0800

    Time: 210 ms (88.96%), Space: 18.9 MB (48.81%) - LeetHub

--- 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.py
+++ 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.py
@@ -1,39 +1,28 @@
 class Solution:
     def pacificAtlantic(self, heights: List[List[int]]) -> List[List[int]]:
-        pac, atl = set(), set()
+        pacific = set()
+        atlantic = set()
+        
+        # start from the edges of pacific and atlantic
+        # dfs, visiting nodes that are strictly greater and add to set
+        
+        def dfs(i, j, ocean_set):
+            if (i, j) in ocean_set: return
+            
+            ocean_set.add((i,j))
+            
+            for dx, dy in [(-1,0), (1,0), (0,-1), (0,1)]:
+                if i+dx < 0 or j+dy < 0 or i+dx >= n or j+dy >= m: continue
+                if heights[i][j] <= heights[i+dx][j+dy]:
+                    dfs(i+dx, j+dy, ocean_set)
+            
+        
+        
         n, m = len(heights), len(heights[0])
         
-        def dfs(r, c, s, prev):
-            if r < 0 or c < 0 or r >= n or c >= m:
-                return
-            
-            if (r,c) in s:
-                return
-            
-            if prev > heights[r][c]:
-                return
-            
-            s.add((r,c))
-            
-            dfs(r+1,c,s, heights[r][c])
-            dfs(r-1,c,s, heights[r][c])
-            dfs(r,c+1,s, heights[r][c])
-            dfs(r,c-1,s, heights[r][c])
-            
-        
-        # gather all nodes reachable from pacific
-        for row in range(n):
-            dfs(row, 0, pac, heights[row][0])
-        for col in range(m):
-            dfs(0, col, pac, heights[0][col])
-            
-        # gather all nodes reachable from atlantic
-        for row in range(n):
-            dfs(row, m-1, atl, heights[row][m-1])
-        for col in range(m):
-            dfs(n-1, col, atl, heights[n-1][col])
-            
-        return pac.intersection(atl)
-    
-        
-        
+        for i in range(n):
+            for j in range(m):            
+                if j == 0 or i == 0: dfs(i, j, pacific)
+                if i == n-1 or j == m-1: dfs(i, j, atlantic)
+                
+        return pacific.intersection(atlantic)
