cd5c01a5a8dd6bd27b39105d78e37bd6fac31496
Tyler Le <tylerle99@yahoo.com>
2024-01-18 17:47:17 -0800

    Time: 127 ms (68.70%), Space: 18.4 MB (26.78%) - LeetHub

--- 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.py
+++ 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.py
@@ -1,27 +1,21 @@
 class Solution:
     def minFallingPathSum(self, matrix: List[List[int]]) -> int:
+        # dp[i][j] = min falling path at matrix[i][j]
+        # dp[i][j] = matrix[i][j] + min(dp[i-1][j-1], dp[i-1][j], dp[i-1][j+1])
+        # return min(dp[n-1][col]) for all col
+        
         n = len(matrix)
-        dp = [[0 for _ in range(n)] for _ in range(n)]
         
-        # Initialize the first row
-        dp[0] = matrix[0]
+        dp = [[float("inf") for _ in range(n)] for _ in range(n)]  
         
-        for r in range(1, n):
-            for c in range(n):
-                
-                # up and up-right
-                if c == 0:
-                    up, up_right = dp[r-1][c], dp[r-1][c+1]
-                    dp[r][c] = matrix[r][c] + min(up, up_right)
-                
-                # up and up-left
-                elif c == (n-1):
-                    up, up_left = dp[r-1][c], dp[r-1][c-1]
-                    dp[r][c] = matrix[r][c] + min(up, up_left)
-                
-                # up-left and up and up-right
-                else:
-                    up_left, up, up_right = dp[r-1][c-1], dp[r-1][c], dp[r-1][c+1]
-                    dp[r][c] = matrix[r][c] + min(up_left, up, up_right)
-                    
-        return min(dp[-1])
+        for col in range(n):
+            dp[0][col] = matrix[0][col]
+        
+        for i in range(1,n):
+            for j in range(n):
+                first = dp[i-1][j-1] if j-1 >= 0 else float("inf")
+                second = dp[i-1][j] 
+                third = dp[i-1][j+1] if j+1 < n else float("inf")
+                dp[i][j] = matrix[i][j] + min(first, second, third)
+        print(dp)       
+        return min(dp[n-1])
