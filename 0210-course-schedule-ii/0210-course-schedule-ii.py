43b504b5db3f6026e77b8d8c861633f7cd809fef
Tyler Le <tylerle99@yahoo.com>
2024-01-19 18:00:20 -0800

    Time: 82 ms (97.80%), Space: 18.4 MB (77.24%) - LeetHub

---
+++ 0210-course-schedule-ii/0210-course-schedule-ii.py
@@ -0,0 +1,32 @@
+class Solution:
+    def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
+        res = []
+        graph = defaultdict(list)
+        indegree = defaultdict(int)
+        q = deque()
+        num_visited = 0
+        
+        
+        # build graph
+        for u, v in prerequisites:
+            graph[v].append(u)
+            if v not in indegree: indegree[v] = 0
+            if u not in indegree: indegree[u] = 0
+            indegree[u]+=1
+            
+        # get all courses with indegree 0
+        for i in range (numCourses):
+            if not indegree[i]: 
+                q.append(i)
+                
+        while q:
+            popped = q.popleft()
+            res.append(popped)
+            for crs in graph[popped]:
+                indegree[crs]-=1
+                if not indegree[crs]:
+                    q.append(crs)
+
+        return res if len(res) == numCourses else []
+            
+        
