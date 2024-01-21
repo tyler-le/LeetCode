42e3b7b2f8d75e3f6f61da04da14a7c02c8728f4
Tyler Le <tylerle99@yahoo.com>
2024-01-20 21:04:14 -0800

    Time: 427 ms (80.49%), Space: 30.4 MB (95.73%) - LeetHub

--- 2214-minimum-health-to-beat-game/2214-minimum-health-to-beat-game.py
+++ 2214-minimum-health-to-beat-game/2214-minimum-health-to-beat-game.py
@@ -1,13 +1,9 @@
 class Solution:
     def minimumHealth(self, damage: List[int], armor: int) -> int:
-        max_damage = max(damage)
+        sum_damage = sum(damage)
+        max_dmg = max(damage)
         
-        # get all occurrences where we WONT use armor
-        no_armor = sum(damage) - max_damage
-        
-        # get the occurrence where we DO use armor
-        # return the max of max_damage - armor or 0 (if armor > max_damage)
-        yes_armor = max(max_damage - armor, 0)
+        no_armor = sum_damage - max_dmg
+        yes_armor = max(max_dmg - armor, 0)
         
         return no_armor + yes_armor + 1
-    
