820fdbccc5bcbddf6f056cd06b93f0ca16050eaf
Tyler Le <tylerle99@yahoo.com>
2024-01-22 19:04:49 -0800

    Time: 51 ms (74.52%), Space: 16.7 MB (55.55%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.py
+++ 0994-rotting-oranges/0994-rotting-oranges.py
@@ -1,30 +1,38 @@
 class Solution:
     def orangesRotting(self, grid: List[List[int]]) -> int:
+        
+        # init vars
+        n, m, q = len(grid), len(grid[0]), deque()
+        num_fresh, res = 0, 0
         EMPTY, FRESH, ROTTEN = 0, 1, 2
-        n, m = len(grid), len(grid[0])
-        q = deque()
-        res, num_fresh = 0, 0
+        
         
         for i in range(n):
             for j in range(m):
-                if grid[i][j] == ROTTEN: q.append((i,j))
+                # get total number of oranges
                 if grid[i][j] == FRESH: num_fresh+=1
-                    
+                # get all oranges that are initially rotten
+                if grid[i][j] == ROTTEN: q.append((i,j))
+        
+        
+        
+        # run multisource bfs 
         while q and num_fresh:
-            res+=1 
             level_size = len(q)
-            
             for _ in range(level_size):
-                x, y = q.popleft()
-                
+                x,y = q.popleft()
+
                 for dx, dy in [(-1,0), (1,0), (0,-1), (0,1)]:
-                    r, c = x + dx, y + dy
-                    
-                    if r < 0 or c < 0 or r >= n or c >= m or grid[r][c] != FRESH: continue
-                    
+                    r, c = x+dx, y+dy
+                    if r < 0 or c < 0 or r >= n or c >= m: continue
+                    if grid[r][c] != FRESH: continue
                     grid[r][c] = ROTTEN
                     num_fresh-=1
                     q.append((r,c))
-            
-            
+            res+=1
+
         return res if not num_fresh else -1
+                
+            
+        
+        
