09144c2e104a7e7a40f0bea813ac15e1c923af0b
Tyler Le <tylerle99@yahoo.com>
2024-01-31 19:38:25 -0800

    Time: 145 ms (99.25%), Space: 18.5 MB (63.71%) - LeetHub

--- 0846-hand-of-straights/0846-hand-of-straights.py
+++ 0846-hand-of-straights/0846-hand-of-straights.py
@@ -10,17 +10,18 @@ class Solution:
     
         
         for num in hand:
+            # Skip this iteration if the current card count is zero 
+            # since it has previously been allocated
             if not hmap[num]: continue
         
+            # Attempt to form a group starting from the current card
             for i in range(num, num+groupSize):
+                 
+                # Required card for the group is missing or depleted
                 if i not in hmap or not hmap[i]: return False
+                
+                # Else use the code
                 else: hmap[i]-=1
         
         return True
-            
-            
-                    
-        
-            
-            
             
