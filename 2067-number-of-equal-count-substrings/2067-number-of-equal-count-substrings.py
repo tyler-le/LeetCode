9ee25c83bc7806a8d7c522084c94165385f92ab4
Tyler Le <tylerle99@yahoo.com>
2024-02-03 14:44:18 -0800

    Time: 1846 ms (9.76%), Space: 16.7 MB (75.61%) - LeetHub

---
+++ 2067-number-of-equal-count-substrings/2067-number-of-equal-count-substrings.py
@@ -0,0 +1,27 @@
+class Solution:
+    def equalCountSubstrings(self, s: str, count: int) -> int:
+        
+        # sliding window of 1*count, 2*count, ..., 26*count
+        def sliding_window(width):
+            l = 0 
+            ans = 0
+            n = len(s)
+            hmap = defaultdict(int)
+            
+            for r in range(n):
+                hmap[s[r]]+=1
+                if r-l+1 > width:
+                    hmap[s[l]]-=1
+                    if not hmap[s[l]]: del hmap[s[l]]
+                    l+=1
+                    
+                if r-l+1 == width and all(value == count for value in hmap.values()):
+                    ans+=1
+                    
+            return ans
+            
+        res = 0
+        for i in range(1, 27):
+            res+=sliding_window(i*count) 
+        return res
+            
