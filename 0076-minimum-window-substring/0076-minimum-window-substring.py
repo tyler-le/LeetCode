c391458ab50c2bdc18cd80bfe7771df71da1eec5
Tyler Le <tylerle99@yahoo.com>
2024-02-03 22:18:24 -0800

    Time: 91 ms (79.05%), Space: 17.4 MB (55.90%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.py
+++ 0076-minimum-window-substring/0076-minimum-window-substring.py
@@ -1,22 +1,27 @@
 class Solution:
     def minWindow(self, s: str, t: str) -> str:
-        t_cnt, s_cnt = Counter(t), defaultdict(int)
-        conditions_met, required = 0, len(t_cnt.items())
+        s_cnt, t_cnt = defaultdict(int), Counter(t)
+        required = len(t_cnt)
+        conditions_met = 0
         l = 0
         res = [float("inf"), -1, -1]
         
         for r in range(len(s)):
+            
             s_cnt[s[r]]+=1
+            
             if s_cnt[s[r]] == t_cnt[s[r]]:
                 conditions_met+=1
             
-            while l <= r and conditions_met == required:
-                if r - l + 1 < res[0]: res = [r-l+1, l, r]
+            while conditions_met == required:
+                if r-l+1 < res[0]:
+                    res = [r-l+1, l, r]
                 s_cnt[s[l]]-=1
-                if s_cnt[s[l]] < t_cnt[s[l]]: conditions_met-=1    
-                l+=1
-        print(res)
-        return "" if res[0] == float("inf") else s[res[1]:res[2]+1]
-            
-            
                 
+                if s[l] in t_cnt and s_cnt[s[l]] < t_cnt[s[l]]:
+                    conditions_met-=1
+                l+=1
+                    
+        left, right = res[1], res[2]+1
+        return s[left:right]
+            
