55ae193450101f16e244283798cbf114b6f56b49
Tyler Le <tylerle99@yahoo.com>
2024-02-04 22:43:21 -0800

    Time: 119 ms (40.87%), Space: 16.6 MB (68.32%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
@@ -1,19 +1,20 @@
 class Solution:
     def characterReplacement(self, s: str, k: int) -> int:
-        l, res = 0, 0
-        hmap = collections.defaultdict(int)
+        num_replacements_left = k
+        l = 0
+        res = 0
+        n = len(s)
+        hmap = defaultdict(int)
         
-        for r in range(len(s)):
+        for r in range(n):
             hmap[s[r]]+=1
-            most_freq = max(hmap.values())
             
-            while ((r-l+1) - most_freq) > k:
-                # contract window
+            while (r-l+1 - max(hmap.values())) > k:
                 hmap[s[l]]-=1
                 l+=1
             
-            # once window is fully contracted, update res
             res = max(res, r-l+1)
             
-        return res
         
+        return res
+            
