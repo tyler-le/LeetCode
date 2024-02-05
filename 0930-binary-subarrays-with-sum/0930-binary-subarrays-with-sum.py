080d06b51b00345e6339268671cb0efa34e04265
Tyler Le <tylerle99@yahoo.com>
2024-02-05 14:17:26 -0800

    Time: 222 ms (68.70%), Space: 17.1 MB (99.90%) - LeetHub

--- 0930-binary-subarrays-with-sum/0930-binary-subarrays-with-sum.py
+++ 0930-binary-subarrays-with-sum/0930-binary-subarrays-with-sum.py
@@ -12,12 +12,11 @@ class Solution:
                 while curr_sum > k:
                     curr_sum-=nums[l]
                     l+=1
-                
-                res+=(r-l+1)
                     
+                res+=(r-l+1) # number of subarrays ending at nums[r]
                     
             return res
                 
-        
+        # (num subarrays with sum <= k) - (num subarrays with sum <= k-1) = (num subarrays with sum k)
         return at_most(goal) - at_most(goal-1)
             
