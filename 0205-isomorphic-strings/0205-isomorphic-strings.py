e69ae3ed6071fabda65831ef9b77d9338c2638e9
Tyler Le <tylerle99@yahoo.com>
2024-02-08 16:44:22 -0800

    Time: 29 ms (99.11%), Space: 16.8 MB (76.12%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.py
+++ 0205-isomorphic-strings/0205-isomorphic-strings.py
@@ -1,23 +1,15 @@
 class Solution:
     def isIsomorphic(self, s: str, t: str) -> bool:
-        
-        if len(s) != len(t): return False
         hmap_s_t = defaultdict(str)
         hmap_t_s = defaultdict(str)
         
-        for s1, t1 in zip(s, t):
-            # check if the mapping for s1 -> t1 is invalid
-            if s1 in hmap_s_t and hmap_s_t[s1] != t1: 
-                return False
-            
-            # check if the mapping for t1 -> s1 is invalid
-            if t1 in hmap_t_s and hmap_t_s[t1] != s1: 
-                return False
-            
-            # add it to both 
-            hmap_s_t[s1] = t1
-            hmap_t_s[t1] = s1
+        for s1, t1 in zip(s,t):
+            if s1 not in hmap_s_t and t1 not in hmap_t_s:
+                hmap_s_t[s1] = t1
+                hmap_t_s[t1] = s1
                 
+            else:
+                if hmap_s_t[s1] != t1 or hmap_t_s[t1] != s1:
+                    return False
+        
         return True
-        
-        
