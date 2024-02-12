436f92f926c642e93c059e1526e764cd277cbd7b
Tyler Le <tylerle99@yahoo.com>
2024-02-11 17:34:07 -0800

    Time: 131 ms (80.76%), Space: 18.1 MB (50.27%) - LeetHub

--- 0169-majority-element/0169-majority-element.py
+++ 0169-majority-element/0169-majority-element.py
@@ -1,9 +1,8 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        counts = defaultdict(int)
+        count = Counter(nums)
         n = len(nums)
+        
         for num in nums:
-            counts[num]+=1
-            if counts[num] >= ceil(n / 2):
+            if count[num] > floor(n / 2): 
                 return num
-            
