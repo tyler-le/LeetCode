6d36f40c082ad53eb77125befeefb6c9d018b5bf
Tyler Le <tylerle99@yahoo.com>
2024-02-12 12:15:13 -0800

    Time: 27 ms (98.41%), Space: 16.6 MB (83.74%) - LeetHub

--- 0767-reorganize-string/0767-reorganize-string.py
+++ 0767-reorganize-string/0767-reorganize-string.py
@@ -1,22 +1,20 @@
 class Solution:
     def reorganizeString(self, s: str) -> str:
-        
-        res = []
         cnt = Counter(s)
-        prev = []
-        
-        max_heap = [(-freq, ch) for ch,freq in cnt.items()]
+        max_heap = [(-freq, ch) for ch, freq in cnt.items()]
         heapify(max_heap)
+        res = []
+        prev_freq, prev_ch = 0, ""
         
         while max_heap:
-            curr_freq, curr_ch = heappop(max_heap)
-            res.append(curr_ch)
+            popped_freq, popped_ch = heappop(max_heap)
+            popped_freq +=1
             
-            if prev and prev[0] < 0: heappush(max_heap, tuple(prev))
+            if res and res[-1] == popped_ch: return ""
             
-            prev = [curr_freq + 1, curr_ch]
+            res.append(popped_ch)
+            if prev_freq != 0: heappush(max_heap, (prev_freq, prev_ch))
             
-        return "".join(res) if len(res) == len(s) else ""
-            
-            
+            prev_freq, prev_ch = popped_freq, popped_ch
         
+        return "".join(res) if len(res) == len(s) else ""
