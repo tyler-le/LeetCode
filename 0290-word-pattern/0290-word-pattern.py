8eeef7c9b36ecc197354f6c7a6fe481473650249
Tyler Le <tylerle99@yahoo.com>
2024-02-12 22:33:06 -0800

    Time: 37 ms (51.02%), Space: 16.5 MB (76.44%) - LeetHub

--- 0290-word-pattern/0290-word-pattern.py
+++ 0290-word-pattern/0290-word-pattern.py
@@ -1,14 +1,19 @@
 class Solution:
     def wordPattern(self, pattern: str, s: str) -> bool:
-        s, hmap, idx = s.split(" "), collections.defaultdict(str), 0
-        if len(s) != len(pattern): return False
         
-        for p in pattern:
-            if p not in hmap: 
-                hmap[p] = s[idx]
-            else: 
-                if s[idx] != hmap[p]: return False
-            idx+=1
-            
-        return False if len(set(hmap.values())) != len(hmap) else True
+        s = s.split()
+        if len(pattern) != len(s): return False
+        
+        # map pattern -> string
+        p_to_s = {}
+        s_to_p = {}
+        
+        
+        for p1, s1 in zip(pattern, s):
+            if p1 in p_to_s and p_to_s[p1] != s1: return False
+            if s1 in s_to_p and s_to_p[s1] != p1: return False
+            if p1 not in p_to_s: p_to_s[p1] = s1
+            if s1 not in s_to_p: s_to_p[s1] = p1
                 
+        return True
+            
