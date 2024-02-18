b54491593ab72370a21fe76673e7f4c9d2c7ad8a
Tyler Le <tylerle99@yahoo.com>
2024-02-17 21:53:19 -0800

    Time: 107 ms (54.54%), Space: 18 MB (73.73%) - LeetHub

--- 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.py
+++ 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.py
@@ -1,28 +1,27 @@
-from collections import defaultdict, deque
-import math
-
 class Solution:
     def findCheapestPrice(self, n: int, flights: List[List[int]], src: int, dst: int, k: int) -> int:
-        # Convert to adjacency list
+                
+        # convert to adjacency list
         graph = defaultdict(list)
         for u, v, w in flights:
             graph[u].append((v, w))
-        
+            
         # (node, weight, steps)
         q = deque([(src, 0, -1)])
         
-        # Node: distance from src map
-        distances = [math.inf] * n
+        # node : distance from src map
+        distances = defaultdict(lambda : math.inf)
         
         while q:
             popped_node, popped_dist, popped_steps = q.popleft()
+            distances[popped_node] = min(distances[popped_node], popped_dist)
             
-            # Update distance only if it's less than the current distance
-            distances[popped_node] = min(popped_dist, distances[popped_node])
-                
-            # Explore neighbors
+            
             for nbor, weight in graph[popped_node]:
-                if popped_steps < k and popped_dist + weight < distances[nbor]:
+                if popped_steps < k and popped_dist + weight < distances[nbor]: 
                     q.append((nbor, popped_dist + weight, popped_steps + 1))
-        
+            
         return distances[dst] if distances[dst] != math.inf else -1
+            
+        
+                
