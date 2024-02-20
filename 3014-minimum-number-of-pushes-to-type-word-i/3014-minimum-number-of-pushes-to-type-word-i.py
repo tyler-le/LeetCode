34ffd708a3726347d78b236b4d2c390ab86dbe47
Tyler Le <tylerle99@yahoo.com>
2024-02-19 21:49:36 -0800

    Time: 42 ms (16.40%), Space: 16.6 MB (13.88%) - LeetHub

--- 3014-minimum-number-of-pushes-to-type-word-i/3014-minimum-number-of-pushes-to-type-word-i.py
+++ 3014-minimum-number-of-pushes-to-type-word-i/3014-minimum-number-of-pushes-to-type-word-i.py
@@ -1,17 +1,13 @@
 class Solution:
     def minimumPushes(self, word: str) -> int:
-        # place high frequency chars in early positions
-        keypad = [[] for _ in range(8)]
+        buckets = [[] for _ in range(8)]
         hmap = defaultdict(int)
         res = 0
-        cnt = Counter(word)
         
-        i = 0
-        for ch, freq in cnt.items():
-            keypad[i % 8].append(ch)
-            hmap[ch] = len(keypad[i%8])
-            i = (i + 1) % 8 
+        for i in range(len(word)):
             
-        for ch in word:
-            res+=hmap[ch]
-        return res
+            idx = i%8
+            buckets[idx].append(word[i])
+            hmap[word[i]] = len(buckets[idx])
+        
+        return sum(hmap.values())
