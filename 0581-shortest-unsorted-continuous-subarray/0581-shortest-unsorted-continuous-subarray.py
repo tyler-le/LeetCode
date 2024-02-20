d208d24ad14dc1e4bd2f47ec85d410920eec8246
Tyler Le <tylerle99@yahoo.com>
2024-02-20 15:49:40 -0800

    Time: 195 ms (18.31%), Space: 18.3 MB (48.88%) - LeetHub

--- 0581-shortest-unsorted-continuous-subarray/0581-shortest-unsorted-continuous-subarray.py
+++ 0581-shortest-unsorted-continuous-subarray/0581-shortest-unsorted-continuous-subarray.py
@@ -1,24 +1,23 @@
 class Solution:
     def findUnsortedSubarray(self, nums: List[int]) -> int:
-        left_boundary, right_boundary = float("inf"), 0
-        stack, n = [], len(nums)
+        stack = []
+        leftmost, rightmost = math.inf, 0
+        n = len(nums)
         
-        for i in range(n):
-            curr = nums[i]
-            while stack and curr < stack[-1][1]:
-                popped = stack.pop()
-                left_boundary = min(left_boundary, popped[0])
-                
-            stack.append((i, curr))
+        for i, curr in enumerate(nums):
+            while stack and curr < stack[-1][0]:
+                popped_num, popped_idx = stack.pop()
+                leftmost = min(leftmost, popped_idx)
+            stack.append((nums[i], i))
             
         stack = []
         for i in range(n-1, -1, -1):
-            curr = nums[i]
-            while stack and curr > stack[-1][1]:
-                popped = stack.pop()
-                right_boundary = max(right_boundary, popped[0])
-            
-            stack.append((i, curr))
-            
-        if left_boundary == float("inf") or right_boundary == 0: return 0
-        return right_boundary - left_boundary + 1
+            while stack and nums[i] > stack[-1][0]:
+                popped_num, popped_idx = stack.pop()
+                rightmost = max(rightmost, popped_idx)
+            stack.append((nums[i], i))
+        
+        if leftmost == float("inf") or rightmost == 0: return 0
+        return rightmost - leftmost + 1
+        
+        
