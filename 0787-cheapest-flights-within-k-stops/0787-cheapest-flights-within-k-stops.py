c8079b416374c1e96f422c2435ac936bcea1f869
Tyler Le <tylerle99@yahoo.com>
2024-02-22 18:38:54 -0800

    Time: 106 ms (55.17%), Space: 18 MB (63.42%) - LeetHub

--- 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.py
+++ 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.py
@@ -1,6 +1,5 @@
 class Solution:
     def findCheapestPrice(self, n: int, flights: List[List[int]], src: int, dst: int, k: int) -> int:
-                
         # convert to adjacency list
         graph = defaultdict(list)
         for u, v, w in flights:
@@ -11,17 +10,15 @@ class Solution:
         
         # node : distance from src map
         distances = defaultdict(lambda : math.inf)
+        res = math.inf
         
         while q:
             popped_node, popped_dist, popped_steps = q.popleft()
             distances[popped_node] = min(distances[popped_node], popped_dist)
-            
+            if popped_node == dst: res = min(res, popped_dist)
             
             for nbor, weight in graph[popped_node]:
                 if popped_steps < k and popped_dist + weight < distances[nbor]: 
                     q.append((nbor, popped_dist + weight, popped_steps + 1))
             
-        return distances[dst] if distances[dst] != math.inf else -1
-            
-        
-                
+        return res if res != math.inf else -1
