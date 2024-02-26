219f7392b67d8bb356471eef8b3db8a0c348952b
Tyler Le <tylerle99@yahoo.com>
2024-02-25 16:45:33 -0800

    Time: 272 ms (88.99%), Space: 18.1 MB (69.59%) - LeetHub

--- 0934-shortest-bridge/0934-shortest-bridge.py
+++ 0934-shortest-bridge/0934-shortest-bridge.py
@@ -1,50 +1,39 @@
 class Solution:
     def shortestBridge(self, grid: List[List[int]]) -> int:
-        rows, cols = len(grid), len(grid[0])
-        q = deque()
-        
-        def dfs(x, y):
-            if x < 0 or y < 0: return
-            if x >= rows or y >= cols: return
-            if grid[x][y] != 1: return
+        def convert_to_two(i, j):
+            if i < 0 or j < 0 or i >= n or j >= m: return
+            if grid[i][j] != 1: return
             
-            grid[x][y] = 2
-            q.append((x,y,0))
-            
+            grid[i][j] = 2
+            q.append((i,j, 0))
             for dx, dy in [(-1,0), (1,0), (0,-1), (0,1)]:
-                r, c = x+dx, y+dy
-                dfs(r, c)
+                convert_to_two(i+dx, j+dy)
         
-        # find one island
-        for r in range(rows):
-            found = False
-            for c in range(cols):
-                # change it to all 2's
-                if grid[r][c] == 1: 
-                    dfs(r,c)
-                    found = True
+        n, m = len(grid), len(grid[0])
+        q = deque() # (row, col, steps)
+        
+        converted_island = False
+        for i in range(n):
+            if converted_island: break
+            for j in range(m):
+                if grid[i][j]:
+                    convert_to_two(i, j)
+                    converted_island = True
                     break
-            if found: break
-                    
-                    
-        # then for all 2's, find the shortest path to the second island (1's)
+        
         while q:
             x, y, steps = q.popleft()
             
-            for dx, dy in [(-1,0), (1,0), (0,-1), (0,1)]:
-                if 0 <= x+dx < rows and 0 <= y+dy < cols:
-                    if grid[x+dx][y+dy] == 0:
-                        q.append((x+dx, y+dy, steps+1))
-                        grid[x+dx][y+dy] = 2
-                    elif grid[x+dx][y+dy] == 1: return steps
-                    elif grid[x+dx][y+dy] == 2: continue
-                    
-
-        
-        
-        
-        
-        
+            for dx, dy in [(-1,0), (1,0), (0,1), (0,-1)]:
+                r, c = x+dx, y+dy
+                if r < 0 or c < 0 or r >= n or c >= m: continue
+                if grid[r][c] == 0: 
+                    q.append((r,c,steps+1))
+                    grid[r][c] = 2
+                elif grid[r][c] == 1: return steps
+                elif grid[r][c] == 2: continue
             
-                                
+
+            
+        
         
