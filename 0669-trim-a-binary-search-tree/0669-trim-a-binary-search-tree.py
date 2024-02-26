5406dba4004f485c51a195468376b40a37a0c001
Tyler Le <tylerle99@yahoo.com>
2024-02-25 20:20:51 -0800

    Time: 49 ms (35.89%), Space: 19.4 MB (75.77%) - LeetHub

--- 0669-trim-a-binary-search-tree/0669-trim-a-binary-search-tree.py
+++ 0669-trim-a-binary-search-tree/0669-trim-a-binary-search-tree.py
@@ -9,12 +9,17 @@ class Solution:
     def trimBST(self, root: Optional[TreeNode], low: int, high: int) -> Optional[TreeNode]:
         def rec(node):
             if not node: return None
+            
+            # this node falls out of range. disregard node's left subtree
             elif node.val < low: return rec(node.right)
+            
+            # this node falls out of range. disregard node's right subtree
             elif node.val > high: return rec(node.left)
+            
+            # this node is within range, get the trimmed left and right subtrees recursively
             else: node.left, node.right = rec(node.left), rec(node.right)
-               
-            
-            
+                
+            # return trimmed root
             return node
         
         return rec(root)
