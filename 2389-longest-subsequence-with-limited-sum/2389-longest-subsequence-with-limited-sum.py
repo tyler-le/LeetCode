63a0b4dc77bcba5fe82e2668f6af3ce98949e6e4
Tyler Le <tylerle99@yahoo.com>
2024-02-25 17:47:34 -0800

    Time: 701 ms (5.95%), Space: 16.9 MB (92.62%) - LeetHub

--- 2389-longest-subsequence-with-limited-sum/2389-longest-subsequence-with-limited-sum.py
+++ 2389-longest-subsequence-with-limited-sum/2389-longest-subsequence-with-limited-sum.py
@@ -1,19 +1,17 @@
-from typing import List
-from bisect import bisect_right
-
 class Solution:
     def answerQueries(self, nums: List[int], queries: List[int]) -> List[int]:
+
         nums.sort()
-        prefix_sum = []
-        res = []
+        
+        res = defaultdict(int)
+        
         acc = 0
-        
-        for num in nums:
-            acc += num
-            prefix_sum.append(acc)
-        
-        for query in queries:
-            index = bisect_right(prefix_sum, query)
-            res.append(index)
-        
-        return res
+        for i, num in enumerate(nums):
+            acc+=num
+            for query in queries:
+                if acc <= query:
+                    res[query] = max(res[query], i+1)
+            
+                
+        return [res[query] for query in queries]
+            
