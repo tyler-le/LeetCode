9632a0d7998bde2196d17a8210331b4926dc7b87
Tyler Le <tylerle99@yahoo.com>
2024-02-27 19:29:21 -0800

    Time: 378 ms (7.53%), Space: 17.2 MB (67.21%) - LeetHub

--- 2336-smallest-number-in-infinite-set/2336-smallest-number-in-infinite-set.py
+++ 2336-smallest-number-in-infinite-set/2336-smallest-number-in-infinite-set.py
@@ -4,19 +4,13 @@ class SmallestInfiniteSet:
     def __init__(self):
         self.not_in_set = set()
         
-
     def popSmallest(self) -> int:
-        i = 1
-        
-        while i in self.not_in_set:
-            i+=1
-        
-        self.not_in_set.add(i)
-        return i
+        num = 1
+        while num in self.not_in_set: 
+            num+=1
+        self.not_in_set.add(num)
+        return num
             
-        
-        
-
     def addBack(self, num: int) -> None:
         if num in self.not_in_set:
             self.not_in_set.remove(num)
