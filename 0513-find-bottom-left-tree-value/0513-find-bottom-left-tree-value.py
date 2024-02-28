8f5f4fee344f8cd122cd65b3c47635a2e5bab272
Tyler Le <tylerle99@yahoo.com>
2024-02-27 16:59:58 -0800

    Time: 43 ms (51.90%), Space: 18.3 MB (52.22%) - LeetHub

--- 0513-find-bottom-left-tree-value/0513-find-bottom-left-tree-value.py
+++ 0513-find-bottom-left-tree-value/0513-find-bottom-left-tree-value.py
@@ -8,31 +8,18 @@ class Solution:
     def findBottomLeftValue(self, root: Optional[TreeNode]) -> int:
         # find height
         # return first node at that height
-        
-        max_height = 0
-        
-        def get_height(node):
-            nonlocal max_height
-            if not node: return 0
-            
-            left = get_height(node.left)
-            right = get_height(node.right)
-            curr_height = 1 + max(left, right)
-            
-            max_height = max(max_height, curr_height)
-            
-            return curr_height
-        
-        get_height(root)
-        
-        # (node, curr_height)
-        q = deque([(root, 1)])
+        q = deque([root])
+        leftmost = None
         
         while q:
-            popped_node, popped_height = q.popleft()
-            if popped_height == max_height: return popped_node.val
+            level_size = len(q)
             
-            if popped_node.left: q.append((popped_node.left, popped_height+1))
-            if popped_node.right: q.append((popped_node.right, popped_height+1))
+            for i in range(level_size):
+                popped = q.popleft()
+                if i == 0: leftmost = popped.val
+                if popped.left: q.append(popped.left)
+                if popped.right: q.append(popped.right)
+            
                 
+        return leftmost
     
