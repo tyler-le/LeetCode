a135996e4d65d116c3272edc2d6a20f62287cf6a
Tyler Le <tylerle99@yahoo.com>
2024-02-28 20:26:20 -0800

    Time: 223 ms (51.94%), Space: 18.8 MB (97.92%) - LeetHub

--- 2186-minimum-number-of-steps-to-make-two-strings-anagram-ii/2186-minimum-number-of-steps-to-make-two-strings-anagram-ii.py
+++ 2186-minimum-number-of-steps-to-make-two-strings-anagram-ii/2186-minimum-number-of-steps-to-make-two-strings-anagram-ii.py
@@ -1,17 +1,11 @@
 class Solution:
     def minSteps(self, s: str, t: str) -> int:
-        seen = set()
+
         s_cnt, t_cnt = Counter(s), Counter(t)
         res = 0
+        chars = set(s+t)
         
-        for ch in s:
-            if ch in seen: continue
-            res+=(abs(s_cnt[ch] - t_cnt[ch]))
-            seen.add(ch)
-        
-        for ch in t:
-            if ch in seen: continue
-            res+=(abs(t_cnt[ch] - s_cnt[ch]))
-            seen.add(ch)
+        for ch in chars:
+            res+=abs(s_cnt[ch] - t_cnt[ch])
         
         return res
