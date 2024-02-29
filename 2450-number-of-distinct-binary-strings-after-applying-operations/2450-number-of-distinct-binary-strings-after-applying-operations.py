f3e836f7a15aeaae0eccee39d89c957c8b96b7b6
Tyler Le <tylerle99@yahoo.com>
2024-02-28 20:43:33 -0800

    Time: 38 ms (57.14%), Space: 17.1 MB (71.43%) - LeetHub

--- 2450-number-of-distinct-binary-strings-after-applying-operations/2450-number-of-distinct-binary-strings-after-applying-operations.py
+++ 2450-number-of-distinct-binary-strings-after-applying-operations/2450-number-of-distinct-binary-strings-after-applying-operations.py
@@ -1,8 +1,5 @@
 class Solution:
     def countDistinctStrings(self, s: str, k: int) -> int:
-        total_windows = 0
-        
-        for i in range(len(s) - k + 1):
-            total_windows+=1
+        total_windows = len(s) - k + 1
         
         return (2**total_windows) % (10**9 + 7)
