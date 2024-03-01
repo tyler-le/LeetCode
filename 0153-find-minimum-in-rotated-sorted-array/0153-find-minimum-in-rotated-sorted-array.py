01d3c1632b7abc5ebec087b38a45312267869e66
Tyler Le <tylerle99@yahoo.com>
2024-03-01 15:02:58 -0800

    Time: 38 ms (86.25%), Space: 16.9 MB (82.59%) - LeetHub

--- 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.py
+++ 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.py
@@ -6,10 +6,8 @@ class Solution:
             mid = low + ((high - low) // 2)
             
             # check if right side is sorted
-            if nums[mid] <= nums[high]:
-                high = mid
-            else:
-                low = mid + 1
+            if nums[mid] < nums[high]: high = mid
+            else: low = mid + 1
             
             
         return nums[high]
