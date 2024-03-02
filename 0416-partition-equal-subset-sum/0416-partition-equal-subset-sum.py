bfae4a930b0c3a361475e51372372ea3a41787d5
Tyler Le <tylerle99@yahoo.com>
2024-03-01 16:00:33 -0800

    Time: 4266 ms (5.00%), Space: 48.6 MB (27.06%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.py
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.py
@@ -1,28 +1,22 @@
-class Solution(object):
-    def canPartition(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: bool
-        """
-        rows = len(nums) + 1
-        cols = sum(nums) + 1
+class Solution:
+    def canPartition(self, nums: List[int]) -> bool:
         
-        if sum(nums) % 2: return False
-        
-        dp = [[False for _ in range(cols)] for _ in range(rows)]
-                    
-        for r in range(rows): dp[r][0] = True
+        sumi, n = sum(nums), len(nums)
+        dp = [[False for _ in range(sumi + 1)] for _ in range(n+1)]
+        n, m = len(dp), len(dp[0])
+        if sumi % 2: return False
+        for i in range(n):
+            dp[i][0] = True
             
-        for i in range(1, rows):
-            for j in range(1, cols):
+        for i in range(1, n):
+            for j in range(1, m):
                 
+                # can we make sum 'j' when including i'th element
+                include = False if j - nums[i-1] < 0 else dp[i-1][j - nums[i-1]]
+                
+                # can we make sum 'j' when excluding i'th element
                 exclude = dp[i-1][j]
                 
-                if j - nums[i-1] < 0: include = False
-                else: include = dp[i-1][j-nums[i-1]]
-                    
                 dp[i][j] = include or exclude
-
-        return dp[-1][sum(nums) // 2]
         
-        
+        return dp[-1][sumi // 2]
