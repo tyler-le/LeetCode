1cc4c255752ebc2d51a544356c979551098e849a
Tyler Le <tylerle99@yahoo.com>
2024-03-08 15:02:57 -0800

    Time: 112 ms (70.17%), Space: 17.4 MB (82.91%) - LeetHub

--- 1647-minimum-deletions-to-make-character-frequencies-unique/1647-minimum-deletions-to-make-character-frequencies-unique.py
+++ 1647-minimum-deletions-to-make-character-frequencies-unique/1647-minimum-deletions-to-make-character-frequencies-unique.py
@@ -1,16 +1,29 @@
 class Solution:
-    def minDeletions(self, s: str) -> int:      
+    def minDeletions(self, s: str) -> int:
         
-        cnt = Counter(s)
-        seen = set()
+        # max_heap
+#         a - 3
+#         b - 3
+#         c - 2
+        
+#         seen_freqs [3 ]
+                
+        max_heap = [-freq for freq in Counter(s).values() ]
+        heapify(max_heap)
+        seen_freqs = set()
         res = 0
-        freqs = sorted(cnt.values(), reverse = True)
         
-        for f in freqs:
-            while f in seen: 
-                f-=1
-                res+=1
-            if f: seen.add(f)
+        for _ in range(len(max_heap)):
+            popped_freq = -heappop(max_heap)
             
+            while popped_freq and popped_freq in seen_freqs:
+                res+=1
+                popped_freq-=1
+            
+            if popped_freq:
+                seen_freqs.add(popped_freq)
+        
         return res
-   
+        
+            
+            
