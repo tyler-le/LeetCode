09698d276ba6b6185273bd99e8384ebfc380f371
Tyler Le <tylerle99@yahoo.com>
2024-03-08 17:56:17 -0800

    Time: 209 ms (56.35%), Space: 37.8 MB (24.43%) - LeetHub

--- 0494-target-sum/0494-target-sum.py
+++ 0494-target-sum/0494-target-sum.py
@@ -1,29 +1,29 @@
 class Solution:
     def findTargetSumWays(self, nums: List[int], target: int) -> int:
-        # backtracking
-        # map (acc, index) : number of ways target can be formed
-        # each choice, we can (+) the current num or (-) the current num
         
-        def backtrack(index, acc):
-            
-            if (acc, index) in hmap: 
-                return hmap[(acc, index)]
-            
-            if index >= n:
-                if acc == target: 
-                    return 1
-                return 0
-                
-            # make a current choice for the index
-            add = backtrack(index+1, acc + nums[index])
-            sub = backtrack(index+1, acc - nums[index])
-            
-            hmap[(acc, index)] = (add + sub)
-            
-            return hmap[(acc, index)]
-            
-            
-        
+        # for each num, we can backtrack o +num and -num
+        res = 0
         n = len(nums)
-        hmap = defaultdict(int)
+        state = defaultdict(int) # (curr_sum, index) -> number of ways
+        
+        
+        def backtrack(curr, i):
+            nonlocal res
+            acc = 0
+            if (curr, i) in state: return state[(curr, i)]
+            
+            if i >= n: 
+                if curr == target: return 1
+                else: return 0            
+                
+            acc+=backtrack(curr + nums[i], i+1)
+            acc+=backtrack(curr - nums[i], i+1)
+            
+            state[(curr, i)] = acc
+            return acc
+            
+            
+            
         return backtrack(0, 0)
+            
+        
