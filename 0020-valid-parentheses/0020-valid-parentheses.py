dd63c432d776738b56f1197399e3a2a9045250cf
Tyler Le <tylerle99@yahoo.com>
2024-03-15 22:30:56 -0700

    Time: 37 ms (46.89%), Space: 16.6 MB (77.46%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.py
+++ 0020-valid-parentheses/0020-valid-parentheses.py
@@ -1,21 +1,19 @@
 class Solution:
     def isValid(self, s: str) -> bool:
-        # map closed : open
-        hmap = {")":"(", "}":"{", "]":"["}
-        opens = set(hmap.values())
         stack = []
+        OPEN = set(['(', '[', '{'])
+        CLOSED = set([')', ']', '}'])
+        hmap = {}
+        hmap[")"] = "("
+        hmap["}"] = "{"
+        hmap["]"] = "["
         
         for ch in s:
-            if ch in opens:
-                stack.append(ch) # push open brace
+            if ch in OPEN:
+                stack.append(ch)
             else:
                 if not stack: return False
                 popped = stack.pop()
+                if hmap[ch] != popped: return False
                 
-                # check if popped open brace matches 
-                # the mapped value to the corresponding closed brace
-                if popped != hmap[ch]:
-                    return False
-                
-        return len(stack) == 0
-            
+        return not stack
