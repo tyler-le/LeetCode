8ea0719d516b4a18f4268a742135c95bb9b2e16b
Tyler Le <tylerle99@yahoo.com>
2024-03-25 17:42:53 -0700

    Time: 46 ms (17.70%), Space: 16.7 MB (50.65%) - LeetHub

--- 0090-subsets-ii/0090-subsets-ii.py
+++ 0090-subsets-ii/0090-subsets-ii.py
@@ -1,24 +1,22 @@
 class Solution:
     def subsetsWithDup(self, nums: List[int]) -> List[List[int]]:
+        res = []
         
-        def backtrack(idx, path):
+        def backtrack(path, idx):
             if idx == len(nums):
                 res.append(path.copy())
                 return
             
-            # dfs on future elements with nums[idx] anchored
-            backtrack(idx+1, path+[nums[idx]]) 
             
-            # skip duplicates from this anchored position 
-            # since we already explored all possibilities with nums[idx]
-            # that are anchored here
-            while idx + 1 < len(nums) and nums[idx] == nums[idx+1]:
+            cand = nums[idx]
+            
+            backtrack(path+[cand], idx+1)
+            
+            while idx+1 < len(nums) and nums[idx] == nums[idx+1]:
                 idx+=1
-                
-            # dfs on future elements without nums[idx] anchored
-            backtrack(idx+1, path)
+            
+            backtrack(path, idx+1)
         
-        res = []
-        nums.sort() # to avoid dupes
-        backtrack(0, [])
+        nums.sort()
+        backtrack([], 0)
         return res
