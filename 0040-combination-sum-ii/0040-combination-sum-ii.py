7c4df71ffd384207e99a831c726477f77f67718e
Tyler Le <tylerle99@yahoo.com>
2024-03-25 17:12:54 -0700

    Time: 95 ms (7.40%), Space: 16.5 MB (97.73%) - LeetHub

--- 0040-combination-sum-ii/0040-combination-sum-ii.py
+++ 0040-combination-sum-ii/0040-combination-sum-ii.py
@@ -1,33 +1,26 @@
 class Solution:
     def combinationSum2(self, candidates: List[int], target: int) -> List[List[int]]:
-        
-        
-        def backtrack(idx, path):
+        res = []
+        def backtrack(path, index):
+            nonlocal res
             
-            # we found a valid combination 
-            if sum(path) == target: 
+            if sum(path) == target:
                 res.append(path.copy())
                 return
-            
-            # we added too much to the combination
-            if sum(path) > target:
+            elif index >= len(candidates):
+                return
+            elif sum(path) > target:
                 return
             
-            # we have no more items to consider
-            if idx >= len(candidates):
-                return
+            else:
+                cand = candidates[index]
+                IN = backtrack(path + [cand], index+1)
+                
+                while index + 1 < len(candidates) and candidates[index] == candidates[index+1]:
+                    index+=1
+                
+                OUT = backtrack(path, index+1)
             
-            # dfs on all future elements and include candidates[idx]
-            backtrack(idx+1, path + [candidates[idx]])
-            
-            # after we've considered candidates[idx] at this position, we need to skip over all identical candidates
-            while idx + 1 < len(candidates) and candidates[idx] == candidates[idx+1]:
-                idx+=1
-            
-            # dfs on all future elements and don't include candidates[idx]
-            backtrack(idx+1, path)
-        
-        res = []
         candidates.sort()
-        backtrack(0, [])
+        backtrack([], 0)
         return res
