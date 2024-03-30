1eb01e6ebf817d4643c09c9f79104abb15bd5eb9
Tyler Le <tylerle99@yahoo.com>
2024-03-29 22:19:23 -0700

    Time: 335 ms (72.07%), Space: 20 MB (59.53%) - LeetHub

--- 0992-subarrays-with-k-different-integers/0992-subarrays-with-k-different-integers.py
+++ 0992-subarrays-with-k-different-integers/0992-subarrays-with-k-different-integers.py
@@ -1,21 +1,23 @@
 class Solution:
     def subarraysWithKDistinct(self, nums: List[int], k: int) -> int:
+        
+        # number of subarrays where the number of different integers is AT MOST x
         def at_most(x):
-            hmap = defaultdict(int)
-            res = 0
-            l = 0
+            cnt = defaultdict(int)
             n = len(nums)
-
+            l = 0
+            ans = 0
+            
             for r in range(n):
-                hmap[nums[r]]+=1
-
-                while len(hmap) > x:
-                    hmap[nums[l]]-=1
-                    if not hmap[nums[l]]: del hmap[nums[l]]
+                cnt[nums[r]]+=1
+                
+                while len(cnt) > x:
+                    cnt[nums[l]]-=1
+                    if not cnt[nums[l]]: del cnt[nums[l]]
                     l+=1
-
-                res += (r-l+1)
-
-            return res
+                
+                ans+=(r-l+1)
+            
+            return ans
         
         return at_most(k) - at_most(k-1)
