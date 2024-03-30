ce871fdb962309991f4c3f13b15bdf0e6bec1f3b
Tyler Le <tylerle99@yahoo.com>
2024-03-30 11:22:51 -0700

    Time: 104 ms (48.42%), Space: 16.9 MB (12.37%) - LeetHub

--- 2799-count-complete-subarrays-in-an-array/2799-count-complete-subarrays-in-an-array.py
+++ 2799-count-complete-subarrays-in-an-array/2799-count-complete-subarrays-in-an-array.py
@@ -5,6 +5,7 @@ class Solution:
         n = len(nums)
         hmap = defaultdict(int)
         k = len(Counter(nums))
+        
         for r in range(n):
             hmap[nums[r]]+=1
             
@@ -13,10 +14,7 @@ class Solution:
                 if not hmap[nums[l]]: del hmap[nums[l]]
                 l+=1
                 
-                # The operation res += len(nums) - r is used to update the count of complete subarrays by taking into account all possible extensions of the current subarray [l...r] towards the right.
-                
-                # this is because if nums[l:r] is complete, then any extension past index r will also yield in a complete subarray
-                res += len(nums) - r 
+            res+=l
                 
         return res
             
