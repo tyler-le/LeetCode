b3ae1b84deda17d086a76a5453964a75bb851582
Tyler Le <tylerle99@yahoo.com>
2024-04-01 17:17:56 -0700

    Time: 39 ms (77.41%), Space: 16.9 MB (32.93%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.py
+++ 0205-isomorphic-strings/0205-isomorphic-strings.py
@@ -4,9 +4,12 @@ class Solution:
         t_to_s = defaultdict(str)
         
         for ch1, ch2 in zip(s, t):
+            # if no mapping
             if ch1 not in s_to_t and ch2 not in t_to_s:
                 s_to_t[ch1] = ch2
                 t_to_s[ch2] = ch1
+                
+            # elif invalid bidirectional mapping
             elif s_to_t[ch1] != ch2 or t_to_s[ch2] != ch1:
                 return False
         
