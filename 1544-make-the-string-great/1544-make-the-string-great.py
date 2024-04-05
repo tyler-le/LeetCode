be69af22eab76b89ef4f876d4fc91530bd1172f0
Tyler Le <tylerle99@yahoo.com>
2024-04-04 21:07:51 -0700

    Time: 42 ms (27.78%), Space: 16.5 MB (46.83%) - LeetHub

--- 1544-make-the-string-great/1544-make-the-string-great.py
+++ 1544-make-the-string-great/1544-make-the-string-great.py
@@ -1,17 +1,18 @@
 class Solution:
     def makeGood(self, s: str) -> str:
         stack = []
+        hmap = {}
         
+        for ch in ascii_lowercase:
+            hmap[ch] = ch.upper()
+            hmap[ch.upper()] = ch
+            
         for curr_ch in s:
-            
-            curr_case = curr_ch.islower()
-            
-            if stack and stack[-1][0].lower() == curr_ch.lower() and curr_case != stack[-1][1]:
-                stack.pop()
-            else:
-                stack.append((curr_ch, curr_case))
+                        
+            if stack and hmap[curr_ch] == stack[-1]: stack.pop()
+            else: stack.append(curr_ch)
         
-        return "".join([ch for (ch, _) in stack])
+        return "".join(stack)
                 
                 
                     
