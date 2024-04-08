059297a19ec355e1636d9168cae7d6b062f77c18
Tyler Le <tylerle99@yahoo.com>
2024-04-07 18:37:28 -0700

    Time: 45 ms (23.85%), Space: 16.5 MB (59.90%) - LeetHub

--- 1700-number-of-students-unable-to-eat-lunch/1700-number-of-students-unable-to-eat-lunch.py
+++ 1700-number-of-students-unable-to-eat-lunch/1700-number-of-students-unable-to-eat-lunch.py
@@ -1,24 +1,24 @@
 class Solution:
     def countStudents(self, students: List[int], sandwiches: List[int]) -> int:
+        CIRCLE, RECT = 0, 1
+        prefers_circle = students.count(CIRCLE)
+        prefers_rect = students.count(RECT)
         
-        students = deque(students)
         sandwiches = deque(sandwiches)
         
-        while students:
-            n = len(students)
-            cnt = 0
-            
-            # keep circulating students if they don't match
-            while students[0] != sandwiches[0]:
-                if cnt == n: 
-                    # we've done a full pass of students and need to exit loop
-                    return n
-                cnt+=1
-                students.append(students.popleft())
-            
-            # we found a match, so pair the two together
-            sandwiches.popleft()
-            students.popleft()
+        while sandwiches:
+            if sandwiches[0] == CIRCLE:
+                if prefers_circle: 
+                    prefers_circle-=1
+                    sandwiches.popleft()
+                else: 
+                    return len(sandwiches)
+            else:
+                if prefers_rect: 
+                    prefers_rect-=1
+                    sandwiches.popleft()
+                else: 
+                    return len(sandwiches)
         
         return 0
-            
+                
