77661d935c55a2a4d2dd1b059a0ad12da2c10ca2
Tyler Le <tylerle99@yahoo.com>
2024-04-07 18:32:41 -0700

    Time: 39 ms (63.55%), Space: 16.6 MB (59.90%) - LeetHub

--- 1700-number-of-students-unable-to-eat-lunch/1700-number-of-students-unable-to-eat-lunch.py
+++ 1700-number-of-students-unable-to-eat-lunch/1700-number-of-students-unable-to-eat-lunch.py
@@ -4,17 +4,19 @@ class Solution:
         students = deque(students)
         sandwiches = deque(sandwiches)
         
-        
-        
         while students:
             n = len(students)
             cnt = 0
+            
+            # keep circulating students if they don't match
             while students[0] != sandwiches[0]:
                 if cnt == n: 
+                    # we've done a full pass of students and need to exit loop
                     return n
                 cnt+=1
                 students.append(students.popleft())
             
+            # we found a match, so pair the two together
             sandwiches.popleft()
             students.popleft()
         
