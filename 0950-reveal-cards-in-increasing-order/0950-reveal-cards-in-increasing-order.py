ec90e85c7f87dfcf220c92ff6aa56d063324f924
Tyler Le <tylerle99@yahoo.com>
2024-04-09 18:07:36 -0700

    Time: 43 ms (74.67%), Space: 16.7 MB (97.97%) - LeetHub

--- 0950-reveal-cards-in-increasing-order/0950-reveal-cards-in-increasing-order.py
+++ 0950-reveal-cards-in-increasing-order/0950-reveal-cards-in-increasing-order.py
@@ -1,14 +1,17 @@
 class Solution:
     def deckRevealedIncreasing(self, deck: List[int]) -> List[int]:
-        # even positions should be sorted in increasing order
+
         n = len(deck)
-        deck.sort()
+        deck.sort() # start from sorted position and perform the simulation
         indices = deque([x for x in range(n)])
-        res = [0 for _ in range(n)]
+        res = [None for _ in range(n)]
         
         for card in deck:
+            
+            # reveal this card
             res[indices.popleft()] = card
             
+            # move this card to the end
             if indices:
                 indices.append(indices.popleft())
         
