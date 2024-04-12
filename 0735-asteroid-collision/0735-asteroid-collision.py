cb86c33fc696c3ba74a2f71fbca40f978eae275c
Tyler Le <tylerle99@yahoo.com>
2024-04-12 15:14:36 -0700

    Time: 81 ms (67.46%), Space: 17.7 MB (90.41%) - LeetHub

--- 0735-asteroid-collision/0735-asteroid-collision.py
+++ 0735-asteroid-collision/0735-asteroid-collision.py
@@ -5,31 +5,35 @@ class Solution:
         
         for ast in asteroids:
 
-           # rightward ast
+           # rightward ast, add to stack
             if ast > 0:
                 stack.append(ast)
                 
-            # leftward ast
+            # leftward ast, handle collision
             else:
-                
-                # if stack is empty, no collision, add to stack
+                # if stack is empty or has leftward asteroid as top, 
+                # then no collision and add to stack
                 if not stack or stack[-1] < 0:
                     stack.append(ast)
                     
                 else:
+                    # stack is not empty or has rightward asteroid as top, 
+                    # simulate collisions
                     
-                    
-                    # stack is not empty, simulate collisions
                     while stack and stack[-1] > 0 and stack[-1] < abs(ast):
+                        # top is colliding with ast and top is smaller, so remove from stack
                         stack.pop()
 
                     if stack and stack[-1] == abs(ast):
+                        # top is equal to ast, so destroy both
                         stack.pop()
 
-                    elif stack and stack[-1] < abs(ast):
+                    elif stack and stack[-1] < 0 and ast < 0:
+                        # top is smaller than ast, so add ast
                         stack.append(ast)
                         
                     elif not stack:
+                        # ast cleared out entire stack, add ast to stack
                         stack.append(ast)
 
         return stack
