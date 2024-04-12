9676a5bf5514983e677d8551c6845c43b1d16b75
Tyler Le <tylerle99@yahoo.com>
2024-04-12 15:24:57 -0700

    Time: 79 ms (77.38%), Space: 17.9 MB (68.12%) - LeetHub

--- 0735-asteroid-collision/0735-asteroid-collision.py
+++ 0735-asteroid-collision/0735-asteroid-collision.py
@@ -5,17 +5,21 @@ class Solution:
         for ast in asteroids:
             current_ast_survived = True
             
+            # while current asteroid is left and top is right (collision)
             while stack and stack[-1] > 0 and ast < 0:
                 
+                # current ast beats top ast
                 if stack[-1] < abs(ast):
                     stack.pop()
                     
+                # asteroids destroy each other
                 elif stack[-1] == abs(ast):
                     stack.pop()
                     current_ast_survived = False
                     break
                 
-                else:
+                # top asteroid beats current asteroid
+                elif stack[-1] > abs(ast):
                     current_ast_survived = False
                     break
             
