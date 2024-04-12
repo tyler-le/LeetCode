5c37f4932097f5ea636f8ba3f2eb19e3c5076e86
Tyler Le <tylerle99@yahoo.com>
2024-04-11 18:25:18 -0700

    Time: 119 ms (16.99%), Space: 18.2 MB (99.42%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.py
+++ 0042-trapping-rain-water/0042-trapping-rain-water.py
@@ -1,22 +1,22 @@
 class Solution:
     def trap(self, height: List[int]) -> int:
+        
         n = len(height)
-        
         max_from_left = [0 for _ in range(n)]        
-        max_from_right = [0 for _ in range(n)]
-        
-        max_from_left[0] = height[0]
-        max_from_right[-1] = height[-1]
-        
+        max_from_right = [0 for _ in range(n)]        
         res = 0
         
-        for i in range(1, n):
-            max_from_left[i] = max(max_from_left[i-1], height[i])
-            
-        for i in range(n-2, -1, -1):
-            max_from_right[i] = max(max_from_right[i+1], height[i])
-            
-        for i in range(1, n):
-            res += min(max_from_left[i], max_from_right[i]) - height[i]
-            
+        for i in range(n):
+            if i == 0: max_from_left[0] = height[0]
+            else: max_from_left[i] = max(max_from_left[i-1], height[i])
+    
+        for i in range(n-1, -1, -1):
+            if i == n-1: max_from_right[-1] = height[-1]
+            else: max_from_right[i] = max(max_from_right[i+1], height[i])
+                
+        for i in range(n):
+            h = min(max_from_right[i], max_from_left[i])
+            res+=(h - height[i])
+        
         return res
+            
