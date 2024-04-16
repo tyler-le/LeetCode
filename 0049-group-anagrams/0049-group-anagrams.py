742bd795d5e6ca10467ded376560db0947e5984e
Tyler Le <tylerle99@yahoo.com>
2024-04-16 14:59:34 -0700

    Time: 92 ms (48.61%), Space: 22.4 MB (5.33%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -1,14 +1,14 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        # map character count : list of words
+        
         hmap = defaultdict(list)
         
         for s in strs:
-            freq = [0]*26
+            cnt = [0] * 26
+            
             for ch in s:
-                freq[ord(ch) - ord('a')]+=1
-                
-            hmap[tuple(freq)].append(s)
+                cnt[ord(ch) - ord('a')]+=1
+            
+            hmap[tuple(cnt)].append(s)
         
         return hmap.values()
-            
