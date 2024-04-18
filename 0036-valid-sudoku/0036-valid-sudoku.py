9de4581d79d0fcb750f59bf5b5ed44e14b0ac04f
Tyler Le <tylerle99@yahoo.com>
2024-04-17 17:45:01 -0700

    Time: 81 ms (96.86%), Space: 16.6 MB (21.15%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.py
+++ 0036-valid-sudoku/0036-valid-sudoku.py
@@ -1,24 +1,27 @@
 class Solution:
     def isValidSudoku(self, board: List[List[str]]) -> bool:
-        
-        # hash map problem
+        # hashmap of sets
         rows = defaultdict(set)
         cols = defaultdict(set)
-        section = defaultdict(set)
+        subgrid = defaultdict(set) 
         
         n, m = len(board), len(board[0])
         
         for i in range(n):
             for j in range(m):
-                curr = board[i][j]
-                if curr == '.': 
-                    continue
-                if curr in rows[i] or curr in cols[j] or curr in section[(i//3, j//3)]:
+                elem = board[i][j]
+                
+                if elem == ".": continue
+                if elem in rows[i] or elem in cols[j] or elem in subgrid[(i//3, j//3)]:
                     return False
                 
-                rows[i].add(curr)
-                cols[j].add(curr)
-                section[(i//3, j//3)].add(curr)
-                
+                rows[i].add(elem)
+                cols[j].add(elem)
+                subgrid[(i//3,j//3)].add(elem)
+        
         return True
                 
+                
+                    
+        
+        
