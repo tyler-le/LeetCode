cd0ea180171b75846ac87100ea2249882f50a77e
Tyler Le <tylerle99@yahoo.com>
2024-04-18 17:06:26 -0700

    Time: 233 ms (78.80%), Space: 19 MB (61.54%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -1,32 +1,29 @@
-class Solution(object):
-    def numIslands(self, grid):
-        """
-        :type grid: List[List[str]]
-        :rtype: int
-        """
-        n, m, res = len(grid), len(grid[0]), 0
+class Solution:
+    def numIslands(self, grid: List[List[str]]) -> int:
         
-        def dfs(r,c):
-            
-            if r < 0 or c < 0 or r >= n or c >= m:
-                return 
-            
-            if grid[r][c] == "0":
-                return 
-            
-            grid[r][c] = "0"
-            
-            dfs(r+1,c)
-            dfs(r-1,c)
-            dfs(r,c+1)
-            dfs(r,c-1)
-            
-            return
-            
         
-        for r in range(n):
-            for c in range(m):
-                if grid[r][c] == "1":
-                    dfs(r,c)
+        def dfs(i, j):
+            # base case
+            if i < 0 or j < 0 or i >= n or j >= m:
+                return
+            
+            if grid[i][j] == "0":
+                return
+            
+            grid[i][j] = "0"
+            
+            # recursive call
+            for di, dj in [(-1,0), (1,0), (0,-1), (0,1)]:
+                r, c = i+di, j+dj
+                dfs(r, c)
+        
+        n, m = len(grid), len(grid[0])
+        res = 0
+        
+        for i in range(n):
+            for j in range(m):
+                if grid[i][j] == "1":
                     res+=1
+                    dfs(i,j)
+        
         return res
