1647b0271f602b8bfe8194a6eb37a1574386f4eb
Tyler Le <tylerle99@yahoo.com>
2024-04-29 18:31:15 -0700

    Time: 1634 ms (26.61%), Space: 61.8 MB (5.09%) - LeetHub

--- 1899-merge-triplets-to-form-target-triplet/1899-merge-triplets-to-form-target-triplet.py
+++ 1899-merge-triplets-to-form-target-triplet/1899-merge-triplets-to-form-target-triplet.py
@@ -1,11 +1,11 @@
 class Solution:
     def mergeTriplets(self, triplets: List[List[int]], target: List[int]) -> bool:
-        res = [-1, -1, -1]
         
-        for x, y, z in triplets:
-            if x <= target[0] and y <= target[1] and z <= target[2]:
-                res[0] = max(res[0], x)
-                res[1] = max(res[1], y)
-                res[2] = max(res[2], z)
-            
-        return res == target
+        curr = [0, 0, 0]
+        for a, b, c in triplets:
+            if a <= target[0] and b <= target[1] and c <= target[2]:
+                curr[0] = max(curr[0], a)
+                curr[1] = max(curr[1], b)
+                curr[2] = max(curr[2], c)
+                
+        return curr == target
