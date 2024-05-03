153edf47260eeeb11caff62197f6775ee5214f71
Tyler Le <tylerle99@yahoo.com>
2024-05-02 21:15:48 -0700

    Time: 46 ms (71.89%), Space: 16.8 MB (87.67%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.py
+++ 0496-next-greater-element-i/0496-next-greater-element-i.py
@@ -1,18 +1,23 @@
 class Solution:
     def nextGreaterElement(self, nums1: List[int], nums2: List[int]) -> List[int]:
-        
         stack = []
         hmap = {}
         res = []
         
         for num in nums2:
-            while stack and num > stack[-1]:
-                x = stack.pop()
-                hmap[x] = num
+            
+            while stack and stack[-1] < num:
+                hmap[stack.pop()] = num
+            
             stack.append(num)
-        
+            
         for num in nums1:
-            if num not in hmap: res.append(-1)
-            else: res.append(hmap[num])
+            if num not in hmap:
+                res.append(-1)
+            else:
+                res.append(hmap[num])
         
         return res
+
+    
+    [6,5,4,3,2,]
