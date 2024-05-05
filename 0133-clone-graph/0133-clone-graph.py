1f1aa355f1e7c6a4dbb605aad37affdf798b6ef7
Tyler Le <tylerle99@yahoo.com>
2024-05-04 18:02:35 -0700

    Time: 39 ms (68.02%), Space: 16.8 MB (85.47%) - LeetHub

--- 0133-clone-graph/0133-clone-graph.py
+++ 0133-clone-graph/0133-clone-graph.py
@@ -9,20 +9,20 @@ class Node:
 from typing import Optional
 class Solution:
     def cloneGraph(self, node: Optional['Node']) -> Optional['Node']:
-        # map node : copy
         hmap = {}
         
-        def dfs(curr):
-            if not curr: return None
-            if curr in hmap: return hmap[curr]
+        def dfs(node):
             
-            copy = Node(curr.val)
-            hmap[curr] = copy
-            nbors = []
+            if not node: return None
+            if node in hmap: return hmap[node]
             
-            for nbor in curr.neighbors:
+            copy = Node(node.val)
+            hmap[node] = copy
+            
+            for nbor in node.neighbors:
                 copy.neighbors.append(dfs(nbor))
             
-            return copy
-            
+            return hmap[node]
+        
         return dfs(node)
+         
