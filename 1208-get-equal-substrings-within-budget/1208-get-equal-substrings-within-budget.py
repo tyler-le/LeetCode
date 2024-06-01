21e0efe324a5895a39aec8acb8e241d4e2c4c903
Tyler Le <tylerle99@yahoo.com>
2024-05-31 19:37:12 -0700

    Time: 62 ms (67.04%), Space: 17.1 MB (79.25%) - LeetHub

--- 1208-get-equal-substrings-within-budget/1208-get-equal-substrings-within-budget.py
+++ 1208-get-equal-substrings-within-budget/1208-get-equal-substrings-within-budget.py
@@ -1,20 +1,21 @@
 class Solution:
     def equalSubstring(self, s: str, t: str, maxCost: int) -> int:
-        # sliding window
-        # contract window when we exceed budget
+        # sliding window, contract window when we exceed the max cost
+        # record the max length substring
         
-        l = 0
         res = 0
-        curr_cost = 0
+        l = 0
         n = len(s)
+        curr = 0
         
         for r in range(n):
-            curr_cost += abs(ord(s[r]) - ord(t[r]))
+            curr+=(abs(ord(s[r]) - ord(t[r])))
             
-            while curr_cost > maxCost:
-                curr_cost -= abs(ord(s[l]) - ord(t[l]))
+            while curr > maxCost:
+                curr-=(abs(ord(s[l]) - ord(t[l])))
                 l+=1
             
-            res = max(res, r-l+1)
+            if curr <= maxCost:
+                res = max(res, r-l+1)
         
         return res
