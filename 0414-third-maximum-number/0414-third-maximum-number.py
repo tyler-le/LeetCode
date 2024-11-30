2b3a740450a64d9ee8d70733dfbadc6e1174f074
Tyler Le <tylerle99@yahoo.com>
2024-11-30 11:23:34 -0800

    Time: 2 ms (41.64%), Space: 18.5 MB (6.37%) - LeetHub

---
+++ 0414-third-maximum-number/0414-third-maximum-number.py
@@ -0,0 +1,16 @@
+class Solution:
+    def thirdMax(self, nums: List[int]) -> int:
+        
+        nums = list(set(nums))
+        
+        nums = list(set([-num for num in nums]))
+        
+        heapify(nums)
+        
+        if len(nums) < 3: return -nums[0]
+        
+        for i in range(2):
+            heappop(nums)
+        
+        return -nums[0]
+        
