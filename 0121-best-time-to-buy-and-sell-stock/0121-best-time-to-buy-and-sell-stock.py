4a52e805c8a067d8664a7e6d9fa2f744572d1204
Tyler Le <tylerle99@yahoo.com>
2024-12-07 22:00:21 -0800

    Time: 87 ms (58.23%), Space: 26.4 MB (25.30%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
@@ -1,16 +1,15 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        # if we encounter a smaller number, we have a new buy day
-        # treat every day as a sell day
-        
-        buy_price = float("inf")
         res = 0
+        curr_profit = 0
+        buy_day = prices[0]
         
-        for i in range(len(prices)):
-    
-            if prices[i] < buy_price:
-                buy_price = prices[i]
-            res = max(res, prices[i] - buy_price)
-            
+        for price in prices:
+            curr_profit = price - buy_day
+            res = max(res, curr_profit)
+            if price < buy_day:
+                buy_day = price
+        
         return res
+            
             
