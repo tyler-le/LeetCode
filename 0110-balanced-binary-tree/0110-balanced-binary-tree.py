33077c2401b513015ffa3f8b1db0c130722b2faf
Tyler Le <tylerle99@yahoo.com>
2024-12-14 21:43:26 -0800

    Time: 3 ms (69.17%), Space: 18.4 MB (10.17%) - LeetHub

--- 0110-balanced-binary-tree/0110-balanced-binary-tree.py
+++ 0110-balanced-binary-tree/0110-balanced-binary-tree.py
@@ -4,26 +4,24 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
+
 class Solution:
     def isBalanced(self, root: Optional[TreeNode]) -> bool:
-        
-        # find the difference of height of left and right subtrees.
-        # repeat recursively on left and rights
-        
-        
-        def dfs(node):
 
-            is_balanced = True
-            if not node: return [0, True]
-            
-            left_height, left_balanced = dfs(node.left)
-            right_height, right_balanced = dfs(node.right)
-                
-            if not left_balanced or not right_balanced or abs(left_height - right_height) > 1:
-                is_balanced = False
-            
-            return [1 + max(left_height, right_height), is_balanced]
-        
-      
-        return dfs(root)[1]
+        # returns (height, isBalanced)
+        def get_height(node):
+            if not node: return (0, True)
+
+            left_height, left_balanced = get_height(node.left)
+            right_height, right_balanced = get_height(node.right)
+
+            is_balanced = abs(left_height - right_height) <= 1 and left_balanced and right_balanced
+
+            return (max(left_height, right_height) + 1, is_balanced)
+
+        _, res = get_height(root)
+        return res
+
+
+
         
