f7f333ea7255278e26f2925c38795bb76dcef489
Tyler Le <tylerle99@yahoo.com>
2026-01-13 16:48:30 -0800

    Time: 19 ms (70.1%), Space: 20.6 MB (9.57%) - LeetHub

--- 0055-jump-game/0055-jump-game.py
+++ 0055-jump-game/0055-jump-game.py
@@ -1,9 +1,21 @@
 class Solution:
     def canJump(self, nums: List[int]) -> bool:
-        n = len(nums)
-        goal = n-1
         
-        for i in range(n-1, -1, -1):
+        n = len(nums)
+        goal = n - 1
+
+        # iterate from right to left
+        # 'goal' starts at last position
+        # 'i' starts at second-to-last position
+        # we will move 'goal' to the left as much as possible 
+        # as long as 'i' can jump to it
+        for i in range(n-2, -1, -1):
+
+
+            # if position 'i' can jump to goal (or even past it)
+            # then set the goal to 'i'
+            # since it is the left most reachable position from the end
             if i + nums[i] >= goal:
                 goal = i
+        
         return goal == 0
