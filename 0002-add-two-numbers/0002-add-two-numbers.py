5706ff1a0be8ab1f07851786b059db0171f1b348
Tyler Le <le.tyler.h@gmail.com>
2026-01-15 21:07:11 -0800

    Time: 8 ms (18.06%), Space: 19.3 MB (15.24%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.py
+++ 0002-add-two-numbers/0002-add-two-numbers.py
@@ -3,29 +3,28 @@
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
+
 class Solution:
     def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
-        
-        power, curr, l1_val = 0, l1, 0
-        while curr:
-            l1_val+=curr.val * (10**power)
+        sum1, sum2 = 0, 0
+
+        def find_sum(node):
+            summ, power = 0, 0
+            while node:
+                summ+=(node.val * 10**power)
+                power+=1
+                node = node.next
+            return summ
+
+        total = find_sum(l1) + find_sum(l2)
+        dummy = ListNode(0)
+        if not total: return dummy
+
+        curr = dummy
+        while total:
+            curr.next = ListNode(total % 10)
             curr = curr.next
-            power+=1
-        
-        power, curr, l2_val = 0, l2, 0
-        while curr:
-            l2_val+=curr.val * (10**power)
-            curr = curr.next
-            power+=1
-            
-        head = ListNode() 
-        curr = head
-        sumi = reversed(str(l1_val + l2_val))
-        for ch in sumi:
-            curr.next = ListNode(int(ch))
-            curr = curr.next
-            
-        return head.next
-            
-        
-        
+            total = total // 10
+
+        return dummy.next
+
