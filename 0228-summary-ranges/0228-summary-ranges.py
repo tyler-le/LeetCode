04e533cfe4130c7b298353fb12c5c659b34088d5
Tyler Le <le.tyler.h@gmail.com>
2026-01-17 17:15:37 -0800

    Time: 0 ms (100%), Space: 19.3 MB (18.09%) - LeetHub

--- 0228-summary-ranges/0228-summary-ranges.py
+++ 0228-summary-ranges/0228-summary-ranges.py
@@ -1,19 +1,19 @@
 class Solution:
     def summaryRanges(self, nums: List[int]) -> List[str]:
-        l, r, n, res = 0, 0, len(nums), []
 
+        l, r, n = 0, 0, len(nums)
+        res = []
 
         while r < n:
             l = r
 
+            # move r until nonsequential
             while (r + 1 < n) and nums[r] + 1 == nums[r+1]:
                 r+=1
             
-            if l == r: res.append(f"{nums[l]}")
+            if r == l: res.append(f"{nums[r]}")
             else: res.append(f"{nums[l]}->{nums[r]}")
-            r+=1
-        
-        return res
-            
-        
 
+            r+=1
+
+        return res
