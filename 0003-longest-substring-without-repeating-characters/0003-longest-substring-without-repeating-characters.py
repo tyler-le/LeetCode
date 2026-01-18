5aac6020d9cab534461ecc987daf518fa2e5223a
Tyler Le <le.tyler.h@gmail.com>
2026-01-18 12:48:07 -0800

    Time: 13 ms (78.45%), Space: 19.3 MB (20.62%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
@@ -1,19 +1,17 @@
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
-        # scan thru s
-        # keep track of character counts
-        # if a character count > 2, shrink window from left until valid window
-        # record width and take the max
-        
-        l = 0
         res = 0
+        l = 0
+        n = len(s)
         hmap = defaultdict(int)
-        
-        for r in range(len(s)):
-            hmap[s[r]]+=1    
+
+        for r in range(n):
+            hmap[s[r]]+=1
+
             while hmap[s[r]] > 1:
                 hmap[s[l]]-=1
                 l+=1
-            res = max(res, r-l+1)
             
+            res = max(res, r-l+1)
+        
         return res
