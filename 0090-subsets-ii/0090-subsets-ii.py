a8cb30131728973bc63e051aa8de6af530f33fb1
Tyler Le <le.tyler.h@gmail.com>
2026-01-17 17:56:29 -0800

    Time: 0 ms (100%), Space: 19.5 MB (17.25%) - LeetHub

--- 0090-subsets-ii/0090-subsets-ii.py
+++ 0090-subsets-ii/0090-subsets-ii.py
@@ -1,22 +1,28 @@
 class Solution:
     def subsetsWithDup(self, nums: List[int]) -> List[List[int]]:
-        res = []
-        
-        def backtrack(path, idx):
-            if idx == len(nums):
+        res, n = [], len(nums)
+        def backtrack(path, index):
+            nonlocal res, n
+
+            if index >= n:
                 res.append(path.copy())
                 return
             
+            # include nums[index]
+            backtrack(path + [nums[index]], index + 1)
+
+            # once we've included nums[index] in our subset, 
+            # we've explored all choices with nums[index] anchored 
+            # at this position in our path
+            while (index + 1 < n) and (nums[index] == nums[index + 1]):
+                index+=1
             
-            cand = nums[idx]
-            
-            backtrack(path+[cand], idx+1)
-            
-            while idx+1 < len(nums) and nums[idx] == nums[idx+1]:
-                idx+=1
-            
-            backtrack(path, idx+1)
-        
+
+            # so when we exlude nums[index], 
+            # we should not explore any more choices with nums[index]
+            # hence we need to skip them
+            backtrack(path, index + 1)
+
         nums.sort()
         backtrack([], 0)
         return res
