3085cf87bf5eae3a4b1eafc960d287b6933f2cff
Tyler Le <le.tyler.h@gmail.com>
2026-01-20 17:20:35 -0800

    Time: 131 ms (58.73%), Space: 77.2 MB (85.5%) - LeetHub

--- 0146-lru-cache/0146-lru-cache.py
+++ 0146-lru-cache/0146-lru-cache.py
@@ -1,27 +1,60 @@
+class Node:
+    def __init__(self, key, value):
+        self.key = key
+        self.value = value
+        self.next = None
+        self.prev = None
+
+class DLL:
+    def __init__(self):
+        self.lru = Node(-1, -1)
+        self.mru = Node(-1,-1)
+        self.lru.next = self.mru
+        self.mru.prev = self.lru
+    
+    def insert(self, node):
+        prev = self.mru.prev
+        node.next = self.mru
+        self.mru.prev = node
+        prev.next = node
+        node.prev = prev
+
+
+    def remove(self, node):
+        prv, nxt = node.prev, node.next
+        prv.next = nxt
+        nxt.prev = prv
+
+
 class LRUCache:
 
     def __init__(self, capacity: int):
-        self.size = capacity
-        self.hmap = collections.defaultdict(int)
-        self.deck = deque()
+        self.dll = DLL()
+        self.capacity = capacity
+        self.hmap = {}
         
 
     def get(self, key: int) -> int:
         if key not in self.hmap: return -1
-        self.deck.remove(key)
-        self.deck.append(key)
-        return self.hmap[key]
+        node = self.hmap[key]
+        self.dll.remove(node)
+        self.dll.insert(node)
+        return node.value
+        
 
     def put(self, key: int, value: int) -> None:
-                
         if key in self.hmap:
-            self.deck.remove(key)
-        elif len(self.deck) == self.size:
-            popped = self.deck.popleft()
-            del self.hmap[popped]
-        self.deck.append(key)
-        self.hmap[key] = value
-            
+            node = self.hmap[key]
+            self.dll.remove(node)
+        
+        node = Node(key, value)
+        self.hmap[key] = node
+        self.dll.insert(node)
+
+        if len(self.hmap) > self.capacity:
+            lru = self.dll.lru.next
+            self.dll.remove(lru)
+            del self.hmap[lru.key]
         
 
 
