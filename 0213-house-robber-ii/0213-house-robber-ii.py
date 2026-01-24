3730e45f9e3d374d4e262eca2d92625e18a15ca9
Tyler Le <le.tyler.h@gmail.com>
2026-01-23 18:40:46 -0800

    Time: 0 ms (100%), Space: 19.5 MB (10.24%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.py
+++ 0213-house-robber-ii/0213-house-robber-ii.py
@@ -1,16 +1,19 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
-        def house_robber(sublist):
-            n = len(sublist)
-            
-            dp = [float('inf') for _ in range(n)]
-            dp[0], dp[1] = sublist[0], max(sublist[0], sublist[1])
-            
-            for i in range(2, n):
+        n = len(nums)
+
+        def rob(sublist):
+            # dp[i] = max(dp[i-1] , dp[i-2] + nums[i])
+
+            dp = [0 for _ in range(len(sublist))]
+            dp[0] = sublist[0]
+            dp[1] = max(sublist[0], sublist[1])
+
+            for i in range(2, len(sublist)):
                 dp[i] = max(dp[i-1], dp[i-2] + sublist[i])
-            
+
             return dp[-1]
-        
+
         if len(nums) == 1: return nums[0]
         if len(nums) == 2: return max(nums[0], nums[1])
-        return max(house_robber(nums[1:]), house_robber(nums[0:-1]))
+        return max(rob(nums[:n-1]), rob(nums[1:]))
