886218d73b1db4728c2ebeeebe090638be618c47
Tyler Le <le.tyler.h@gmail.com>
2026-01-27 19:29:45 -0800

    Time: 3 ms (87.18%), Space: 22.4 MB (15.01%) - LeetHub

--- 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.py
+++ 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.py
@@ -4,23 +4,24 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
+
 class Solution:
     def diameterOfBinaryTree(self, root: Optional[TreeNode]) -> int:
-        max_diam = 0
+        # get_height() dfs for each node
+        # diameter = left_height + right_height + 1
+        # keep a global max
+
+        res = 0
+        def dfs(node):
+            nonlocal res
+            if not node: return 0
+
+            left_height = dfs(node.left)
+            right_height = dfs(node.right)
+            diameter = left_height + right_height
+            res = max(res, diameter)
+
+            return 1 + max(left_height, right_height)
         
-        # find longest depth rooted at node
-        def get_height(node):
-            nonlocal max_diam
-            
-            if not node:
-                return 0
-            
-            left = get_height(node.left)
-            right = get_height(node.right)
-            
-            max_diam = max(max_diam, left + right)
-            
-            return 1 + max(left, right)
-            
-        get_height(root)
-        return max_diam
+        dfs(root)
+        return res
