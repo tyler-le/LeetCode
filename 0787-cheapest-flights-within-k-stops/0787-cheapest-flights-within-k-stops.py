124f95b9b0c84ae91fd7476bd36c21b9bff1647e
Tyler Le <le.tyler.h@gmail.com>
2026-01-30 21:08:22 -0800

    Time: 0 ms (100%), Space: 20.9 MB (19.14%) - LeetHub

--- 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.py
+++ 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.py
@@ -1,34 +1,57 @@
 class Solution:
     def findCheapestPrice(self, n: int, flights: List[List[int]], src: int, dst: int, k: int) -> int:
 
+        # dijkstras (out-of-the-box) doesn't work here because of its greediness.
+        # it only takes the path to a node with the cheapest price
+        # but sometimes, we might want to choose a path with a larger price and fewer stops
+
+        # hence we need to modify djikstras to add to the heap when:
+            # 1. Found path w/ shorter distance (normal)
+            # 2. Found path w/ less stops
+
+        # (price, node, num_stops)
+        min_heap = [(0, src, 0)]
+        
+        # create graph
         graph = defaultdict(list)
-        min_heap = [(0, 0, src)] # cost, num_stops, node
-        res = math.inf
-        min_costs_seen = [math.inf for _ in range(n)]
-        min_stops_seen = [math.inf for _ in range(n)]
-        
+        for u, v, price in flights:
+            graph[u].append((v, price))
 
-        for u, v, w in flights:
-            graph[u].append((v, w))
-        
+        prices, stops = {}, {}
+        for node in range(n): 
+            prices[node] = math.inf
+            stops[node] = math.inf
+
+        prices[src], stops[src] = 0, 0
+
         while min_heap:
-            popped_cost, popped_stops, popped_node = heappop(min_heap)
 
-            if popped_node == dst: 
-                res = min(res, popped_cost)
-                continue
+            # this stop is valid, so record the min price
+            popped_price, popped_node, popped_stops = heappop(min_heap)
+            if popped_node == dst: return popped_price
+            if popped_stops > k: continue
+            
+            for nbor, weight in graph[popped_node]:
 
-            for nbor_node, nbor_cost in graph[popped_node]:
-                cost = popped_cost + nbor_cost
-                stops = popped_stops + 1
-                
-                if stops > k + 1: continue
-                
-                if cost < min_costs_seen[nbor_node] or stops < min_stops_seen[nbor_node]:
-                    # print(f"found a more optimal path")
-                    min_costs_seen[nbor_node] = cost
-                    min_stops_seen[nbor_node] = stops
-                    heappush(min_heap, (cost, stops, nbor_node))
+                new_price = popped_price + weight
+                new_stops = popped_stops + 1
 
-        return res if res != math.inf else -1               
+                # We explore a new state IFF it improves EITHER:
+                #  - we find a cheaper price, OR
+                #  - fewer stops (regardless of price)
+
+                # Inversely, we will NOT explore a path if it is 
+                # - more expensive, AND 
+                # - has more stops 
+                # than what we've currently seen
                 
+                if new_price < prices[nbor] or new_stops < stops[nbor]:
+
+                    # Update pruning thresholds.
+                    # These do NOT represent the final answer.
+                    # They simply mark whether future states are worth exploring (see above).
+                    prices[nbor], stops[nbor] = new_price, new_stops
+                    heappush(min_heap, (popped_price + weight, nbor, popped_stops + 1))
+            
+        return -1
+            
