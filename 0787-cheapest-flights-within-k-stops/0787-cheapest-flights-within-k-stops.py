f05725ea78248c2fc9b1f46f480b0bad9183a491
Tyler Le <le.tyler.h@gmail.com>
2026-01-30 21:08:11 -0800

    Time: 1675 ms (5.39%), Space: 36.7 MB (5.14%) - LeetHub

--- 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.py
+++ 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.py
@@ -1,24 +1,34 @@
 class Solution:
     def findCheapestPrice(self, n: int, flights: List[List[int]], src: int, dst: int, k: int) -> int:
-        # convert to adjacency list
+
         graph = defaultdict(list)
+        min_heap = [(0, 0, src)] # cost, num_stops, node
+        res = math.inf
+        min_costs_seen = [math.inf for _ in range(n)]
+        min_stops_seen = [math.inf for _ in range(n)]
+        
+
         for u, v, w in flights:
             graph[u].append((v, w))
-            
-        # (node, weight, steps)
-        q = deque([(src, 0, 0)])
         
-        # node : distance from src map
-        distances = defaultdict(lambda : math.inf)
-        res = math.inf
-        
-        while q:
-            popped_node, popped_dist, popped_steps = q.popleft()
-            distances[popped_node] = min(distances[popped_node], popped_dist)
-            if popped_node == dst: res = min(res, popped_dist)
-            
-            for nbor, weight in graph[popped_node]:
-                if popped_steps < k+1 and popped_dist + weight < distances[nbor]: 
-                    q.append((nbor, popped_dist + weight, popped_steps + 1))
-            
-        return res if res != math.inf else -1
+        while min_heap:
+            popped_cost, popped_stops, popped_node = heappop(min_heap)
+
+            if popped_node == dst: 
+                res = min(res, popped_cost)
+                continue
+
+            for nbor_node, nbor_cost in graph[popped_node]:
+                cost = popped_cost + nbor_cost
+                stops = popped_stops + 1
+                
+                if stops > k + 1: continue
+                
+                if cost < min_costs_seen[nbor_node] or stops < min_stops_seen[nbor_node]:
+                    # print(f"found a more optimal path")
+                    min_costs_seen[nbor_node] = cost
+                    min_stops_seen[nbor_node] = stops
+                    heappush(min_heap, (cost, stops, nbor_node))
+
+        return res if res != math.inf else -1               
+                
