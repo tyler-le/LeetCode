df315be1eed6e062f29627b18e73f1113067e1eb
Tyler Le <le.tyler.h@gmail.com>
2026-02-01 19:51:32 -0800

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0846-hand-of-straights/0846-hand-of-straights.py
+++ 0846-hand-of-straights/0846-hand-of-straights.py
@@ -1,27 +1,19 @@
 class Solution:
     def isNStraightHand(self, hand: List[int], groupSize: int) -> bool:
-        if len(hand) % groupSize != 0: return False
-        
-
         hand.sort()
-        res = []
-        n = len(hand)
-        hmap = Counter(hand)
-    
-        
-        for num in hand:
-            # Skip this iteration if the current card count is zero 
-            # since it has previously been allocated
-            if not hmap[num]: continue
-        
-            # Attempt to form a group starting from the current card
-            for i in range(num, num+groupSize):
-                 
-                # Required card for the group is missing or depleted
-                if i not in hmap or not hmap[i]: return False
-                
-                # Else use the code
-                else: hmap[i]-=1
-        
-        return True
+        groups = 0
+        cnt = Counter(hand)
+
+        for card in hand:
+            if not cnt[card]: continue
+            is_valid = True
+
+            for i in range(card, card + groupSize):
+                if not cnt[i]: 
+                    is_valid = False
+                else:
+                    cnt[i]-=1
             
+            if is_valid: groups+=1
+        
+        return groups * groupSize == len(hand)
