a411c3ddcf3e26eaffda30635f4bd39eb4d42695
Tyler Le <le.tyler.h@gmail.com>
2026-02-04 19:37:22 -0800

    Time: 79 ms (85.15%), Space: 31.7 MB (37.73%) - LeetHub

--- 2962-count-subarrays-where-max-element-appears-at-least-k-times/2962-count-subarrays-where-max-element-appears-at-least-k-times.py
+++ 2962-count-subarrays-where-max-element-appears-at-least-k-times/2962-count-subarrays-where-max-element-appears-at-least-k-times.py
@@ -1,21 +1,19 @@
 class Solution:
     def countSubarrays(self, nums: List[int], k: int) -> int:
-        # count total subarrays n(n+1) // 2
-        # count the number of subarrays where max element appears less than k times
-
-        mx, mx_freq = max(nums), 0
-        l, n = 0, len(nums)
+        mx = max(nums)
+        mx_freq = 0
+        l = 0
+        n = len(nums)
         res = 0
 
         for r in range(n):
-            if nums[r] == mx:
-                mx_freq+=1
-            
+            if nums[r] == mx: mx_freq+=1
+
             while mx_freq == k:
                 if nums[l] == mx:
                     mx_freq-=1
                 l+=1
-
+            
             res+=l
         
         return res
