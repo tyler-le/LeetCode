307e63b8872ee44d2f237d06a6a0aef359170d5f
Tyler Le <le.tyler.h@gmail.com>
2026-02-08 20:08:18 -0800

    Time: 3 ms (68.27%), Space: 19.5 MB (21.01%) - LeetHub

--- 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.py
+++ 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.py
@@ -4,12 +4,12 @@ class Solution:
         dp = [math.inf for _ in range(n+1)] # dp[n] is the top
         
         # dp[i] = cost to reach step i
-        dp[0] = 0
-        dp[1] = 0
+        dp[0] = 0 # we can start here for free
+        dp[1] = 0 # we can start here for free
 
         for i in range(2, n+1):
-            one_step = dp[i-1] + cost[i-1]
-            two_step = dp[i-2] + cost[i-2]
+            one_step = dp[i-1] + cost[i-1] # how much it took to reach step i-1 and the cost to jump from i-1 to i
+            two_step = dp[i-2] + cost[i-2] # how much it took to reach step i-2 and the cost to jump from i-2 to i
             dp[i] = min(two_step, one_step)
 
         return dp[n]
