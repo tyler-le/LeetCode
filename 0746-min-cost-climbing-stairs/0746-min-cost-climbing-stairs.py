72dc58abc0b98f2276dff2d9f662ab808fba190d
Tyler Le <le.tyler.h@gmail.com>
2026-02-08 20:06:34 -0800

    Time: 4 ms (35.25%), Space: 19.3 MB (61.37%) - LeetHub

--- 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.py
+++ 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.py
@@ -1,14 +1,15 @@
 class Solution:
     def minCostClimbingStairs(self, cost: List[int]) -> int:
-        cost.append(0)
         n = len(cost)
-        dp = [math.inf for _ in range(n)]
-        if n == 0: return 0
-        if n == 1: return cost[0]
-
-        dp[0], dp[1] = cost[0], cost[1]
-
-        for i in range(2, n):
-            dp[i] = cost[i] + min(dp[i-1], dp[i-2])
+        dp = [math.inf for _ in range(n+1)] # dp[n] is the top
         
-        return dp[-1]
+        # dp[i] = cost to reach step i
+        dp[0] = 0
+        dp[1] = 0
+
+        for i in range(2, n+1):
+            one_step = dp[i-1] + cost[i-1]
+            two_step = dp[i-2] + cost[i-2]
+            dp[i] = min(two_step, one_step)
+
+        return dp[n]
