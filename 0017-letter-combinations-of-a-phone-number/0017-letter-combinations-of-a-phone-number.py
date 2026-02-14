2dfa3e594971ba85590fb6a30571b45b9aef0377
Tyler Le <le.tyler.h@gmail.com>
2026-02-13 17:43:22 -0800

    Time: 0 ms (100%), Space: 19.5 MB (7.12%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.py
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.py
@@ -1,30 +1,29 @@
 class Solution:
     def letterCombinations(self, digits: str) -> List[str]:
-        hmap = {
-            "2" : ["a", "b", "c"],
-            "3" : ["d", "e", "f"],
-            "4" : ["g", "h", "i"],
-            "5" : ["j", "k", "l"],
-            "6" : ["m", "n", "o"],
-            "7" : ["p", "q", "r", "s"],
-            "8" : ["t", "u", "v"],
-            "9" : ["w", "x", "y", "z"]
-        }
-        res = []
-        if not digits: return res
-        
-        def backtrack(idx, curr):
-            if idx >= len(digits): 
-                res.append("".join(curr))
+        res, n = [], len(digits)
+
+        hmap = defaultdict(str)
+        hmap["2"] = ["a", "b", "c"]
+        hmap["3"] = ["d", "e", "f"]
+        hmap["4"] = ["g", "h", "i"]
+        hmap["5"] = ["j", "k", "l"]
+        hmap["6"] = ["m", "n", "o"]
+        hmap["7"] = ["p", "q", "r", "s"]
+        hmap["8"] = ["t", "u", "v"]
+        hmap["9"] = ["w", "x", "y", "z"]
+
+        def backtrack(index, path):
+            nonlocal res, n
+            # base case
+            if index >= n:
+                res.append("".join(path.copy()))
                 return
 
-            for x in hmap[digits[idx]]:
-                curr.append(x)
-                backtrack(idx+1, curr)
-                curr.pop()
-            
-            
-            
+            # recursive case
+            for ch in hmap[digits[index]]:
+                path.append(ch)
+                backtrack(index + 1, path)
+                path.pop()
+        
         backtrack(0, [])
         return res
-            
