448bb1b52a1d289567161177a8cf4c63fb0c07c2
Tyler Le <le.tyler.h@gmail.com>
2026-02-16 17:08:56 -0800

    Time: 3226 ms (5.01%), Space: 19.6 MB (81.22%) - LeetHub

--- 3719-longest-balanced-subarray-i/3719-longest-balanced-subarray-i.py
+++ 3719-longest-balanced-subarray-i/3719-longest-balanced-subarray-i.py
@@ -3,14 +3,15 @@ class Solution:
         n = len(nums)
         res = 0
 
-        for i in range (n):
-            evens = set()
-            odds = set()
+        for i in range(n):
+            even_map = defaultdict(int)
+            odd_map = defaultdict(int)
+
             for j in range(i, n):
                 num = nums[j]
-                if num % 2: odds.add(num)
-                else: evens.add(num)
-                
-                if len(odds) == len(evens): res = max(res, j - i + 1)
-        
+                if num % 2: odd_map[num]+=1
+                else: even_map[num]+=1
+            
+                if len(even_map.values()) == len(odd_map.values()):
+                    res = max(res, j-i+1)
         return res
