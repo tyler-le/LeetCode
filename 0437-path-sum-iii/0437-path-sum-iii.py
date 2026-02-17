c56ad2aa3414e407056f3864509fed562dd4c75a
Tyler Le <le.tyler.h@gmail.com>
2026-02-16 18:10:24 -0800

    Time: 3 ms (89.38%), Space: 20.5 MB (36.43%) - LeetHub

--- 0437-path-sum-iii/0437-path-sum-iii.py
+++ 0437-path-sum-iii/0437-path-sum-iii.py
@@ -13,18 +13,26 @@ class Solution:
         def dfs(node, curr_sum):
             nonlocal prefix_sums, res
             if not node: return 0
+
+            # add node.val to current sum
             curr_sum+=node.val
 
+            # check if currSum - targetSum in map.
+            # if it is, then BOTH (curr_sum - targetSum) and (targetSum) EXIST in the path
+            # because curr_sum = (curr_sum - targetSum) + (targetSum)
             if curr_sum - targetSum in prefix_sums:
                 res+=prefix_sums[curr_sum - targetSum]
             
+            # add the prefix current sum to map
             prefix_sums[curr_sum]+=1
 
+            # backtrack on left and right subtrees
             if node.left: dfs(node.left, curr_sum)
             if node.right: dfs(node.right, curr_sum)
 
+            # undo our choice after exploring subtrees
             prefix_sums[curr_sum]-=1
-            # curr_sum-=node.val
+            curr_sum-=node.val
         
         dfs(root, 0)
         return res
