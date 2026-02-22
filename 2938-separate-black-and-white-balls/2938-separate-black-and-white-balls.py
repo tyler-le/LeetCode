14891c0c9b1090de56ff270abec689dbced7411d
Tyler Le <le.tyler.h@gmail.com>
2026-02-22 13:10:26 -0800

    Time: 43 ms (94.1%), Space: 20.2 MB (43.89%) - LeetHub

--- 2938-separate-black-and-white-balls/2938-separate-black-and-white-balls.py
+++ 2938-separate-black-and-white-balls/2938-separate-black-and-white-balls.py
@@ -1,16 +1,12 @@
 class Solution:
     def minimumSteps(self, s: str) -> int:
-        # find right most 0 index
-
-        # find left most 1 index
-
         zeros_seen = 0
         res = 0
 
-        for ch in s[::-1]:
+        for ch in s:
             if ch == "0":
-                zeros_seen+=1
-            else:
                 res+=zeros_seen
+            else:
+                zeros_seen+=1
         
         return res
