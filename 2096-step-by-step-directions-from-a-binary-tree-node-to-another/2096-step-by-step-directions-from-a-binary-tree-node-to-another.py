ea85f925781087b0b21de38e44d58d12b1257e51
Tyler Le <le.tyler.h@gmail.com>
2026-02-22 21:30:19 -0800

    Time: 592 ms (6.39%), Space: 121.6 MB (5.05%) - LeetHub

---
+++ 2096-step-by-step-directions-from-a-binary-tree-node-to-another/2096-step-by-step-directions-from-a-binary-tree-node-to-another.py
@@ -0,0 +1,41 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def getDirections(self, root: Optional[TreeNode], startValue: int, destValue: int) -> str:
+        UP, LEFT, RIGHT = "U", "L", "R"
+        graph = defaultdict(list) # (node, edge type)
+
+        def dfs(node, parent):
+            nonlocal graph
+            if not node: return
+
+            if parent: graph[node.val].append((parent.val, UP))
+
+            if node.left:
+                dfs(node.left, node)
+                graph[node.val].append((node.left.val, LEFT))
+            if node.right:
+                dfs(node.right, node)
+                graph[node.val].append((node.right.val, RIGHT))
+        
+        dfs(root, None)
+        
+        
+        # run bfs on start -> dest
+        q = deque([(startValue, "")]) # (node, curr_path)
+        res = []
+        visited = set()
+
+        while q:
+            popped_node, popped_path = q.popleft()
+        
+            for nbor_node, nbor_edge_type in graph[popped_node]:
+                if nbor_node in visited: continue
+                if nbor_node == destValue: return popped_path + nbor_edge_type
+                
+                q.append((nbor_node, popped_path + nbor_edge_type))
+                visited.add(nbor_node)
