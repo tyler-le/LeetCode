d3c11bdc74f5b6165299ff758867334c900d918b
Tyler Le <le.tyler.h@gmail.com>
2026-02-22 16:16:16 -0800

    Time: 7 ms (83.05%), Space: 23.9 MB (47.15%) - LeetHub

--- 0124-binary-tree-maximum-path-sum/0124-binary-tree-maximum-path-sum.py
+++ 0124-binary-tree-maximum-path-sum/0124-binary-tree-maximum-path-sum.py
@@ -12,15 +12,18 @@ class Solution:
         # return (longest path from this node)
         def dfs(node):
             nonlocal res
-
             if not node: return 0
             
+            # only take left or right if it is positive
+            # if it is negative, we might as well NOT take it
             left = max(dfs(node.left), 0)
             right = max(dfs(node.right), 0)
 
-            
+            # calculate the result of the longest path with this node at the root
             res = max(res, node.val + left + right)
 
+            # return longest single-branch path starting at this node.
+            # because parent CANNOT use both branch-paths. 
             return node.val + max(left, right)
         
         dfs(root)
