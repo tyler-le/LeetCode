781bdcfe37e6ebdbbbc0207e3aba1e94e693120d
Tyler Le <le.tyler.h@gmail.com>
2026-02-24 19:10:17 -0800

    Time: 175 ms (16.55%), Space: 20 MB (66.91%) - LeetHub

--- 2038-remove-colored-pieces-if-both-neighbors-are-the-same-color/2038-remove-colored-pieces-if-both-neighbors-are-the-same-color.py
+++ 2038-remove-colored-pieces-if-both-neighbors-are-the-same-color/2038-remove-colored-pieces-if-both-neighbors-are-the-same-color.py
@@ -1,14 +1,11 @@
 class Solution:
     def winnerOfGame(self, colors: str) -> bool:
-        n = len(colors)
         alice, bob = 0, 0
+
+        n = len(colors)
+
         for i in range(1, n-1):
-            
-            if colors[i-1] == colors[i] == colors[i+1]:
-                if colors[i] == "A":
-                    alice+=1
-                else:
-                    bob+=1
-                
+            if colors[i-1] == colors[i] == colors[i+1] == "A": alice+=1
+            elif colors[i-1] == colors[i] == colors[i+1] == "B": bob+=1
+        
         return alice > bob
-                
