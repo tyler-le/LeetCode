a36249799032235bc20bdd72eb67134739af8419
Tyler Le <le.tyler.h@gmail.com>
2026-02-26 19:15:42 -0800

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0494-target-sum/0494-target-sum.py
+++ 0494-target-sum/0494-target-sum.py
@@ -1,26 +1,28 @@
 class Solution:
     def findTargetSumWays(self, nums: List[int], target: int) -> int:
-        n = len(nums)
-        res = 0
-        state = defaultdict(int) # (path, index) : num ways to solve that state
         
-        def backtrack(curr, index):
-            nonlocal res
-            acc = 0
-            
-            if (curr, index) in state:
-                return state[(curr, index)]
-            
+        res, n = 0, len(nums)
+        cache = {}
+
+        def backtrack(index, curr_sum):
+            nonlocal res, n, cache
+
+            cnt = 0
+
             if index == n:
-                if curr == target: return 1
-                else: return 0
+                if curr_sum == target: 
+                    return 1
+                else:
+                    return 0
+
+            if (index, curr_sum) in cache:
+                res+=cache[(index, curr_sum)]
+
+            cnt+=backtrack(index + 1, curr_sum + nums[index])
+            cnt+=backtrack(index + 1, curr_sum - nums[index])
+            cache[(index, curr_sum)] = cnt
+
+            return cnt
             
-            acc+=backtrack(curr + nums[index], index+1)
-            acc+=backtrack(curr - nums[index], index+1)
-            
-            state[(curr, index)] = acc
-            
-            return acc
         
-        return backtrack(0, 0)
-            
+        return backtrack(index = 0, curr_sum = 0)
