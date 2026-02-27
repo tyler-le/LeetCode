933f2175ef98db9ac29718ecc4955c4438966035
Tyler Le <le.tyler.h@gmail.com>
2026-02-26 18:52:02 -0800

    Time: 3 ms (92.07%), Space: 20.7 MB (7.81%) - LeetHub

--- 0152-maximum-product-subarray/0152-maximum-product-subarray.py
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.py
@@ -1,17 +1,34 @@
 class Solution:
     def maxProduct(self, nums: List[int]) -> int:
-        res = -math.inf
-        curr_prod = 1
+        left_to_right = []
+        right_to_left = deque()
+        res = nums[0]
+        n = len(nums)
+        num_negatives = 0
 
+        curr = 1
         for num in nums:
-            curr_prod*=num
-            res = max(res, curr_prod)
-            if not curr_prod: curr_prod = 1
-        
-        curr_prod = 1
+            if num < 0: num_negatives+=1
+            curr*=num
+            left_to_right.append(curr)
+            if not curr: curr = 1
+
+        curr = 1
         for num in nums[::-1]:
-            curr_prod*=num
-            res = max(res, curr_prod)
-            if not curr_prod: curr_prod = 1
+            curr*=num
+            right_to_left.appendleft(curr)
+            if not curr: curr = 1
+
+        # even num_negatives
+        if not num_negatives % 2:
+            return max(max(left_to_right), max(right_to_left))
+                
         
+        else:
+            for i in range(n):
+                prefix = left_to_right[i-1] if i-1 >= 0 else -math.inf
+                suffix = right_to_left[i+1] if i+1 < n else -math.inf
+                res = max(res, prefix, suffix)
+
         return res
+    
