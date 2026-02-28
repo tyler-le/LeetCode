a110dff5da3cd36489fb81017153b01065c6cc27
Tyler Le <le.tyler.h@gmail.com>
2026-02-27 20:11:54 -0800

    Time: 125 ms (96.81%), Space: 43.6 MB (6.34%) - LeetHub

--- 0295-find-median-from-data-stream/0295-find-median-from-data-stream.py
+++ 0295-find-median-from-data-stream/0295-find-median-from-data-stream.py
@@ -1,39 +1,40 @@
-from heapq import heappop, heappush, heappop_max, heappush_max
+from heapq import heappush, heappop, heappush_max, heappop_max
 
 class MedianFinder:
 
     def __init__(self):
         self.left_heap = [] # max heap
-        self.right_heap = [] # min heap
+        self.right_heap = [] # min_heap
         
 
     def addNum(self, num: int) -> None:
-        # insert num into one of the heaps
-        if not self.left_heap or num <= self.left_heap[0]:
-            heappush_max(self.left_heap, num)
-        else:
+        # initially add to heap
+
+        # add to right heap
+        if self.left_heap and num > self.left_heap[0]:
             heappush(self.right_heap, num)
         
-        # rebalance from left to right
-        if len(self.left_heap) > len(self.right_heap) + 1:
-            popped = heappop_max(self.left_heap)
-            heappush(self.right_heap, popped)
+        # else add to left heap
+        else:
+            heappush_max(self.left_heap, num)
 
-        # rebalance from right to left
-        elif len(self.right_heap) > len(self.left_heap):
-            popped = heappop(self.right_heap)
-            heappush_max(self.left_heap, popped)
+        # rebalance - right heap too big
+        if len(self.right_heap) > len(self.left_heap):
+            heappush_max(self.left_heap, heappop(self.right_heap))
+
+        # rebalance - left heap too big
+        elif len(self.left_heap) > len(self.right_heap) + 1:
+            heappush(self.right_heap, heappop_max(self.left_heap))
         
 
     def findMedian(self) -> float:
-        if len(self.left_heap) == len(self.right_heap):
-            return (self.left_heap[0] + self.right_heap[0]) / 2
-        else:
+
+        # odd length -> top of left heap
+        if len(self.left_heap) != len(self.right_heap):
             return self.left_heap[0]
+
+        # even length -> top of both / 2
+        else:
+            return (self.left_heap[0] + self.right_heap[0]) / 2
         
-
-
-# Your MedianFinder object will be instantiated and called as such:
-# obj = MedianFinder()
-# obj.addNum(num)
-# param_2 = obj.findMedian()
+        
