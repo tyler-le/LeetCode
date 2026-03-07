d9487d1f7cdc60eab7fab5b915bd07b301948136
Tyler Le <le.tyler.h@gmail.com>
2026-03-07 15:18:16 -0800

    Time: 0 ms (100%), Space: 19.6 MB (28.6%) - LeetHub

--- 0078-subsets/0078-subsets.py
+++ 0078-subsets/0078-subsets.py
@@ -1,18 +1,22 @@
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
+        
         res = []
         n = len(nums)
 
-        def backtrack(index, path):
+        def backtrack(start, path):
+
             nonlocal res, n
 
-            if index >= n: 
-                res.append(path.copy())
-                return 
+            # this actually never happens because the for-loop is bounded
+            if start > n: return
+            
+            res.append(path.copy())
 
-            IN = backtrack(index + 1, path + [nums[index]])
-            OUT = backtrack(index + 1, path)
+            for i in range(start, n):
+                path.append(nums[i])
+                backtrack(i + 1, path)
+                path.pop()
         
         backtrack(0, [])
-
         return res
