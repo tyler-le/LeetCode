d29cbe45c321f8d71c344a58866f94a96934b110
Tyler Le <le.tyler.h@gmail.com>
2026-03-08 14:09:55 -0700

    Time: 3 ms (24.9%), Space: 19.6 MB (37.06%) - LeetHub

--- 1980-find-unique-binary-string/1980-find-unique-binary-string.py
+++ 1980-find-unique-binary-string/1980-find-unique-binary-string.py
@@ -3,16 +3,15 @@ class Solution:
         nums = set(nums)
         n = len(nums)
 
-        def backtrack(index, path):
+        def backtrack(start, path):
             nonlocal nums, n
-
-            if index == n:
+            if len("".join(path.copy())) == n:
                 if "".join(path.copy()) not in nums:
                     return "".join(path.copy())
-                
                 return
 
-            return backtrack(index + 1, path + ["0"]) or backtrack(index + 1, path + ["1"])
+            for i in range(start, n):
+                return backtrack(i + 1, path + ["0"]) or backtrack(i + 1, path + ["1"])
         
         return backtrack(0, [])
 
