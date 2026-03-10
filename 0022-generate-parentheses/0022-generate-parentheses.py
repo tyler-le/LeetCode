c8a305200dc60d3a8938aa771be6e040530fd965
Tyler Le <le.tyler.h@gmail.com>
2026-03-09 18:06:31 -0700

    Time: 0 ms (100%), Space: 19.5 MB (33.77%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.py
+++ 0022-generate-parentheses/0022-generate-parentheses.py
@@ -1,35 +1,19 @@
 class Solution:
     def generateParenthesis(self, n: int) -> List[str]:
-
-        # base case
-        # - have n open and n closed parens
-
-        # in each step, we have two choices
-        # - place "(" only if num_open < n
-        # - place ")" only if num_open > num_closed
-
-
         res = []
 
         def backtrack(path, num_open, num_closed):
+            nonlocal res 
 
             if num_open == n and num_closed == n:
-                res.append("".join(path))
+                res.append("".join(path.copy()))
                 return
             
-            if num_open < n:
-                path.append("(")
-                backtrack(path, num_open+1, num_closed)
-                path.pop()
+            if num_open < n: 
+                backtrack(path + ["("], num_open + 1, num_closed)
 
             if num_open > num_closed:
-                path.append(")")
-                backtrack(path, num_open, num_closed+1)
-                path.pop()
-            
-
-
+                backtrack(path + [")"], num_open, num_closed + 1)
+        
         backtrack([], 0, 0)
         return res
-
-        
