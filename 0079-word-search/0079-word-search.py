a38902846359b59573a477a7369c5e14511a4559
Tyler Le <le.tyler.h@gmail.com>
2026-03-10 20:02:15 -0700

    Time: 4615 ms (24.69%), Space: 19.6 MB (55.7%) - LeetHub

--- 0079-word-search/0079-word-search.py
+++ 0079-word-search/0079-word-search.py
@@ -1,35 +1,35 @@
 class Solution:
     def exist(self, board: List[List[str]], word: str) -> bool:
+        
 
-        n, m = len(board), len(board[0])
+        n = len(word)
+        rows, cols = len(board), len(board[0])
+        visited = set()
 
-        def backtrack(x, y, index, visited):
+        def backtrack(index, x, y):
+            nonlocal rows, cols, visited
 
-            nonlocal n, m
-
-            # base case
+            if index == n: return True
+            if x < 0 or y < 0 or x >= rows or y >= cols: return False
+            if (x,y) in visited: return False
             if word[index] != board[x][y]: return False
-            if index == len(word) - 1: return True
 
-            # recursive step
+            visited.add((x,y))
+            
             for dx, dy in [(-1,0), (1,0), (0,-1), (0,1)]:
-                r = x + dx
-                c = y + dy
-
-                if r < 0 or c < 0 or r >= n or c >= m: continue
-                if (r,c) in visited: continue
-
-                visited.add((r, c))
-                if backtrack(r, c, index+1, visited): return True
-                visited.remove((r, c))
+                r, c = x+dx, y+dy
+                if backtrack(index + 1, r, c):
+                    return True
+            
+            visited.remove((x,y))
+            
 
             return False
 
-        for i in range(n):
-            for j in range(m):
-                visited = set()
-                visited.add((i,j))
-
-                if backtrack(i, j, 0, visited): return True
+        
+        for r in range(rows):
+            for c in range(cols):
+                if board[r][c] == word[0] and backtrack(0, r, c):
+                    return True
         
         return False
