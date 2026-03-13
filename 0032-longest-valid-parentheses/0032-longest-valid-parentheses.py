9c2c76c6e941047ce599958bbe03c6002fec578a
Tyler Le <le.tyler.h@gmail.com>
2026-03-13 12:54:56 -0700

    Time: 9 ms (54.79%), Space: 20.5 MB (57.63%) - LeetHub

--- 0032-longest-valid-parentheses/0032-longest-valid-parentheses.py
+++ 0032-longest-valid-parentheses/0032-longest-valid-parentheses.py
@@ -1,16 +1,31 @@
 class Solution:
     def longestValidParentheses(self, s: str) -> int:
         stack = [-1]
-        n = len(s)
         OPEN, CLOSED = "(", ")"
         res = 0
 
+
+        # The idea is to keep track of the boundary
         for i, ch in enumerate(s):
-            if ch == OPEN:  
+
+            if ch == OPEN:
                 stack.append(i)
             else:
+
+                # pop from stack and determine boundary validity
                 stack.pop()
-                if not stack: stack.append(i)
-                else: res = max(res, i - stack[-1])
-            
+
+                # if popping leads the stack to be empty 
+                # that means it hit the boundary
+                # which means it is invalid, so we update the boundary
+                if not stack:
+                    stack.append(i)
+
+                # if popping does not lead the stack to be empty
+                # then we did not hit the boundary
+                # so we update the result
+                else:
+                    res = max(res, i - stack[-1])
+        
         return res
+
