85467f8e1f66f77748a2ec9965c0bcbcff497a9b
Tyler Le <le.tyler.h@gmail.com>
2026-03-16 16:18:46 -0700

    Time: 41 ms (89.84%), Space: 24.6 MB (78.16%) - LeetHub

--- 0528-random-pick-with-weight/0528-random-pick-with-weight.py
+++ 0528-random-pick-with-weight/0528-random-pick-with-weight.py
@@ -1,8 +1,35 @@
 class Solution:
 
     def __init__(self, w: List[int]):
-        # keep track of total sum
-        # map a number to its probability
+        
+        """
+        i.e. given [3, 2, 4]
+
+        we *could* build something like [3, 3, 3, 2, 2, 4, 4, 4, 4], pick a random one, and scan left for the index 
+
+        but this is a waste of space
+
+        instead, let's examine the prefix sum
+
+        prefix_sum([3, 2, 4]) = [3, 5, 9]. 
+
+        We can think of these numbers as the bucket. i.e. prefix_sum[0] = 3 means the 0th index ranges from [0...(3-1)]
+
+        so here, each elem in prefix sum [3, 5, 9] represents the right boundary in [3, 3, 3, 2, 2, 4, 4, 4, 4] 
+
+        so we could just pick a random number between [0, 9] and determine which bucket it belongs to
+        
+        lets say we have random number 2
+        
+        to find which bucket it belongs to in prefix sum [3, 5, 9], we just bisect_right
+
+        here '2' belongs to the bucket of the 0th index.
+
+        which means index 0 was chosen with probability 3/10. since we chose 2 from a range of [0,9]
+
+        """
+
+
         acc = 0
         self.prefix_sum = []
 
