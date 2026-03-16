3704c4cc74054af2d29d15b23d2a3ddb591f30e4
Tyler Le <le.tyler.h@gmail.com>
2026-03-16 16:01:55 -0700

    Time: 1520 ms (17.56%), Space: 24.4 MB (93.8%) - LeetHub

---
+++ 0528-random-pick-with-weight/0528-random-pick-with-weight.py
@@ -0,0 +1,23 @@
+class Solution:
+
+    def __init__(self, w: List[int]):
+        # keep track of total sum
+        # map a number to its probability
+        acc = 0
+        self.prefix_sum = []
+
+        for num in w:
+            acc+=num
+            self.prefix_sum.append(acc)
+
+    def pickIndex(self) -> int:
+        rand_int = randint(0, max(self.prefix_sum) - 1)
+        index = bisect_right(self.prefix_sum, rand_int)
+        return index
+
+        
+
+
+# Your Solution object will be instantiated and called as such:
+# obj = Solution(w)
+# param_1 = obj.pickIndex()
