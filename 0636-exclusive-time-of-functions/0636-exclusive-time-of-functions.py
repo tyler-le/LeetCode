bfcd405aaeb8bbec9b3db62a966626ac4ef036a9
Tyler Le <le.tyler.h@gmail.com>
2026-03-17 18:54:03 -0700

    Time: 9 ms (18.78%), Space: 19.2 MB (95.9%) - LeetHub

--- 0636-exclusive-time-of-functions/0636-exclusive-time-of-functions.py
+++ 0636-exclusive-time-of-functions/0636-exclusive-time-of-functions.py
@@ -1,9 +1,8 @@
 class Solution:
     def exclusiveTime(self, n: int, logs: List[str]) -> List[int]:
         stack = []  
-        backlog = []
         times = defaultdict(int)
-        min_id, max_id = 0, 0
+        max_id = 0
 
         for log in logs:
             function_id, category, time = log.split(":")
@@ -14,22 +13,20 @@ class Solution:
                 if not stack: 
                     stack.append((function_id, time))
                 else:
-                    popped_id, popped_time = stack.pop()
-                    times[popped_id]+=(time - popped_time)
-                    backlog.append(popped_id)
+                    top_fxn, top_time = stack[-1]
+                    times[top_fxn]+=(time - top_time)
                     stack.append((function_id, time))
             
             else:
-                popped_id, popped_time = stack.pop()
-                times[popped_id]+=(time - popped_time + 1)
-                if not stack and backlog:
-                    backlogged_item = backlog.pop()
-                    stack.append((backlogged_item, time + 1))
+                popped_fxn, popped_time = stack.pop()
+                times[popped_fxn] += (time - popped_time + 1)
+                if stack: stack[-1] = (stack[-1][0], time + 1) 
+                
 
-        res = [0 for _ in range(min_id, max_id + 1)]
+        res = []
 
-        for i in range(min_id, max_id + 1):
-            res[i] = times[str(i)]
+        for i in range(max_id+1):
+            res.append(times[str(i)])
         
         return res
 
