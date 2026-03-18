79b1fc3345627ed3614a3beafa6e3cc98f88309b
Tyler Le <le.tyler.h@gmail.com>
2026-03-17 17:59:58 -0700

    Time: 71 ms (53.76%), Space: 19.8 MB (32.42%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.py
+++ 0076-minimum-window-substring/0076-minimum-window-substring.py
@@ -1,26 +1,30 @@
 class Solution:
     def minWindow(self, s: str, t: str) -> str:
-        s_map = Counter(s)
-        t_map = Counter(t)
-        window = defaultdict(int)
-        num_valid = 0
-        res = (math.inf, -1, -1)
+        
         l = 0
-        needed = len(t_map)
+        res = [-math.inf, math.inf]
         n = len(s)
+        t_cnt = Counter(t)
+        window_cnt = defaultdict(int)
+
+        conditions_met, conditions_needed = 0, len(t_cnt)
 
         for r in range(n):
-            window[s[r]]+=1
-            if s[r] in t_map and window[s[r]] == t_map[s[r]]:
-                num_valid+=1
+            # extend the window
+            window_cnt[s[r]]+=1
+            if window_cnt[s[r]] == t_cnt[s[r]]:
+                conditions_met+=1
 
-            while l < r and (window[s[l]] > t_map[s[l]]):
-                window[s[l]]-=1
+            # shrink the window
+            while l < r and (s[l] not in t_cnt or window_cnt[s[l]] > t_cnt[s[l]]):
+                window_cnt[s[l]]-=1
+                if not window_cnt[s[l]]: del window_cnt[s[l]]
                 l+=1
-            
-            if num_valid == needed and r-l+1 < res[0]:
-                res = (r-l+1, l, r)
-        
-        return s[res[1]: res[2]+1]
 
-            
+            # check validity and record res
+            if conditions_met == conditions_needed and r-l+1 < (res[1] - res[0] + 1):
+                res = [l, r]
+        
+        print(res)
+        if res == [-math.inf, math.inf]: return ""
+        return s[res[0]:(res[1] + 1)]
