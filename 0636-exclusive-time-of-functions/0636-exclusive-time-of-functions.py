4519605fdb71b7158925139bbd7c6bdb58535009
Tyler Le <le.tyler.h@gmail.com>
2026-03-17 18:38:53 -0700

    Time: 7 ms (60.67%), Space: 19.3 MB (81.27%) - LeetHub

---
+++ 0636-exclusive-time-of-functions/0636-exclusive-time-of-functions.py
@@ -0,0 +1,35 @@
+class Solution:
+    def exclusiveTime(self, n: int, logs: List[str]) -> List[int]:
+        stack = []  
+        backlog = deque()
+        times = defaultdict(int)
+        min_id, max_id = 0, 0
+
+        for log in logs:
+            function_id, category, time = log.split(":")
+            max_id = max(max_id, int(function_id))
+            time = int(time)
+
+            if category == "start":
+                if not stack: 
+                    stack.append((function_id, time))
+                else:
+                    popped_id, popped_time = stack.pop()
+                    times[popped_id]+=(time - popped_time)
+                    backlog.append(popped_id)
+                    stack.append((function_id, time))
+            
+            else:
+                popped_id, popped_time = stack.pop()
+                times[popped_id]+=(time - popped_time + 1)
+                if not stack and backlog:
+                    backlogged_item = backlog.pop()
+                    stack.append((backlogged_item, time + 1))
+
+        res = [0 for _ in range(min_id, max_id + 1)]
+
+        for i in range(min_id, max_id + 1):
+            res[i] = times[str(i)]
+        
+        return res
+
