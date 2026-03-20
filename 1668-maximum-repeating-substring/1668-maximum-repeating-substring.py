27243f44fc7c2a0dda31c6db721f3cdb4f2ae7dd
Tyler Le <le.tyler.h@gmail.com>
2026-03-19 20:54:40 -0700

    Time: 0 ms (100%), Space: 19.3 MB (46.12%) - LeetHub

--- 1668-maximum-repeating-substring/1668-maximum-repeating-substring.py
+++ 1668-maximum-repeating-substring/1668-maximum-repeating-substring.py
@@ -1,10 +1,17 @@
 class Solution:
     def maxRepeating(self, sequence: str, word: str) -> int:
+        
         n, m = len(sequence), len(word)
+        dp = [0 for _ in range(n+1)]
 
-        x = n // m
+        for i in range(n+1):
+            x = sequence[i-m:i]
+            if not x: continue
+            if x == word:
+                if i - m >= 0:
+                    dp[i] = 1 + dp[i-m]
+                else:
+                    dp[i] = 1
 
-        for i in range(x, -1, -1):
-            if word*i in sequence: return i
-        return 0
-    
+        
+        return max(dp)
