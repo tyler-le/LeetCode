4211b0ce4ec6be2e4f0d1aa2f15c8401ed691781
Tyler Le <le.tyler.h@gmail.com>
2026-03-19 20:57:34 -0700

    Time: 0 ms (100%), Space: 19.3 MB (46.12%) - LeetHub

--- 1668-maximum-repeating-substring/1668-maximum-repeating-substring.py
+++ 1668-maximum-repeating-substring/1668-maximum-repeating-substring.py
@@ -1,14 +1,15 @@
 class Solution:
     def maxRepeating(self, sequence: str, word: str) -> int:
         
+        # dp[i] = max number of consecutive repeats that END at index i
+        # so dp[i] = 1 + dp[i - len(word)]
+
         n, m = len(sequence), len(word)
         dp = [0 for _ in range(n+1)]
 
-        for i in range(n+1):
+        for i in range(m, n+1):
             x = sequence[i-m:i]
-            if not x: continue
             if x == word:
                 dp[i] = 1 + dp[i-m] if i - m >= 0 else 1
-
 
         return max(dp)
