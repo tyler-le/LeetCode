91dea66afd166a137353c48d05df4a27ebb4c472
Tyler Le <le.tyler.h@gmail.com>
2026-03-21 14:02:21 -0700

    Time: 0 ms (100%), Space: 19.3 MB (85.09%) - LeetHub

--- 0118-pascals-triangle/0118-pascals-triangle.py
+++ 0118-pascals-triangle/0118-pascals-triangle.py
@@ -1,24 +1,48 @@
 class Solution:
     def generate(self, numRows: int) -> List[List[int]]:
-        res = []
 
-        def f(i):
-            nonlocal res
 
+        """
+        DP SOLUTION
+        """
+        dp = [
+            [1]
+        ]
+
+        for i in range(1, numRows):
+            prev_res = dp[i-1]
             sublist = [1]
-            if i == 1: 
-                res.append(sublist)
-                return sublist
 
-            
-            prev_res = f(i-1)
-
-            for x in range(len(prev_res) - 1):
-                sublist.append(prev_res[x] + prev_res[x+1])
+            for j in range(len(prev_res) - 1):
+                sublist.append(prev_res[j] + prev_res[j+1])
             
             sublist.append(1)
-            res.append(sublist)
-            return sublist
+            dp.append(sublist)
+
+        return dp
+
+
+        """
+        RECURSIVE SOLUTION
+        """
+        # res = []
+
+        # def f(i):
+        #     nonlocal res
+
+        #     sublist = [1]
+        #     if i == 1: 
+        #         res.append(sublist)
+        #         return sublist
+
+        #     prev_res = f(i-1)
+
+        #     for x in range(len(prev_res) - 1):
+        #         sublist.append(prev_res[x] + prev_res[x+1])
+            
+        #     sublist.append(1)
+        #     res.append(sublist)
+        #     return sublist
         
-        f(numRows)
-        return res
+        # f(numRows)
+        # return res
