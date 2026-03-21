186214492527b6bd0c74c709ba4b4efacfd65388
Tyler Le <le.tyler.h@gmail.com>
2026-03-20 19:25:29 -0700

    Time: 19 ms (7%), Space: 23.4 MB (5.15%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.py
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.py
@@ -1,33 +1,53 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        cache = {}
+        
+        n = len(prices)
+        dp = [[0 for _ in range(2)] for _ in range(n)]
+        BUY_STATE, SELL_STATE = 0, 1
 
-        def f(index, can_buy):
-            # if we can buy 
-            # -> -prices[i] + f(index + 1, False)
-            # -> 0 + f(index + 1, True)
+        dp[0][BUY_STATE] = -prices[0]
 
-            # if we cannot buy
-            # -> prices[i] + f(index + 1, True)
-            # -> 0 + f(index + 1, False)
+        for i in range(1, n):
+            buy = -prices[i] + dp[i-1][SELL_STATE]
+            hold = 0 + dp[i-1][BUY_STATE]
+            dp[i][BUY_STATE] = max(buy, hold)
 
-            if index == len(prices): 
-                return 0
+            sell = prices[i] + dp[i-1][BUY_STATE]
+            hold = 0 + dp[i-1][SELL_STATE]
+            dp[i][SELL_STATE] = max(sell, hold) 
 
-            if (index, can_buy) in cache:
-                return cache[(index, can_buy)]
-
-            if can_buy:
-                buy = -prices[index] + f(index + 1, False)
-                hold = 0 + f(index + 1, True)
-                cache[(index, can_buy)] = max(buy, hold)
-                return cache[(index, can_buy)]
-
-            else:
-                sell = prices[index] + f(index + 1, True)
-                hold = 0 + f(index + 1, False)
-                cache[(index, can_buy)] = max(sell, hold)
-                return cache[(index, can_buy)]
+        return max(dp[-1][BUY_STATE], dp[-1][SELL_STATE])
 
 
-        return f(0, True)
+        
+        # cache = {}
+
+        # def f(index, can_buy):
+        #     # if we can buy 
+        #     # -> -prices[i] + f(index + 1, False)
+        #     # -> 0 + f(index + 1, True)
+
+        #     # if we cannot buy
+        #     # -> prices[i] + f(index + 1, True)
+        #     # -> 0 + f(index + 1, False)
+
+        #     if index == len(prices): 
+        #         return 0
+
+        #     if (index, can_buy) in cache:
+        #         return cache[(index, can_buy)]
+
+        #     if can_buy:
+        #         buy = -prices[index] + f(index + 1, False)
+        #         hold = 0 + f(index + 1, True)
+        #         cache[(index, can_buy)] = max(buy, hold)
+        #         return cache[(index, can_buy)]
+
+        #     else:
+        #         sell = prices[index] + f(index + 1, True)
+        #         hold = 0 + f(index + 1, False)
+        #         cache[(index, can_buy)] = max(sell, hold)
+        #         return cache[(index, can_buy)]
+
+
+        # return f(0, True)
