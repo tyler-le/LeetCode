3e50e140be45a8261facc7ca28d8bdee60e2236c
Tyler Le <le.tyler.h@gmail.com>
2026-03-22 12:48:43 -0700

    Time: 4121 ms (12.87%), Space: 27.9 MB (5.09%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
@@ -1,28 +1,59 @@
 class Solution:
     def longestPalindrome(self, s: str) -> str:
+        # dp[i][j] returns True if s[i...j] is a palindrome
         n = len(s)
-        dp = [[False]*n for _ in range(n)]
-        ans = [-1,-1]
-        
-        
-        # dp[i][j] = True when s[i][j] is a palindrome
+
+        dp = [[False for _ in range(n)] for _ in range(n)]
+        res = (1, 0, 0)
+
+        # Base cases
         for i in range(n):
-            dp[i][i] = True
-            ans = [i,i]
-            
-        for i in range(n-1):
-            if s[i] == s[i+1]:
-                dp[i][i+1] = True
-                ans = [i, i+1]
+            for j in range(n):
+                if i >= j: 
+                    dp[i][j] = True
+                    
         
-        
-        for diff in range(2,n):
-            for i in range(n - diff):
-                j = i + diff
+        # Fill out the rest
+        for length in range(2, n+1):
+            for i in range(n - length + 1):
+                j = i + length - 1
                 if s[i] == s[j] and dp[i+1][j-1]:
                     dp[i][j] = True
-                    ans = [i,j]
+                
+                if dp[i][j] and j-i+1 > res[0]:
+                    res = (j-i+1, i, j)
         
-        i,j = ans
-        return s[i:j+1]
+        return s[res[1]: res[2]+1]
+
+
+
+        """
+        RECURSION + MEMOIZATION
+        """
+        # f(i, j) returns if s[i...j] is a palindrome
+        # cache = {}
+
+        # def f(i, j):
+        #     if i >= j: return True
+        #     if (i, j) in cache: return cache[(i, j)]
+            
+        #     res = False
+        #     if s[i] == s[j]: res = f(i+1, j-1)
+        #     else: res = False
+            
+        #     cache[(i, j)] = res
+        #     return cache[(i, j)]
+
+        # res = (0, -1, -1)
+        # n = len(s)
+
+        # for i in range(n):
+        #     for j in range(i, n):
+        #         if f(i, j) and (j-i+1 > res[0]):
+        #             res = (j-i+1, i, j)
         
+        # return s[res[1] : res[2] + 1]
+
+
+            
+          
