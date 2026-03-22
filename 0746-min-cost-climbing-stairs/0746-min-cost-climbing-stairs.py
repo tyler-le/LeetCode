eb114ce28a22657d85ab97c044446aade527257c
Tyler Le <le.tyler.h@gmail.com>
2026-03-22 11:28:42 -0700

    Time: 3 ms (63.38%), Space: 19.3 MB (91.34%) - LeetHub

--- 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.py
+++ 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.py
@@ -1,15 +1,35 @@
 class Solution:
     def minCostClimbingStairs(self, cost: List[int]) -> int:
-        n = len(cost)
-        dp = [math.inf for _ in range(n+1)] # dp[n] is the top
         
-        # dp[i] = cost to reach step i
-        dp[0] = 0 # we can start here for free
-        dp[1] = 0 # we can start here for free
+        """
+        DP
+        """
+        n = len(cost)
+        dp = [0 for _ in range(n+1)]
+        dp[0] = dp[1] = 0
 
         for i in range(2, n+1):
-            one_step = dp[i-1] + cost[i-1] # how much it took to reach step i-1 and the cost to jump from i-1 to i
-            two_step = dp[i-2] + cost[i-2] # how much it took to reach step i-2 and the cost to jump from i-2 to i
-            dp[i] = min(two_step, one_step)
-
+            one_step = dp[i-1] + cost[i-1]
+            two_step = dp[i-2] + cost[i-2]
+            dp[i] = min(one_step, two_step)
+        
         return dp[n]
+
+
+        """
+        RECURSION + MEMOIZATION
+        """
+        n = len(cost)
+        cache = {}
+
+        def f(i):
+            if i == 0 or i == 1: return 0
+            if i in cache: return cache[i]
+            
+            one_step = f(i-1) + cost[i-1]
+            two_step = f(i-2) + cost[i-2]
+            cache[i] = min(one_step, two_step)
+            
+            return cache[i]
+        
+        return f(n)
