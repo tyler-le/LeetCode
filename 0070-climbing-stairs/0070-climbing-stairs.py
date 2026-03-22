a4a7197d413e52a178d64229bc9202d33f514805
Tyler Le <le.tyler.h@gmail.com>
2026-03-22 11:17:33 -0700

    Time: 0 ms (100%), Space: 19.2 MB (75.76%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.py
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -1,13 +1,27 @@
 class Solution:
     def climbStairs(self, n: int) -> int:
-
-        dp = [0 for _ in range(n+1)]
-        dp[0] = 1
-        dp[1] = 1
-
-        if n <= 1: return 1
-
-        for i in range(2, n+1):
-            dp[i] = dp[i-1] + dp[i-2]
         
+        """
+        DP
+        """
+        dp = [0 for _ in range(n+1)]
+        dp[0] = dp[1] = 1
+        for i in range(2, n+1):
+            one_step = dp[i-1]
+            two_step = dp[i-2]
+            dp[i] = one_step + two_step
         return dp[n]
+
+
+        """
+        RECURSION + MEMOIZATION
+        """
+        cache = {}
+        def f(i):
+            if i == 0 or i == 1: return 1
+            if i in cache: return cache[i]
+            one_step = f(i-1)
+            two_step = f(i-2)
+            cache[i] = one_step + two_step
+            return cache[i]
+        return f(n)
