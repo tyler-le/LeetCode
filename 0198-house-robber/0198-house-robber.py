566d866f8d38b875b0b21fa79496a9eb4e5ff4af
Tyler Le <le.tyler.h@gmail.com>
2026-03-22 11:44:23 -0700

    Time: 0 ms (100%), Space: 19.4 MB (48.79%) - LeetHub

--- 0198-house-robber/0198-house-robber.py
+++ 0198-house-robber/0198-house-robber.py
@@ -1,17 +1,35 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
-
+        
+        """
+        DP
+        """
         n = len(nums)
-
-        if n == 1:
-            return nums[0]
-        elif n == 2:
-            return max(nums[0], nums[1])
-
+        if n == 1: return nums[0]
         dp = [0 for _ in range(n)]
-        dp[0], dp[1] = nums[0], max(nums[0], nums[1])
+        dp[0] = nums[0]
+        dp[1] = max(nums[0], nums[1])
 
         for i in range(2, n):
-            dp[i] = max(nums[i] + dp[i-2], dp[i-1])
+            one_step = dp[i-1]
+            two_step = dp[i-2] + nums[i]
+            dp[i] = max(one_step, two_step)
+
+        return dp[n-1]
         
-        return dp[-1]
+        
+        """
+        RECURSION + MEMOIZATION
+        """
+        n = len(nums)
+        cache = {}
+        def f(i):
+            if i < 0: return 0
+            if i == 0: nums[0]
+            if i == 1: max(nums[0], nums[1])
+            if i in cache: return cache[i]
+            one_step = f(i-1)
+            two_step = f(i-2) + nums[i]
+            cache[i] = max(one_step, two_step)
+            return cache[i]
+        return f(n-1)
