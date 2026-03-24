fef06d5de817590b6eae76965daf2bcf269e2bb9
Tyler Le <le.tyler.h@gmail.com>
2026-03-23 19:53:57 -0700

    Time: 1421 ms (5.04%), Space: 356.1 MB (5.01%) - LeetHub

--- 0647-palindromic-substrings/0647-palindromic-substrings.py
+++ 0647-palindromic-substrings/0647-palindromic-substrings.py
@@ -1,29 +1,27 @@
 class Solution:
     def countSubstrings(self, s: str) -> int:
-        # for each position, check if it is part of a palindrome
         
-        def check_even(i):
-            cnt = 0
-            j = i
+        # "is s[i...j] a palindrome?"
+        cache = {}
+        def f(i, j):
+            if i >= j: return True
+            if (i, j) in cache: return cache[i, j]
             
-            while i >= 0 and j < n and s[i] == s[j]:
-                cnt+=1
-                i-=1
-                j+=1
-            
-            return cnt
+            inner_res = f(i+1, j-1)
+
+            if s[i] == s[j] and inner_res: 
+                cache[(i, j)] = True
+                return True
+
+            cache[(i, j)] = False
+            return False
+
+        n = len(s)
+        res = 0
+
+        for i in range(n):
+            for j in range(i, n):
+                if f(i, j):
+                    res+=1
         
-        def check_odd(i):
-            cnt = 0
-            j = i+1
-            while i >= 0 and j < n and s[i] == s[j]:
-                cnt+=1
-                i-=1
-                j+=1
-                
-            return cnt
-        
-        n = len(s)        
-    
-        return sum(check_even(i) + check_odd(i) for i in range(n)) 
-            
+        return res
