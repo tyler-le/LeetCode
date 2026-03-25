c28f78e6aae654fa1bbabc788783905b0b79119c
Tyler Le <le.tyler.h@gmail.com>
2026-03-24 20:07:48 -0700

    Time: 3 ms (24.91%), Space: 20 MB (7.22%) - LeetHub

--- 0091-decode-ways/0091-decode-ways.py
+++ 0091-decode-ways/0091-decode-ways.py
@@ -5,20 +5,25 @@ class Solution:
 
         @cache
         def f(x):
-            print(x)
+
+            # base case(s) - x turns into empty string or singleton
             if not x: return 1
             if len(x) == 1: 
-                if x == "0": return 0
-                else: return 1
+                if x == "0": return 0 # invalid so return 0
+                else: return 1 # singleton is between 1-9, which means it has one valid encoding
 
             res = 0
 
             # take x[0] and recurse on x[1:]
+            # don't recurse on x[0], since we want to break the 'rest' part of the string 
+            # and dwindle it down to the base case
             first, rest = x[0], x[1:]
             if 1 <= int(first) <= 26 and not (len(first) == 2 and first[0] == "0"): 
                 res += f(rest)
 
-            # tale x[0:2] and recurse on x[2:]     
+            # take x[0:2] and recurse on x[2:] 
+            # don't recurse on x[0], since we want to break the 'rest' part of the string 
+            # and dwindle it down to the base case    
             first, rest = x[0:2], x[2:]   
             if 1 <= int(first) <= 26 and not (len(first) == 2 and first[0] == "0"): 
                 res += f(rest)
