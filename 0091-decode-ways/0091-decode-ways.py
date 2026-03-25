96ad9a39f9e42b4831664b6617874fc7a74142db
Tyler Le <le.tyler.h@gmail.com>
2026-03-24 20:08:34 -0700

    Time: 1 ms (34.82%), Space: 19.6 MB (13.35%) - LeetHub

--- 0091-decode-ways/0091-decode-ways.py
+++ 0091-decode-ways/0091-decode-ways.py
@@ -1,9 +1,11 @@
 class Solution:
     def numDecodings(self, s: str) -> int:
         
+        """
+        RECURSION + MEMOIZATION
+        """
+        cache = {}
         # f(x) is the number of ways to decode x
-
-        @cache
         def f(x):
 
             # base case(s) - x turns into empty string or singleton
@@ -13,6 +15,7 @@ class Solution:
                 else: return 1 # singleton is between 1-9, which means it has one valid encoding
 
             res = 0
+            if x in cache: return cache[x]
 
             # take x[0] and recurse on x[1:]
             # don't recurse on x[0], since we want to break the 'rest' part of the string 
@@ -28,6 +31,7 @@ class Solution:
             if 1 <= int(first) <= 26 and not (len(first) == 2 and first[0] == "0"): 
                 res += f(rest)
 
+            cache[x] = res
             return res    
         
         return f(s)
