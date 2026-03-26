1561afd21667cd969397cea5b7fca125cec9d350
Tyler Le <le.tyler.h@gmail.com>
2026-03-25 18:13:52 -0700

    Time: 72 ms (5.72%), Space: 19.3 MB (91.72%) - LeetHub

--- 0139-word-break/0139-word-break.py
+++ 0139-word-break/0139-word-break.py
@@ -1,18 +1,52 @@
-class Solution(object):
-    def wordBreak(self, s, wordDict):
-        """
-        :type s: str
-        :type wordDict: List[str]
-        :rtype: bool
-        """
-        n = len(s)
-        dp = [False for _ in range(n+1)]
-        dp[0] = True
+class Solution:
+    def wordBreak(self, s: str, wordDict: List[str]) -> bool:
         
-        for i in range(n+1):
-            for w in wordDict:
-                if (i - len(w) >= 0) and (dp[i - len(w)]) and (s[i-len(w):i] == w):
-                    dp[i] = True
-                    
-        return dp[-1]
-            
+
+        """
+        DP
+        """
+        # dp[i] = dp[0:i] inclusive is word breakable
+
+        # return dp[n]
+
+        n = len(s)
+        dp = [False for _ in range(n)]
+
+        for i in range(n-1, -1, -1):
+            for word in wordDict:
+                if i + len(word) <= n:
+                    prefix = s[i : i+len(word)]
+
+                    if prefix in wordDict:
+                        if i + len(word) >= len(dp):
+                            dp[i] = True
+                        elif dp[i + len(word)]:
+                            dp[i] = True
+
+        
+        
+        
+        return dp[0]
+
+
+
+
+
+
+        """
+        RECURSION + MEMOIZATION
+        """
+        cache = {}
+        def f(x):
+            if x == "": return True
+            if x in cache: return cache[x]
+
+            for word in wordDict:
+                prefix = x[:len(word)]
+                if prefix == word and f(x[len(word):]):
+                    cache[x] = True
+                    return True
+            cache[x] = False
+            return False
+        
+        return f(s)
