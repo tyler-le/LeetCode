da3961e57f999c24de3ca05cdf5358baffd7b4ef
Tyler Le <le.tyler.h@gmail.com>
2026-03-25 20:23:22 -0700

    Time: 121 ms (5.79%), Space: 44.2 MB (5.79%) - LeetHub

--- 3032-count-numbers-with-unique-digits-ii/3032-count-numbers-with-unique-digits-ii.py
+++ 3032-count-numbers-with-unique-digits-ii/3032-count-numbers-with-unique-digits-ii.py
@@ -2,14 +2,17 @@ class Solution:
     def numberCount(self, a: int, b: int) -> int:
         # f(a,b) = f(b) - f(a-1)
 
+        cache = {}
         def f(x):
             if x == 0: return 0
             if x == 1: return 1
-            
+            if x in cache: return cache[x]
             res = 0
             s = str(x)
 
             res += f(x-1) + (len(set(s)) == len(s))
+
+            cache[x] = res
             return res
         
         return f(b) - f(a-1)
