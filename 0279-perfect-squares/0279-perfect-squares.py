2c1d2b8dec89c686f65d31511de5eac58501b9b6
Tyler Le <le.tyler.h@gmail.com>
2026-03-28 12:48:58 -0700

    Time: 1448 ms (67.55%), Space: 19.6 MB (45.51%) - LeetHub

--- 0279-perfect-squares/0279-perfect-squares.py
+++ 0279-perfect-squares/0279-perfect-squares.py
@@ -1,17 +1,48 @@
+from math import sqrt
+
 class Solution:
     def numSquares(self, n: int) -> int:
-        # dp[i] = least number of perfect square that sum to i
         
-        dp = [float("inf") for _ in range(n+1)]
         
-        dp[0] = 0
-        dp[1] = 1
+        def is_perfect_square(x):
+            return sqrt(x).is_integer()
+
+        """
+        DP
+        """
+        dp = [math.inf for _ in range(n+1)]
+        perfect_squares = [x for x in range(n+1) if is_perfect_square(x)]
+
+        for i in range(len(dp)):
+            if is_perfect_square(i): 
+                dp[i] = 1
         
         for i in range(len(dp)):
-            square = 1
-            
-            while square * square <= i:
-                dp[i] = min(dp[i], 1 + dp[i-(square*square)])
-                square+=1
+            for j in perfect_squares:
+                if j > i: break
+                dp[i] = min(dp[i], 1 + dp[i-j])
         
         return dp[n]
+        
+        """
+        RECURSION + MEMOIZATION
+        """
+        cache = {}
+        def f(x) -> int:
+            res = math.inf
+
+            if x <= 0: return math.inf
+            if is_perfect_square(x): return 1
+
+            if x in cache: return cache[x]
+        
+            for num in range(1, x):
+                if is_perfect_square(num):
+                    res = min(res, 1 + f(x-num))
+            
+            cache[x] = res
+            return res
+
+
+        
+        return f(n)
