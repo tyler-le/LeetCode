eecb039f74b8969340c21a467cc08212042bc5d3
Tyler Le <le.tyler.h@gmail.com>
2026-03-29 18:23:22 -0700

    Time: 139 ms (14.39%), Space: 35.3 MB (8.41%) - LeetHub

--- 0463-island-perimeter/0463-island-perimeter.py
+++ 0463-island-perimeter/0463-island-perimeter.py
@@ -1,42 +1,24 @@
 class Solution:
     def islandPerimeter(self, grid: List[List[int]]) -> int:
-        # for each land cell, count how many sides touch water or border
-        
-        
-        def dfs(i, j):
-            nonlocal perimeter
-            
-            # base case
-            if i < 0 or j < 0 or i >= n or j >= m:
-                return
-            if grid[i][j] == WATER: 
-                return
-            
-            if (i,j) in visited:
-                return
-            
-            # handle current cell
-            visited.add((i, j))
-            
-            for di, dj in [(-1,0), (1,0), (0,-1), (0,1)]:
-                if di+i < 0 or dj+j < 0 or i+di >= n or j+dj >= m:
-                    perimeter+=1
-                elif grid[i+di][j+dj] == WATER:
-                    perimeter+=1
-                    
-            
-            # recursive call
-            for di, dj in [(-1,0), (1,0), (0,-1), (0,1)]:
-                dfs(i+di, j+dj)
         
         n, m = len(grid), len(grid[0])
-        perimeter = 0
-        LAND, WATER = 1, 0
         visited = set()
-        
-        for r in range(n):
-            for c in range(m):
-                if grid[r][c] == LAND:
-                    dfs(r,c)
-                    return perimeter
-                
+
+        def dfs(x, y):
+            if x < 0 or y < 0 or x >= n or y >= m: return 1
+            if grid[x][y] == 0: return 1
+            if (x, y) in visited: return 0
+
+            visited.add((x,y))
+
+            res = 0
+            res+=dfs(x+1, y)
+            res+=dfs(x-1, y)
+            res+=dfs(x, y+1)
+            res+=dfs(x, y-1)
+            return res
+
+        for i in range(n):
+            for j in range(m):
+                if grid[i][j]:
+                    return dfs(i, j)
