13991827e717f4902cb949381b1b44c39d10a5a9
Tyler Le <le.tyler.h@gmail.com>
2026-03-29 17:53:59 -0700

    Time: 3 ms (31.04%), Space: 19.4 MB (38.34%) - LeetHub

--- 0343-integer-break/0343-integer-break.py
+++ 0343-integer-break/0343-integer-break.py
@@ -1,18 +1,30 @@
 class Solution:
     def integerBreak(self, n: int) -> int:
         
+        dp = [0 for _ in range(n+1)]
+
+        for x in range(1, len(dp)):
+            for i in range(1, x):
+                if (x-i) <= 0: 
+                    dp[i] = 0
+                
+                else: 
+                    break_choice = i * dp[x-i]
+                    dont_break_choice = i * (x-i)
+                    dp[x] = max(dp[x], break_choice, dont_break_choice)
+
+        return dp[n]
 
         cache = {}
         def f(x):
             if x <= 0: return 0
-
             if x in cache: return cache[x]
 
             res = 0
-
             for i in range(1, x):
-                res = max(res, i * f(x - i))
-                res = max(res, i * (x-i))
+                # here we split x into two parts: i and (x-i)
+                res = max(res, i * f(x - i)) # break (x-i)
+                res = max(res, i * (x - i)) # don't break (x-i)
             
             cache[x] = res
             return res
