e063985f7e737e5140fc8be717f36f644d8da804
Tyler Le <le.tyler.h@gmail.com>
2026-03-30 21:39:04 -0700

    Time: 21 ms (26.89%), Space: 22.7 MB (27.52%) - LeetHub

--- 0997-find-the-town-judge/0997-find-the-town-judge.py
+++ 0997-find-the-town-judge/0997-find-the-town-judge.py
@@ -1,13 +1,18 @@
 class Solution:
     def findJudge(self, n: int, trust: List[List[int]]) -> int:
-        incoming = defaultdict(int)
-        outcoming = defaultdict(int)
+        # (u -> v) if u trusts v
+
+        # find the one with no outgoing edges AND n-1 incoming edges
+
+        indegrees = defaultdict(int)
+        outdegrees = defaultdict(int)
         
         for u, v in trust:
-            incoming[v]+=1
-            outcoming[u]+=1
+            indegrees[v]+=1
+            outdegrees[u]+=1
         
-        for i in range(1, n+1):
-            if incoming[i] == n-1 and outcoming[i] == 0: return i
+        for person in range(1, n+1):
+            if indegrees[person] == n-1 and outdegrees[person] == 0:
+                return person
         
         return -1
