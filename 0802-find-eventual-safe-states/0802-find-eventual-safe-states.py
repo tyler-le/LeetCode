e4f6a6b5f9b17a9db19c05e99b7d84655f18553a
Tyler Le <le.tyler.h@gmail.com>
2026-03-31 22:25:23 -0700

    Time: 1840 ms (5.02%), Space: 30.5 MB (5.66%) - LeetHub

--- 0802-find-eventual-safe-states/0802-find-eventual-safe-states.py
+++ 0802-find-eventual-safe-states/0802-find-eventual-safe-states.py
@@ -10,7 +10,7 @@ class Solution:
             if node in cache: return cache[node]
 
             if node in visited:
-                bad.update(visited)
+                # bad.update(visited)
                 cache[node] = True
                 return True
             
