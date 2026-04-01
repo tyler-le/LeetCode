285f2e364c6ea80edf9dbe4e8964deb3c9c97b19
Tyler Le <le.tyler.h@gmail.com>
2026-03-31 22:21:23 -0700

    Time: 2727 ms (5.02%), Space: 30.2 MB (5.66%) - LeetHub

---
+++ 0802-find-eventual-safe-states/0802-find-eventual-safe-states.py
@@ -0,0 +1,41 @@
+class Solution:
+    def eventualSafeNodes(self, graph: List[List[int]]) -> List[int]:
+        visited = set()
+        n = len(graph)
+        bad = set()        
+        cache = {}
+
+        def has_cycle(node) -> bool:
+
+            if node in cache: return cache[node]
+
+            if node in visited:
+                bad.update(visited)
+                cache[node] = True
+                return True
+            
+            if not graph[node]:
+                cache[node] = False
+                return False
+
+            visited.add(node)
+
+            for nbor in graph[node]:
+                if has_cycle(nbor):
+                    bad.update(visited)
+                    cache[node] = True
+                    return True
+            
+            visited.remove(node)
+            cache[node] = False
+            return False
+            
+        
+        for node in range(n):
+            if node in visited: continue
+            if has_cycle(node):
+                bad.update(visited)
+        
+        return sorted(list(set([i for i in range(n)]) - bad))
+
+
