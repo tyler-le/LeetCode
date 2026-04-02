fcbbe8a55600084d30c9e952b5cfab9b6555efff
Tyler Le <le.tyler.h@gmail.com>
2026-04-02 16:49:55 -0700

    Time: 75 ms (5.12%), Space: 26.1 MB (14.19%) - LeetHub

---
+++ 0694-number-of-distinct-islands/0694-number-of-distinct-islands.py
@@ -0,0 +1,37 @@
+class Solution:
+    def numDistinctIslands(self, grid: List[List[int]]) -> int:
+        
+        visited = set()
+        n, m = len(grid), len(grid[0])
+        res = set()
+        
+        def dfs(x, y, path, direction):
+            if x < 0 or y < 0 or x >= n or y >= m:
+                return 
+
+            if (x,y) in visited:
+                return 
+            
+            if grid[x][y] == 0:
+                return 
+
+            visited.add((x,y))
+            if direction: path.append(f"{direction} ")
+            else: path.append("START ")
+
+            dfs(x, y-1, path, "L")
+            dfs(x, y+1, path, "R")
+            dfs(x-1, y, path, "U")
+            dfs(x+1, y, path, "D")
+
+            path.append("B")
+            return tuple(path)
+
+        for i in range(n):
+            for j in range(m):
+                path = dfs(i, j, [], None)
+                if path: res.add(path)
+        
+        return len(res)
+        
+
