1a6b6bce51f6ff35209503345a5f4d9abb52ab65
Tyler Le <le.tyler.h@gmail.com>
2026-04-03 21:08:22 -0700

    Time: 3 ms (84.24%), Space: 21.5 MB (62.47%) - LeetHub

--- 0253-meeting-rooms-ii/0253-meeting-rooms-ii.py
+++ 0253-meeting-rooms-ii/0253-meeting-rooms-ii.py
@@ -1,27 +1,18 @@
 class Solution:
     def minMeetingRooms(self, intervals: List[List[int]]) -> int:
         
-        # keep a min-heap of end times signifying booked rooms that end the earliest
-        
-        if not intervals: return 0
-        
         intervals.sort(key = lambda x : x[0])
-        res = 0
-        min_heap = [intervals[0][1]]
-        
-        for curr_start, curr_end in intervals[1:]:
-            prev_end = min_heap[0] 
-            
-            # meeting room is freed up, so curr interval can take it
-            if prev_end <= curr_start: 
+        min_heap = []
+
+        for start, end in intervals:
+            if not min_heap:
+                heappush(min_heap, end)
+            elif start < min_heap[0]:
+                heappush(min_heap, end)
+            else:
                 heappop(min_heap)
-                heappush(min_heap, curr_end)
-            
-            # otherwise we create a new meeting room
-            else: 
-                res+=1
-                heappush(min_heap, curr_end)
-                
-        return len(min_heap)
-            
+                heappush(min_heap, end)
         
+        return len(min_heap)
+
+        # keep track of the earliest end time
