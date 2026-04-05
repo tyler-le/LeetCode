a00d825916f47807ba62debb7037e07cd2241aa5
Tyler Le <le.tyler.h@gmail.com>
2026-04-04 22:59:41 -0700

    Time: 0 ms (100%), Space: 20.5 MB (51.13%) - LeetHub

--- 0210-course-schedule-ii/0210-course-schedule-ii.py
+++ 0210-course-schedule-ii/0210-course-schedule-ii.py
@@ -1,33 +1,31 @@
 class Solution:
     def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
-        # initialize vars
+        
         graph = defaultdict(list)
         indegrees = defaultdict(int)
         q = deque()
         visited = set()
         res = []
+
+        for dst, src in prerequisites:
+            graph[src].append(dst)
+            indegrees[dst]+=1
         
-        # convert to adjacency matrix
-        for u, v in prerequisites:
-            graph[v].append(u)
-            indegrees[u]+=1
-                    
-        # add all source nodes to queue
         for crs in range(numCourses):
-            if indegrees[crs] == 0: 
+            if not indegrees[crs]:
                 q.append(crs)
-        
-        # for each outgoing edge from source node u -> v, decrement indegree[v]
+
         while q:
             popped = q.popleft()
-            visited.add(popped)
             res.append(popped)
-            
+            visited.add(popped)
+
             for nbor in graph[popped]:
                 if nbor in visited: continue
                 indegrees[nbor]-=1
-                if indegrees[nbor] == 0:
+                if not indegrees[nbor]:
                     q.append(nbor)
-                    
-        
+
         return res if len(visited) == numCourses else []
+
+
