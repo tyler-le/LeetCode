b3186b119220e94fa8e3d3cb38461ba8bfeefaab
Tyler Le <le.tyler.h@gmail.com>
2026-04-06 17:57:37 -0700

    Time: 19 ms (64.55%), Space: 22.3 MB (70.2%) - LeetHub

--- 1268-search-suggestions-system/1268-search-suggestions-system.py
+++ 1268-search-suggestions-system/1268-search-suggestions-system.py
@@ -1,25 +1,59 @@
 class Solution:
     def suggestedProducts(self, products: List[str], searchWord: str) -> List[List[str]]:
-        products.sort()
+
         res = []
-        for i in range(len(searchWord)):
-            word = searchWord[0:i+1]
+        products.sort()
+        n = len(products)
+        m = len(searchWord)
+
+        def binary_search(prefix):
+
+            left_bound, right_bound = None, None
+
+            # find left bound
+            low, high = 0, n-1
+            while low < high:
+                mid = (high + low) // 2
+                word = products[mid]
+
+                if prefix <= word[:len(prefix)]:
+                    high = mid
+                else:
+                    low = mid + 1
+
+            if products[low][:len(prefix)] == prefix:
+                left_bound = low
             
-            #binary search word in products
-            low, high = 0, len(products)-1
-            
-            while low <= high:
-                mid = low + (high - low) // 2
-                if products[mid] < word: low = mid + 1
-                elif products[mid] >= word: high = mid - 1
-                    
-            curr = []
-            for i in range(3):
-                if low+i < len(products) and word in products[low + i][0:len(word)]:
-                    curr.append(products[low + i])
-            res.append(curr)
-                        
+            # find right bound
+            low, high = 0, n-1
+            while low < high:
+                mid = (low + high + 1) // 2
+
+                word = products[mid]
+
+                if prefix < word[:len(prefix)]:
+                    high = mid - 1
+                elif prefix > word[:len(prefix)]:
+                    low = mid + 1
+                else:
+                    low = mid
+
+            if products[low][:len(prefix)] == prefix:
+                right_bound = low
+
+            return [left_bound, right_bound]
+                
+                
+
+        # sort
+        # for every prefix, binary search for that prefix
+
+        print(products)
+
+        for i in range(1, m + 1):
+            prefix = searchWord[:i]
+            l, r = binary_search(prefix)
+            if l is None or r is None: res.append([])
+            else: res.append(products[l:r+1] if r+1-l <= 3 else products[l:l+3])
+
         return res
-                
-                
-            
