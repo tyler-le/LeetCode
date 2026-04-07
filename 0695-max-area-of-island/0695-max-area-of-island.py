1dca464b52ecdb2f02db44bd308531375ee4e5db
Tyler Le <le.tyler.h@gmail.com>
2026-04-06 20:11:12 -0700

    Time: 27 ms (28.01%), Space: 19.7 MB (80.26%) - LeetHub

--- 0695-max-area-of-island/0695-max-area-of-island.py
+++ 0695-max-area-of-island/0695-max-area-of-island.py
@@ -1,26 +1,41 @@
 class Solution:
     def maxAreaOfIsland(self, grid: List[List[int]]) -> int:
-        
-        def dfs(i, j):
-            if i < 0 or j < 0: return 0
-            if i >= n or j >= m: return 0
-            if not grid[i][j]: return 0
-            
-            cnt = 1
-            grid[i][j] = 0
-            
-            for dx, dy in [(-1,0), (1,0), (0,-1), (0,1)]:
-                r, c = i+dx, j+dy
-                cnt+=dfs(r,c)
-            
-            return cnt
-                
-        
+        res = 0
         n, m = len(grid), len(grid[0])
-        res = -math.inf
+        visited = set()
+
+        def bfs(x, y):
+            q = deque([(x,y)])
+            area = 1
+            visited.add((x,y))
+
+            while q: 
+                popped_x, popped_y = q.popleft()
+
+                for dx, dy in [(-1,0), (1,0), (0,-1), (0,1)]:
+                    r = popped_x + dx
+                    c = popped_y + dy
+
+                    if r < 0 or c < 0 or r >= n or c >= m: continue
+                    if (r,c) in visited: continue
+                    if not grid[r][c]: continue
+                    visited.add((r,c))
+                    q.append((r,c))
+                    area+=1
+                    
+
+            return area
+
+
+
+
+
+
         for i in range(n):
             for j in range(m):
-                if grid[i][j]: 
-                    res = max(res, dfs(i, j))
-        
-        return res if res != -math.inf else 0
+                if grid[i][j] and (i,j) not in visited:
+                    print(i,j)
+                    res = max(res, bfs(i, j))
+                    print(res)
+
+        return res
