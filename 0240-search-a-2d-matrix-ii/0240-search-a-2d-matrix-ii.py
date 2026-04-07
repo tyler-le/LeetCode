993d27dd1247fffd57dec5fa4b2d03d64eb4ee9b
Tyler Le <le.tyler.h@gmail.com>
2026-04-06 17:02:26 -0700

    Time: 136 ms (84.52%), Space: 26.7 MB (13.66%) - LeetHub

--- 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.py
+++ 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.py
@@ -2,12 +2,15 @@ class Solution:
     def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
         n, m = len(matrix), len(matrix[0])
 
+        """
+        The idea is to look at the top right corner.
+        Compare top right corner with the target
+        We can easily eliminate a single row or column with this comparison
+        """
+
         # f(i, j) = target exists within top right corner (i,j) to bottom left
         def f(i, j):
-            print(i,j)
             if i == n or j < 0: return False
-
-            
 
             top_right = matrix[i][j]
 
