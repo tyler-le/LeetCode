142e1cd14a6c7e83c3e0e73923beeab6ec6d7a12
Tyler Le <le.tyler.h@gmail.com>
2026-04-07 18:35:59 -0700

    Time: 32 ms (87.21%), Space: 24.1 MB (21.54%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.py
+++ 0072-edit-distance/0072-edit-distance.py
@@ -1,23 +1,35 @@
 class Solution:
     def minDistance(self, word1: str, word2: str) -> int:
-        n = len(word1)
-        m = len(word2)
-        INF = float('inf')
-        
-        # create a n x m matrix of infinities
-        
-        A = [[INF for x in range(m+1)] for y in range(n+1)] 
-        
-        for row in range(n+1): A[row][0] = row
-            
-        for col in range(m+1): A[0][col] = col
+        n, m = len(word1), len(word2)
+        cache = {}        
+        def f(i, j):
 
-        for i in range(1, n+1):
-            for j in range(1, m+1):
-                if word1[i-1] == word2[j-1]:
-                     A[i][j] = min(0+A[i-1][j-1], 1+A[i-1][j], 1+A[i][j-1])
-                else:
-                    A[i][j] = min(1+A[i-1][j-1], 1+A[i-1][j], 1+A[i][j-1])
-                    
-        return A[-1][-1]
-                    
+            # exhaust word1, can only perform inserts to get to word2
+            if i == n and j < m:
+                return m - j
+            
+            # exhaust word2, can only perform deletes to get to word2
+            if j == m and i < n:
+                return n - i
+
+            # reached the end on both words
+            if i == n and j == m:
+                return 0
+
+            if (i,j) in cache:
+                return cache[(i,j)]
+
+            res = math.inf
+
+            if word1[i] == word2[j]:
+                res = 0 + f(i+1, j+1)
+            else:
+                insert = 1 + f(i, j+1)
+                delete = 1 + f(i+1, j)
+                replace = 1 + f(i+1, j+1)
+                res = min(insert, delete, replace)
+            
+            cache[(i,j)] = res
+            return res
+
+        return f(0, 0)
