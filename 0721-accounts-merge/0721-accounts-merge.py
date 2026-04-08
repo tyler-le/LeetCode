55657cc1b5b8a52a32a6dca324d94887bb96b279
Tyler Le <le.tyler.h@gmail.com>
2026-04-08 16:54:50 -0700

    Time: 75 ms (11.62%), Space: 23.1 MB (45.75%) - LeetHub

--- 0721-accounts-merge/0721-accounts-merge.py
+++ 0721-accounts-merge/0721-accounts-merge.py
@@ -1,50 +1,73 @@
-from pprint import pprint
+class UnionFind:
+    def __init__(self):
+        self.parents = [] 
+        self.email_to_index = {}
+        self.index_to_email = {}
+
+    def union(self, a, b):
+        self._populate(a)
+        self._populate(b)
+
+        parent_a = self.find(self.email_to_index[a])
+        parent_b = self.find(self.email_to_index[b])
+
+        self.parents[parent_b] = parent_a
+    
+    def find(self, x):
+        if x == self.parents[x]: return x
+        return self.find(self.parents[x])
+    
+    def _populate(self, x):
+        if x not in self.email_to_index:
+            self.parents.append(len(self.parents))
+            self.email_to_index[x] = len(self.parents) - 1
+            self.index_to_email[len(self.parents) - 1] = x
+        
+
 
 class Solution:
-    
     def accountsMerge(self, accounts: List[List[str]]) -> List[List[str]]:
-        graph = defaultdict(list)
-        visited = set() # emails
+        # union find based on emails
+        # have a hashmap (email -> name)
+
+
+        # johnsmith@mail.com john_newyork@mail.com
+
+        # john00@mail.com johnsmith@mail.com
+
+        # mary@mail.com
+
+        # johnnybravo@mail.com
+
+        uf = UnionFind()
         email_to_name_map = {}
-        res = []
 
         for account in accounts:
-            name = account[0]
-            hub_email, rest = account[1], account[2:]
+            name, first_email, rest = account[0], account[1], account[2:]
+            if first_email not in email_to_name_map:
+                email_to_name_map[first_email] = name
+
             if not rest:
-                graph[hub_email].append(hub_email)
-                email_to_name_map[hub_email] = name
-            for x in rest:
-                graph[hub_email].append(x)
-                graph[x].append(hub_email)
-                email_to_name_map[hub_email] = name
-                email_to_name_map[x] = name
+                uf.union(first_email, first_email)
+            else:
+                for x in rest:
+                    uf.union(first_email, x)
+
+        res = defaultdict(list)
+        result = []
+
+        for i in range(len(uf.parents)):
+            parent = uf.find(i)
+            child = i
+            res[parent].append(child)
+
+        for parent_index, child_indices in res.items():
+            root_email = uf.index_to_email[parent_index]
+            name = email_to_name_map[root_email]
+            emails = sorted([uf.index_to_email[i] for i in child_indices])
+            result.append([name] + emails)
+
+        return result
+
+
             
-    
-        def bfs(node):
-            q = deque([node])
-            visited.add(node)
-            sublist = []
-
-            while q:
-                popped = q.popleft()
-                sublist.append(popped)
-
-                for nbor in graph[popped]:
-                    if nbor in visited: continue
-                    q.append(nbor)
-                    visited.add(nbor)
-            
-            return sublist
-
-        for k, v in graph.items():
-            if k in visited: continue
-            emails = bfs(k)
-            entry = []
-            entry.append(email_to_name_map[k])
-            entry.extend(sorted(emails))
-
-            res.append(entry)
-        
-        return res
-        
