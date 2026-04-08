f870060a45c6d6770e83fc25363c771f776c77aa
Tyler Le <le.tyler.h@gmail.com>
2026-04-07 18:56:01 -0700

    Time: 63 ms (13.97%), Space: 23 MB (33.08%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.py
+++ 0072-edit-distance/0072-edit-distance.py
@@ -1,5 +1,45 @@
 class Solution:
     def minDistance(self, word1: str, word2: str) -> int:
+        
+        """
+        DP (translated from recursive method)
+        filling bottom-right to top-left
+        """
+        n, m = len(word1), len(word2)
+        dp = [[math.inf for _ in range(m+1)] for _ in range(n+1)]
+
+        # exhausted word1 but not word2
+        for j in range(m+1):
+            dp[n][j] = m-j
+        
+        # exhausted word2 but not word1
+        for i in range(n+1):
+            dp[i][m] = n - i
+
+        # exhausted both words
+        dp[n][m] = 0
+
+        for i in range(n-1, -1, -1):
+            for j in range(m-1, -1, -1):
+                if word1[i] == word2[j]:
+                    dp[i][j] = dp[i+1][j+1]
+                else:
+                    insert = 1 + dp[i][j+1]
+                    delete = 1 + dp[i+1][j] 
+                    replace = 1 + dp[i+1][j+1]  
+                    dp[i][j] = min(insert, delete, replace)
+        
+        return dp[0][0]
+
+
+
+        """
+        RECURSIVE + MEMOIZATION
+        - Insert → Add a char to word1 to match word2[j], advance j but not i (since we inserted before i)
+        - Delete → Remove word1[i], advance i but not j (since we have not matched j yet)
+        - Replace → word1[i] becomes word2[j], advance both since they match now
+        """
+        
         n, m = len(word1), len(word2)
         cache = {}        
         def f(i, j):
