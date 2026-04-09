b47af6aba1c56bdd6ee4de2eb699cfd0a2678094
Tyler Le <le.tyler.h@gmail.com>
2026-04-08 17:57:08 -0700

    Time: 7 ms (66.85%), Space: 20.4 MB (7.19%) - LeetHub

--- 0777-swap-adjacent-in-lr-string/0777-swap-adjacent-in-lr-string.py
+++ 0777-swap-adjacent-in-lr-string/0777-swap-adjacent-in-lr-string.py
@@ -1,30 +1,33 @@
 class Solution:
     def canTransform(self, start: str, result: str) -> bool:
-        n, m = len(start), len(result)
-
         
-        if n != m: return False
+        """
+        Intuition:
+        Remove all of the X's from both strings.
 
+        start = "RXXLRXRXL" --->    R _ _ L R _ R _ L
+        result = "XRLXXRRLX" --->   _ R L _ _ R R L _
+
+        Notice that they should have the same pattern (RLRRL == RLRRL)
+
+        But also Rs can only move right (or stay) and Ls can only move left (or stay)
+        So we must validate that each R did not move left and each L did not move right.
+        """
+        
+        n, m = len(start), len(result)
+        if n != m: return False
+        
         arr1, arr2 = [], []
 
-        for i, (ch1, ch2) in enumerate(zip(start, result)):
-            if ch1 != "X": arr1.append((i, ch1))
-            if ch2 != "X": arr2.append((i, ch2))
+        for i in range(n):
+            if start[i] != "X": arr1.append((i, start[i]))
+            if result[i] != "X": arr2.append((i, result[i]))
 
         if len(arr1) != len(arr2): return False
 
-
-        for x, y in zip(arr1, arr2):
-            print(x, y)
-            x_idx, x_ch = x
-            y_idx, y_ch = y
-
+        for (x_index, x_ch), (y_index, y_ch) in zip(arr1, arr2):
             if x_ch != y_ch: return False
-            if x_idx == y_idx: continue
-
-            if x_ch == "R" and x_idx > y_idx: return False
-            if x_ch == "L" and x_idx < y_idx: return False
-
-
+            if x_ch == "R" and x_index > y_index: return False
+            if x_ch == "L" and x_index < y_index: return False
+        
         return True
-
