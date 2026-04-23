2b286106fce9402f089683afa1e352fd0bbaae6c
Tyler Le <le.tyler.h@gmail.com>
2026-04-23 16:57:06 -0700

    Time: 151 ms (6.28%), Space: 36.7 MB (6.28%) - LeetHub

--- 2971-find-polygon-with-the-largest-perimeter/2971-find-polygon-with-the-largest-perimeter.py
+++ 2971-find-polygon-with-the-largest-perimeter/2971-find-polygon-with-the-largest-perimeter.py
@@ -1,20 +1,17 @@
 class Solution:
     def largestPerimeter(self, nums: List[int]) -> int:
-        
-        """
-        The longest side of the polygon must be smaller 
-        than the sum of the lengths of its other sides.
-        """
-        
-        n, acc, longest_side, res = len(nums), 0, 0, -1
         nums.sort()
-        
-        for i in range(n):
-            longest_side = max(longest_side, nums[i])
-            acc+=nums[i]
-            
-            if i >= 2 and acc - longest_side > longest_side: 
-                res = acc 
-        
+        acc = 0
+        prefix_sum = []
+        res = -1
+        n = len(nums)
+
+        for num in nums:
+            acc+=num
+            prefix_sum.append(acc)
+
+        for i in range(1, n-1):
+            if prefix_sum[i] > nums[i+1]:
+                res = max(res, prefix_sum[i] + nums[i+1])
+
         return res
-            
