a669a32844494a2dd25c80ea372d33701ddabed1
Tyler Le <le.tyler.h@gmail.com>
2026-04-25 23:56:48 -0700

    Time: 414 ms (52.93%), Space: 126.6 MB (24.25%) - LeetHub

--- 1559-detect-cycles-in-2d-grid/1559-detect-cycles-in-2d-grid.py
+++ 1559-detect-cycles-in-2d-grid/1559-detect-cycles-in-2d-grid.py
@@ -3,26 +3,26 @@ class Solution:
         visited = set()
         n, m = len(grid), len(grid[0])
 
-        def has_cycle(x, y, prev):
-            if (x, y) in visited: return True
-
-            visited.add((x,y))
+        def has_cycle(x, y, parent):
             
             for dx, dy in [(-1,0), (1,0), (0,-1), (0,1)]:
                 r = x + dx
                 c = y + dy
 
                 if r < 0 or c < 0 or r >= n or c >= m: continue
-                if prev and (r,c) == prev: continue
-                if grid[r][c] != grid[x][y]: continue
-
-                if has_cycle(r, c, (x,y)): return True
-
+                if grid[x][y] != grid[r][c]: continue
+                if (r,c) == parent: continue
+                if (r,c) in visited: return True
+                visited.add((r,c))
+                if has_cycle(r,c, (x,y)): return True
+            
+            return False
 
         for i in range(n):
             for j in range(m):
                 if (i, j) in visited: continue
-                if has_cycle(i, j, None): 
-                    return True
-        
+                visited.add((i,j))
+                if has_cycle(i,j, None): return True
+
+
         return False
