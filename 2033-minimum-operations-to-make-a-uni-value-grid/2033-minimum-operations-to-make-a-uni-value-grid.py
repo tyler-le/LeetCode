d0324c16fa936619baccd9bcf4751acc1149ad26
Tyler Le <le.tyler.h@gmail.com>
2026-04-27 20:41:50 -0700

    Time: 172 ms (45.21%), Space: 40.9 MB (76.71%) - LeetHub

---
+++ 2033-minimum-operations-to-make-a-uni-value-grid/2033-minimum-operations-to-make-a-uni-value-grid.py
@@ -0,0 +1,30 @@
+class Solution:
+    def minOperations(self, grid: List[List[int]], x: int) -> int:
+        # calculate the number of operations to get all elements to the median element
+
+        nums = []
+        n, m = len(grid), len(grid[0])
+        res = 0
+
+        if n == 1 and m == 1: return 0
+
+        for i in range(n):
+            for j in range(m):
+                nums.append(grid[i][j])
+
+        nums.sort()
+        median = nums[len(nums) // 2]
+
+        # check if possible
+        for num in nums:
+
+            # cannot reach median by adding or subtract x any number of times
+            if ( median - num ) % x:
+                return -1
+
+        for num in nums:
+
+            # count number of operations to go from num to median in increments of x
+            res+= ( abs(num - median) // x )
+
+        return res
