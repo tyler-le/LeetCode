98ab7d93c45a512887716c6b39986f4b416f330b
Tyler Le <le.tyler.h@gmail.com>
2026-04-28 19:18:44 -0700

    Time: 4 ms (27.03%), Space: 19.3 MB (68.21%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.py
+++ 0496-next-greater-element-i/0496-next-greater-element-i.py
@@ -1,23 +1,26 @@
 class Solution:
     def nextGreaterElement(self, nums1: List[int], nums2: List[int]) -> List[int]:
-        # map from num1 : index in nums 2
-        hmap = {}
-        nums2_max = max(nums2)
-        nums1_set = set(nums1)
-        res = []
-        for i, num in enumerate(nums2):
-            if num in nums1_set:
-                hmap[num] = i
+        # keep a monotonically decreasing stack
+        # build the (element, next_greater) of the numbers in nums2 using this stack
 
+        decreasing = []
+        next_greater_in_nums_2 = {}
+
+        for num in nums2:
+
+            # with a monotonically decreasing stack
+            # the element we popped is greater than num
+            # this must mean that popped's next greater element is num
+            while decreasing and decreasing[-1] < num:
+                smaller = decreasing.pop()
+                next_greater_in_nums_2[smaller] = num
+            
+            decreasing.append(num)
+        
+        res = []
 
         for num in nums1:
-            index_2 = hmap[num]
-            found = False
-            for i in range(index_2 + 1, len(nums2)):
-                if nums2[i] > num:
-                    res.append(nums2[i])
-                    found = True
-                    break
-            if not found: res.append(-1)
+            if num not in next_greater_in_nums_2: res.append(-1)
+            else: res.append(next_greater_in_nums_2[num])
 
         return res
