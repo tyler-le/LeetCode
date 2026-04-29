d1c08868123a2c1d01099e8b0390504617879826
Tyler Le <le.tyler.h@gmail.com>
2026-04-28 17:53:46 -0700

    Time: 155 ms (21.39%), Space: 24.7 MB (68.72%) - LeetHub

--- 1248-count-number-of-nice-subarrays/1248-count-number-of-nice-subarrays.py
+++ 1248-count-number-of-nice-subarrays/1248-count-number-of-nice-subarrays.py
@@ -1,26 +1,23 @@
 class Solution:
     def numberOfSubarrays(self, nums: List[int], k: int) -> int:
         
-        # number of subarrays with at most k odds
-        
-        def at_most(k):
-            if k < 0: return 0
-            
-            l = 0
+        def at_most(x):
+            # returns the number of subarrays with at most x odds
+            l, n = 0, len(nums)
             num_odds = 0
             res = 0
-            
-            for r in range(len(nums)):
-                num_odds += (1 if nums[r] % 2 else 0)
-                
-                while num_odds > k:
-                    num_odds -= (1 if nums[l] % 2 else 0)
+
+            for r in range(n):
+                if nums[r] % 2:
+                    num_odds+=1
+
+                while num_odds > x:
+                    if nums[l] % 2:
+                        num_odds-=1
                     l+=1
                 
-                res += (r-l+1)
-                
+                if num_odds <= x:
+                    res+=(r-l+1)
+
             return res
-                
-            
-        
         return at_most(k) - at_most(k-1)
