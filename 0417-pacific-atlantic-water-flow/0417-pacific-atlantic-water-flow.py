2339ee9e517dec731af35afb90b0f33fe61e72ed
Tyler Le <le.tyler.h@gmail.com>
2026-04-29 18:01:38 -0700

    Time: 27 ms (75.24%), Space: 22.2 MB (38.26%) - LeetHub

--- 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.py
+++ 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.py
@@ -2,27 +2,32 @@ class Solution:
     def pacificAtlantic(self, heights: List[List[int]]) -> List[List[int]]:
         pacific = set()
         atlantic = set()
-        
-        # start from the edges of pacific and atlantic
-        # dfs, visiting nodes that are strictly greater and add to set
-        
-        def dfs(i, j, ocean_set):
-            if (i, j) in ocean_set: return
-            
-            ocean_set.add((i,j))
-            
-            for dx, dy in [(-1,0), (1,0), (0,-1), (0,1)]:
-                if i+dx < 0 or j+dy < 0 or i+dx >= n or j+dy >= m: continue
-                if heights[i][j] <= heights[i+dx][j+dy]:
-                    dfs(i+dx, j+dy, ocean_set)
-            
-        
-        
+
         n, m = len(heights), len(heights[0])
-        
-        for i in range(n):
-            for j in range(m):            
-                if j == 0 or i == 0: dfs(i, j, pacific)
-                if i == n-1 or j == m-1: dfs(i, j, atlantic)
+
+        def dfs(x, y, reachable):
+            
+            if (x,y) in reachable: return
+            reachable.add((x,y))
+
+            for dx, dy in [(-1,0), (1,0), (0,-1), (0,1)]:
+
+                r = x + dx
+                c = y + dy
+
+                if r < 0 or c < 0 or r >= n or c >= m: continue
+                if heights[x][y] > heights[r][c]: continue
                 
-        return pacific.intersection(atlantic)
+                dfs(r, c, reachable)
+
+
+
+        for i in range(n):
+            dfs(i, 0, pacific)
+            dfs(i, m-1, atlantic)
+
+        for j in range(m):
+            dfs(0, j, pacific)
+            dfs(n-1, j, atlantic)
+
+        return list(atlantic & pacific)
