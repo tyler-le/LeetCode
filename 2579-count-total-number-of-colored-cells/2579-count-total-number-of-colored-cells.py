912c41ea5df1aecd192caba9652cef16b9e5287e
Tyler Le <le.tyler.h@gmail.com>
2026-05-04 20:07:54 -0700

    Time: 0 ms (100%), Space: 19.5 MB (8.24%) - LeetHub

--- 2579-count-total-number-of-colored-cells/2579-count-total-number-of-colored-cells.py
+++ 2579-count-total-number-of-colored-cells/2579-count-total-number-of-colored-cells.py
@@ -8,5 +8,17 @@ class Solution:
         
         # return f(n)
 
+
+        '''
+        n = 1: 1
+        n = 2: 1 + 4
+        n = 3: 1 + 4 + 8
+        n = 4: 1 + 4 + 8 + 12
+
+        = 1 + sum from i=1 to n-1 (4i)
+        = 1 + [ 4(1) + 4(2) + 4(3) + ... + 4(n-1]) ]
+        = 1 + [ 4 (1 + 2 + ... + n-1) ]
+        = the answer below
+        '''
         if n == 1: return 1
         return int(1 + (4 * ( (n-1) * (n) / 2 )))
