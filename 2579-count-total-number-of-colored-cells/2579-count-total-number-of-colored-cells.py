59e2c73c8864618f542ae034747af81d8c772cbd
Tyler Le <le.tyler.h@gmail.com>
2026-05-04 20:05:17 -0700

    Time: 0 ms (100%), Space: 19.3 MB (19.41%) - LeetHub

--- 2579-count-total-number-of-colored-cells/2579-count-total-number-of-colored-cells.py
+++ 2579-count-total-number-of-colored-cells/2579-count-total-number-of-colored-cells.py
@@ -1,9 +1,12 @@
 class Solution:
     def coloredCells(self, n: int) -> int:
         
-        @cache
-        def f(i):
-            if i == 1: return 1
-            return f(i-1) + (4*(i-1))
+        # @cache
+        # def f(i):
+        #     if i == 1: return 1
+        #     return f(i-1) + (4*(i-1))
         
-        return f(n)
+        # return f(n)
+
+        if n == 1: return 1
+        return int(1 + (4 * ( (n-1) * (n) / 2 )))
