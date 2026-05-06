d38cd55e4fa1b84f1bb07f71a7f5991641d3ff83
Tyler Le <le.tyler.h@gmail.com>
2026-05-05 20:57:29 -0700

    Time: 47 ms (12.37%), Space: 23.7 MB (6.28%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.py
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.py
@@ -1,20 +1,13 @@
 class Solution:
     def subarraySum(self, nums: List[int], k: int) -> int:
-        
-        # map prefix sum : number of times that prefix sum occurs
-        hmap = defaultdict(int)
-        hmap[0] += 1
-        acc = 0
+        freq = defaultdict(int)
+        freq[0] = 1
+        curr_sum = 0
         res = 0
-        
-        for i in range(len(nums)):
-            acc+=nums[i]
-            
-            if acc - k in hmap:
-                res+=hmap[acc - k]
-            
-            hmap[acc]+=1
+
+        for num in nums:
+            curr_sum+=num
+            res+=freq[curr_sum - k]
+            freq[curr_sum]+=1
         
         return res
-        
-        
