e9c9b58bacb94928ec80d655eb3ff1d1a0bc6a2b
Tyler Le <le.tyler.h@gmail.com>
2026-05-07 16:42:29 -0700

    Time: 0 ms (100%), Space: 20.7 MB (7.81%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.py
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.py
@@ -1,40 +1,12 @@
 class Solution:
     def searchRange(self, nums: List[int], target: int) -> List[int]:
         
+        if not nums: return [-1, -1]
         n = len(nums)
-        res = [-1, -1]
+        left = bisect_left(nums, target)
+        right = bisect_right(nums, target)
 
-        if not nums: return res
 
-        # left bound
-        low, high = 0, n-1
-        while low <= high:
-            mid = (high + low) // 2
-
-            if nums[mid] < target:
-                low = mid + 1
-            elif nums[mid] > target:
-                high = mid - 1
-            else:
-                # we found target, so we save it as the current left bound
-                # and continue searching left for future occurrences
-                res[0] = mid
-                high = mid - 1
-
-        # right bound
-        low, high = 0, n-1
-        while low <= high:
-            mid = (high + low) // 2
-
-            if nums[mid] < target:
-                low = mid + 1
-            elif nums[mid] > target:
-                high = mid - 1
-            else:
-                # we found target, so we save it as the current right bound
-                # and continue searching right for future occurrences
-                res[1] = mid
-                low = mid + 1
-                
-        return res
-        
+        if left == n or right - 1 == n or nums[left]!=target or nums[right-1] != target:
+            return [-1, -1]
+        return [left, right - 1]
