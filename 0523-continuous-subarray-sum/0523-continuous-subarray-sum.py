0c3e40c9f06e4da43b77f499a4c8bf739fa2edb0
Tyler Le <le.tyler.h@gmail.com>
2026-05-07 17:50:14 -0700

    Time: 54 ms (52.38%), Space: 39.5 MB (78.38%) - LeetHub

--- 0523-continuous-subarray-sum/0523-continuous-subarray-sum.py
+++ 0523-continuous-subarray-sum/0523-continuous-subarray-sum.py
@@ -1,6 +1,25 @@
 class Solution:
     def checkSubarraySum(self, nums: List[int], k: int) -> bool:
 
+
+        """
+        Two prefix sums that have the same remainder is a factor of k
+
+        WHY?
+
+        Let a = q1 * k + r1
+            b = q2 * k + r2
+
+        Then a-b = (q1k + r1) - (q2k + r2)
+                 = q1*k + r1 - q2*k + r2
+
+        IF r1 == r2, then we get:
+
+                 = q1*k - q2*k
+                 = (q1-q2) * k
+                 
+        which is a factor of k!
+        """
         remainders = {}
         remainders[0] = -1
         acc = 0
