d82637e72b968fd8404378c3705f57309f2b2037
Tyler Le <le.tyler.h@gmail.com>
2026-05-07 17:45:25 -0700

    Time: 59 ms (29.74%), Space: 39.7 MB (24.53%) - LeetHub

--- 0523-continuous-subarray-sum/0523-continuous-subarray-sum.py
+++ 0523-continuous-subarray-sum/0523-continuous-subarray-sum.py
@@ -1,14 +1,21 @@
 class Solution:
     def checkSubarraySum(self, nums: List[int], k: int) -> bool:
-        hmap = defaultdict(int)
+
+        remainders = {}
+        remainders[0] = -1
         acc = 0
-        hmap[0] = -1
+
+
         for i, num in enumerate(nums):
             acc+=num
-            r = acc % k
-            if r in hmap and i - hmap[r] >= 2: 
-                return True
-            if r not in hmap: 
-                hmap[r] = i
+
+            if (acc % k) in remainders:
+                if i - remainders[acc % k] >= 2:
+                    return True
+            
+            else:
+                remainders[acc % k] = i
+
+
         
         return False
