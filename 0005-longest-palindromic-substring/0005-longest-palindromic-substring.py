1e31539f1dc5c272f066986e03a4349be4f3ab19
Tyler Le <le.tyler.h@gmail.com>
2026-05-09 14:32:50 -0700

    Time: 399 ms (35.24%), Space: 19.2 MB (70.33%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
@@ -1,75 +1,47 @@
 class Solution:
     def longestPalindrome(self, s: str) -> str:
-        
+
         n = len(s)
-        dp = [[False for _ in range(n)] for _ in range(n)]
-        res = (1, [0,0])
 
-        for substr_len in range(n):
 
-            for i in range(n - substr_len):
-                j = i + substr_len
+        # returns longest palindrome 
+        def expand_outward(i):
+            res = [0,0]
 
-                # base case: length 0,1 strings always true
-                if j-i+1 <= 1: 
-                    dp[i][j] = True
-                    continue
+            # check even
+            left = i
+            right = left + 1
+            while left >= 0 and right < n:
+                if s[left] == s[right]:
+                    if (right - left + 1) > (res[1] - res[0] + 1):
+                        res = [left, right]
+                    left-=1
+                    right+=1
+                else:
+                    break
 
-                # base case: length 2 substrings just compare their chars
-                # can't be found via dp[i+1][j-1] because j will be > than i
-                elif j-i+1 == 2:
-                    if s[i] == s[j]:
-                        dp[i][j] = True
+            # check odd
+            left = i
+            right = i
+            while left >= 0 and right < n:
+                if s[left] == s[right]:
+                    if (right - left + 1) > (res[1] - res[0] + 1):
+                        res = [left, right]
+                    left-=1
+                    right+=1
+                else:
+                    break
 
-                # recursive call: length 3+ substrings compare chars at the end 
-                # AND check if the inner substring is a palindrome
-
-                elif s[i] == s[j] and dp[i+1][j-1]:
-                    dp[i][j] = True
-                
-                # update result
-                if dp[i][j] and j-i+1 > res[0]: 
-                    res = (j-i+1, (i,j))
-
-        start, end = res[1]
-        return s[start: end+1]
-
-                
+            return res
 
 
 
-
-        """
-        n = len(s)
-        res = (1, [0,0])
-        cache = {}
-
-
-        def f(i, j):
-            nonlocal res
-
-            if (i, j) in cache: 
-                return cache[(i,j)]
-
-            if i >= j: 
-                cache[(i,j)] = True
-                return cache[(i,j)]
-
-            if s[i] == s[j] and f(i+1, j-1):
-                if j-i+1 > res[0]: res = (j-i+1, (i,j))
-                cache[(i,j)] = True
-                return cache[(i,j)]
-            
-            cache[(i,j)] = False
-            return cache[(i,j)]
-
+        res = (0, 0)
 
         for i in range(n):
-            for j in range(i, n):
-                f(i, j)
+            start, end = expand_outward(i)
 
-        start, end = res[1]
-        return s[start: end+1]
-        """
-
-
+            if (end - start + 1) > (res[1] - res[0] + 1):
+                res = (start, end)
+        
+        return s[res[0]:res[1] + 1]
