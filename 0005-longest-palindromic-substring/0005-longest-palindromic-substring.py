4712a651bb1db6d34d5c03cd9bb412c6d9f29568
Tyler Le <le.tyler.h@gmail.com>
2026-05-09 12:47:46 -0700

    Time: 3121 ms (15.04%), Space: 27.8 MB (5.02%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
@@ -1,67 +1,68 @@
 class Solution:
     def longestPalindrome(self, s: str) -> str:
-        # dp[i][j] returns True if s[i...j] is a palindrome
+        
         n = len(s)
+        dp = [[0 for _ in range(n)] for _ in range(n)]
+        res = (1, [0,0])
 
-        dp = [[False for _ in range(n)] for _ in range(n)]
-        res = (1, 0, 0)
+        for substr_len in range(n):
 
-        # Base cases
-        for i in range(n):
-            for j in range(n):
-                if i >= j: 
+            for i in range(n - substr_len):
+                j = i + substr_len
+                if i == j: 
                     dp[i][j] = True
-                    
-        
-        # Fill out the rest of dp. 
-        # we should fill out smaller substrings first since our dp access pattern queries smaller substrings
-        # we will explore fixed windows from length 2 to n+1. i.e. substrings of length 2, then 3, then ...
-        
-        # length is the current substring/window length
-        for length in range(2, n+1):
+                    continue
 
-            # i is the the starting index of substring
-            for i in range(0, n - length + 1):
-
-                # j is the ending index of substring/window for a fixed window of size 'length'
-                j = i + length - 1
-                if s[i] == s[j] and dp[i+1][j-1]:
-                    dp[i][j] = True
+                if j-i+1 == 2:
+                    if s[i] == s[j]:
+                        dp[i][j] = True
+                        if j-i+1 > res[0]: 
+                            res = (j-i+1, (i,j))
                 
-                if dp[i][j] and j-i+1 > res[0]:
-                    res = (j-i+1, i, j)
-        
-        return s[res[1]: res[2]+1]
+                elif s[i] == s[j] and dp[i+1][j-1]:
+                    dp[i][j] = True
+                    if j-i+1 > res[0]: 
+                        res = (j-i+1, (i,j))
+
+        start, end = res[1]
+        return s[start: end+1]
+
+                
+
 
 
 
         """
-        RECURSION + MEMOIZATION
+        n = len(s)
+        res = (1, [0,0])
+        cache = {}
+
+
+        def f(i, j):
+            nonlocal res
+
+            if (i, j) in cache: 
+                return cache[(i,j)]
+
+            if i >= j: 
+                cache[(i,j)] = True
+                return cache[(i,j)]
+
+            if s[i] == s[j] and f(i+1, j-1):
+                if j-i+1 > res[0]: res = (j-i+1, (i,j))
+                cache[(i,j)] = True
+                return cache[(i,j)]
+            
+            cache[(i,j)] = False
+            return cache[(i,j)]
+
+
+        for i in range(n):
+            for j in range(i, n):
+                f(i, j)
+
+        start, end = res[1]
+        return s[start: end+1]
         """
-        # f(i, j) returns if s[i...j] is a palindrome
-        # cache = {}
-
-        # def f(i, j):
-        #     if i >= j: return True
-        #     if (i, j) in cache: return cache[(i, j)]
-            
-        #     res = False
-        #     if s[i] == s[j]: res = f(i+1, j-1)
-        #     else: res = False
-            
-        #     cache[(i, j)] = res
-        #     return cache[(i, j)]
-
-        # res = (0, -1, -1)
-        # n = len(s)
-
-        # for i in range(n):
-        #     for j in range(i, n):
-        #         if f(i, j) and (j-i+1 > res[0]):
-        #             res = (j-i+1, i, j)
-        
-        # return s[res[1] : res[2] + 1]
 
 
-            
-          
