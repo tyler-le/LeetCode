dc85c4882760ccf7b188800de0203fcea5bb3ad7
Tyler Le <le.tyler.h@gmail.com>
2026-05-09 20:45:38 -0700

    Time: 39 ms (94.72%), Space: 36.6 MB (67.77%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.py
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.py
@@ -4,7 +4,7 @@ class Solution:
         res = 0
 
         for num in seen:
-            if num - 1 not in seen and num in seen:
+            if num - 1 not in seen:
                 cnt = 0
                 x = num
                 while x in seen:
