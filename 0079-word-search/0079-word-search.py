beb8d48f9409801208636e64b581c574fe15dce5
Tyler Le <le.tyler.h@gmail.com>
2026-05-09 21:26:41 -0700

    Time: 4681 ms (21.96%), Space: 19.5 MB (57.36%) - LeetHub

--- 0079-word-search/0079-word-search.py
+++ 0079-word-search/0079-word-search.py
@@ -1,35 +1,32 @@
 class Solution:
     def exist(self, board: List[List[str]], word: str) -> bool:
-        
-
-        n = len(word)
-        rows, cols = len(board), len(board[0])
         visited = set()
+        n, m = len(board), len(board[0])
 
-        def backtrack(index, x, y):
-            nonlocal rows, cols, visited
+        def dfs(x, y, word_index):
 
-            if index == n: return True
-            if x < 0 or y < 0 or x >= rows or y >= cols: return False
+            if word_index >= len(word): return True
+            if x < 0 or y < 0 or x >= n or y >= m: return False
             if (x,y) in visited: return False
-            if word[index] != board[x][y]: return False
-
+            if board[x][y] != word[word_index]: return False
+            
             visited.add((x,y))
-            
+
             for dx, dy in [(-1,0), (1,0), (0,-1), (0,1)]:
-                r, c = x+dx, y+dy
-                if backtrack(index + 1, r, c):
+                if dfs(x+dx, y+dy, word_index+1):
                     return True
-            
+
             visited.remove((x,y))
-            
 
             return False
 
-        
-        for r in range(rows):
-            for c in range(cols):
-                if board[r][c] == word[0] and backtrack(0, r, c):
-                    return True
-        
+        for i in range(n):
+            for j in range(m):
+                if board[i][j] == word[0] and dfs(i,j,0): return True
+
         return False
+
+
+
+        
+            
