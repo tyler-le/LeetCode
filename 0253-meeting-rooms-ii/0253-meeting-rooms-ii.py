394cfbb08a84e7c146206c251cca5ccaaadf6212
Tyler Le <le.tyler.h@gmail.com>
2026-05-10 00:33:53 -0700

    Time: 0 ms (100%), Space: 21.3 MB (95.27%) - LeetHub

--- 0253-meeting-rooms-ii/0253-meeting-rooms-ii.py
+++ 0253-meeting-rooms-ii/0253-meeting-rooms-ii.py
@@ -1,39 +1,21 @@
 class Solution:
     def minMeetingRooms(self, intervals: List[List[int]]) -> int:
-        
-        # -------------------------------------------------[0,30]
-        #      ------------ [5,10]
-        #                             ------ [15,20]
-
-        # # sort by start
-
-        # [[0,30],[5,10],[15,20]]
-
-        # data structure min heap - keep track of each rooms end time
-        # [20, 30]
-
+        # count the min number of meeting rooms required
+        # keep track of the room that ends first (min heap end time)
+        # if we cannot reserve a room for that one, 
+        # then we cannot reserve a room for any of them
 
         intervals.sort(key = lambda x : x[0])
-        min_heap = [] # end times
-        n = len(intervals)
+        min_heap = [intervals[0][1]]
 
-        for i in range(n):
-            curr_start, curr_end = intervals[i]
-
-            if not min_heap:
+        for curr_start, curr_end in intervals[1:]:
+            
+            top = min_heap[0]
+            if curr_start < top:
                 heappush(min_heap, curr_end)
             else:
-                # merge with the best room
-                if min_heap[0] <= curr_start:
-                    heappop(min_heap)
-                    heappush(min_heap, curr_end)
-
-                # create a new room
-                else:
-                    heappush(min_heap, curr_end)
-
-        return len(min_heap)
-
-
-
+                heappop(min_heap)
+                heappush(min_heap, curr_end)
         
+        print(min_heap)
+        return len(min_heap)
