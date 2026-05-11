fb38f4fbede40be3e45b39e309f2f7ddfa01b9b6
Tyler Le <le.tyler.h@gmail.com>
2026-05-10 20:23:42 -0700

    Time: 479 ms (35.38%), Space: 20 MB (15.32%) - LeetHub

--- 1930-unique-length-3-palindromic-subsequences/1930-unique-length-3-palindromic-subsequences.py
+++ 1930-unique-length-3-palindromic-subsequences/1930-unique-length-3-palindromic-subsequences.py
@@ -1,28 +1,27 @@
 class Solution:
     def countPalindromicSubsequence(self, s: str) -> int:
-        earliest = [float("inf") for _ in range(26)]
-        latest = [-float("inf") for _ in range(26)]
-        res = 0
-        
-        for i in range(len(s)):
-            ch = ord(s[i]) - ord("a")
-            earliest[ch] = min(i, earliest[ch])
-            latest[ch] = max(i, latest[ch])
-        
-        for c in ascii_lowercase:
-            idx = ord(c) - ord("a")
-            start, end = earliest[idx], latest[idx]
-            if start == float("inf") or end == -float("inf"):
-                continue
-                
-            uniques = set()
+        unique_chars = set(s)
 
-            for i in range(start+1, end):
-                uniques.add(s[i])
+        first = {}
+        last = {}
+
+        res = 0
+
+        for i, ch in enumerate(s):
+            if ch not in first:
+                first[ch] = i
+                last[ch] = i
             
-            res+=len(uniques)
+            else:
+                last[ch] = i
+
+        for ch in unique_chars:
+            start, end = first[ch], last[ch]
+            letters_in_between = set()
+            
+            for i in range(start+1, end):
+                letters_in_between.add(s[i])
+            
+            res+=len(letters_in_between)
         
         return res
-            
-        
-        
