68ff67a2cea24450583889bb0012e8abb24fe872
Tyler Le <le.tyler.h@gmail.com>
2026-05-11 19:57:19 -0700

    Time: 0 ms (100%), Space: 19.4 MB (38.68%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.py
+++ 0088-merge-sorted-array/0088-merge-sorted-array.py
@@ -3,20 +3,31 @@ class Solution:
         """
         Do not return anything, modify nums1 in-place instead.
         """
-        p1, p2, p3 = m-1, n-1, n+m-1
+        p1, p2 = m-1, n-1
 
-        while p1 >= 0 and p2 >= 0:
-            if nums2[p2] >= nums1[p1]:
+        for p3 in range(n+m-1, -1, -1):
+
+            
+
+            # compare nums1[p1] and nums2[p2]
+
+            # nothing left to merge in nums2
+            if p2 < 0: break
+
+            # nothing left to merge in nums1 (but still stuff in nums2)
+            if p1 < 0: 
                 nums1[p3] = nums2[p2]
-                p3-=1
                 p2-=1
+
+            # pull from nums2
+            elif nums2[p2] >= nums1[p1]:
+                nums1[p3] = nums2[p2]
+                p2-=1
+
+            # pull from nums1
             else:
-                nums1[p1], nums1[p3] = nums1[p3], nums1[p1]
+                nums1[p3] = nums1[p1]
                 p1-=1
-                p3-=1
         
-        while p2 >= 0:
-            nums1[p3] = nums2[p2]
-            p3-=1
-            p2-=1
+
         
