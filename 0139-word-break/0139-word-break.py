c11e257532935529c10936bec36beecad4248888
Tyler Le <le.tyler.h@gmail.com>
2026-05-14 18:49:17 -0700

    Time: 0 ms (100%), Space: 19.6 MB (14.8%) - LeetHub

--- 0139-word-break/0139-word-break.py
+++ 0139-word-break/0139-word-break.py
@@ -1,49 +1,17 @@
 class Solution:
     def wordBreak(self, s: str, wordDict: List[str]) -> bool:
         
+        @cache
+        def f(index):
+            if index <= 0: return True
 
-        """
-        DP
-        """
-        # dp[i] = dp[0:i] inclusive is word breakable
+            for word in wordDict:
+                k = len(word)
+                if s[index - k : index] == word:
+                    if f(index - k): return True
 
-        # return dp[n]
+            return False
+            
 
         n = len(s)
-        dp = [False for _ in range(n)]
-
-        for i in range(n-1, -1, -1):
-            for word in wordDict:
-                if i + len(word) <= n:
-                    prefix = s[i : i+len(word)]
-
-                    if prefix == word:
-                        if i + len(word) >= len(dp): 
-                            dp[i] = True
-                            break
-                        elif dp[i + len(word)]: 
-                            dp[i] = True
-                            break
-                        
-        
-        return dp[0]
-
-
-
-        """
-        RECURSION + MEMOIZATION
-        """
-        cache = {}
-        def f(x):
-            if x == "": return True
-            if x in cache: return cache[x]
-
-            for word in wordDict:
-                prefix = x[:len(word)]
-                if prefix == word and f(x[len(word):]):
-                    cache[x] = True
-                    return True
-            cache[x] = False
-            return False
-        
-        return f(s)
+        return f(n)
