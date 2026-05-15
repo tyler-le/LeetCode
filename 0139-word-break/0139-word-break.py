b7fb54915c49826e9eaf00105a26ef3e357165bb
Tyler Le <le.tyler.h@gmail.com>
2026-05-14 18:56:09 -0700

    Time: 7 ms (23.38%), Space: 19.5 MB (26.56%) - LeetHub

--- 0139-word-break/0139-word-break.py
+++ 0139-word-break/0139-word-break.py
@@ -1,6 +1,30 @@
 class Solution:
     def wordBreak(self, s: str, wordDict: List[str]) -> bool:
         
+
+        """
+        DP
+        dp[i] = is s[0...index] word-breakable
+        """
+        n = len(s)
+        dp = [False for _ in range(n)]
+        for i in range(n):
+            for word in wordDict:
+                k = len(word)
+
+                if i-k+1 >=0 and s[i-k+1:i+1] == word:
+                    print(word)
+                    if i-k+1 == 0: dp[i] = True
+                    else: dp[i] |= dp[i-k]
+
+        print(dp)
+        return dp[-1]
+                    
+
+        """
+        Recursive
+        f(index) = is s[0...index] word-breakable
+        """
         @cache
         def f(index):
             if index <= 0: return True
@@ -9,9 +33,7 @@ class Solution:
                 k = len(word)
                 if s[index - k : index] == word:
                     if f(index - k): return True
-
             return False
             
-
         n = len(s)
         return f(n)
