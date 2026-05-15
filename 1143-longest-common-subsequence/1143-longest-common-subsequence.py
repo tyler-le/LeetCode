13db87f8dd1088016673c163e58b16218f477ed8
Tyler Le <le.tyler.h@gmail.com>
2026-05-15 16:14:21 -0700

    Time: 1279 ms (8.64%), Space: 322.1 MB (5.12%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.py
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.py
@@ -6,22 +6,22 @@ class Solution:
         dp[i][j] = longest common sequence between text1[0:i] and text2[0:j]
         """
 
-        n, m = len(text1), len(text2)
-        dp = [[0 for _ in range(m)] for _ in range(n)]
+        # n, m = len(text1), len(text2)
+        # dp = [[0 for _ in range(m)] for _ in range(n)]
 
-        for i in range(n):
-            for j in range(m):
-                if text1[i] == text2[j]:
-                    dp[i][j] += 1 + (
-                        dp[i-1][j-1] if i-1 >= 0 and j-1 >= 0 else 0
-                    )
-                else:
-                    first = dp[i-1][j] if i-1 >= 0 else 0
-                    second = dp[i][j-1] if j-1 >= 0 else 0
-                    dp[i][j] += max(first, second)
+        # for i in range(n):
+        #     for j in range(m):
+        #         if text1[i] == text2[j]:
+        #             dp[i][j] += 1 + (
+        #                 dp[i-1][j-1] if i-1 >= 0 and j-1 >= 0 else 0
+        #             )
+        #         else:
+        #             first = dp[i-1][j] if i-1 >= 0 else 0
+        #             second = dp[i][j-1] if j-1 >= 0 else 0
+        #             dp[i][j] += max(first, second)
 
 
-        return dp[n-1][m-1]
+        # return dp[n-1][m-1]
 
 
 
@@ -40,7 +40,7 @@ class Solution:
             if text1[i] == text2[j]:
                 res += 1 + f(i+1, j+1)
             else:
-                choice = max(f(i+1, j),f(i, j+1))
+                choice = max(f(i+1, j),f(i, j+1), f(i+1, j+1))
                 res+=choice
             
             return res
