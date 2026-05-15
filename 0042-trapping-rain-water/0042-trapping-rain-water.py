f35fc0c69167d6feb3dd6a85e33eb3a498d12e7e
Tyler Le <le.tyler.h@gmail.com>
2026-05-14 17:06:12 -0700

    Time: 23 ms (7.84%), Space: 21 MB (49.75%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.py
+++ 0042-trapping-rain-water/0042-trapping-rain-water.py
@@ -1,22 +1,26 @@
 class Solution:
     def trap(self, height: List[int]) -> int:
-        
+        tallest_from_left = []
+        tallest_from_right = []
         n = len(height)
-        max_from_left = [0 for _ in range(n)]        
-        max_from_right = [0 for _ in range(n)]        
         res = 0
+
+        for i in range(n):
+            if not tallest_from_left: tallest_from_left.append(height[i])
+            else: tallest_from_left.append(max(height[i], tallest_from_left[i-1]))
         
-        for i in range(n):
-            if i == 0: max_from_left[0] = height[0]
-            else: max_from_left[i] = max(max_from_left[i-1], height[i])
-    
         for i in range(n-1, -1, -1):
-            if i == n-1: max_from_right[n-1] = height[n-1]
-            else: max_from_right[i] = max(max_from_right[i+1], height[i])
-                
-        for i in range(n):
-            h = min(max_from_right[i], max_from_left[i])
-            res+=(h - height[i])
+            if not tallest_from_right: tallest_from_right.append(height[i])
+            else: tallest_from_right.append(max(height[i], tallest_from_right[-1]))
+
+        tallest_from_right.reverse()
+        
+        for i in range(1, n-1):
+            h = height[i]
+            smallest = min(tallest_from_left[i-1], tallest_from_right[i+1])
+            res+=(max(0, smallest-h))
         
         return res
-            
+
+
+        
