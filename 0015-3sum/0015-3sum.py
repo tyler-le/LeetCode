03f877de858da4ccf55664d9f904d3f230d19d3b
Tyler Le <le.tyler.h@gmail.com>
2026-05-16 11:46:58 -0700

    Time: 742 ms (26.92%), Space: 22.3 MB (54.12%) - LeetHub

--- 0015-3sum/0015-3sum.py
+++ 0015-3sum/0015-3sum.py
@@ -18,7 +18,7 @@ class Solution:
                     l+=1
                     r-=1
                     while l < n and nums[l] == nums[l-1]: l+=1
-                    while r>=0 and nums[r] == nums[r+1]: r-=1
+                    while r >= 0 and nums[r] == nums[r+1]: r-=1
 
         return res
 
