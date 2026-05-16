ed765e7422f5db18f8d72b86d2e051316a2a9867
Tyler Le <le.tyler.h@gmail.com>
2026-05-15 21:03:56 -0700

    Time: 0 ms (100%), Space: 21.3 MB (95.29%) - LeetHub

--- 0253-meeting-rooms-ii/0253-meeting-rooms-ii.py
+++ 0253-meeting-rooms-ii/0253-meeting-rooms-ii.py
@@ -1,21 +1,14 @@
 class Solution:
     def minMeetingRooms(self, intervals: List[List[int]]) -> int:
-        # count the min number of meeting rooms required
-        # keep track of the room that ends first (min heap end time)
-        # if we cannot reserve a room for that one, 
-        # then we cannot reserve a room for any of them
-
+        
         intervals.sort(key = lambda x : x[0])
         min_heap = [intervals[0][1]]
 
         for curr_start, curr_end in intervals[1:]:
-            
-            top = min_heap[0]
-            if curr_start < top:
+            if curr_start < min_heap[0]:
                 heappush(min_heap, curr_end)
             else:
-                heappop(min_heap)
-                heappush(min_heap, curr_end)
+                heappush(min_heap, max(heappop(min_heap), curr_end))
         
-        print(min_heap)
         return len(min_heap)
+        
