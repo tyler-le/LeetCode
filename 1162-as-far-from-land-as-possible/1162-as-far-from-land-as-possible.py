402f5f113892b1e86eda1beaa8e9ac88f3c104b8
Tyler Le <le.tyler.h@gmail.com>
2026-05-20 19:08:04 -0700

    Time: 170 ms (25.44%), Space: 21.2 MB (12.53%) - LeetHub

--- 1162-as-far-from-land-as-possible/1162-as-far-from-land-as-possible.py
+++ 1162-as-far-from-land-as-possible/1162-as-far-from-land-as-possible.py
@@ -1,31 +1,31 @@
 class Solution:
     def maxDistance(self, grid: List[List[int]]) -> int:
-        
-        n, m, level = len(grid), len(grid[0]), 0       
-        q, visited, res = deque(), set(), 0
-        
-        for i in range(n):
+        q = deque()
+        visited = set()
+        n, m = len(grid), len(grid[0])
+        res = -1
+
+        for i in range(n): 
             for j in range(m):
-                if grid[i][j]:
-                    q.append((i, j, 0))
-        
-        if not q or n*m == len(q): return -1
-        
+                if grid[i][j] == 1:
+                    q.append((i,j,0))
+                    visited.add((i,j))
+
         while q:
-            x, y, d = q.popleft()
-            
-            for dx, dy in [(-1,0), (1,0), (0,-1), (0,1)]:
-                r = x+dx
-                c = y+dy
-                
-                if r < 0 or c < 0 or r >= n or c >= m or (r,c) in visited or grid[r][c]:
-                    continue
-                    
-                q.append((r,c, d+1))
-                res = max(res, d+1)
-                visited.add((r,c))
-                
+            level_size = len(q)
+
+            for _ in range(level_size):
+                popped_x, popped_y, popped_dist = q.popleft()
+
+                for dx, dy in [(-1,0), (1,0), (0,-1), (0,1)]:
+                    r = popped_x + dx
+                    c = popped_y + dy
+
+                    if r < 0 or c < 0 or r >= n or c >= m: continue
+                    if (r,c) in visited: continue
+                    if grid[r][c] != 0: continue
+                    res = max(res, popped_dist + 1)
+                    q.append((r,c,popped_dist+1))
+                    visited.add((r,c))
+
         return res
-            
-                    
-        
