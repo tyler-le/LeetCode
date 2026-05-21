ecb984abb3460fc812dc79b6ae43a25cb42d1f35
Tyler Le <le.tyler.h@gmail.com>
2026-05-20 18:58:35 -0700

    Time: 67 ms (39.88%), Space: 22.5 MB (47.27%) - LeetHub

--- 0934-shortest-bridge/0934-shortest-bridge.py
+++ 0934-shortest-bridge/0934-shortest-bridge.py
@@ -1,39 +1,51 @@
 class Solution:
     def shortestBridge(self, grid: List[List[int]]) -> int:
-        def convert_to_two(i, j):
-            if i < 0 or j < 0 or i >= n or j >= m: return
-            if grid[i][j] != 1: return
-            
-            grid[i][j] = 2
-            q.append((i,j, 0))
-            for dx, dy in [(-1,0), (1,0), (0,-1), (0,1)]:
-                convert_to_two(i+dx, j+dy)
         
         n, m = len(grid), len(grid[0])
-        q = deque() # (row, col, steps)
-        
-        converted_island = False
-        for i in range(n):
-            if converted_island: break
-            for j in range(m):
-                if grid[i][j]:
-                    convert_to_two(i, j)
-                    converted_island = True
-                    break
-        
-        while q:
-            x, y, steps = q.popleft()
-            
-            for dx, dy in [(-1,0), (1,0), (0,1), (0,-1)]:
-                r, c = x+dx, y+dy
-                if r < 0 or c < 0 or r >= n or c >= m: continue
-                if grid[r][c] == 0: 
-                    q.append((r,c,steps+1))
-                    grid[r][c] = 2
-                elif grid[r][c] == 1: return steps
-                elif grid[r][c] == 2: continue
+        res = math.inf
+
+        # change "1"-island to a "2"-island
+        def dfs(i,j):
+            if i < 0 or j < 0 or i >= n or j >= m: return
+            if grid[i][j] != 1: return
+            grid[i][j] = 2
+            for di, dj in [(-1,0), (1,0), (0,-1), (0,1)]:
+                r = i + di
+                c = j + dj
+                dfs(r,c)
+
+        def change_island():
+            for i in range(n):
+                for j in range(m):
+                    if grid[i][j] == 1:
+                        dfs(i,j)
+                        return
             
 
-            
-        
-        
+        # first change an island to all 2s        
+        change_island()
+
+        # run multisource bfs from 1-nodes to 2-nodes
+        q = deque()
+        visited = set()
+        for i in range(n):
+            for j in range(m):
+                if grid[i][j] == 1:
+                    q.append((i,j,0))
+                    visited.add((i,j))
+
+        while q:
+            level_size = len(q)
+            for _ in range(level_size):
+                popped_x, popped_y, popped_dist = q.popleft()
+
+                for dx, dy in [(-1,0),(1,0),(0,-1),(0,1)]:
+                    r = popped_x + dx
+                    c = popped_y + dy
+                    if r < 0 or c < 0 or r >= n or c >= m: continue
+                    if (r,c) in visited: continue
+                    if grid[r][c] == 2: return popped_dist
+                    visited.add((r,c))
+                    q.append((r,c,popped_dist + 1))
+
+    
