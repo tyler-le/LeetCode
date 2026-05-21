5def433ad4d94924a16e77b78deecf6305022281
Tyler Le <le.tyler.h@gmail.com>
2026-05-20 22:38:58 -0700

    Time: 45 ms (43.12%), Space: 34.1 MB (61.89%) - LeetHub

--- 0131-palindrome-partitioning/0131-palindrome-partitioning.py
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.py
@@ -5,24 +5,24 @@ class Solution:
         res = []
 
         def is_pal(l, r):
-
             while l <= r:
                 if s[l] != s[r]: return False
                 l+=1
                 r-=1
             return True
-
+        
         def f(start, path):
-            nonlocal res
+
             if start == n:
                 res.append(path.copy())
                 return
-            
+
             for end in range(start, n):
                 if is_pal(start, end):
                     path.append(s[start:end+1])
                     f(end + 1, path)
                     path.pop()
-
+        
         f(0, [])
         return res
+            
