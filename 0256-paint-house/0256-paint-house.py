75c33fc50f07c6397d5d8d1b925b6d26bee4a656
Tyler Le <le.tyler.h@gmail.com>
2026-05-20 19:39:01 -0700

    Time: 3 ms (62.18%), Space: 19.4 MB (49.86%) - LeetHub

--- 0256-paint-house/0256-paint-house.py
+++ 0256-paint-house/0256-paint-house.py
@@ -1,5 +1,35 @@
 class Solution:
     def minCost(self, costs: List[List[int]]) -> int:
+        """
+        dp[i][j] = min cost to paint house i 
+        with prev house being color j
+        """
+        n, m = len(costs), len(costs[0])
+
+        dp = [[math.inf for _ in range(m)] for _ in range(n)]
+
+        for j in range(len(costs[0])):
+            dp[0][j] = costs[0][j]
+        
+
+        for i in range(1, n):
+            
+            for j in range(m):
+
+                subproblem = math.inf
+                for k in range(m):
+                    if k != j: subproblem = min(subproblem, dp[i-1][k])
+                total = costs[i][j] + subproblem
+                dp[i][j] = min(dp[i][j], total)
+
+        
+        return min(dp[n-1])
+        
+        """
+        RECURSIVE + MEMO
+        f(index, prev_color) = min cost to paint house 'index' 
+        with prev house being color 'prev_color'
+        """
         n = len(costs)
         @cache
         def f(index, prev_color):
