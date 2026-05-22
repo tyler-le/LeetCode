7ed30293448b078876ebafb246987ad0c756447f
Tyler Le <le.tyler.h@gmail.com>
2026-05-21 20:11:57 -0700

    Time: 29 ms (89.3%), Space: 20.2 MB (58.68%) - LeetHub

--- 0729-my-calendar-i/0729-my-calendar-i.py
+++ 0729-my-calendar-i/0729-my-calendar-i.py
@@ -5,31 +5,52 @@ class MyCalendar:
         
 
     def book(self, startTime: int, endTime: int) -> bool:
-        res = []
+    
+        if not self.intervals: 
+            self.intervals.append([startTime, endTime])
+            return True
 
-        for i in range(len(self.intervals)):
-            curr_start, curr_end = self.intervals[i]
+        low, high = 0, len(self.intervals)
 
-            # [startTime, endTime] comes after [curr_start, curr_end]
-            if curr_end <= startTime:
-                res.append([curr_start, curr_end])
+        while low < high:
+            mid = low + ((high - low) // 2)
 
+            # compare (start, end) with self.intervals[mid] 
+            mid_start, mid_end = self.intervals[mid]
 
-            # [startTime, endTime] comes before [curr_start, curr_end]
-            elif endTime <= curr_start:
-                res.append([startTime, endTime])
-                res.extend(self.intervals[i:])
-                self.intervals = res
-                return True
-
-            # [startTime, endTime] conflicts [curr_start, curr_end]
+            if startTime >= mid_end:
+                low = mid + 1
+            elif endTime <= mid_start:
+                high = mid 
             else:
                 return False
-        
-        res.append([startTime, endTime])
-        self.intervals = res
-        return True
 
+            
+        if low == 0:
+            # compare with first interval
+            first_start, first_end = self.intervals[0]
+            if endTime <= first_start: 
+                self.intervals.insert(low, [startTime, endTime])
+                return True
+
+
+        elif low == len(self.intervals):
+            # compare with last interval
+            last_start, last_end = self.intervals[-1]
+            if last_end <= startTime:
+                self.intervals.append([startTime, endTime])
+                return True
+        else:
+            # compare with adjacent intervals
+            prev_start, prev_end = self.intervals[low-1]
+            after_start, after_end = self.intervals[low]
+
+            if prev_end <= startTime and endTime <= after_start:
+                self.intervals.insert(low, [startTime, endTime])
+                return True
+
+        return False
+            
         
 
 
