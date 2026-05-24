6cf9059a1f5913a46664216154a705ad783e2ca7
Tyler Le <le.tyler.h@gmail.com>
2026-05-23 17:33:56 -0700

    Time: 207 ms (39.08%), Space: 30.4 MB (59.91%) - LeetHub

--- 1438-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit/1438-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit.py
+++ 1438-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit/1438-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit.py
@@ -1,26 +1,33 @@
 class Solution:
     def longestSubarray(self, nums: List[int], limit: int) -> int:
-        res = 0
-        l = 0
-        n = len(nums)
         
-        hmap = defaultdict(int)
-        
-        # keep track of min/max in window
-        # contract window when max - min > limit
-        # to (re)set the max/min values, we can use a min heap and max heap for the window
-        
+        # max in window is decreasing[0]
+        decreasing = deque()
+
+        # min in window is increasing[0]
+        increasing = deque()
+
+        l, n, res = 0, len(nums), 1
+
         for r in range(n):
-            
-            
-            hmap[nums[r]]+=1
-            
-            while max(hmap.keys()) - min(hmap.keys()) > limit:
-                
-                hmap[nums[l]]-=1
-                if not hmap[nums[l]]: del hmap[nums[l]]
+            # add nums[r] to window
+            while decreasing and decreasing[-1] < nums[r]:
+                decreasing.pop()
+
+            while increasing and increasing[-1] > nums[r]:
+                increasing.pop()
+
+            decreasing.append(nums[r])
+            increasing.append(nums[r])
+
+            # shrink window from left
+            while decreasing and increasing and decreasing[0] - increasing[0] > limit:
+                popped = nums[l]
+                if decreasing[0] == popped: decreasing.popleft()
+                if increasing[0] == popped: increasing.popleft()
                 l+=1
-            
+
+            # record result
             res = max(res, r-l+1)
-        
+
         return res
