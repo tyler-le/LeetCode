f729fdc506598bfeb28dd0d368076d7921eb3c14
Tyler Le <le.tyler.h@gmail.com>
2026-05-24 16:54:54 -0700

    Time: 7 ms (45.68%), Space: 20.5 MB (55.9%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.py
+++ 0207-course-schedule/0207-course-schedule.py
@@ -1,29 +1,30 @@
 class Solution:
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
-        
-        # build the graph
+        visited = set()
         graph = defaultdict(list)
         indegrees = defaultdict(int)
         q = deque()
-        visited = set()
 
-        for (u, v) in prerequisites:
+
+        for u, v in prerequisites:
             graph[v].append(u)
             indegrees[u]+=1
 
-        for course in range(numCourses):
-            if not indegrees[course]: 
-                q.append(course)        
+        for i in range(numCourses):
+            if not indegrees[i]: 
+                q.append(i)
 
         while q:
             popped = q.popleft()
+            if popped in visited: return False
             visited.add(popped)
-            
+
             for nbor in graph[popped]:
                 indegrees[nbor]-=1
                 if not indegrees[nbor]: 
                     q.append(nbor)
-                
-        
+
         return len(visited) == numCourses
+
+
         
