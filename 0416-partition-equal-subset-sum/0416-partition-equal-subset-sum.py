057272ce5ce3bd8d5eec223106e2adb07410cefa
Tyler Le <le.tyler.h@gmail.com>
2026-05-24 19:25:50 -0700

    Time: 2713 ms (5.62%), Space: 36 MB (25.31%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.py
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.py
@@ -1,43 +1,42 @@
 class Solution:
     def canPartition(self, nums: List[int]) -> bool:
-        """
-        DP
-        dp[x] = "can we make sum of x?"
-        return dp[target]
-        """
-
-        if sum(nums) % 2: return False
-
-        target = int(sum(nums) / 2)
-        dp = [False for _ in range(sum(nums))]
-        dp[0] = True
-
-        # fix the number 
-        for num in nums:
-            # can we reach x?
-            # if we can make (x - num), then we can make x by adding num
-            for x in range(target, num - 1, -1):
-                dp[x] = dp[x] or dp[x - num]
-
-
-        return dp[target]
-
-
-        """
-        RECURSION + MEMOIZATION
-        """
+        total = sum(nums)
+        if total % 2 != 0: return False
+        m = total // 2
         n = len(nums)
-        cache = {}
-        def f(index, sum1, sum2):
-            if index >= n: return sum1 == sum2
-            if index in cache: return cache[(index, sum1, sum2)]
+        dp = [[False for _ in range(m+1)] for _ in range(n+1)]
 
-            cache[(index, sum1, sum2)] = True
+        dp[n][m] = True
+
+
+        for i in range(n-1, -1, -1):
+            for j in range(m, -1, -1):
+                num = nums[i]
+                first_choice = dp[i+1][j+num] if i+1 <= n and j+num <= m else False
+                second_choice = dp[i+1][j] if i+1 <= n else False
+                dp[i][j] = first_choice or second_choice
+
+        return dp[0][0]
+        
+        
+        
+        target = sum(nums) / 2
+        if (target * 2) != sum(nums): return False
+        n = len(nums)
+        nums.sort(reverse=True)
+
+        @cache
+        def backtrack(index, path_sum):
+            if index >= n: 
+                if path_sum == target: return True
+                return False
+            if path_sum > target: return False
+
             num = nums[index]
-            if f(index + 1, sum1 + num, sum2): return True
-            if f(index + 1, sum1, sum2 + num): return True
+            first_choice = backtrack(index + 1, path_sum + num)
+            second_choice = backtrack(index + 1, path_sum)
 
-            cache[(index, sum1, sum2)] = False
-            return False
+            return first_choice or second_choice
 
-        return f(0, 0, 0)
+        return backtrack(0, 0)
+
