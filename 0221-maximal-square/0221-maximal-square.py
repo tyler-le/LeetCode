321e7e3011ce0a3f7f6f9c1e18f84ae596f53bf8
Tyler Le <le.tyler.h@gmail.com>
2026-05-24 18:38:27 -0700

    Time: 103 ms (35.17%), Space: 33.3 MB (27.92%) - LeetHub

--- 0221-maximal-square/0221-maximal-square.py
+++ 0221-maximal-square/0221-maximal-square.py
@@ -3,6 +3,34 @@ class Solution:
         
         res = 0
         n, m = len(matrix), len(matrix[0])
+        dp = [[0 for _ in range(m)] for _ in range(n)]
+
+        for i in range(n):
+            for j in range(m):
+                if matrix[i][j] == "0": 
+                    dp[i][j] = 0
+                    continue
+
+                up = dp[i-1][j] if i-1 >= 0 else 0
+                left = dp[i][j-1] if j-1 >= 0 else 0
+                diag = dp[i-1][j-1] if i-1 >= 0 and j-1 >= 0 else 0
+
+                subproblem = min(up, left, diag)
+                dp[i][j] = 1 + subproblem
+                res = max(res, dp[i][j])
+
+        return res**2
+
+
+        """
+        f(i,j) = the side length of the maximal square with (i,j) in the bottom right corner
+
+        So if matrix[i][j] == "1", then we look at the squares formed from up, left, diag. 
+        We can form a new square based off the minimum of these 3 results. 
+        """
+        
+        res = 0
+        n, m = len(matrix), len(matrix[0])
 
         @cache
         def f(i, j):
