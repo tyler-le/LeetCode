2ecd1c45c08bbd79c027b57d1f5a222de2cfd809
Tyler Le <le.tyler.h@gmail.com>
2026-05-24 22:03:25 -0700

    Time: 0 ms (100%), Space: 20 MB (7.24%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.py
+++ 0063-unique-paths-ii/0063-unique-paths-ii.py
@@ -1,25 +1,22 @@
 class Solution:
     def uniquePathsWithObstacles(self, obstacleGrid: List[List[int]]) -> int:
-        # dp[i][j] = number of unique paths to reach [i][j]
-        n, m = len(obstacleGrid), len(obstacleGrid[0])
         
-        dp = [[0 for _ in range(m)] for _ in range(n)]
+        n, m = len(obstacleGrid), len(obstacleGrid[0])
 
-        if obstacleGrid[0][0] or obstacleGrid[-1][-1]:
-            return 0
+        @cache
+        def f(i, j):
+            if i == n or j == m: return 0
+            if obstacleGrid[i][j] == 1: return 0
+            if i == n-1 and j == m-1: return 1
 
-        # Initialize the first row and column
-        dp[0][0] = 1 - obstacleGrid[0][0]
-        for r in range(1, n):
-            dp[r][0] = dp[r-1][0] * (1 - obstacleGrid[r][0])
-        for c in range(1, m):
-            dp[0][c] = dp[0][c-1] * (1 - obstacleGrid[0][c])
+            res = 0
 
-        for i in range(1, n):
-            for j in range(1, m):
-                if obstacleGrid[i][j]:
-                    dp[i][j] = 0
-                else:
-                    dp[i][j] = dp[i-1][j] + dp[i][j-1]
+            # down
+            res+=f(i+1, j)
 
-        return dp[n-1][m-1]
+            # right
+            res+=f(i, j+1)
+
+            return res
+
+        return f(0,0)
