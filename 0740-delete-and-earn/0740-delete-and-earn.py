fc3c59c714375383d9795a784568778127b73ed6
Tyler Le <le.tyler.h@gmail.com>
2026-05-24 21:56:18 -0700

    Time: 10 ms (37.65%), Space: 21.3 MB (32.28%) - LeetHub

--- 0740-delete-and-earn/0740-delete-and-earn.py
+++ 0740-delete-and-earn/0740-delete-and-earn.py
@@ -4,10 +4,8 @@ class Solution:
         cnt = Counter(nums)
         arr = [i * cnt[i] for i in range(max(nums) + 1)]
         n = len(arr)
-
         dp = [0 for _ in range(n)]
         for index in range(n):
-            
             take = arr[index] + ( dp[index - 2]  if index - 2 >= 0 else 0)
             skip = dp[index - 1] if index - 1 >= 0 else 0
             dp[index] = max(take, skip)
@@ -19,14 +17,11 @@ class Solution:
 
         cnt = Counter(nums)
         arr = [i * cnt[i] for i in range(max(nums) + 1)]
-
         @cache
         def f(index):
             if index < 0: return 0
-
             take = arr[index] + f(index - 2)
             skip = f(index - 1)
-
             return max(take, skip)
 
         return f(len(arr) - 1)
