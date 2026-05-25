b459f301db14036d805114f386d8d884c6561faf
Tyler Le <le.tyler.h@gmail.com>
2026-05-24 18:05:39 -0700

    Time: 0 ms (100%), Space: 19.7 MB (28.6%) - LeetHub

--- 0684-redundant-connection/0684-redundant-connection.py
+++ 0684-redundant-connection/0684-redundant-connection.py
@@ -1,37 +1,29 @@
 class UnionFind:
     def __init__(self, n):
         self.parents = [i for i in range(n+1)]
-        self.parents[0] = -1
-        self.sizes = [1 for _ in range(n+1)]
+        # self.sizes = [1 for i in range(n+1)]
 
+
+    def union(self, x, y):
+        x_par = self.find(x)
+        y_par = self.find(y)
+        self.parents[y_par] = x_par
+    
     def find(self, x):
         if self.parents[x] == x: 
             return x
         else: 
-            self.parents[x] = self.find(self.parents[x])
-            return self.parents[x]
-        
-    # union by size 
-    # if sizes[x] >= sizes[y] -> x wins
-    # else sizes[x] < sizes[y] -> y wins
-    def union(self, x,y):
-        par_x, par_y = self.find(x), self.find(y)
-        if par_x == par_y: return
-        size_x, size_y = self.sizes[par_x], self.sizes[par_y]
-        if size_x >= size_y:
-            self.parents[par_y] = par_x
-            self.sizes[par_x]+=self.sizes[par_y]
-        else:
-            self.parents[par_x] = par_y
-            self.sizes[par_y]+=self.sizes[par_x]
+            root = self.find(self.parents[x])
+            self.parents[x] = root
+            return root
+
 
 class Solution:
     def findRedundantConnection(self, edges: List[List[int]]) -> List[int]:
-        uf = UnionFind(len(edges))
+        n = len(edges)
+        uf = UnionFind(n)
 
-        for x, y in edges:
-            par_x, par_y = uf.find(x), uf.find(y)
-            if par_x == par_y: return [x,y]
-            else: uf.union(x,y)
-        
+        for u, v in edges:
+            if uf.find(u) == uf.find(v): return [u,v]
+            uf.union(u,v)
         
