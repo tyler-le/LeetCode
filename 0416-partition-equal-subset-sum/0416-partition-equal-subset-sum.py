eb0f8c79b105517e2fb11b4210063c56f8584cee
Tyler Le <le.tyler.h@gmail.com>
2026-05-24 19:33:23 -0700

    Time: 2767 ms (5.27%), Space: 35.7 MB (27.72%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.py
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.py
@@ -1,22 +1,18 @@
 class Solution:
     def canPartition(self, nums: List[int]) -> bool:
+        n = len(nums)        
         total = sum(nums)
         if total % 2 != 0: return False
         target = total // 2
-        n = len(nums)
-        
         dp = [[False for _ in range(target+1)] for _ in range(n+1)]
-        nums.sort(reverse=True)
 
         """
-        corresponds to this base case is recursive sol:
-            
+        corresponds to this base case in recursive sol:
             if index >= n: 
                 if path_sum == target: return True
             
         """
         dp[n][target] = True
-
 
         for i in range(n-1, -1, -1):
             for path_sum in range(target, -1, -1):
