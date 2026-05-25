5b43217f11c4eef5895408579ce5bb391f09cc00
Tyler Le <le.tyler.h@gmail.com>
2026-05-24 21:52:47 -0700

    Time: 15 ms (19.98%), Space: 29.5 MB (8.34%) - LeetHub

--- 0740-delete-and-earn/0740-delete-and-earn.py
+++ 0740-delete-and-earn/0740-delete-and-earn.py
@@ -1,28 +1,18 @@
 class Solution:
     def deleteAndEarn(self, nums: List[int]) -> int:
+
         cnt = Counter(nums)
-        mx = max(nums)
-        arr = [0 for _ in range(mx + 1)]
+        arr = [i * cnt[i] for i in range(max(nums) + 1)]
 
-        for num, freq in cnt.items():
-            arr[num] = num * freq
-
-        
-
-        # f(index) is the most we can earn from 0...index
         @cache
         def f(index):
             if index < 0: return 0
 
-            # take from index i
             take = arr[index] + f(index - 2)
-            
+            skip = f(index - 1)
 
-            # do not take from index i
-            not_take = f(index - 1)
+            return max(take, skip)
 
-            return max(take, not_take)
-
-        n = len(arr)
-        return f(n-1)
-
+        return f(len(arr) - 1)
+        
+        
