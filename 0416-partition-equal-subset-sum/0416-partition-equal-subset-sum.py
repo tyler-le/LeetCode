75a08333f42025e19cb71246d2cc638e2ac494b9
Tyler Le <le.tyler.h@gmail.com>
2026-05-24 19:31:56 -0700

    Time: 2777 ms (5.19%), Space: 35.7 MB (26.39%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.py
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.py
@@ -2,19 +2,26 @@ class Solution:
     def canPartition(self, nums: List[int]) -> bool:
         total = sum(nums)
         if total % 2 != 0: return False
-        m = total // 2
+        target = total // 2
         n = len(nums)
-        dp = [[False for _ in range(m+1)] for _ in range(n+1)]
+        dp = [[False for _ in range(target+1)] for _ in range(n+1)]
 
-        dp[n][m] = True
+        """
+        corresponds to this base case is recursive sol:
+            
+            if index >= n: 
+                if path_sum == target: return True
+            
+        """
+        dp[n][target] = True
 
 
         for i in range(n-1, -1, -1):
-            for j in range(m, -1, -1):
+            for path_sum in range(target, -1, -1):
                 num = nums[i]
-                first_choice = dp[i+1][j+num] if i+1 <= n and j+num <= m else False
-                second_choice = dp[i+1][j] if i+1 <= n else False
-                dp[i][j] = first_choice or second_choice
+                first_choice = dp[i+1][path_sum+num] if i+1 <= n and path_sum+num <= target else False
+                second_choice = dp[i+1][path_sum] if i+1 <= n else False
+                dp[i][path_sum] = first_choice or second_choice
 
         return dp[0][0]
         
