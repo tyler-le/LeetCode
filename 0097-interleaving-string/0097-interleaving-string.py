16e6a51adab7910727ad7331a2f39203c46e812c
Tyler Le <le.tyler.h@gmail.com>
2026-05-25 18:01:41 -0700

    Time: 54 ms (40.2%), Space: 19.6 MB (35.17%) - LeetHub

--- 0097-interleaving-string/0097-interleaving-string.py
+++ 0097-interleaving-string/0097-interleaving-string.py
@@ -1,5 +1,27 @@
 class Solution:
     def isInterleave(self, s1: str, s2: str, s3: str) -> bool:
+        n, m = len(s1), len(s2)
+        if len(s1) + len(s2) != len(s3):
+            return False
+        dp = [[False for _ in range(m+1)] for _ in range(n+1)]
+        
+        for p1 in range(n, -1, -1):
+            for p2 in range(m, -1, -1):
+                if p1 == n and p2 == m: 
+                    dp[p1][p2] = True
+                
+                index = p1 + p2
+                
+                if p1 < n and s1[p1] == s3[index]:
+                    if dp[p1+1][p2]: 
+                        dp[p1][p2] |= True
+
+                if p2 < m and s2[p2] == s3[index]:
+                    if dp[p1][p2+1]:
+                        dp[p1][p2] |= True
+
+        return dp[0][0]
+
         
 
         @cache
