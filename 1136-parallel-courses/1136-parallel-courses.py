da93745b6c66c048cdfa5f03194d630a2997b726
Tyler Le <le.tyler.h@gmail.com>
2026-05-27 17:23:15 -0700

    Time: 13 ms (46.47%), Space: 21.7 MB (25.81%) - LeetHub

--- 1136-parallel-courses/1136-parallel-courses.py
+++ 1136-parallel-courses/1136-parallel-courses.py
@@ -1,33 +1,34 @@
 class Solution:
     def minimumSemesters(self, n: int, relations: List[List[int]]) -> int:
-        
-        # topological sort
-        
         res = 0
         graph = defaultdict(list)
         indegrees = defaultdict(int)
         q = deque()
-        num_visited = 0
-        
-        for prev_course, next_course in relations:      
-            graph[prev_course].append(next_course)
-            indegrees[next_course]+=1
-            
-        for i in range(1, n+1):
-            if not indegrees[i]: 
-                q.append((i, 1))
-        
+        visited = set()
+
+        for u, v in relations:
+            graph[u].append(v)
+            indegrees[v]+=1
+
+        for node in range(1, n+1):
+            if not indegrees[node]:
+                q.append(node)
+                visited.add(node)
+
         while q:
-            popped_course, popped_semester = q.popleft()
-            num_visited+=1
-            res = max(res, popped_semester)
-            
-            for nbor in graph[popped_course]:
-                indegrees[nbor]-=1
-                if not indegrees[nbor]:
-                    q.append((nbor, popped_semester+1))
-            
-        
-        return res if (num_visited == n) else -1
+            level_size = len(q)
+            res+=1
+
+            for _ in range(level_size):
+                popped = q.popleft()
+                
+                for nbor in graph[popped]:
+                    indegrees[nbor]-=1
+                    if not indegrees[nbor]:
+                        q.append(nbor)
+                        visited.add(nbor)
+
+        return res if len(visited) == n else -1
+
         
         
