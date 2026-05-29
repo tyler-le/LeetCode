701ccf0e63d66859a523644392dd97b48e063019
Tyler Le <le.tyler.h@gmail.com>
2026-05-28 18:13:02 -0700

    Time: 474 ms (12.86%), Space: 21 MB (9.34%) - LeetHub

--- 1334-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/1334-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.py
+++ 1334-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/1334-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.py
@@ -1,39 +1,36 @@
 class Solution:
     def findTheCity(self, n: int, edges: List[List[int]], distanceThreshold: int) -> int:
-        
-        res = []
-        reachable = defaultdict(set)
 
-        def dijkstras(node):
-            min_heap = [(0, node)] # (travel_cost, node)
+        graph = defaultdict(list)
+        minimum_neighbors = math.inf
+        res = -1
+
+        def dijkstras(root):
             visited = set()
+            min_heap = [(0, root)] # (dist, node)
 
             while min_heap:
-                popped_cost, popped_node = heappop(min_heap)
+                popped_dist, popped_node = heappop(min_heap)
+                if popped_node in visited: continue
+                if popped_dist > distanceThreshold: continue
                 visited.add(popped_node)
-                if node != popped_node: reachable[node].add(popped_node)
-                
-                for nbor_node, nbor_weight in graph[popped_node]:
-                    new_cost = nbor_weight + popped_cost
-                    if new_cost > distanceThreshold: continue
-                    if nbor_node in visited: continue
-                    heappush(min_heap, (new_cost, nbor_node))
+
+                for nbor, edge_weight in graph[popped_node]:
+                    heappush(min_heap, (popped_dist + edge_weight, nbor))
+
+            return len(visited)
+
         
-        # 1. build graph
-        graph = defaultdict(list) # (nbor, weight)
         for u, v, w in edges:
             graph[u].append((v,w))
             graph[v].append((u,w))
 
-        # 2. run dijkstras for each node
         for i in range(n):
-            dijkstras(i)
+            num_neighbors = dijkstras(i)
+            if num_neighbors <= minimum_neighbors:
+                minimum_neighbors = num_neighbors
+                res = i
 
-        # 3. process cities 
-        mn_cities, res = math.inf, 0
-        for node in range(n):
-            if len(reachable[node]) <= mn_cities:
-                mn_cities = len(reachable[node])
-                res = node
-                
         return res
+
+        
