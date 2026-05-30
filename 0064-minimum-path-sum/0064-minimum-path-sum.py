c3fa63e7165708ee92908f0e6880cb8aabb4dea4
Tyler Le <le.tyler.h@gmail.com>
2026-05-30 15:02:23 -0700

    Time: 15 ms (33.44%), Space: 21.7 MB (49.86%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.py
+++ 0064-minimum-path-sum/0064-minimum-path-sum.py
@@ -1,7 +1,28 @@
 class Solution:
     def minPathSum(self, grid: List[List[int]]) -> int:
+        
+        """
+        DP
+        """
         n, m = len(grid), len(grid[0])
+        dp = [[math.inf for _ in range(m)] for _ in range(n)]
 
+        for i in range(n-1, -1, -1):
+            for j in range(m-1, -1, -1):
+                cost = grid[i][j]
+                if i == n-1 and j == m-1: 
+                    dp[i][j] = cost
+                    continue
+                right_cost = dp[i][j+1] if j+1 < m else math.inf
+                down_cost = dp[i+1][j] if i+1 < n else math.inf
+                dp[i][j] = cost + min(right_cost, down_cost)
+
+        return dp[0][0]
+        
+        """
+        Recursive
+        """
+        n, m = len(grid), len(grid[0])
 
         @cache
         def backtrack(i, j):
