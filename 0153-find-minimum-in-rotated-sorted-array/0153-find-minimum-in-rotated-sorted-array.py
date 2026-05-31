180cb8ed6d75373c180d37b2fb7457cf3ffe5da1
Tyler Le <le.tyler.h@gmail.com>
2026-05-31 08:57:38 -0700

    Time: 0 ms (100%), Space: 19.4 MB (67.12%) - LeetHub

--- 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.py
+++ 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.py
@@ -7,9 +7,16 @@ class Solution:
         while low <= high:
             mid = low + ((high - low) // 2)
 
+            # low to mid is sorted
+            # record low as a potential min, since it's smaller
+            # but search right for the inflection point
             if nums[low] <= nums[mid]:
                 res = min(res, nums[low])
                 low = mid + 1
+
+            # low to mid is not sorted (nums[low] > nums[mid])
+            # record nums[mid] as a potential min, since it's smaller
+            # search left for the inflection point
             else:
                 res = min(res, nums[mid])
                 high = mid - 1
