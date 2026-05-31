881bb29ecdf8262dfe5d8e25bf361354128a6bc5
Tyler Le <le.tyler.h@gmail.com>
2026-05-30 21:19:37 -0700

    Time: 15 ms (22.18%), Space: 19.4 MB (82.79%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.py
+++ 0039-combination-sum/0039-combination-sum.py
@@ -1,21 +1,22 @@
 class Solution:
-    def combinationSum(self, nums: List[int], target: int) -> List[List[int]]:
-        res, n = [], len(nums)
+    def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
+        
+        res = []
+        n = len(candidates)
 
-        def backtrack(index, curr_path):
-            nonlocal res, n
+        def f(index, target, path):
+            nonlocal res
 
-            if index == n: return
-
-            if sum(curr_path) == target: 
-                res.append(curr_path.copy())
+            if index == n:
+                if target == 0:
+                    res.append(path.copy())
                 return
             
-            if sum(curr_path) > target: 
+            if target < 0:
                 return
 
-            for i in range(index, n):
-                backtrack(i, curr_path + [nums[i]])
-
-        backtrack(0, [])
+            f(index, target - candidates[index], path + [candidates[index]])
+            f(index + 1, target, path)
+    
+        f(0, target, [])
         return res
