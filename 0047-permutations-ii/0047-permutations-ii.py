fef583f2018fcdd910336bbf383a8fe3be7b8681
Tyler Le <le.tyler.h@gmail.com>
2026-05-30 21:33:10 -0700

    Time: 8 ms (24.13%), Space: 20.1 MB (5.12%) - LeetHub

--- 0047-permutations-ii/0047-permutations-ii.py
+++ 0047-permutations-ii/0047-permutations-ii.py
@@ -1,32 +1,25 @@
 class Solution:
     def permuteUnique(self, nums: List[int]) -> List[List[int]]:
-        res, n = [], len(nums)
+        
+        res = []
+        n = len(nums)
         visited = set()
         nums.sort()
 
-        def backtrack(index, path):
-            nonlocal res, n
+        def f(index, path):
+            nonlocal res
 
             if index == n: 
                 res.append(path.copy())
                 return
 
-            for i in range(n):
-                if i in visited: continue
-
-                # Make the rule that:
-                # You cannot use the second duplicate until the first duplicate has already been used.
-                # This helps us avoid duplicates
-                if i > 0 and nums[i] == nums[i-1] and (i-1) not in visited:
+            for j in range(n):
+                if j in visited: continue
+                if j > 0 and nums[j] == nums[j-1] and j-1 not in visited: 
                     continue
+                visited.add(j)
+                f(index + 1, path + [nums[j]])
+                visited.remove(j)
 
-
-                visited.add(i)
-                path.append(nums[i])
-                backtrack(index + 1, path)
-                path.pop()
-                visited.remove(i)
-
-            
-        backtrack(0, [])
+        f(0, [])
         return res
