7bfa568966b4cce54c3ad47d054d8a018e6514e5
Tyler Le <le.tyler.h@gmail.com>
2026-05-31 14:14:33 -0700

    Time: 346 ms (40.22%), Space: 21.5 MB (27.24%) - LeetHub

--- 0743-network-delay-time/0743-network-delay-time.py
+++ 0743-network-delay-time/0743-network-delay-time.py
@@ -1,31 +1,22 @@
 class Solution:
     def networkDelayTime(self, times: List[List[int]], n: int, k: int) -> int:
         
-        res = 0
-
-        # create graph
         graph = defaultdict(list)
+        min_heap = [(0, k)]
+        visited = set()
+        res = 0
 
         for u, v, w in times:
             graph[u].append((v,w))
 
-        # create heap queue
-        min_heap = [(0, k)] # (distance, node)
-        visited = set()
-
-
-        # run dijkstras from the source
         while min_heap:
-            popped_dist, popped_node = heappop(min_heap)
+            popped_time, popped_node = heappop(min_heap)
             if popped_node in visited: continue
             visited.add(popped_node)
-            res = popped_dist
-            
+            res = popped_time
+
             for nbor, edge_weight in graph[popped_node]:
-                heappush(min_heap, (popped_dist + edge_weight, nbor))
-
-            
-
-        # if we visited all nodes -> return res
-        # else -> return -1
+                heappush(min_heap, (popped_time + edge_weight, nbor))
+        
         return res if len(visited) == n else -1
+
