a04a1f3acb418236c2ee98f7214a24326683508e
Tyler Le <le.tyler.h@gmail.com>
2026-05-31 17:49:50 -0700

    Time: 3 ms (75%), Space: 20.5 MB (44.55%) - LeetHub

--- 0210-course-schedule-ii/0210-course-schedule-ii.py
+++ 0210-course-schedule-ii/0210-course-schedule-ii.py
@@ -1,31 +1,29 @@
 class Solution:
     def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
-        
         graph = defaultdict(list)
         indegrees = defaultdict(int)
         q = deque()
-        visited = set()
         res = []
+        visited = set()
 
-        for dst, src in prerequisites:
-            graph[src].append(dst)
-            indegrees[dst]+=1
-        
-        for crs in range(numCourses):
-            if not indegrees[crs]:
-                q.append(crs)
+        for u, v in prerequisites:
+            graph[v].append(u)
+            indegrees[u]+=1
+
+        for i in range(numCourses):
+            if not indegrees[i]:
+                q.append(i)
+                visited.add(i)
 
         while q:
             popped = q.popleft()
             res.append(popped)
-            visited.add(popped)
-
             for nbor in graph[popped]:
                 if nbor in visited: continue
                 indegrees[nbor]-=1
                 if not indegrees[nbor]:
+
+                    visited.add(nbor)
                     q.append(nbor)
-
         return res if len(visited) == numCourses else []
-
-
+                
