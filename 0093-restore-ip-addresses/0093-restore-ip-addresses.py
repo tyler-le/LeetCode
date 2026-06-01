fdf941aee6121f7c7fe69550e08dc361466a9486
Tyler Le <le.tyler.h@gmail.com>
2026-06-01 09:31:35 -0700

    Time: 18 ms (7.53%), Space: 19.3 MB (67.41%) - LeetHub

--- 0093-restore-ip-addresses/0093-restore-ip-addresses.py
+++ 0093-restore-ip-addresses/0093-restore-ip-addresses.py
@@ -4,29 +4,28 @@ class Solution:
         n = len(s)
         res = []
 
-        def is_valid(start, end):
-            if end - start + 1 > 3: return False
-            piece_str = s[start : end + 1]
-            if piece_str.startswith("0") and len(piece_str) > 1: return False
-            piece = int(piece_str)
-            if not 0 <= piece <= 255: return False
-
+        def is_valid(segment):
+            if len(segment) > 3: return False
+            if segment.startswith("0") and len(segment) > 1: return False
+            x = int(segment)
+            if not (0 <= x <= 255): return False
             return True
 
 
         def backtrack(start, path):
             nonlocal res
 
-            if len(path) > 4: return
-
+            # base case
             if start == n:
-                if len(path.copy()) == 4:
+                if len(path) == 4:
                     res.append(".".join(path.copy()))
-                return 
-            
+                return
+
+            # recursive calls
             for end in range(start, n):
-                if is_valid(start, end):
-                    path.append(s[start : end + 1])
+                prefix = s[start : end + 1]
+                if is_valid(prefix):
+                    path.append(prefix)
                     backtrack(end + 1, path)
                     path.pop()
         
