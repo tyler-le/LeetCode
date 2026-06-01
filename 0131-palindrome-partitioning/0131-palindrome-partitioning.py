99a87ad00b3111c83c4cda99b0dcea2bf9a1d18b
Tyler Le <le.tyler.h@gmail.com>
2026-05-31 22:14:05 -0700

    Time: 43 ms (60.57%), Space: 33.6 MB (77.18%) - LeetHub

--- 0131-palindrome-partitioning/0131-palindrome-partitioning.py
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.py
@@ -1,41 +1,22 @@
 class Solution:
     def partition(self, s: str) -> List[List[str]]:
-        
         n = len(s)
         res = []
 
-        def is_pal(l, r):
-            while l <= r:
-                if s[l] != s[r]: return False
-                l+=1
-                r-=1
-            return True
-        
-        """
-        The comments below are for example "aab"
-        """
-        def f(start, path):
-
-            if start == n:
+        # backtrack(i) returns the palindromes partitions from i:
+        def backtrack(index, path):
+            nonlocal res
+            
+            if index >= n:
                 res.append(path.copy())
                 return
 
-            for end in range(start, n):
-
-                # if s[start:end+1] is a palindrome (i.e. "aa")
-                if is_pal(start, end):
-
-                    # add s[start:end+1] to path (i.e. path = ["aa"])
-                    path.append(s[start:end+1])
-
-                    # recurse on s[end+1:] (i.e. recurse on "b")
-                    # this call will then append "b" to path
-                    # so now path = ["aa", "b"]
-                    f(end + 1, path)
-
-                    # undo choice
+            for i in range(index, n):
+                prefix = s[index : i+1]
+                if prefix == prefix[::-1]:
+                    path.append(prefix)
+                    backtrack(i + 1, path)
                     path.pop()
-        
-        f(0, [])
+
+        backtrack(0, [])
         return res
-            
