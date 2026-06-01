1334c7980093309f1d3bd4333c728054021a79fd
Tyler Le <le.tyler.h@gmail.com>
2026-05-31 17:45:16 -0700

    Time: 19 ms (23.43%), Space: 19.3 MB (68.59%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
@@ -1,9 +1,9 @@
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
-        res = 0
+        hmap = defaultdict(int)
         l = 0
         n = len(s)
-        hmap = defaultdict(int)
+        res = 0
 
         for r in range(n):
             hmap[s[r]]+=1
@@ -12,6 +12,6 @@ class Solution:
                 hmap[s[l]]-=1
                 l+=1
             
-            res = max(res, r-l+1)
-        
+            if hmap[s[r]] <= 1:
+                res = max(res, r-l+1)
         return res
