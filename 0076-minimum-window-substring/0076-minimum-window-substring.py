2f466607d793e803056fe0a9b4ea1457eaa44b64
Tyler Le <le.tyler.h@gmail.com>
2026-05-31 21:27:04 -0700

    Time: 63 ms (77.06%), Space: 19.6 MB (73.56%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.py
+++ 0076-minimum-window-substring/0076-minimum-window-substring.py
@@ -1,30 +1,25 @@
 class Solution:
     def minWindow(self, s: str, t: str) -> str:
-        
-        l = 0
-        res = [-math.inf, math.inf]
-        n = len(s)
         t_cnt = Counter(t)
+        required = len(t_cnt.keys())
         window_cnt = defaultdict(int)
-
-        conditions_met, conditions_needed = 0, len(t_cnt)
+        conditions_met = 0
+        n = len(s)
+        l = 0
+        res = (math.inf, -1, -1)
 
         for r in range(n):
-            # extend the window
             window_cnt[s[r]]+=1
             if window_cnt[s[r]] == t_cnt[s[r]]:
                 conditions_met+=1
+            
+            while conditions_met == required:
+                if r-l+1 < res[0]:
+                    res = (r-l+1, l, r)
 
-            # shrink the window
-            while l < r and (s[l] not in t_cnt or window_cnt[s[l]] > t_cnt[s[l]]):
                 window_cnt[s[l]]-=1
-                if not window_cnt[s[l]]: del window_cnt[s[l]]
+                if window_cnt[s[l]] == t_cnt[s[l]] - 1:
+                    conditions_met-=1
                 l+=1
-
-            # check validity and record res
-            if conditions_met == conditions_needed and r-l+1 < (res[1] - res[0] + 1):
-                res = [l, r]
         
-        print(res)
-        if res == [-math.inf, math.inf]: return ""
-        return s[res[0]:(res[1] + 1)]
+        return s[res[1] : res[2] + 1]
