0f4b927bdb8c8e49d141d0c09786eca2d28f4e83
Tyler Le <le.tyler.h@gmail.com>
2026-06-08 17:13:49 -0700

    Time: 23 ms (9.67%), Space: 19.3 MB (54.55%) - LeetHub

--- 1282-group-the-people-given-the-group-size-they-belong-to/1282-group-the-people-given-the-group-size-they-belong-to.py
+++ 1282-group-the-people-given-the-group-size-they-belong-to/1282-group-the-people-given-the-group-size-they-belong-to.py
@@ -1,17 +1,22 @@
 class Solution:
     def groupThePeople(self, groupSizes: List[int]) -> List[List[int]]:
-        # map group size -> list of groups
         
         hmap = defaultdict(list)
+
+        for person, group_size in enumerate(groupSizes):
+            groups = hmap[group_size]
+            added_to_group = False
+            for group in groups:
+                if len(group) < group_size:
+                    group.append(person)
+                    added_to_group = True
+                    break
+            if not added_to_group: 
+                groups.append([person])
+                        
+
         res = []
-        
-        for i in range(len(groupSizes)):
-            gs = groupSizes[i]
-            
-            hmap[gs].append(i)
-            
-            if len(hmap[gs]) == gs:
-                res.append(hmap[gs])
-                hmap[gs] = []
-        
+
+        for group in hmap.values():
+            res.extend(group)
         return res
