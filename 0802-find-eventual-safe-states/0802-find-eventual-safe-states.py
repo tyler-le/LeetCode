730e95dc504446c66b90a2d727aa61920a6597b6
Tyler Le <le.tyler.h@gmail.com>
2026-06-09 18:30:00 -0700

    Time: 85 ms (9.51%), Space: 25.2 MB (97.33%) - LeetHub

--- 0802-find-eventual-safe-states/0802-find-eventual-safe-states.py
+++ 0802-find-eventual-safe-states/0802-find-eventual-safe-states.py
@@ -1,26 +1,35 @@
 class Solution:
     def eventualSafeNodes(self, graph: List[List[int]]) -> List[int]:
-        # topological sort - prune off leaves
-
-        n = len(graph)
+        inverted_graph = defaultdict(list)
         indegrees = defaultdict(int)
-        inverted = defaultdict(list)
         q = deque()
+        n = len(graph)
+        res = []
 
-        for u in range(n):
-            for v in graph[u]:
-                inverted[v].append(u)
-                indegrees[u]+=1            
+        for i in range(n):
+            for nbor in graph[i]:
+                inverted_graph[nbor].append(i)
+                indegrees[i]+=1
 
         for node in range(n):
             if not indegrees[node]:
                 q.append(node)
-
+                # visited.add(node)
+                res.append(node)
+        
         while q:
             popped_node = q.popleft()
-            for nbor in inverted[popped_node]:
+
+            for nbor in inverted_graph[popped_node]:
                 indegrees[nbor]-=1
                 if not indegrees[nbor]:
                     q.append(nbor)
+                    res.append(nbor)
+                    # visited.add(nbor)
 
-        return [i for i in range(n) if indegrees[i] == 0]
+        return sorted(res)
+        
+
+
+
+        
