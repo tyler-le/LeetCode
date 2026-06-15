e50586df0062ab658940ab4761e1562835e12403
Tyler Le <le.tyler.h@gmail.com>
2026-06-15 15:57:21 -0700

    Time: 0 ms (100%), Space: 19.2 MB (72.3%) - LeetHub

--- 1640-check-array-formation-through-concatenation/1640-check-array-formation-through-concatenation.py
+++ 1640-check-array-formation-through-concatenation/1640-check-array-formation-through-concatenation.py
@@ -1,36 +1,18 @@
 class Solution:
     def canFormArray(self, arr: List[int], pieces: List[List[int]]) -> bool:
+        hmap = defaultdict(list)
+
+        for piece in pieces:
+            hmap[piece[0]].extend(piece)
+
         built = []
         ptr = 0
-        pieces.sort(key = lambda x : x[0])
-        
-        """
-        Input: x - a number to search for in pieces
-        Output: The piece s.t. piece[0] == x else None
-        """
-        def search(x):
-            low, high = 0, len(pieces) - 1
-
-            while low <= high:
-                mid = low + ((high - low) // 2)
-                piece = pieces[mid][0]
-                if x < piece: high = mid - 1
-                elif x > piece: low = mid + 1
-                else: return pieces[mid]
-
-            return None
-
 
         while ptr < len(arr):
             x = arr[ptr]
-            found = False
-            piece = search(x)
-            if piece: 
-                built.extend(piece)
-                ptr+=len(piece)
-            else:
-                return False
+            if not hmap[x]: return False
 
+            built.extend(hmap[x])
+            ptr+=len(hmap[x])
+        
         return built == arr
-
-    
