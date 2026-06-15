08ebebf08ca38b77675dba5fe88dd1da0e36b709
Tyler Le <le.tyler.h@gmail.com>
2026-06-15 15:54:04 -0700

    Time: 3 ms (20.19%), Space: 19.3 MB (29.11%) - LeetHub

--- 1640-check-array-formation-through-concatenation/1640-check-array-formation-through-concatenation.py
+++ 1640-check-array-formation-through-concatenation/1640-check-array-formation-through-concatenation.py
@@ -2,20 +2,35 @@ class Solution:
     def canFormArray(self, arr: List[int], pieces: List[List[int]]) -> bool:
         built = []
         ptr = 0
+        pieces.sort(key = lambda x : x[0])
+        
+        """
+        Input: x - a number to search for in pieces
+        Output: The piece s.t. piece[0] == x else None
+        """
+        def search(x):
+            low, high = 0, len(pieces) - 1
+
+            while low <= high:
+                mid = low + ((high - low) // 2)
+                piece = pieces[mid][0]
+                if x < piece: high = mid - 1
+                elif x > piece: low = mid + 1
+                else: return pieces[mid]
+
+            return None
+
 
         while ptr < len(arr):
             x = arr[ptr]
             found = False
-            for piece in pieces: 
-                if piece[0] == x:
-                    built.extend(piece)
-                    ptr+=len(piece)
-                    found = True
-                    break
-            if not found: 
+            piece = search(x)
+            if piece: 
+                built.extend(piece)
+                ptr+=len(piece)
+            else:
                 return False
 
-        
         return built == arr
 
     
