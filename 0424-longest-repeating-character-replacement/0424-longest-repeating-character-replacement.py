4279da358f7a3471408293f87091b4399288203f
Tyler Le <le.tyler.h@gmail.com>
2026-06-16 17:01:07 -0700

    Time: 158 ms (18.39%), Space: 19.6 MB (85.1%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
@@ -1,15 +1,16 @@
 class Solution:
     def characterReplacement(self, s: str, k: int) -> int:
         l = 0
-        window_cnt = defaultdict(int)
-        n = len(s)
         res = 0
+        n = len(s)
+        hmap = defaultdict(int)
 
         for r in range(n):
-            window_cnt[s[r]]+=1
+            hmap[s[r]]+=1
 
-            while (r-l+1) - max(window_cnt.values()) > k:
-                window_cnt[s[l]]-=1
+            while ((r-l+1) - max(hmap.values())) > k:
+                hmap[s[l]]-=1
+                if not hmap[s[l]]: del hmap[s[l]]
                 l+=1
             
             res = max(res, r-l+1)
