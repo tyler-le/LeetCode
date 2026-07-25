1ce09573778b24c9d9b5c58dfe1524c9d5e0bad5
Tyler Le <le.tyler.h@gmail.com>
2026-07-25 15:18:57 -0700

    Time: 66 ms (50.84%), Space: 22.8 MB (43.85%) - LeetHub

--- 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.py
+++ 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.py
@@ -1,33 +1,14 @@
 # Definition for a binary tree node.
 # class TreeNode:
-#     def __init__(self, x):
-#         self.val = x
-#         self.left = None
-#         self.right = None
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
 
 class Solution:
-    def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
-        
-        # return the LCA
-        def dfs(node):
-
-            # base case
-            if not node: return None
-
-            # if split, return node
-            if p and q and p.val < node.val < q.val: return node
-            if p and q and p.val > node.val > q.val: return node
-            
-            # encounter p or q
-            if node.val == p.val: return node
-            if node.val == q.val: return node
-
-            # recurse
-            left, right = False, False
-            if p.val < node.val and q.val < node.val:
-                return dfs(node.left)
-            
-            if p.val > node.val and q.val > node.val:
-                return dfs(node.right)
-        
-        return dfs(root)
+    def lowestCommonAncestor(self, root: TreeNode, p: TreeNode, q: TreeNode) -> TreeNode:
+        if root.val == p.val or root.val == q.val: return root
+        if p.val < root.val < q.val: return root
+        if q.val < root.val < p.val: return root
+        if p.val < root.val and q.val < root.val: return self.lowestCommonAncestor(root.left, p, q)
+        else: return self.lowestCommonAncestor(root.right, p, q)
