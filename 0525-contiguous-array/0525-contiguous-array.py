0ddf1bb11c4a2b72a546536c6286b0f0be1d3ebb
Tyler Le <le.tyler.h@gmail.com>
2026-08-30 10:07:20 -0700

    Time: 86 ms (29.41%), Space: 25.2 MB (50.28%) - LeetHub

--- 0525-contiguous-array/0525-contiguous-array.py
+++ 0525-contiguous-array/0525-contiguous-array.py
@@ -1,20 +1,20 @@
 class Solution:
     def findMaxLength(self, nums: List[int]) -> int:
-        # 1 -> +1
-        # 0 -> -1
-        
-        prefix_sums = {}
+        arr = [-1 if x == 0 else 1 for x in nums]
+
+        prefix_sums = defaultdict(int)
         prefix_sums[0] = -1
-        acc = 0
+        curr_sum = 0
         res = 0
-        
-        for i in range(len(nums)):
-            num = nums[i]
-                    
-            acc = acc + 1 if num else acc - 1
-            
-            if acc in prefix_sums: res = max(res, i - prefix_sums[acc])
-            else: prefix_sums[acc] = i
-        
+
+
+        for i in range(len(arr)):
+            num = arr[i]
+            curr_sum+=num
+            if curr_sum in prefix_sums:
+                res = max(res, i - prefix_sums[curr_sum])
+            else:
+                prefix_sums[curr_sum] = i
         return res
-        
+
+
