338cdcab20b35d018a016b9565756986a469369e
Tyler Le <le.tyler.h@gmail.com>
2026-09-23 18:48:00 -0700

    Time: 27 ms (91.38%), Space: 22.2 MB (53.33%) - LeetHub

--- 0286-walls-and-gates/0286-walls-and-gates.py
+++ 0286-walls-and-gates/0286-walls-and-gates.py
@@ -1,32 +1,35 @@
 class Solution:
-    def wallsAndGates(self, rooms: List[List[int]]) -> None:
+    def wallsAndGates(self, rooms: list[list[int]]) -> None:
         """
         Do not return anything, modify rooms in-place instead.
         """
-        
-        def bfs(i, j):
-            visited = set()
-            
-            while q:
-                x, y, dist = q.popleft()
-                rooms[x][y] = dist if rooms[x][y] == 0 else min(rooms[x][y], dist)
-                
-                for dx, dy in [(-1,0), (1,0), (0,-1), (0,1)]:
-                    if x+dx < 0 or y+dy < 0 or x+dx >= n or y+dy >= m: continue   
-                    if rooms[x+dx][y+dy] != EMPTY: continue
-                    if (x+dx, y+dy) in visited: continue
-                    q.append((x+dx, y+dy, dist+1))
-                    visited.add((x+dx, y+dy))
-                    
-        
-        # for each gate, run bfs and update accordingly
-        n, m, q = len(rooms), len(rooms[0]), deque()
-        GATE, WALL, EMPTY = 0, -1, 2**31 - 1
-        
+        # multisource BFS
+
+        q = deque() # (i, j, dist)
+        n, m = len(rooms), len(rooms[0])
+        WALL, GATE, EMPTY = -1, 0, 2**31 - 1
+
         for i in range(n):
             for j in range(m):
                 if rooms[i][j] == GATE:
                     q.append((i, j, 0))
         
-        bfs(i, j)
-                    
+        while q:
+            level_size = len(q)
+            for _ in range(level_size):
+                popped_x, popped_y, popped_dist = q.popleft()
+
+                for dx, dy in [(-1,0), (1,0), (0,-1), (0,1)]:
+                    r = popped_x + dx
+                    c = popped_y + dy
+
+                    if r < 0 or c < 0 or r >= n or c >= m: continue
+                    if rooms[r][c] == EMPTY:
+                        rooms[r][c] = popped_dist + 1
+                        q.append((r, c, popped_dist + 1))
+
+
+                
+
+                
+
