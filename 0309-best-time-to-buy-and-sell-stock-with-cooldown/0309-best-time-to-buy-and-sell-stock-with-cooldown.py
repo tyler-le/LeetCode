e2a1c82775330d17084aa3df24a242ac9efce1c9
Tyler Le <le.tyler.h@gmail.com>
2026-09-24 17:25:12 -0700

    Time: 9 ms (9.01%), Space: 24.6 MB (8.8%) - LeetHub

--- 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.py
+++ 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.py
@@ -1,40 +1,31 @@
 class Solution:
-    def maxProfit(self, prices: List[int]) -> int:
-        n = len(prices)
-        dp = [[0 for _ in range(2)] for _ in range(n+2)]
-        can_buy, cannot_buy = 0, 1
-
-
-        for i in range(n - 1, -1, -1):
-            buy = -prices[i] + dp[i+1][cannot_buy]
-            hold = dp[i + 1][can_buy]
-            dp[i][can_buy] = max(dp[i][can_buy], buy, hold)
-
-            sell = prices[i] + dp[i+2][can_buy]
-            hold = dp[i+1][cannot_buy]
-            dp[i][cannot_buy] = max(dp[i][cannot_buy], sell, hold)
-
-        return dp[0][can_buy]
-            
-
+    def maxProfit(self, prices: list[int]) -> int:
 
         n = len(prices)
+
         @cache
-        def f(index, can_buy):
-            if index >= n: return 0
-
+        def rec(index, can_buy):
             res = 0
 
+            if index >= n:
+                return 0
+
             if can_buy:
-                buy = -prices[index] + f(index + 1, False)
-                hold = f(index + 1, True)
+                buy = -prices[index] + rec(index + 1, False)
+                hold = rec(index + 1, True)
                 res = max(res, buy, hold)
+
             else:
-                sell = prices[index] + f(index + 2, True)
-                hold = f(index + 1, False)
+                sell = prices[index] + rec(index + 2, True)
+                hold = rec(index + 1, False)
                 res = max(res, sell, hold)
 
             return res
 
-        return f(0, True)
+        return rec(0, True)
+
+            
+
+
+
 
