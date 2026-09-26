dc433d163af1c53816fa68675fbe2caa917d353e
Tyler Le <le.tyler.h@gmail.com>
2026-09-25 19:26:03 -0700

    Time: 7 ms (27.24%), Space: 24.4 MB (10.33%) - LeetHub

--- 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.py
+++ 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.py
@@ -1,31 +1,22 @@
 class Solution:
     def maxProfit(self, prices: list[int]) -> int:
-
-        n = len(prices)
-
         @cache
-        def rec(index, can_buy):
+        def f(index, can_buy):
             res = 0
 
-            if index >= n:
-                return 0
+            # base case
+            if index >= len(prices): return 0
 
+            # recursive calls
             if can_buy:
-                buy = -prices[index] + rec(index + 1, False)
-                hold = rec(index + 1, True)
-                res = max(res, buy, hold)
-
+                buy = -prices[index] + f(index + 1, False)
+                hold = f(index + 1, True)
+                res = max(buy, hold)
             else:
-                sell = prices[index] + rec(index + 2, True)
-                hold = rec(index + 1, False)
-                res = max(res, sell, hold)
-
+                sell = prices[index] + f(index + 2, True)
+                hold = f(index + 1, False)
+                res = max(sell, hold)
+            
             return res
 
-        return rec(0, True)
-
-            
-
-
-
-
+        return f(0, True)
